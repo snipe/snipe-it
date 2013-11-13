261049e8480176ffb76f29b853a8586681b7f883
snipe <snipe@snipe.net>
2013-11-13 16:23:18 -0500

    placeholder model/category models

---
+++ app/models/Manufacturer.php
@@ -0,0 +1,30 @@
+<?php
+
+class Manufacturer extends Eloquent {
+
+	/**
+	 * Deletes a blog post and all the associated comments.
+	 *
+	 * @return bool
+	 */
+	public function delete()
+	{
+		
+
+		// Delete the blog post
+		return parent::delete();
+	}
+
+	/**
+	 * Returns a formatted post content entry, this ensures that
+	 * line breaks are returned.
+	 *
+	 * @return string
+	 */
+	public function name()
+	{
+		return nl2br($this->content);
+	}
+
+	
+}
