b18f69330fe1cd4373c0eaccc023cffb293c6bd8
snipe <snipe@snipe.net>
2013-11-13 17:02:25 -0500

    language files for category view

---
+++ app/lang/en/admin/categories/message.php
@@ -0,0 +1,22 @@
+<?php
+
+return array(
+
+	'does_not_exist' => 'Category does not exist.',
+
+	'create' => array(
+		'error'   => 'Category was not created, please try again.',
+		'success' => 'Category created successfully.'
+	),
+
+	'update' => array(
+		'error'   => 'Category was not updated, please try again',
+		'success' => 'Category updated successfully.'
+	),
+
+	'delete' => array(
+		'error'   => 'There was an issue deleting the category. Please try again.',
+		'success' => 'The category post was deleted successfully.'
+	)
+
+);
---
+++ app/lang/en/admin/categories/table.php
@@ -0,0 +1,10 @@
+<?php
+
+return array(
+
+	'id'      => 'ID',
+	'title'      => 'Asset Category Name',
+	'parent'   => 'Parent',
+	'created_at' => 'Created at',
+
+);
