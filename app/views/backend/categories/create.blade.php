590dbb2bd54f3785401df4aa1dfe7cdcf8d0392b
snipe <snipe@snipe.net>
2013-11-14 21:46:13 -0500

    Create form
    I will probably make these modals later - the UX is a little clumsy this way

--- app/views/backend/categories/create.blade.php
+++ app/views/backend/categories/create.blade.php
@@ -2,7 +2,7 @@
 
 {{-- Page title --}}
 @section('title')
-Create a New Blog Post ::
+Create a New Asset Category ::
 @parent
 @stop
 
@@ -13,16 +13,11 @@ Create a New Blog Post ::
 		Create a New Blog Post
 
 		<div class="pull-right">
-			<a href="{{ route('blogs') }}" class="btn btn-small btn-inverse"><i class="icon-circle-arrow-left icon-white"></i> Back</a>
+			<a href="{{ route('categories') }}" class="btn btn-small btn-inverse"><i class="icon-circle-arrow-left icon-white"></i> Back</a>
 		</div>
 	</h3>
 </div>
 
-<!-- Tabs -->
-<ul class="nav nav-tabs">
-	<li class="active"><a href="#tab-general" data-toggle="tab">General</a></li>
-	<li><a href="#tab-meta-data" data-toggle="tab">Meta Data</a></li>
-</ul>
 
 <form class="form-horizontal" method="post" action="" autocomplete="off">
 	<!-- CSRF Token -->
@@ -30,78 +25,32 @@ Create a New Blog Post ::
 
 	<!-- Tabs Content -->
 	<div class="tab-content">
-		<!-- General tab -->
+
 		<div class="tab-pane active" id="tab-general">
-			<!-- Post Title -->
-			<div class="control-group {{ $errors->has('title') ? 'error' : '' }}">
-				<label class="control-label" for="title">Post Title</label>
+			<!-- Category Title -->
+			<div class="control-group {{ $errors->has('name') ? 'error' : '' }}">
+				<label class="control-label" for="name">Category Name</label>
 				<div class="controls">
-					<input class="span10" type="text" name="title" id="title" value="{{ Input::old('title') }}" />
-					{{ $errors->first('title', '<span class="help-inline">:message</span>') }}
+					<input class="span6" type="text" name="name" id="name" value="{{ Input::old('name') }}" />
+					{{ $errors->first('name', '<span class="help-inline">:message</span>') }}
 				</div>
 			</div>
-
-			<!-- Post Slug -->
-			<div class="control-group">
-				<label class="control-label" for="slug">Slug</label>
+			<!-- Category Parent Title -->
+			<div class="control-group {{ $errors->has('parent') ? 'error' : '' }}">
+				<label class="control-label" for="parent">Category Parent</label>
 				<div class="controls">
-					<div class="input-prepend">
-						<span class="add-on">
-							{{ str_finish(URL::to('/'), '/') }}
-						</span>
-						<input class="span6" type="text" name="slug" id="slug" value="{{ Input::old('slug') }}">
-					</div>
-				</div>
-			</div>
-
-			<!-- Content -->
-			<div class="control-group {{ $errors->has('content') ? 'error' : '' }}">
-				<label class="control-label" for="content">Content</label>
-				<div class="controls">
-					<textarea class="span10" name="content" id="content" value="content" rows="10">{{ Input::old('content') }}</textarea>
-					{{ $errors->first('content', '<span class="help-inline">:message</span>') }}
+					<input class="span6" type="text" name="parent" id="parent" value="{{ Input::old('parent') }}" />
+					{{ $errors->first('parent', '<span class="help-inline">:message</span>') }}
 				</div>
 			</div>
 		</div>
 
-		<!-- Meta Data tab -->
-		<div class="tab-pane" id="tab-meta-data">
-			<!-- Meta Title -->
-			<div class="control-group {{ $errors->has('meta-title') ? 'error' : '' }}">
-				<label class="control-label" for="meta-title">Meta Title</label>
-				<div class="controls">
-					<input class="span10" type="text" name="meta-title" id="meta-title" value="{{ Input::old('meta-title') }}" />
-					{{ $errors->first('meta-title', '<span class="help-inline">:message</span>') }}
-				</div>
-			</div>
 
-			<!-- Meta Description -->
-			<div class="control-group {{ $errors->has('meta-description') ? 'error' : '' }}">
-				<label class="control-label" for="meta-description">Meta Description</label>
-				<div class="controls">
-					<input class="span10" type="text" name="meta-description" id="meta-description" value="{{ Input::old('meta-description') }}" />
-					{{ $errors->first('meta-description', '<span class="help-inline">:message</span>') }}
-				</div>
-			</div>
-
-			<!-- Meta Keywords -->
-			<div class="control-group {{ $errors->has('meta-keywords') ? 'error' : '' }}">
-				<label class="control-label" for="meta-keywords">Meta Keywords</label>
-				<div class="controls">
-					<input class="span10" type="text" name="meta-keywords" id="meta-keywords" value="{{ Input::old('meta-keywords') }}" />
-					{{ $errors->first('meta-keywords', '<span class="help-inline">:message</span>') }}
-				</div>
-			</div>
-		</div>
-	</div>
 
 	<!-- Form actions -->
 	<div class="control-group">
 		<div class="controls">
 			<a class="btn btn-link" href="{{ route('blogs') }}">Cancel</a>
-
-			<button type="reset" class="btn">Reset</button>
-
 			<button type="submit" class="btn btn-success">Publish</button>
 		</div>
 	</div>
