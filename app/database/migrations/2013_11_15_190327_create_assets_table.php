9159d5f771f3f7bc0d986241580a06fef5d1b683
snipe <snipe@snipe.net>
2013-11-15 14:25:28 -0500

    Added license/asset tables

---
+++ app/database/migrations/2013_11_15_190327_create_assets_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAssetsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('assets', function($table)
+		{
+			$table->increments('id');
+			$table->string('name');
+			$table->string('asset_tag');
+	        $table->integer('model_id');
+	        $table->string('serial');
+	        $table->date('purchase_date')->nullable();
+	        $table->decimal('purchase_cost', 8, 2)->nullable();
+	        $table->string('order_number');
+	        $table->integer('assigned_to');
+	        $table->text('notes');
+	        $table->integer('user_id');
+	        $table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('assets');
+	}
+
+}
---
+++ app/database/migrations/2013_11_15_190357_create_licenses_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLicensesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('licenses', function($table)
+		{
+		$table->increments('id');
+		$table->string('name');
+		$table->integer('model_id');
+		$table->string('serial');
+		$table->string('license_email');
+		$table->date('purchase_date')->nullable();
+		$table->decimal('purchase_cost', 8, 2)->nullable();
+		$table->string('order_number');
+		$table->integer('assigned_to');
+		$table->text('notes');
+		$table->integer('user_id');
+		$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('licenses');
+	}
+
+}
