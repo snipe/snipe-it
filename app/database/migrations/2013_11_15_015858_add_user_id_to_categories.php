963d3a764adecb638c4f482b3bb8613530924e6d
snipe <snipe@snipe.net>
2013-11-14 21:44:19 -0500

    adding user ID to category table

---
+++ app/database/migrations/2013_11_15_015858_add_user_id_to_categories.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class AddUserIdToCategories extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('categories', function($table)
+		{
+			$table->integer('user_id');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+	}
+
+}
