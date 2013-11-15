9bbc25fc76c455b7e6640735d9f97589d7bc9c5c
snipe <snipe@snipe.net>
2013-11-15 15:00:50 -0500

    License language files

---
+++ app/lang/en/admin/licenses/message.php
@@ -0,0 +1,22 @@
+<?php
+
+return array(
+
+	'does_not_exist' => 'License does not exist.',
+
+	'create' => array(
+		'error'   => 'License was not created, please try again.',
+		'success' => 'License created successfully.'
+	),
+
+	'update' => array(
+		'error'   => 'License was not updated, please try again',
+		'success' => 'License updated successfully.'
+	),
+
+	'delete' => array(
+		'error'   => 'There was an issue deleting the category. Please try again.',
+		'success' => 'The category was deleted successfully.'
+	)
+
+);
---
+++ app/lang/en/admin/licenses/table.php
@@ -0,0 +1,11 @@
+<?php
+
+return array(
+
+	'id'      => 'ID',
+	'title'      => 'License',
+	'purchased'   => 'Purchased',
+	'serial'   => 'Serial',
+	'assigned_to'   => 'Assigned To',
+
+);
