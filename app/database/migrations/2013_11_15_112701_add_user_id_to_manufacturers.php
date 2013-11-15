884131f7291a37ba8c9a9aa87a6ace95a4e4dbe4
snipe <snipe@snipe.net>
2013-11-15 06:38:50 -0500

    added user_id to manufacturer table

---
+++ app/database/migrations/2013_11_15_112701_add_user_id_to_manufacturers.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class AddUserIdToManufacturers extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('manufacturers', function($table)
+		{
+			$table->integer('user_id');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+	}
+
+}
