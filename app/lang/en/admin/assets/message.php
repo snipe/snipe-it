b0eb5e5bddd45626db02d70ec0adb409ceadef5c
snipe <snipe@snipe.net>
2013-11-15 19:19:41 -0500

    Language files for messages + index table

---
+++ app/lang/en/admin/assets/message.php
@@ -0,0 +1,22 @@
+<?php
+
+return array(
+
+	'does_not_exist' => 'Asset does not exist.',
+
+	'create' => array(
+		'error'   => 'Asset was not created, please try again. :(',
+		'success' => 'Asset created successfully. :)'
+	),
+
+	'update' => array(
+		'error'   => 'Asset was not updated, please try again',
+		'success' => 'Asset updated successfully.'
+	),
+
+	'delete' => array(
+		'error'   => 'There was an issue deleting the asset. Please try again.',
+		'success' => 'The asset was deleted successfully.'
+	)
+
+);
---
+++ app/lang/en/admin/assets/table.php
@@ -0,0 +1,14 @@
+<?php
+
+return array(
+
+	'id'      => 'ID',
+	'title'      => 'Asset ',
+	'asset_tag'   => 'Asset Tag',
+	'serial'   => 'Serial',
+	'purchase_date'   => 'Purchased',
+	'purchase_cost'   => 'Cost',
+	'book_value'   => 'Book Value',
+	'status'   => 'Status',
+
+);
