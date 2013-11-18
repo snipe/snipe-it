50387874bf7440d4ccae9d331848f0eb8db0110a
snipe <snipe@snipe.net>
2013-11-18 10:26:47 -0500

    Added complete info to the locations seeder

--- app/database/seeds/LocationsSeeder.php
+++ app/database/seeds/LocationsSeeder.php
@@ -12,9 +12,12 @@ class LocationsSeeder extends Seeder {
 		$date = new DateTime;
 		$location[] = array(
 			'name'      => 'NY - Broad St',
+			'address'      => '200 Broad Street',
+			'address2'      => 'Suite 142',
 			'city'      => 'New York',
 			'state'      => 'NY',
 			'country'      => 'US',
+			'zip'      => '10004',
 			'created_at' => $date->modify('-10 day'),
 			'updated_at' => $date->modify('-3 day'),
 			'user_id' => 1,
@@ -22,9 +25,12 @@ class LocationsSeeder extends Seeder {
 
 		$location[] = array(
 			'name'      => 'NY - Water St',
+			'address'      => '200 Broad Street',
+			'address2'      => 'Suite 142',
 			'city'      => 'New York',
 			'state'      => 'NY',
-			'country'      => 'US',
+			'country'      => '10038',
+			'zip'      => '10004',
 			'created_at' => $date->modify('-10 day'),
 			'updated_at' => $date->modify('-3 day'),
 			'user_id' => 1,
@@ -32,9 +38,12 @@ class LocationsSeeder extends Seeder {
 
 		$location[] = array(
 			'name'      => 'SF - Broadway',
+			'address'      => '200 Broadway',
+			'address2'      => 'Suite 142',
 			'city'      => 'San Francisco',
 			'state'      => 'CA',
 			'country'      => 'US',
+			'zip'      => '94111',
 			'created_at' => $date->modify('-10 day'),
 			'updated_at' => $date->modify('-3 day'),
 			'user_id' => 1,
@@ -42,9 +51,12 @@ class LocationsSeeder extends Seeder {
 
 		$location[] = array(
 			'name'      => 'LA - Hollywood Blvd',
+			'address'      => '6253 Hollywood Blvd',
+			'address2'      => 'Suite 142',
 			'city'      => 'Los Angeles',
 			'state'      => 'CA',
 			'country'      => 'US',
+			'zip'      => '90028',
 			'created_at' => $date->modify('-10 day'),
 			'updated_at' => $date->modify('-3 day'),
 			'user_id' => 1,
