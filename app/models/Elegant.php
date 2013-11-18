16afe9eb02b8a32e61793360344ed7326461a409
snipe <snipe@snipe.net>
2013-11-17 21:46:19 -0500

    Validation abstraction model

---
+++ app/models/Elegant.php
@@ -0,0 +1,29 @@
+<?
+
+class Elegant extends Eloquent
+{
+    protected $rules = array();
+    protected $errors;
+
+    public function validate($data)
+    {
+        // make a new validator object
+        $v = Validator::make($data, $this->rules);
+
+        // check for failure
+        if ($v->fails())
+        {
+            // set errors and return false
+            $this->errors = $v->errors();
+            return false;
+        }
+
+        // validation pass
+        return true;
+    }
+
+    public function errors()
+    {
+        return $this->errors;
+    }
+}
