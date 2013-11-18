897bc63621fc4686e7aea9cc8df283b80de63850
snipe <snipe@snipe.net>
2013-11-18 09:54:37 -0500

    Location drop down to user in views

--- app/views/backend/users/edit.blade.php
+++ app/views/backend/users/edit.blade.php
@@ -2,7 +2,7 @@
 
 {{-- Page title --}}
 @section('title')
-User Update ::
+Update User {{ $user->fullName() }} ::
 @parent
 @stop
 
@@ -10,7 +10,7 @@ User Update ::
 @section('content')
 <div class="page-header">
 	<h3>
-		User Update
+		Update User {{ $user->fullName() }}
 
 		<div class="pull-right">
 			<a href="{{ route('users') }}" class="btn-flat gray"><i class="icon-circle-arrow-left icon-white"></i> Back</a>
@@ -49,6 +49,36 @@ User Update ::
 					{{ $errors->first('last_name', '<span class="help-inline">:message</span>') }}
 				</div>
 			</div>
+
+			<!-- Jobtitle -->
+			<div class="control-group {{ $errors->has('jobtitle') ? 'error' : '' }}">
+				<label class="control-label" for="jobtitle">Job Title</label>
+				<div class="controls">
+					<input type="text" name="jobtitle" id="jobtitle" value="{{ Input::old('jobtitle', $user->jobtitle) }}" />
+					{{ $errors->first('jobtitle', '<span class="help-inline">:message</span>') }}
+				</div>
+			</div>
+
+			<!-- Location -->
+			<div class="control-group {{ $errors->has('phone') ? 'error' : '' }}">
+				<label class="control-label" for="location_id">Location</label>
+				<div class="controls">
+					<div class="field-box">
+					{{ Form::select('location_id', $location_list , Input::old('location_id', $user->location_id), array('class'=>'select2', 'style'=>'width:250px')) }}
+					{{ $errors->first('location_id', '<span class="help-inline">:message</span>') }}
+					</div>
+				</div>
+			</div>
+
+			<!-- Phone -->
+			<div class="control-group {{ $errors->has('phone') ? 'error' : '' }}">
+				<label class="control-label" for="jobtitle">Phone</label>
+				<div class="controls">
+					<input type="text" name="phone" id="phone" value="{{ Input::old('phone', $user->phone) }}" />
+					{{ $errors->first('phone', '<span class="help-inline">:message</span>') }}
+				</div>
+			</div>
+
 
 			<!-- Email -->
 			<div class="control-group {{ $errors->has('email') ? 'error' : '' }}">
--- app/views/backend/users/view.blade.php
+++ app/views/backend/users/view.blade.php
@@ -14,7 +14,7 @@ View User {{ $user->fullName() }} ::
                     <div class="span8">
                         <img src="{{ $user->gravatar() }}" class="avatar img-circle">
                         <h3 class="name">{{ $user->fullName() }}</h3>
-                        <span class="area">Placeholder Title</span>
+                        <span class="area">{{ $user->jobtitle }}</span>
                     </div>
 
                     <a href="{{ route('update/user', $user->id) }}" class="btn-flat white large pull-right edit"><i class="icon-pencil"></i> @lang('button.edit') This User</a>
@@ -35,19 +35,27 @@ View User {{ $user->fullName() }} ::
                             <table class="table table-hover">
                                 <thead>
                                     <tr>
+                                    	<th class="span3">Asset Type</th>
                                         <th class="span3">Asset Tag</th>
                                         <th class="span4"><span class="line"></span>Name</th>
-                                        <th class="span2"><span class="line"></span>Checked Out</th>
+                                        <th class="span2"><span class="line"></span>Date</th>
                                         <th class="span2"><span class="line"></span>Actions</th>
                                     </tr>
                                 </thead>
                                 <tbody>
 									@foreach ($user->assets as $asset)
 									<tr>
+										<td>
+										@if ($asset->physical=='1')
+										Hardware
+										@else
+										Software
+										@endif
+										</td>
 										<td>{{ $asset->asset_tag }}</td>
 										<td>{{ $asset->name }}</td>
-										<td> </td>
-										<td> </td>
+										<td></td>
+										<td> <a href="{{ route('checkin/asset', $asset->id) }}" class="btn-flat info">Checkin</a></td>
 									</tr>
 									@endforeach
                                 </tbody>
@@ -57,6 +65,12 @@ View User {{ $user->fullName() }} ::
 
                     <!-- side address column -->
                     <div class="span3 address pull-right">
+                    	@if ($user->last_login!='')
+                    	<ul>
+                    		<li><strong>Last Login:</strong></li>
+                            <li>{{ $user->last_login->diffForHumans() }}</li>
+                        </ul>
+                        @endif
                         <h6>Address</h6>
                         <iframe width="300" height="133" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com.mx/?ie=UTF8&amp;t=m&amp;ll=19.715081,-155.071421&amp;spn=0.010746,0.025749&amp;z=14&amp;output=embed"></iframe>
                         <ul>
@@ -64,11 +78,11 @@ View User {{ $user->fullName() }} ::
                             <li>City, Arlington. United States,</li>
                             <li>Zip Code, TX 76006.</li>
                             <li class="ico-li">
-                                <i class="ico-phone"></i>
-                                1817 274 2933
+                                <i class="icon-phone"></i>
+                               {{ $user->phone }}
                             </li>
                              <li class="ico-li">
-                                <i class="ico-mail"></i>
+                                <i class="icon-envelope-alt"></i>
                                 <a href="mailto:{{ $user->email }}">{{ $user->email }}</a>
                             </li>
                         </ul>
