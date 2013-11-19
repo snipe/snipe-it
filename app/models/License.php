7352eaec2ea6f69dda021b549e1f77ba001bd4b5
snipe <snipe@snipe.net>
2013-11-19 17:32:33 -0500

    Helper methods for license view display

--- app/models/License.php
+++ app/models/License.php
@@ -21,6 +21,29 @@ class License extends Elegant {
     	return $this->belongsTo('User', 'assigned_to');
   	}
 
+	/**
+	* Get the asset's location based on the assigned user
+	**/
+  	public function assetloc()
+  	{
+  		return $this->assigneduser->hasOne('Location');
+  	}
+
+  	/**
+	* Get action logs for this asset
+	*/
+	public function assetlog()
+	{
+		return $this->hasMany('Actionlog','asset_id')->orderBy('added_on', 'desc');
+	}
+
+	/**
+	* Get action logs for this asset
+	*/
+	public function adminuser()
+	{
+		return $this->belongsTo('User','id');
+	}
 
 
 }
