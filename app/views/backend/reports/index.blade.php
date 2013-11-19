5bcfca18c772eff9963c72ea4b79c05e74a14f62
snipe <snipe@snipe.net>
2013-11-18 22:03:05 -0500

    Added download button, right-aligned fields, added diff math

--- app/views/backend/reports/index.blade.php
+++ app/views/backend/reports/index.blade.php
@@ -15,8 +15,18 @@ Depreciation Report
 		Depreciation Report
 
 		<div class="pull-right">
-			<a href="{{ route('create/asset') }}" class="btn-flat success"><i class="icon-plus-sign icon-white"></i> Create New</a>
+			<div class="btn-group settings">
+				<button class="btn glow"><i class="icon-download-alt"></i></button>
+				<button class="btn glow dropdown-toggle" data-toggle="dropdown">
+					<span class="caret"></span>
+				</button>
+				<ul class="dropdown-menu">
+					<li><a href="#">Download as CSV</a></li>
+					<li><a href="#">Download as PDF</a></li>
+				</ul>
+			</div>
 		</div>
+
 	</h3>
 </div>
 
@@ -31,8 +41,9 @@ Depreciation Report
 			<th class="span3"><span class="line"></span>@lang('admin/assets/table.checkoutto')</th>
 			<th class="span2"><span class="line"></span>@lang('admin/assets/table.location')</th>
 			<th class="span2"><span class="line"></span>@lang('admin/assets/table.purchase_date')</th>
-			<th class="span1"><span class="line"></span>@lang('admin/assets/table.purchase_cost')</th>
-			<th class="span1"><span class="line"></span>@lang('admin/assets/table.book_value')</th>
+			<th class="span1 align-right"><span class="line"></span>@lang('admin/assets/table.purchase_cost')</th>
+			<th class="span1 align-rightt"><span class="line"></span>@lang('admin/assets/table.book_value')</th>
+			<th class="span1 align-right"><span class="line"></span>Diff</th>
 		</tr>
 	</thead>
 	<tbody>
@@ -51,12 +62,15 @@ Depreciation Report
 			</td>
 			<td>
 			@if (($asset->assigned_to > 0) && ($asset->assigneduser->location_id > 0))
-					{{ Location::find($asset->assigneduser->location_id)->name }}
+					{{ Location::find($asset->assigneduser->location_id)->city }}
+					,
+					{{ Location::find($asset->assigneduser->location_id)->state }}
 			@endif
 			</td>
 			<td>{{ $asset->purchase_date }}</td>
-			<td>${{ number_format($asset->purchase_cost) }}</td>
-			<td>${{ number_format($asset->depreciation()) }}</td>
+			<td class="align-right">${{ number_format($asset->purchase_cost) }}</td>
+			<td class="align-right">${{ number_format($asset->depreciation()) }}</td>
+			<td class="align-right">-${{ ($asset->purchase_cost - $asset->depreciation()) }}</td>
 
 		</tr>
 		@endforeach
