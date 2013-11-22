37e1ae7f5a03aca459d0bad73808bb6bdfe4f649
snipe <snipe@snipe.net>
2013-11-22 18:04:16 -0500

    Changed settings table structure
    Need to refactor this later

---
+++ app/database/migrations/2013_11_22_213400_edits_to_settings_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class EditsToSettingsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('settings', function($table)
+		{
+			$table->dropColumn('option_label');
+			$table->dropColumn('option_name');
+			$table->dropColumn('option_value');
+			$table->integer('per_page')->default(20);
+			$table->string('site_name','100')->default("Snipe IT Asset Management");
+		});
+
+
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+	}
+
+}
