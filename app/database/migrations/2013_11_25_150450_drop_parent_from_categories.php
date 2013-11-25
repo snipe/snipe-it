447a74928bad562ec67d8df0113f39151db5ae84
snipe <snipe@snipe.net>
2013-11-25 10:11:40 -0500

    Ditched parent ID - extraneous feature, not really needed

---
+++ app/database/migrations/2013_11_25_150450_drop_parent_from_categories.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class DropParentFromCategories extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('categories', function($table)
+		{
+			$table->dropColumn('parent');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+	}
+
+}
