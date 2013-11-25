dab7ec34572c35c6ade6256ee7a3a14086929387
snipe <snipe@snipe.net>
2013-11-25 05:41:57 -0500

    Sigh.

--- app/views/backend/assets/edit.blade.php
+++ app/views/backend/assets/edit.blade.php
@@ -107,7 +107,7 @@
 
 			<!-- Warrantee -->
 			<div class="control-group {{ $errors->has('warrantee_months') ? 'error' : '' }}">
-				<label class="control-label" for="serial">Warrantee</label>
+				<label class="control-label" for="serial">Warranty</label>
 				<div class="controls">
 					<input class="span1" type="text" name="warrantee_months" id="warrantee_months" value="{{ Input::old('warrantee_months', $asset->warrantee_months) }}" />  months
 					{{ $errors->first('warrantee_months', '<span class="help-inline">:message</span>') }}
