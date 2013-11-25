6fee1067bc31dae4b59eb590a76b454ddcc9f6e7
snipe <snipe@snipe.net>
2013-11-25 11:31:02 -0500

    Added depreciate flag for assets and licenses

---
+++ app/database/migrations/2013_11_25_151920_add_depreciate_to_assets.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class AddDepreciateToAssets extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		//
+		Schema::table('assets', function($table)
+		{
+			$table->boolean('depreciate');
+
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('assets', function($table)
+		{
+			$table->dropColumn('depreciate');
+		});
+
+
+
+	}
+
+}
---
+++ app/database/migrations/2013_11_25_152903_add_depreciate_to_licenses_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class AddDepreciateToLicensesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		//
+		Schema::table('licenses', function($table)
+		{
+			$table->boolean('depreciate');
+
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('licenses', function($table)
+		{
+			$table->dropColumn('depreciate');
+		});
+
+
+
+	}
+
+}
