e6fc1956fc93132d04bf5ac4c0d108c1ae9eee0b
snipe <snipe@snipe.net>
2013-11-26 03:13:09 -0500

    Fixes #55

--- app/models/Asset.php
+++ app/models/Asset.php
@@ -117,11 +117,26 @@ class Asset extends Elegant {
 	 public function months_until_depreciated()
 	{
 
-			$date = date_create($this->purchase_date);
-			date_add($date, date_interval_create_from_date_string($this->depreciation()->months.' months'));
-			return date_format($date, 'Y-m-d');
+			$today = date("Y-m-d");
+
+			// @link http://www.php.net/manual/en/class.datetime.php
+			$d1 = new DateTime($today);
+			$d2 = new DateTime($this->depreciated_date());
+
+			// @link http://www.php.net/manual/en/class.dateinterval.php
+			$interval = $d1->diff($d2);
+			return $interval;
 
 	}
+
+
+	 public function depreciated_date()
+	{
+			$date = date_create($this->purchase_date);
+			date_add($date, date_interval_create_from_date_string($this->depreciation->months.' months'));
+			return date_format($date, 'Y-m-d');
+	}
+
 
 	public function depreciation()
 	{
