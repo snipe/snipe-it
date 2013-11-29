103faca6ef53bb78d98ee7f361b03b7f56acc0e5
snipe <snipe@snipe.net>
2013-11-28 19:45:25 -0500

    Clearer display of current vs trashed

--- app/views/backend/users/index.blade.php
+++ app/views/backend/users/index.blade.php
@@ -11,16 +11,29 @@ User Management ::
 <div class="page-header">
 
 	<div class="pull-right">
-		<a href="{{ route('create/user') }}" class="btn-flat success"><i class="icon-plus-sign icon-white"></i> Create New</a>
+	@if (Input::get('onlyTrashed'))
+		<a class="btn-flat white" href="{{ URL::to('admin/users') }}">Show Current Users</a>
+	@else
+		<a class="btn-flat white" href="{{ URL::to('admin/users?onlyTrashed=true') }}">Show Deleted Users</a>
+	@endif
+
+		<a href="{{ route('create/user') }}" class="btn-flat success"><i class="icon-plus-sign icon-white"></i> New User</a>
 	</div>
 
 	<h3>
-		User Management
+		@if (Input::get('onlyTrashed'))
+			Deleted
+		@else
+			Current
+		@endif
+
+		 Users
 	</h3>
 </div>
 
-<a class="btn-flat white" href="{{ URL::to('admin/users?withTrashed=true') }}">Include Deleted Users</a>
-<a class="btn-flat white" href="{{ URL::to('admin/users?onlyTrashed=true') }}">Include Only Deleted Users</a>
+
+
+
 <br><br>
 
 @if ($users->getTotal() > 10)
