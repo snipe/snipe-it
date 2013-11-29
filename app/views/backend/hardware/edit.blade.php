7003eff42ca0e0ea2b9070187c0afd8ba1a8d8b0
snipe <snipe@snipe.net>
2013-11-29 14:36:46 -0500

    More fixes for #88

--- app/views/backend/hardware/edit.blade.php
+++ app/views/backend/hardware/edit.blade.php
@@ -117,16 +117,6 @@
 				</div>
 			</div>
 
-			<!-- Depreciation -->
-			<div class="form-group {{ $errors->has('depreciation_id') ? ' has-error' : '' }}">
-				<label for="parent" class="col-md-2 control-label">Depreciation</label>
-					<div class="col-md-7">
-						{{ Form::select('depreciation_id', $depreciation_list , Input::old('depreciation_id', $asset->depreciation_id), array('class'=>'select2', 'style'=>'width:350px')) }}
-						{{ $errors->first('depreciation_id', '<span class="alert-msg"><i class="icon-remove-sign"></i> :message</span>') }}
-					</div>
-			</div>
-
-
 			<!-- Status -->
 			<div class="form-group {{ $errors->has('status_id') ? ' has-error' : '' }}">
 				<label for="parent" class="col-md-2 control-label">Status</label>
