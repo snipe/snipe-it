7693e83d7f03854450a496e5d309ab31b9538936
snipe <snipe@snipe.net>
2013-11-30 17:02:11 -0500

    wtf

--- app/config/app.php
+++ app/config/app.php
@@ -113,6 +113,7 @@ return array(
 		'Illuminate\Validation\ValidationServiceProvider',
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
+		'Barryvdh\Debugbar\ServiceProvider',
 
 		'Cartalyst\Sentry\SentryServiceProvider',
 
