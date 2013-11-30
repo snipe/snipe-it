067ddb9aac205bdf4eeae99c74d768f12d6bccb6
snipe <snipe@snipe.net>
2013-11-30 17:53:21 -0500

    Config tweaks

--- app/config/app.php
+++ app/config/app.php
@@ -29,19 +29,6 @@ return array(
 
 	/*
 	|--------------------------------------------------------------------------
-	| Application Debug Mode
-	|--------------------------------------------------------------------------
-	|
-	| When your application is in debug mode, detailed error messages with
-	| stack traces will be shown on every error that occurs within your
-	| application. If disabled, a simple generic error page is shown.
-	|
-	*/
-
-	'debug' => false,
-
-	/*
-	|--------------------------------------------------------------------------
 	| Autoloaded Service Providers
 	|--------------------------------------------------------------------------
 	|
