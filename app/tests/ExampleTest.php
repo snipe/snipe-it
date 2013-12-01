70c446cb35e68123af4a8d21f6930ddb78c7edbf
snipe <snipe@snipe.net>
2013-12-01 18:47:40 -0500

    Baseline test class - nothing good here yet

--- app/tests/ExampleTest.php
+++ app/tests/ExampleTest.php
@@ -7,11 +7,10 @@ class ExampleTest extends TestCase {
 	 *
 	 * @return void
 	 */
-	public function testBasicExample()
+	 public function testSomethingIsTrue()
 	{
-		$crawler = $this->client->request('GET', '/');
-
-		$this->assertTrue($this->client->getResponse()->isOk());
+        $this->assertTrue(true);
 	}
+
 
 }
