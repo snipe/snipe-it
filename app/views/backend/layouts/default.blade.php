6737a9a91a54b49ecbb222506340392462057bf8
snipe <snipe@snipe.net>
2013-12-03 05:14:56 -0500

    Added quick create menu to topnav

--- app/views/backend/layouts/default.blade.php
+++ app/views/backend/layouts/default.blade.php
@@ -84,6 +84,30 @@
         <ul class="nav navbar-nav pull-right hidden-xs">
             @if (Sentry::check())
 
+				 <li class="dropdown">
+                    <a href="#" class="dropdown-toggle hidden-xs hidden-sm" data-toggle="dropdown">
+                        <i class="icon-plus"></i> Create New
+                        <b class="caret"></b>
+                    </a>
+                    <ul class="dropdown-menu">
+                       <li {{ (Request::is('hardware/create') ? 'class="active"' : '') }}>
+                       		<a href="{{ route('create/hardware') }}">
+                       			<i class="icon-plus"></i>
+                       			Asset</a>
+                       	</li>
+						<li {{ (Request::is('admin/licenses/create') ? 'class="active"' : '') }}>
+							<a href="{{ route('create/licenses') }}">
+								<i class="icon-plus"></i>
+								License</a>
+						</li>
+						<li {{ (Request::is('admin/users/create') ? 'class="active"' : '') }}>
+							<a href="{{ route('create/user') }}">
+							<i class="icon-plus"></i>
+							User</a>
+						</li>
+                    </ul>
+                </li>
+
                 <li class="dropdown">
                     <a href="#" class="dropdown-toggle hidden-xs hidden-sm" data-toggle="dropdown">
                         Welcome, {{ Sentry::getUser()->first_name }}
