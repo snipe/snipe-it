da9f28fbe70eb40a7ea5f06c99e48ca2bcdb30fb
snipe <snipe@snipe.net>
2013-12-10 03:48:20 -0500

    DB migration for #112

---
+++ app/database/migrations/2013_12_10_084038_add_eol_on_models_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class AddEolOnModelsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('models', function($table)
+		{
+			$table->integer('eol')->nullable()->default(NULL);
+
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('models', function($table)
+		{
+			$table->dropColumn('old');
+		});
+	}
+
+}
