90eb883b671919ea360122c2bf44433995a12144
snipe <snipe@snipe.net>
2014-07-09 05:04:49 -0400

    Fixed #187 - replaced dummy site name with site name from admin panel preferences

--- app/views/emails/forgot-password.blade.php
+++ app/views/emails/forgot-password.blade.php
@@ -3,11 +3,11 @@
 @section('content')
 <p>Hello {{ $user->first_name }},</p>
 
-<p>Please click on the following link to updated your password:</p>
+<p>Please click on the following link to update your {{{ Setting::getSettings()->site_name }}} password:</p>
 
 <p><a href="{{ $forgotPasswordUrl }}">{{ $forgotPasswordUrl }}</a></p>
 
 <p>Best regards,</p>
 
-<p>SiteNameHere Team</p>
+<p>{{{ Setting::getSettings()->site_name }}}</p>
 @stop
--- app/views/emails/register-activate.blade.php
+++ app/views/emails/register-activate.blade.php
@@ -3,11 +3,11 @@
 @section('content')
 <p>Hello {{ $user->first_name }},</p>
 
-<p>Welcome to SiteNameHere! Please click on the following link to confirm your SiteNameHere account:</p>
+<p>Welcome to {{{ Setting::getSettings()->site_name }}}! Please click on the following link to confirm your {{{ Setting::getSettings()->site_name }}} account:</p>
 
 <p><a href="{{ $activationUrl }}">{{ $activationUrl }}</a></p>
 
 <p>Best regards,</p>
 
-<p>SiteNameHere Team</p>
+<p>{{{ Setting::getSettings()->site_name }}}</p>
 @stop
