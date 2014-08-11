7ac06fa004ab2cb552160b4b0bd0db3c6c30d719
Cordeos Team <jay.richards@cordeos.com>
2014-08-11 17:45:11 +0900

    DB migration script Rev 0.11

--- app/database/migrations/20140810_0.4.0_snipeit_sqlmig.sql
+++ app/database/migrations/20140810_0.4.0_snipeit_sqlmig.sql
@@ -1,7 +1,7 @@
 
 /*
 SNIPE IT SQL migration script
-20140810_0.4.0
+20140810_0.4.0 - Rev 0.11
 Created by Cordeos
 */
 
@@ -65,6 +65,8 @@ ALTER TABLE `users`
 CHANGE COLUMN `created_at` `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ,
 CHANGE COLUMN `updated_at` `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ;
 
+UPDATE assets SET asset_tag='' WHERE asset_tag is null;
+
 ALTER TABLE `assets` 
 CHANGE COLUMN `name` `name` VARCHAR(255) CHARACTER SET 'utf8' COLLATE 'utf8_unicode_ci' NULL DEFAULT NULL ,
 CHANGE COLUMN `asset_tag` `asset_tag` VARCHAR(255) CHARACTER SET 'utf8' COLLATE 'utf8_unicode_ci' NOT NULL ,
@@ -82,10 +84,15 @@ CHANGE COLUMN `depreciate` `depreciate` TINYINT(1) NULL DEFAULT '0' ;
 ALTER TABLE `license_seats` 
 CHANGE COLUMN `assigned_to` `assigned_to` INT(11) NULL DEFAULT NULL ;
 
+UPDATE locations SET state='' where state is null;
+
 ALTER TABLE `locations` 
 CHANGE COLUMN `state` `state` VARCHAR(64) CHARACTER SET 'utf8' COLLATE 'utf8_unicode_ci' NOT NULL ,
 CHANGE COLUMN `address2` `address2` VARCHAR(80) CHARACTER SET 'utf8' COLLATE 'utf8_unicode_ci' NULL DEFAULT NULL ,
 CHANGE COLUMN `zip` `zip` VARCHAR(10) CHARACTER SET 'utf8' COLLATE 'utf8_unicode_ci' NULL DEFAULT NULL ;
+
+UPDATE models SET manufacturer_id=0 where manufacturer_id is null;
+UPDATE models SET category_id=0 where category_id is null;
 
 ALTER TABLE `models` 
 CHANGE COLUMN `modelno` `modelno` VARCHAR(255) CHARACTER SET 'utf8' COLLATE 'utf8_unicode_ci' NULL DEFAULT NULL ,
@@ -93,6 +100,8 @@ CHANGE COLUMN `manufacturer_id` `manufacturer_id` INT(11) NOT NULL ,
 CHANGE COLUMN `category_id` `category_id` INT(11) NOT NULL ,
 CHANGE COLUMN `depreciation_id` `depreciation_id` INT(11) NOT NULL DEFAULT '0' ,
 CHANGE COLUMN `eol` `eol` INT(11) NULL DEFAULT '0' ;
+
+UPDATE users SET location_id=0 WHERE location_id is null;
 
 ALTER TABLE `users` 
 CHANGE COLUMN `first_name` `first_name` VARCHAR(255) CHARACTER SET 'utf8' COLLATE 'utf8_unicode_ci' NOT NULL ,
