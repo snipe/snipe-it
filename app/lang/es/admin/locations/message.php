2bf57febd8830087897fa428c3c5de2cad4d02cd
mizar1616 <mizar1616@gmail.com>
2014-09-08 15:19:35 +0200

    Spanish Translation
    Ver 1.1

---
+++ app/lang/es/admin/locations/message.php
@@ -0,0 +1,25 @@
+<?php
+
+return array(
+
+    'does_not_exist' => 'Localización no existente.',
+    'assoc_users'	 => 'Esta localización está asignada al menos a un usuario y no puede ser eliminada. ',
+
+
+    'create' => array(
+        'error'   => 'Localización no creada, Intentalo de nuevo.',
+        'success' => 'Localización creada.'
+    ),
+
+    'update' => array(
+        'error'   => 'Localización no actualizada, Intentalo de nuevo',
+        'success' => 'Localización actualizada.'
+    ),
+
+    'delete' => array(
+        'confirm'   	=> 'Are you sure you wish to delete this location?',
+        'error'   => 'Localización no eliminada por un problema. Intentalo de nuevo.',
+        'success' => 'Localización eliminada.'
+    )
+
+);
---
+++ app/lang/es/admin/settings/message.php
@@ -0,0 +1,11 @@
+<?php
+
+return array(
+
+
+    'update' => array(
+        'error'   => 'Ha ocurrido un error al actualizar. ',
+        'success' => 'Parámetros actualizados correctamente.'
+    ),
+
+);
