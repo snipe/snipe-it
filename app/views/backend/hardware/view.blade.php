d3266dd4e8da7565cb9f9defb4f07723bdc16d68
diwanicki <david.iwanicki8@gmail.com>
2014-11-02 10:40:10 -0500

    still more

--- app/views/backend/hardware/view.blade.php
+++ app/views/backend/hardware/view.blade.php
@@ -20,7 +20,7 @@
      </h3>
 
     @if ($asset->serial)
-        <h4 small>[{{{ $asset->serial }}}]</h4>
+        <div><h4 small>[{{{ $asset->serial }}}]</h4></div>
     @endif
 
         <div class="btn-group pull-right">
@@ -134,8 +134,10 @@
 
         <!-- Asset notes -->
         @if ($asset->notes)
-            <p><div class="col-md-6"><strong>@lang('admin/hardware/form.notes'):</strong>
-            {{{ $asset->notes }}} </div></p>
+            <div></div>
+            <div class="col-md-6"><strong>@lang('admin/hardware/form.notes'):</strong>
+            {{{ $asset->notes }}} </div>
+            <div></div>
         @endif
 
     </div>
