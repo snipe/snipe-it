7849320db096d26a8cbb02dabca414e3f67f8988
Svpernova09 <joe@joeferguson.me>
2014-12-02 06:59:16 -0600

    Convert note fields to text areas for licenses views

--- app/views/backend/licenses/edit.blade.php
+++ app/views/backend/licenses/edit.blade.php
@@ -156,7 +156,7 @@
             <div class="form-group {{ $errors->has('notes') ? ' has-error' : '' }}">
                 <label for="notes" class="col-md-3 control-label">@lang('admin/licenses/form.notes')</label>
                 <div class="col-md-7">
-                    <input class="col-md-6 form-control" type="text" name="notes" id="notes" value="{{ Input::old('notes', $license->notes) }}" />
+                    <textarea class="col-md-6 form-control" id="notes" name="notes">{{{ Input::old('notes', $license->notes) }}}</textarea>
                     {{ $errors->first('notes', '<span class="alert-msg"><i class="icon-remove-sign"></i> :message</span>') }}
                 </div>
             </div>
