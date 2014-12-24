094ec50a68e395e74d73c436a641cc2a54e69f5d
Hereward Mills <hereward.mills@softwire.com>
2014-12-24 23:03:06 +0000

    TidyUp: Commonise the depreciate method of Asset and License.

--- app/models/Asset.php
+++ app/models/Asset.php
@@ -27,30 +27,11 @@ class Asset extends Elegant
     */
     public function depreciate()
     {
-        $depreciation_id = Model::find($this->model_id)->depreciation_id;
-        if ($depreciation_id) {
-            $depreciation_term = Depreciation::find($depreciation_id)->months;
-            if($depreciation_term>0) {
-
-                $purchase_date = strtotime($this->purchase_date);
-
-                $todaymonthnumber=date("Y")*12+(date("m")-1); //calculate the month number for today as YEAR*12 + (months-1) - number of months since January year 0
-                $purchasemonthnumber=date("Y",$purchase_date)*12+(date("m",$purchase_date)-1); //purchase date calculated similarly
-                $diff_months=$todaymonthnumber-$purchasemonthnumber;
-
-                // fraction of value left
-                $current_value = round((( $depreciation_term - $diff_months) / ($depreciation_term)) * $this->purchase_cost,2);
-
-                if ($current_value < 0) {
-                    $current_value = 0;
-                }
-                return $current_value;
-            } else {
-                return $this->purchase_cost;
-            }
-        } else {
-            return $this->purchase_cost;
-        }
+        return $this->getCurrentValue(
+            Model::find($this->model_id)->depreciation_id,
+            $this->purchase_cost,
+            $this->purchase_date
+        );
     }
 
     public function assigneduser()
