d85e5246807e597c1efac85c368a642a6fdcf944
snipe <snipe@snipe.net>
2015-01-22 14:29:53 -0800

    Fixes #451 - allows spaces in state name

--- app/models/Location.php
+++ app/models/Location.php
@@ -7,8 +7,8 @@ class Location extends Elegant
     protected $rules = array(
             'name'  		=> 'required|alpha_space|min:3|max:255|unique:locations,name,{id}',
             'city'   		=> 'required|alpha_space|min:3|max:255',
-            'state'   		=> 'required|alpha|min:2|max:32',
-            'country'   	=> 'required|alpha|min:2|max:2|max:2',
+            'state'   		=> 'required|alpha_space|min:2|max:32',
+            'country'   	=> 'required|alpha_space|min:2|max:2|max:2',
             'address'		=> 'required|alpha_space|min:5|max:80',
             'address2'		=> 'alpha_space|min:5|max:80',
             'zip'   		=> 'alpha_dash|min:3|max:10',
