a1ac7f8676d25d7988702cf1fe355af567eaba3e
snipe <snipe@snipe.net>
2015-02-04 15:01:08 -0800

    Removed cruft

--- app/views/backend/hardware/index.blade.php
+++ app/views/backend/hardware/index.blade.php
@@ -1,14 +1,5 @@
 @extends('backend/layouts/default')
 
-<?php
-use DebugBar\StandardDebugBar;
-
-$debugbar = new StandardDebugBar();
-$debugbarRenderer = $debugbar->getJavascriptRenderer();
-
-$debugbar["messages"]->addMessage("hello world!");
-?>
-
 @section('title0')
     @if (Input::get('Pending') || Input::get('Undeployable') || Input::get('RTD')  || Input::get('Deployed'))
         @if (Input::get('Pending'))
