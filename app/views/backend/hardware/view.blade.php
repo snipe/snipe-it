56e090f3e7d1f69182035cd56b1aac85f598f7b2
snipe <snipe@snipe.net>
2015-02-11 21:19:18 -0800

    Unlink username if they've been deleted

--- app/views/backend/hardware/view.blade.php
+++ app/views/backend/hardware/view.blade.php
@@ -214,9 +214,15 @@
                     <td>{{ $log->action_type }}</td>
                     <td>
                         @if ((isset($log->checkedout_to)) && ($log->checkedout_to!=0) && ($log->checkedout_to!=''))
+
+                        @if ($log->userlog->deleted_at=='')
                         <a href="{{ route('view/user', $log->checkedout_to) }}">
                         {{{ $log->userlog->fullName() }}}
-                        </a>
+                         </a>
+                        @else
+ 						<del>{{{ $log->userlog->fullName() }}}</del>
+                        @endif
+
                         @endif
                     </td>
                     <td>
