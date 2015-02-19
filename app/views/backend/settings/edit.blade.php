ad9e7f1d9e4ed0161bbe8e9ad81bd81c553fec24
snipe <snipe@snipe.net>
2015-02-19 15:34:55 -0800

    Cleaned up settings form for nicer display

--- app/views/backend/settings/edit.blade.php
+++ app/views/backend/settings/edit.blade.php
@@ -52,56 +52,72 @@ padding: 0px 20px;
                                 @foreach ($settings as $setting)
 
                                     <div class="form-group {{ $errors->has('site_name') ? 'error' : '' }}">
+	                                    <div class="col-md-3">
                                         {{ Form::label('site_name', Lang::get('admin/settings/general.site_name')) }}
+	                                    </div>
+	                                    <div class="col-md-9">
 										{{ Form::text('site_name', Input::old('site_name', $setting->site_name), array('class' => 'form-control')) }}
 										{{ $errors->first('site_name', '<span class="help-inline">:message</span>') }}
+	                                    </div>
                                     </div>
                                     
                                     <div class="form-group {{ $errors->has('logo') ? 'has-error' : '' }}">
-						                <label class="control-label" for="logo">@lang('admin/settings/general.logo')</label>
-						             
+	                                    
+	                                    <div class="col-md-3">
+										 	{{ Form::label('site_name', Lang::get('admin/settings/general.logo')) }}
+	                                    </div>
+	                                    <div class="col-md-9">				                						             
 						                    {{ Form::file('logo') }}
 						                    {{ $errors->first('logo', '<span class="alert-msg">:message</span>') }}
 						                    {{ Form::checkbox('clear_logo', '1', Input::old('clear_logo')) }} Remove
-
+	                                    </div>
 						                
 						            </div>
 
 									<div class="form-group {{ $errors->has('header_color') ? 'error' : '' }}">
+										<div class="col-md-3">
                                         {{ Form::label('header_color', Lang::get('admin/settings/general.header_color')) }}
+                                        </div>
+	                                    <div class="col-md-9">
+
 										{{ Form::text('header_color', Input::old('header_color', $setting->header_color), array('class' => 'form-control', 'style' => 'width: 100px;')) }}
 										{{ $errors->first('header_color', '<span class="help-inline">:message</span>') }}
+	                                    </div>
                                     </div>
 
 									 <div class="form-group {{ $errors->has('per_page') ? 'error' : '' }}">
-                                        {{ Form::label('per_page', Lang::get('admin/settings/general.per_page')) }}
+                                        <div class="col-md-3">
+											{{ Form::label('per_page', Lang::get('admin/settings/general.per_page')) }}
+										</div>
+	                                    <div class="col-md-9">
 										{{ Form::text('per_page', Input::old('per_page', $setting->per_page), array('class' => 'form-control', 'style'=>'width: 100px;')) }}
 										{{ $errors->first('per_page', '<span class="help-inline">:message</span>') }}
-
+										</div>
                                     </div>
 
 
-                                    <div class="checkbox">
+                                    <div class="checkbox col-md-offset-3">
+	                                    
 										<label>
 											{{ Form::checkbox('display_asset_name', '1', Input::old('display_asset_name', $setting->display_asset_name)) }}
 											@lang('admin/settings/general.display_asset_name')
 										</label>
                                     </div>
 
-                                     <div class="checkbox">
+                                     <div class="checkbox col-md-offset-3">
 										<label>
 											{{ Form::checkbox('display_eol', '1', Input::old('display_eol', $setting->display_eol)) }}
 											@lang('admin/settings/general.display_eol')
 										</label>
                                     </div>
 
-                                     <div class="checkbox">
+                                     <div class="checkbox col-md-offset-3">
 										<label>
 											{{ Form::checkbox('display_checkout_date', '1', Input::old('display_checkout_date', $setting->display_checkout_date)) }}
 											@lang('admin/settings/general.display_checkout_date')
 										</label>
                                     </div>
-                                    <div class="checkbox">
+                                   <div class="checkbox col-md-offset-3">
 										<label>
 											{{ Form::checkbox('load_remote', '1', Input::old('load_remote', $setting->load_remote)) }}
 											@lang('admin/settings/general.load_remote')
@@ -109,30 +125,26 @@ padding: 0px 20px;
                                     </div>
                                     <hr>
 
-                                    <div class="checkbox">
+                                    <div class="checkbox col-md-offset-3">
 										<label>
 											{{ Form::checkbox('auto_increment_assets', '1', Input::old('auto_increment_assets', $setting->auto_increment_assets)) }}
 											@lang('admin/settings/general.auto_increment_assets')
 										</label>
                                     </div>
-                                    
-                                    
-                                    
-
+                   
                                     <div class="form-group {{ $errors->has('auto_increment_prefix') ? 'error' : '' }}">
-                                            {{ Form::label('auto_increment_prefix', Lang::get('admin/settings/general.auto_increment_prefix')) }}
-
+                                            <div class="col-md-3">
+	                                            {{ Form::label('auto_increment_prefix', Lang::get('admin/settings/general.auto_increment_prefix')) }}
+											</div>
+	                                    <div class="col-md-9">
                                          @if ($setting->auto_increment_assets == 1)
 											{{ Form::text('auto_increment_prefix', Input::old('auto_increment_prefix', $setting->auto_increment_prefix), array('class' => 'form-control', 'style'=>'width: 100px;')) }}
 											{{ $errors->first('auto_increment_prefix', '<span class="help-inline">:message</span>') }}
 										@else
 											{{ Form::text('auto_increment_prefix', Input::old('auto_increment_prefix', $setting->auto_increment_prefix), array('class' => 'form-control', 'disabled'=>'disabled', 'style'=>'width: 100px;')) }}
-											<p class="help-inline">
-                                                @lang('admin/settings/general.qr_help')
-                                            </p>
 										@endif
 
-
+										</div>
                                     </div>
 
 
@@ -141,7 +153,7 @@ padding: 0px 20px;
 
                                     @if ($is_gd_installed)
 
-                                    		<div class="checkbox">
+                                    		<div class="checkbox col-md-offset-3">
 												<label>
 													{{ Form::checkbox('qr_code', '1', Input::old('qr_code', $setting->qr_code)) }}
 													@lang('admin/settings/general.display_qr')
@@ -149,7 +161,7 @@ padding: 0px 20px;
 											</div>
 
                                     @else
-                                            <span class="help-inline">
+                                            <span class="help-inline col-md-offset-3">
                                                 @lang('admin/settings/general.php_gd_warning')
                                                 <br>
                                                 @lang('admin/settings/general.php_gd_info')
@@ -158,8 +170,10 @@ padding: 0px 20px;
 
 
 									<div class="form-group {{ $errors->has('qr_text') ? 'error' : '' }}">
+										<div class="col-md-3">
                                         {{ Form::label('qr_text', Lang::get('admin/settings/general.qr_text')) }}
-
+										</div>
+	                                    <div class="col-md-9">
                                          @if ($setting->qr_code == 1)
 											{{ Form::text('qr_text', Input::old('qr_text', $setting->qr_text), array('class' => 'form-control')) }}
 											{{ $errors->first('qr_text', '<span class="help-inline">:message</span>') }}
@@ -169,6 +183,7 @@ padding: 0px 20px;
                                                 @lang('admin/settings/general.qr_help')
                                             </p>
 										@endif
+	                                    </div>
 
                                     </div>
 
@@ -177,7 +192,7 @@ padding: 0px 20px;
 
                                 <!-- Form actions -->
                                     <div class="form-group">
-                                        <div class="controls">
+                                        <div class="controls col-md-offset-3">
                                             <a class="btn btn-link" href="{{ URL::previous() }}">@lang('button.cancel')</a>
                                             <button type="submit" class="btn-flat success"><i class="icon-ok icon-white"></i> @lang('general.save')</button>
                                         </div>
