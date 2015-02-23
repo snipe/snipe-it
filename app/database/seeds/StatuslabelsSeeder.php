ae381576aaf6ffda9d416b0f003fddb15362681d
Tamás Roncsák <tamas@roncsak.hu>
2015-02-23 11:52:09 +0100

    Revert "Revert "Enhance further install without dummy data""
    This reverts commit b28bcce5f1ec81698c8e718cbcf6ab13915fc672.

--- app/database/seeds/StatuslabelsSeeder.php
+++ app/database/seeds/StatuslabelsSeeder.php
@@ -11,90 +11,90 @@ class StatuslabelsSeeder extends Seeder
         $date = new DateTime;
 
 		 $status[] = array(
-            'name'      => 'Ready to Deploy',
-            'created_at' => $date->modify('-10 day'),
-            'updated_at' => $date->modify('-3 day'),
-            'user_id' => 1,
-            'deleted_at' 		=> NULL,
+            'name'			=> 'Ready to Deploy',
+		    'created_at'	=> $date->modify('-10 day'),
+            'updated_at'	=> $date->modify('-3 day'),
+            'user_id'		=> 1,
+            'deleted_at' 	=> NULL,
             'deployable'	=> 1,
-            'pending'	=> 0,
-            'archived' => 0,
-            'notes' => ''
+            'pending'		=> 0,
+            'archived'		=> 0,
+            'notes'			=> ''
         );
 
          $status[] = array(
-            'name'      => 'Pending',
-            'created_at' => $date->modify('-10 day'),
-            'updated_at' => $date->modify('-3 day'),
-            'user_id' => 1,
-            'deleted_at' 		=> NULL,
+            'name'			=> 'Pending',
+            'created_at'	=> $date->modify('-10 day'),
+            'updated_at'	=> $date->modify('-3 day'),
+            'user_id'		=> 1,
+            'deleted_at'	=> NULL,
             'deployable'	=> 0,
-            'pending'	=> 1,
-            'archived' => 0,
-            'notes' => ''
+            'pending'		=> 1,
+            'archived'		=> 0,
+            'notes'			=> ''
         );
 
         $status[] = array(
-            'name'      => 'Archived',
-            'created_at' => $date->modify('-10 day'),
-            'updated_at' => $date->modify('-3 day'),
-            'user_id' => 1,
-            'deleted_at' 		=> NULL,
+            'name'			=> 'Archived',
+            'created_at'	=> $date->modify('-10 day'),
+            'updated_at'	=> $date->modify('-3 day'),
+            'user_id'		=> 1,
+            'deleted_at'	=> NULL,
             'deployable'	=> 0,
-            'pending'	=> 0,
-            'archived' => 1,
-            'notes' => 'These assets are permanently undeployable'
-        );
-
-
-        $status[] = array(
-            'name'      => 'Out for Diagnostics',
-            'created_at' => $date->modify('-10 day'),
-            'updated_at' => $date->modify('-3 day'),
-            'user_id' => 1,
-            'deleted_at' 		=> NULL,
-            'deployable'	=> 0,
-            'pending'	=> 0,
-            'archived' => 0,
-            'notes' => ''
+            'pending'		=> 0,
+            'archived'		=> 1,
+            'notes'			=> 'These assets are permanently undeployable'
         );
 
 
         $status[] = array(
-            'name'      => 'Out for Repair',
-            'created_at' => $date->modify('-10 day'),
-            'updated_at' => $date->modify('-3 day'),
-            'user_id' => 1,
-            'deleted_at' 		=> NULL,
+            'name'			=> 'Out for Diagnostics',
+            'created_at'	=> $date->modify('-10 day'),
+            'updated_at'	=> $date->modify('-3 day'),
+            'user_id'		=> 1,
+            'deleted_at'	=> NULL,
             'deployable'	=> 0,
-            'pending'	=> 0,
-            'archived' => 0,
-             'notes' => ''
+            'pending'		=> 0,
+            'archived'		=> 0,
+            'notes'			=> ''
         );
 
 
         $status[] = array(
-            'name'      => 'Broken - Not Fixable',
-            'created_at' => $date->modify('-10 day'),
-            'updated_at' => $date->modify('-3 day'),
-            'user_id' => 1,
-            'deleted_at' 		=> NULL,
+            'name'			=> 'Out for Repair',
+            'created_at'	=> $date->modify('-10 day'),
+            'updated_at'	=> $date->modify('-3 day'),
+            'user_id'		=> 1,
+            'deleted_at'	=> NULL,
             'deployable'	=> 0,
-            'pending'	=> 0,
-            'archived' => 1,
-             'notes' => ''
+            'pending'		=> 0,
+            'archived'		=> 0,
+            'notes'			=> ''
+        );
+
+
+        $status[] = array(
+            'name'			=> 'Broken - Not Fixable',
+            'created_at'	=> $date->modify('-10 day'),
+            'updated_at'	=> $date->modify('-3 day'),
+            'user_id'		=> 1,
+            'deleted_at'	=> NULL,
+            'deployable'	=> 0,
+            'pending'		=> 0,
+            'archived'		=> 1,
+            'notes'			=> ''
         );
 
         $status[] = array(
-            'name'      => 'Lost/Stolen',
-            'created_at' => $date->modify('-10 day'),
-            'updated_at' => $date->modify('-3 day'),
-            'user_id' => 1,
-            'deleted_at' 		=> NULL,
+            'name'			=> 'Lost/Stolen',
+            'created_at'	=> $date->modify('-10 day'),
+            'updated_at'	=> $date->modify('-3 day'),
+            'user_id'		=> 1,
+            'deleted_at'	=> NULL,
             'deployable'	=> 0,
-            'pending'	=> 0,
-            'archived' => 1,
-            'notes' => '',
+            'pending'		=> 0,
+            'archived'		=> 1,
+            'notes'			=> '',
         );
 
 
