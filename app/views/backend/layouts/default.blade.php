3de1a8f96cff37a0486f13087b7d74ba9329a820
snipe <snipe@snipe.net>
2015-02-26 01:07:34 -0800

    Pre-release version in footer

--- app/views/backend/layouts/default.blade.php
+++ app/views/backend/layouts/default.blade.php
@@ -398,7 +398,7 @@
 						  		<a target="_blank" href="http://docs.snipeitapp.com/">Documentation</a> |
 						  		<a href="https://crowdin.com/project/snipe-it">Help Translate It! </a> |
 						  		<a target="_blank" href="https://github.com/snipe/snipe-it/issues?state=open">Report a Bug</a>
-						  		 &nbsp; &nbsp; (v1.2.5)               
+						  		 &nbsp; &nbsp; (v1.2.6 pre-release)               
                   		</div>         	
         </div>
     </footer>
