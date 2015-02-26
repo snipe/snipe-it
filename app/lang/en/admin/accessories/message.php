62cb1daefde55c58cda9cab0a43e4774cccdb9d3
snipe <snipe@snipe.net>
2015-02-26 11:42:11 -0800

    Language strings for checkin/checkout

--- app/lang/en/admin/accessories/message.php
+++ app/lang/en/admin/accessories/message.php
@@ -19,6 +19,19 @@ return array(
         'confirm'   => 'Are you sure you wish to delete this category?',
         'error'   => 'There was an issue deleting the category. Please try again.',
         'success' => 'The category was deleted successfully.'
+    ),
+    
+     'checkout' => array(
+        'error'   		=> 'Accessory was not checked out, please try again',
+        'success' 		=> 'Accessory checked out successfully.',
+        'user_does_not_exist' => 'That user is invalid. Please try again.'
+    ),
+
+    'checkin' => array(
+        'error'   		=> 'Accessory was not checked in, please try again',
+        'success' 		=> 'Accessory checked in successfully.',
+        'user_does_not_exist' => 'That user is invalid. Please try again.'
     )
+
 
 );
