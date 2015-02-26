525a9225c83c60d692e0d357e388118d93448298
snipe <snipe@snipe.net>
2015-02-25 16:30:59 -0800

    Dashboard

--- app/controllers/admin/DashboardController.php
+++ app/controllers/admin/DashboardController.php
@@ -2,6 +2,8 @@
 
 use AdminController;
 use View;
+use Asset;
+use Actionlog;
 
 class DashboardController extends AdminController
 {
@@ -13,7 +15,57 @@ class DashboardController extends AdminController
     public function getIndex()
     {
         // Show the page
-        return View::make('backend/dashboard');
+        
+        $recent_activity = Actionlog::orderBy('created_at','DESC')->take(7)->get();
+
+        
+        $asset_stats['total'] = Asset::Hardware()->count();
+        
+        $asset_stats['rtd']['total'] = Asset::Hardware()->RTD()->count();
+        
+        if ($asset_stats['rtd']['total'] > 0) {
+	        $asset_stats['rtd']['percent'] = round(($asset_stats['rtd']['total']/$asset_stats['total']) * 100);
+        } else {
+	        $asset_stats['rtd']['percent'] = 0;
+        }
+        
+        
+        $asset_stats['pending']['total'] = Asset::Hardware()->Pending()->count();
+        
+        if ($asset_stats['pending']['total'] > 0) {
+	        $asset_stats['pending']['percent'] = round(($asset_stats['pending']['total']/$asset_stats['total']) * 100);
+        } else {
+	        $asset_stats['pending']['percent'] = 0;
+        }
+        
+        
+        $asset_stats['deployed']['total'] = Asset::Hardware()->Deployed()->count();
+        
+        if ($asset_stats['deployed']['total'] > 0) {
+	         $asset_stats['deployed']['percent'] = round(($asset_stats['deployed']['total']/$asset_stats['total']) * 100);
+        } else {
+	        $asset_stats['deployed']['percent'] = 0;
+        }
+        
+        
+        $asset_stats['undeployable']['total'] = Asset::Hardware()->Undeployable()->count();
+                
+        if ($asset_stats['undeployable']['total'] > 0) {
+	        $asset_stats['undeployable']['percent'] = round(($asset_stats['undeployable']['total']/$asset_stats['total']) * 100);
+        } else {
+	        $asset_stats['undeployable']['percent'] = 0;
+        }
+        
+        $asset_stats['archived']['total'] = Asset::Hardware()->Archived()->count();
+                
+        if ($asset_stats['archived']['total'] > 0) {
+	        $asset_stats['archived']['percent'] = round(($asset_stats['archived']['total']/$asset_stats['total']) * 100);
+        } else {
+	        $asset_stats['archived']['percent'] = 0;
+        }
+        
+        
+        return View::make('backend/dashboard')->with('asset_stats',$asset_stats)->with('recent_activity',$recent_activity);
     }
 
 }
