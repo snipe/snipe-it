5ec91ef5fc78803a8a42b2ac8cccd3b0bef839e6
snipe <snipe@snipe.net>
2015-02-26 18:56:30 -0800

    Removed test url

--- app/views/emails/accept-asset.blade.php
+++ app/views/emails/accept-asset.blade.php
@@ -26,7 +26,7 @@
 <p>{{ $eula }}</p>
 
 @if ($require_acceptance==1)
-<p><strong><a href="{{{ Config::get('app.url') }}}/account/accept-asset/{{ $log_id }}">I have read and agree to the terms of use, and have received this item.<br>{{{ Config::get('app.url') }}}/account/accept-asset/{{ $log_id }}</a></strong></p>
+<p><strong><a href="{{{ Config::get('app.url') }}}/account/accept-asset/{{ $log_id }}">I have read and agree to the terms of use, and have received this item.</a></strong></p>
 @endif
 
 <p>{{{ Setting::getSettings()->site_name }}}</p>
