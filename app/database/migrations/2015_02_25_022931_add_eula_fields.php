2d196b1595ec87f119ad5a82f8f676918c52c896
snipe <snipe@snipe.net>
2015-03-04 14:01:18 -0800

    Fixes #602 for strict_mode

--- app/database/migrations/2015_02_25_022931_add_eula_fields.php
+++ app/database/migrations/2015_02_25_022931_add_eula_fields.php
@@ -14,21 +14,21 @@ class AddEulaFields extends Migration {
 	{
 		//
 		Schema::table('settings', function ($table) {
-			$table->longText('default_eula_text');			
+			$table->longText('default_eula_text')->nullable()->default(NULL);
 		});
-		
+
 		Schema::table('categories', function ($table) {
 			$table->longText('eula_text');
 			$table->boolean('use_default_eula')->default(0);
 			$table->boolean('require_acceptance')->default(0);
 		});
-		
+
 		Schema::table('asset_logs', function ($table) {
 			$table->dateTime('requested_at')->nullable()->default(NULL);
 			$table->dateTime('accepted_at')->nullable()->default(NULL);
 		});
-		
-		
+
+
         Schema::create('requested_assets', function ($table) {
             $table->increments('id');
             $table->integer('asset_id')->default(NULL);
@@ -51,18 +51,18 @@ class AddEulaFields extends Migration {
 		Schema::table('settings', function ($table) {
 			$table->dropColumn('default_eula_text');
 		});
-		
+
 		Schema::table('categories', function ($table) {
 			$table->dropColumn('eula_text');
 			$table->dropColumn('use_default_eula');
 			$table->dropColumn('require_acceptance');
 		});
-		
+
 		Schema::table('asset_logs', function ($table) {
 			$table->dropColumn('requested_at');
 			$table->dropColumn('accepted_at');
 		});
-		
+
 		Schema::drop('requested_assets');
 	}
 
