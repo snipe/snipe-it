70c770a9753b978d3d1f422df7057aa6b31c36c4
madd15 <joshdeakin@adam.com.au>
2015-03-10 14:08:02 +1030

    update icon

--- app/views/backend/users/view.blade.php
+++ app/views/backend/users/view.blade.php
@@ -56,7 +56,7 @@
 
                             <div class="col-md-12">
                                 <div class="alert alert-danger">
-                                    <i class="fa fa-exclamation-sign"></i>
+                                    <i class="fa fa-exclamation"></i>
 
                                     @lang('admin/users/messages.user_deleted_warning')
 
@@ -289,7 +289,7 @@
                         @if ($user->phone)
                             <li><i class="fa fa-phone"></i>{{{ $user->phone }}}</li>
                         @endif
-                            <li><i class="fa fa-envelope-alt"></i><a href="mailto:{{{ $user->email }}}">{{{ $user->email }}}</a></li>
+                            <li><i class="fa fa-envelope-o"></i><a href="mailto:{{{ $user->email }}}">{{{ $user->email }}}</a></li>
                         </ul>
 
                         @if ($user->last_login!='')
