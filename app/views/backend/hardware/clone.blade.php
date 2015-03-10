efebfe3a13161fda0786019e7099842f06ac1e32
madd15 <joshdeakin@adam.com.au>
2015-03-10 13:29:57 +1030

    update icon

--- app/views/backend/hardware/clone.blade.php
+++ app/views/backend/hardware/clone.blade.php
@@ -11,7 +11,7 @@
 <div class="page-header">
 
         <div class="pull-right">
-            <a href="{{ URL::previous() }}" class="btn-flat gray"><i class="fa fa-arrow-circle-left icon-white"></i>  @lang('general.back')</a>
+            <a href="{{ URL::previous() }}" class="btn-flat gray"><i class="fa fa-arrow-left icon-white"></i>  @lang('general.back')</a>
         </div>
 
         <h3>@lang('admin/hardware/general.clone')</h3>
