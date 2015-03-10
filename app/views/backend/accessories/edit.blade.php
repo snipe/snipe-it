dab3a7135cd12026d225ae55123032fc43923d18
madd15 <joshdeakin@adam.com.au>
2015-03-10 13:32:52 +1030

    update icon

--- app/views/backend/accessories/edit.blade.php
+++ app/views/backend/accessories/edit.blade.php
@@ -16,7 +16,7 @@
 
 <div class="row header">
     <div class="col-md-12">
-        <a href="{{ URL::previous() }}" class="btn-flat gray pull-right"><i class="fa fa-arrow-circle-left icon-white"></i> @lang('general.back')</a>
+        <a href="{{ URL::previous() }}" class="btn-flat gray pull-right"><i class="fa fa-arrow-left icon-white"></i> @lang('general.back')</a>
         <h3>
         @if ($accessory->id)
             @lang('admin/accessories/general.update')
