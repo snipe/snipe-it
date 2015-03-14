aabf8794641dc7ba84802fe1b9d084198048fc20
snipe <snipe@snipe.net>
2015-03-14 03:18:06 -0700

    Added contributor code of conduct from http://contributor-covenant.org

--- app/config/version.php
+++ app/config/version.php
@@ -1,5 +1,5 @@
 <?php
 return array (
-  'app_version' => 'v1.2.6.1-38',
-  'hash_version' => 'v1.2.6.1-38-g5cfb2d5',
+  'app_version' => 'v1.2.6.1-39',
+  'hash_version' => 'v1.2.6.1-39-g6f38f76',
 );
