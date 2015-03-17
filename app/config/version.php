0ffb5df40b1983527efabad44e6c32fe0af3141f
snipe <snipe@snipe.net>
2015-03-16 20:22:32 -0700

    Fixed changelog order

--- app/config/version.php
+++ app/config/version.php
@@ -1,5 +1,5 @@
 <?php
 return array (
-  'app_version' => 'v1.2.6.1-62',
-  'hash_version' => 'v1.2.6.1-62-g1597461',
+  'app_version' => 'v1.2.6.1-64',
+  'hash_version' => 'v1.2.6.1-64-g41dd7a2',
 );
