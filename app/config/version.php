1597461c02e6aa994059f2143399161476a1a049
snipe <snipe@snipe.net>
2015-03-16 17:42:01 -0700

    More padding by filter

--- app/config/version.php
+++ app/config/version.php
@@ -1,5 +1,5 @@
 <?php
 return array (
-  'app_version' => 'v1.2.6.1-40',
-  'hash_version' => 'v1.2.6.1-40-g41928e4',
+  'app_version' => 'v1.2.6.1-60',
+  'hash_version' => 'v1.2.6.1-60-g77fd73b',
 );
