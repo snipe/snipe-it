41dd7a2b2a5f78c9d2f03548c40748a40f4878c4
snipe <snipe@snipe.net>
2015-03-16 18:39:13 -0700

    Update to Senry 2.1.5 for security vulnerability

--- app/config/version.php
+++ app/config/version.php
@@ -1,5 +1,5 @@
 <?php
 return array (
-  'app_version' => 'v1.2.6.1-61',
-  'hash_version' => 'v1.2.6.1-61-g46c1f2a',
+  'app_version' => 'v1.2.6.1-62',
+  'hash_version' => 'v1.2.6.1-62-g1597461',
 );
