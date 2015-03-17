4c3033d414c30da9db73318edce6d75e3ebe2fec
madd15 <joshdeakin@adam.com.au>
2015-03-18 09:16:01 +1030

    Fix spacing between Create and Import

--- app/views/backend/users/index.blade.php
+++ app/views/backend/users/index.blade.php
@@ -12,7 +12,7 @@
 <div class="row header">
     <div class="col-md-12">
 	    <a href="{{ route('import/user') }}" class="btn btn-default pull-right"><span class="fa fa-upload"></span> Import</a>
-        <a href="{{ route('create/user') }}" class="btn btn-success pull-right"><i class="fa fa-plus icon-white"></i>  @lang('general.create')</a>
+        <a href="{{ route('create/user') }}" class="btn btn-success pull-right" style="margin-right: 5px;"><i class="fa fa-plus icon-white"></i>  @lang('general.create')</a>
         @if (Input::get('status')=='deleted')
             <a class="btn btn-default pull-right" href="{{ URL::to('admin/users') }}" style="margin-right: 5px;">Show Current Users</a>
         @else
