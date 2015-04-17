ba58e718b4af2e2f60138e0b6fb3f6f6985f30e0
snipe <snipe@snipe.net>
2015-04-16 22:18:35 -0700

    Resolved some deps from vendor changes

--- app/config/version.php
+++ app/config/version.php
@@ -1,5 +1,5 @@
 <?php
 return array (
-  'app_version' => 'v1.2.6.1-195',
-  'hash_version' => 'v1.2.6.1-195-g9de083a',
+  'app_version' => 'v1.2.6.1-206',
+  'hash_version' => 'v1.2.6.1-206-ga560d4d',
 );
