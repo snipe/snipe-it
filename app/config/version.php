3b491d4a6bc02b3a0b46e8eae6804879a119f56e
snipe <snipe@snipe.net>
2015-04-29 16:22:25 -0700

    Fixes #757 - use showAssetName method

--- app/config/version.php
+++ app/config/version.php
@@ -1,5 +1,5 @@
 <?php
 return array (
-  'app_version' => 'v1.2.6.1-227',
-  'hash_version' => 'v1.2.6.1-227-gbce0654',
+  'app_version' => 'v1.2.6.1-228',
+  'hash_version' => 'v1.2.6.1-228-ga0fce2e',
 );
