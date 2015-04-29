767e2c69f324d32e0a49e0a386bd14640ad0304e
snipe <snipe@snipe.net>
2015-04-29 16:31:38 -0700

    Fixes #751 - ignore whether depreciation was selected or not to display purchase cost

--- app/config/version.php
+++ app/config/version.php
@@ -1,5 +1,5 @@
 <?php
 return array (
-  'app_version' => 'v1.2.6.1-228',
-  'hash_version' => 'v1.2.6.1-228-ga0fce2e',
+  'app_version' => 'v1.2.6.1-229',
+  'hash_version' => 'v1.2.6.1-229-g179ef7f',
 );
