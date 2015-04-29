6a7fcd5c40b201cc0744eab8b73bd66bfa8e33aa
snipe <snipe@snipe.net>
2015-04-29 13:54:54 -0700

    Fixes #769 - set sort order for Accessory Category

--- app/config/version.php
+++ app/config/version.php
@@ -1,5 +1,5 @@
 <?php
 return array (
-  'app_version' => 'v1.2.6.1-206',
-  'hash_version' => 'v1.2.6.1-206-ga560d4d',
+  'app_version' => 'v1.2.6.1-216',
+  'hash_version' => 'v1.2.6.1-216-ge6d0d83',
 );
