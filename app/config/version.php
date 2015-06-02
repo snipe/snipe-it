5939e46eeeeb6b1c54cc241ea8e85e358bc21d40
snipe <snipe@snipe.net>
2015-06-01 17:52:03 -0700

    Bumped version for fixed 1.2.7

--- app/config/version.php
+++ app/config/version.php
@@ -1,5 +1,5 @@
 <?php
 return array (
-  'app_version' => 'v1.2.7-beta',
-  'hash_version' => 'v1.2.7-beta-10-g48ccf84',
+  'app_version' => 'v1.2.7-master',
+  'hash_version' => 'v1.2.7-master',
 );
