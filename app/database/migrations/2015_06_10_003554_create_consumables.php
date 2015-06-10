6315c343a73a5afd206028eb8deb997b73498ec7
snipe <snipe@snipe.net>
2015-06-09 17:44:59 -0700

    Add consumables table

---
+++ app/database/migrations/2015_06_10_003554_create_consumables.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateConsumables extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		//
+		 Schema::create('consumables', function ($table) {
+            $table->increments('id');
+            $table->string('name')->nullable()->default(NULL);
+            $table->integer('category_id')->nullable()->default(NULL);
+			$table->integer('location_id')->nullable()->default(NULL);
+            $table->integer('user_id')->nullable()->default(NULL);
+            $table->integer('qty')->default(0);
+            $table->boolean('requestable')->default(0);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+        Schema::table('asset_logs', function ($table) {
+			$table->integer('consumable_id')->nullable()->default(NULL);
+		});
+
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+		Schema::drop('consumables');
+
+		Schema::table('asset_logs', function ($table) {
+			$table->dropColumn('consumable_id');
+		});
+	}
+
+}
