766b4b901f8dbefe4e052d1927f274a0ace4d7d3
snipe <snipe@snipe.net>
2015-06-09 17:34:52 -0700

    Fixes #822 - make user notes field nullable default null

--- app/config/version.php
+++ app/config/version.php
@@ -1,5 +1,5 @@
 <?php
 return array (
   'app_version' => 'v1.2.7-master',
-  'hash_version' => 'v1.2.7-master-38-gf743bac',
+  'hash_version' => 'v1.2.7-master-39-g03bd149',
 );
---
+++ app/database/migrations/2015_06_10_003314_fix_default_for_user_notes.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class FixDefaultForUserNotes extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('users', function ($table) {
+			$table->text('notes')->nullable()->default(NULL);
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+	}
+
+}
