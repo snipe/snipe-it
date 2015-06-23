f30285eaea2a2560430913c5608b59e775570469
snipe <snipe@snipe.net>
2015-06-23 11:39:28 -0700

    Added text string for consumable name

--- app/lang/en/admin/consumables/general.php
+++ app/lang/en/admin/consumables/general.php
@@ -3,6 +3,7 @@
 return array(
     'about_consumables_title' 			=> 'About Consumables',
     'about_consumables_text'  			=> 'Consumables are anything purchased that will be used up over time. For example, printer ink or copier paper.',
+    'consumable_name'                  => 'Consumable Name',
     'remaining' 			             => 'Remaining',
     'total' 			                 => 'Total',
 );
