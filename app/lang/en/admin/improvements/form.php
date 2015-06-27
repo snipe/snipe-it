78f1c0f11f1af4025c6355835a409f01c6824063
Vincent Sposato <vincent.sposato@gmail.com>
2015-06-27 14:36:07 -0400

    Made sure that all language files were there, even though they were blank

---
+++ app/lang/en/admin/improvements/form.php
@@ -0,0 +1,13 @@
+<?php
+
+    return [
+        'improvement_type' => 'Improvement Type',
+        'title'            => 'Title',
+        'start_date'       => 'Start Date',
+        'completion_date'  => 'Completion Date',
+        'cost'             => 'Cost',
+        'is_warranty'      => 'Warranty Improvement',
+        'notes'            => 'Notes',
+        'update'           => 'Update Improvement',
+        'create'           => 'Create Improvement'
+    ];
---
+++ app/lang/en/admin/improvements/message.php
@@ -0,0 +1,15 @@
+<?php
+
+    return [
+        'not_found' => 'Improvement you were looking for was not found!',
+        'delete'    => [
+            'confirm' => 'Are you sure you wish to delete this improvement?',
+            'error'   => 'There was an issue deleting the improvement. Please try again.',
+            'success' => 'The improvement was deleted successfully.'
+        ],
+        'create'    => [
+            'error'   => 'Improvement was not created, please try again.',
+            'success' => 'Improvement created successfully.'
+        ],
+
+    ];
