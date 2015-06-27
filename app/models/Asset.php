e479575825c9e99710e57f0649f3b42d566e69d5
Vincent Sposato <vincent.sposato@gmail.com>
2015-06-27 14:37:40 -0400

    Added all of the functionality for the Improvement to be linked to the Asset

--- app/models/Asset.php
+++ app/models/Asset.php
@@ -86,7 +86,7 @@ class Asset extends Depreciable
      * @author  Vincent Sposato <vincent.sposato@gmail.com>
      * @version v1.0
      */
-    public function improvement()
+    public function improvements()
     {
 
         return $this->hasMany( 'Improvement', 'asset_id' )
