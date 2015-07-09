1c2a409e6a06fd5901af50d5bc58e4bef6e0a0fe
snipe <snipe@snipe.net>
2015-07-09 07:08:33 -0700

    Check that there is an assetstatus

--- app/config/version.php
+++ app/config/version.php
@@ -1,5 +1,5 @@
 <?php
 return array (
   'app_version' => 'v2.0-pre',
-  'hash_version' => 'v2.0-pre-beta-2-gc61adc7',
+  'hash_version' => 'v2.0-pre-beta-3-g16b49c2',
 );
--- app/controllers/admin/AssetsController.php
+++ app/controllers/admin/AssetsController.php
@@ -1058,11 +1058,15 @@ class AssetsController extends AdminController
 
 	   $inout = new \Chumper\Datatable\Columns\FunctionColumn('inout', function ($assets)
       	{
-            if ($assets->assetstatus->deployable != 0) {
-                if (($assets->assigned_to !='') && ($assets->assigned_to > 0)) {
-                    return '<a href="'.route('checkin/hardware', $assets->id).'" class="btn btn-primary btn-sm">'.Lang::get('general.checkin').'</a>';
-                } else {
-                    return '<a href="'.route('checkout/hardware', $assets->id).'" class="btn btn-info btn-sm">'.Lang::get('general.checkout').'</a>';
+
+            if ($assets->assetstatus) {
+                
+                if ($assets->assetstatus->deployable != 0) {
+                    if (($assets->assigned_to !='') && ($assets->assigned_to > 0)) {
+                        return '<a href="'.route('checkin/hardware', $assets->id).'" class="btn btn-primary btn-sm">'.Lang::get('general.checkin').'</a>';
+                    } else {
+                        return '<a href="'.route('checkout/hardware', $assets->id).'" class="btn btn-info btn-sm">'.Lang::get('general.checkout').'</a>';
+                    }
                 }
             }
         });
