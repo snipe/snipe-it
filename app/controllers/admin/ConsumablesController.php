5ff84d80b4db86fbb2bdcfa1977983ac27e2410e
madd15 <joshdeakin@adam.com.au>
2015-07-20 10:16:58 +0930

    Change user list from email to username

--- app/controllers/admin/ConsumablesController.php
+++ app/controllers/admin/ConsumablesController.php
@@ -218,7 +218,7 @@ class ConsumablesController extends AdminController
         }
 
         // Get the dropdown of users and then pass it to the checkout view
-        $users_list = array('' => 'Select a User') + DB::table('users')->select(DB::raw('concat(last_name,", ",first_name," (",email,")") as full_name, id'))->whereNull('deleted_at')->orderBy('last_name', 'asc')->orderBy('first_name', 'asc')->lists('full_name', 'id');
+        $users_list = array('' => 'Select a User') + DB::table('users')->select(DB::raw('concat(last_name,", ",first_name," (",username,")") as full_name, id'))->whereNull('deleted_at')->orderBy('last_name', 'asc')->orderBy('first_name', 'asc')->lists('full_name', 'id');
 
         return View::make('backend/consumables/checkout', compact('consumable'))->with('users_list',$users_list);
 
