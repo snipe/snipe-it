fce38afa4dd7904225fd0d36734650c5a5d41386
thomas1216 <jarby1211@gmail.com>
2015-07-21 11:44:13 -0400

    Create table.php

---
+++ app/lang/es-LATAM/admin/models/table.php
@@ -0,0 +1,17 @@
+<?php
+
+return array(
+
+    'create'				=> 'Crear Modelo de Equipo',
+    'created_at' 			=> 'Creado el',
+    'eol'	 				=> 'EOL',
+    'modelnumber'   		=> 'Modelo No.',
+    'name'      			=> 'Nombre del Modelo',
+    'numassets' 			=> 'Equipos',
+    'title'					=> 'Modelo de Equipos',
+    'update'				=> 'Actualizar Modelo',
+    'view'					=> 'Ver Modelo',
+    'update'				=> 'Actualizar Modelo',
+    'clone'				=> 'Clonar Modelo',
+    'edit'				=> 'Editar Modelo',
+);
