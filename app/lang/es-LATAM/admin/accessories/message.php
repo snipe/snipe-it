2e38fa0aa09a65fb2dd1e0a6bc743a7398983b14
thomas1216 <jarby1211@gmail.com>
2015-07-21 11:26:00 -0400

    Create message.php

---
+++ app/lang/es-LATAM/admin/accessories/message.php
@@ -0,0 +1,37 @@
+<?php
+
+return array(
+
+    'does_not_exist' => 'Categoría inexistente.',
+    'assoc_users'	 => 'Este accesorio actualmente tiene :count entregados a usuarios.  Por favor verifique en los accesorios y vuelva a intentarlo',
+
+    'create' => array(
+        'error'   => 'La categoría no se ha creado, inténtalo de nuevo.',
+        'success' => 'Categoría creada correctamente.'
+    ),
+
+    'update' => array(
+        'error'   => 'La categoría no se ha actualizado, inténtalo de nuevo',
+        'success' => 'Categoría actualizada correctamente.'
+    ),
+
+    'delete' => array(
+        'confirm'   => 'Estás seguro de eliminar esta categoría?',
+        'error'   => 'Ha habido un problema eliminando la categoría. Intentalo de nuevo.',
+        'success' => 'La categoría fue eliminada exitosamente.'
+    ),
+    
+     'checkout' => array(
+        'error'   		=> 'El accesorio no fue retirado, por favor vuelva a intentarlo',
+        'success' 		=> 'Accesorio retirado correctamente.',
+        'user_does_not_exist' => 'Este usuario es inválido . Inténtalo de nuevo.'
+    ),
+
+    'checkin' => array(
+        'error'   		=> 'El accesorio no fue agregado, favor vuelva a intentarlo',
+        'success' 		=> 'Accesorio devuelto correctamente.',
+        'user_does_not_exist' => 'Este usuario es inválido . Inténtalo de nuevo.'
+    )
+
+
+);
