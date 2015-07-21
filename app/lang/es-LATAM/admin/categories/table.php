dd7e3bb586ef8a41ce672e9b7fb3c35b396e408b
thomas1216 <jarby1211@gmail.com>
2015-07-21 11:28:28 -0400

    Create table.php

---
+++ app/lang/es-LATAM/admin/categories/table.php
@@ -0,0 +1,10 @@
+<?php
+
+return array(
+	'eula_text'      			=> 'EULA',
+    'id'      					=> 'ID',
+    'parent'   					=> 'Categoria Principal',
+    'require_acceptance'      	=> 'Aceptación',
+    'title'      				=> 'Categoría del equipo',
+
+);
