776ce7bde7d9f9ccc3e65daa31c9212692141ba6
thomas1216 <jarby1211@gmail.com>
2015-07-21 11:23:44 -0400

    Create reminders.php

---
+++ app/lang/es-LATAM/reminders.php
@@ -0,0 +1,24 @@
+<?php
+
+return array(
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reminder Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    "password" => "La Contraseña deben tener mínimo 6 caracteres y coincidir.",
+
+    "user"     => "Usuario o E-Mail incorrectos",
+
+    "token"    => "El token para restaurar la contraseña no es válido.",
+
+    "sent" => "Si existe una cuenta con el e-mail Suministrado, se le enviará un mensaje para restaurar la contraseña!",
+
+);
