4c756657c7e05227e3594ca17efa13aea01a90bf
thomas1216 <jarby1211@gmail.com>
2015-07-21 11:25:29 -0400

    Create general.php

---
+++ app/lang/es-LATAM/admin/accessories/general.php
@@ -0,0 +1,20 @@
+<?php
+
+return array(
+    'about_accessories_title' 			=> 'Acerca de accesorios',
+    'about_accessories_text'  			=> 'Accesorios son cualquier cosa que entregues a los usuarios pero que no tenga numero de serie (o no te interese seguirlos en forma uníca). Por ejemplo, ratones o teclados.',
+    'accessory_category' 				=> 'Categoría de accesorio',
+    'accessory_name'  					=> 'Nombre de accesorio',
+    'create'  							=> 'Crear Categoría',
+    'eula_text'							=> 'Categoría EULA',
+    'eula_text_help'					=> 'Este campo permite personalizar sus EULAs para tipos específicos de equipos. Si solo tiene un EULA para todos sus equipos, puede seleccionar la siguiente opción para usar el valor predeterminado.',
+    'require_acceptance'				=> 'Requerir confirmación de usuarios para la aceptación de equipos en esta categoría.',
+    'no_default_eula'					=> 'No se encontró un EULA por defecto. Agregue uno en Configuración.',
+    'qty'  								=> 'Cant',
+    'total'  							=> 'Total ',
+    'remaining'  						=> 'Disponibles',
+    'update'  							=> 'Actualizar Categoría',
+    'use_default_eula'					=> 'En su lugar, use el <a href="#" data-toggle="modal" data-target="#eulaModal">EULA predeterminado</a>.',
+    'use_default_eula_disabled'			=> '<del>En su lugar, use el EULA prederterminado.</del> No esta configurado un EULA predeterminado. Por favor agregue uno en Configuración.',
+
+);
