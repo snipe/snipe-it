cbe93f136a2c57303c84d75184ae82d480c37e9f
thomas1216 <jarby1211@gmail.com>
2015-07-21 08:40:05 -0400

    Spanish grammar Changes

--- app/lang/es-ES/reminders.php
+++ app/lang/es-ES/reminders.php
@@ -13,12 +13,12 @@ return array(
     |
     */
 
-    "password" => "Los passwords deben tener mínimo 6 caracteres y coincidir.",
+    "password" => "Las contraseñas deben tener mínimo 6 caracteres y coincidir.",
 
     "user"     => "Usuario o E-Mail incorrectos",
 
-    "token"    => "El token para resetear el password no es válido.",
+    "token"    => "El token para restaurar la contraseña no es válido.",
 
-    "sent" => "Si existe cuenta con el e-mail introducido, se enviará mensaje para resetear el password!",
+    "sent" => "Si existe una cuenta con el e-mail Suministrado, se enviará un mensaje para restaurar la contraseña!",
 
 );
