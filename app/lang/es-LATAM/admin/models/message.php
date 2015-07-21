3d8dabc134aa2dc84f81fb79b2c420c9b0989677
thomas1216 <jarby1211@gmail.com>
2015-07-21 11:43:54 -0400

    Create message.php

---
+++ app/lang/es-LATAM/admin/models/message.php
@@ -0,0 +1,30 @@
+<?php
+
+return array(
+
+    'does_not_exist' => 'Modelo inexistente.',
+    'assoc_users'	 => 'Este modelo está asignado a uno o más equipos y no puede ser eliminado',
+
+
+    'create' => array(
+        'error'   => 'Modelo no creado, Intentalo de nuevo.',
+        'success' => 'Modelo creado.'
+    ),
+
+    'update' => array(
+        'error'   => 'Modelo no actualizado, Intentalo de nuevo',
+        'success' => 'Modelo actualizado.'
+    ),
+
+    'delete' => array(
+        'confirm'   => 'Estás seguro de quieres eliminar este Modelo?',
+        'error'   => 'Ha habido un problema al eliminar este Modelo. Intentalo de nuevo.',
+        'success' => 'Modelo eliminado.'
+    ),
+    
+    'restore' => array(
+        'error'   		=> 'El modelo no fue restaurado, por favor intente nuevamente',
+        'success' 		=> 'Modelo restaurado exitosamente.'
+    ),
+
+);
