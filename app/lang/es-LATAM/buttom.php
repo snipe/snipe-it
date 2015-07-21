0bf879a3b12f17040bb1b0945a01d2ac08d801b6
thomas1216 <jarby1211@gmail.com>
2015-07-21 10:58:09 -0400

    Create buttom.php

---
+++ app/lang/es-LATAM/buttom.php
@@ -0,0 +1,14 @@
+<?php
+
+return array(
+
+    'actions' 	=> 'Acciones',
+    'add'    	=> 'Agregar nuevo',
+    'cancel'    => 'Cancelar',
+    'delete'  	=> 'Borrar',
+    'edit'    	=> 'Editar',
+    'restore' 	=> 'Restaurar',
+    'submit'  	=> 'Enviar',
+    'upload'    => 'Subir',
+
+);
