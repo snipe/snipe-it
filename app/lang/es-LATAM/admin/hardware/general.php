7b38f481d1cfbe8b0694c6650f4040100988b278
thomas1216 <jarby1211@gmail.com>
2015-07-21 11:36:05 -0400

    Create general.php

---
+++ app/lang/es-LATAM/admin/hardware/general.php
@@ -0,0 +1,18 @@
+<?php
+
+return array(
+	'archived'  				=> 'Archivado',
+    'asset'  					=> 'Equipo',
+    'checkin'  					=> 'Quitar Equipo',
+    'checkout'  				=> 'Asignar a un usuario',
+    'clone'  					=> 'Clonar Equipo',
+    'deployable'  				=> 'Desplegable',
+    'deleted'  					=> 'Este equipo ha sido eliminado. <a href="/hardware/:asset_id/restore">Click aqui para restaurarlo</a>.',
+    'edit'  					=> 'Editar Equipo',
+    'model_deleted'  			=> 'Este modelo de equipo ha sido borrado. Debe restaurar el modelo antes de poder restaurar el equipo.<br/><a href="/hardware/models/:model_id/restore">Hacer click aquí para restaurar el modelo</a>.',
+    'requestable'				=> 'Requerible',
+    'restore'  					=> 'Restaurar equipo',
+    'pending'  					=> 'Pendiente',
+    'undeployable'  			=> 'No desplegable',
+    'view'  					=> 'Ver Equipo',
+);
