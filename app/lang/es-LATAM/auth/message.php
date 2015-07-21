bbb1c0ce37f1a930c7ed9ef7690cb6370f645b81
thomas1216 <jarby1211@gmail.com>
2015-07-21 11:53:45 -0400

    Create message.php

---
+++ app/lang/es-LATAM/auth/message.php
@@ -0,0 +1,36 @@
+<?php
+
+return array(
+
+    'account_already_exists' => 'Ya existe un usuario con este e-mail.',
+    'account_not_found'      => 'E-Mail o Contraseña son incorrectos.',
+    'account_not_activated'  => 'Este usuario no está activado.',
+    'account_suspended'      => 'Este usuario está desactivado.',
+    'account_banned'         => 'Este usuario ha sido expulsado.',
+
+    'signin' => array(
+        'error'   => 'Ha habido un problema al iniciar sesión. Por favor, vuelve a intentarlo.',
+        'success' => 'has iniciado sesión correctamente.',
+    ),
+
+    'signup' => array(
+        'error'   => 'Ha habido un problema al crear la cuenta. Por favor, vuelve a intentarlo.',
+        'success' => 'Cuenta creada correctamente.',
+    ),
+
+        'forgot-password' => array(
+            'error'   => 'Ha habido un problema al intentar restablecer la contraseña. Por favor, vuelve a intentarlo.',
+            'success' => 'Email de recuperación de contraseña ha sido enviado.',
+        ),
+
+        'forgot-password-confirm' => array(
+            'error'   => 'Ha habido un problema al intentar restablecer la contraseña. Por favor, vuelve a intentarlo.',
+            'success' => 'la contraseña se ha restablecido correctamente.',
+        ),
+
+    'activate' => array(
+        'error'   => 'Ha habido un problema al intentar activar la cuenta. Por favor, vuelve a intentarlo..',
+        'success' => 'Cuenta correctamente activada.',
+    ),
+
+);
