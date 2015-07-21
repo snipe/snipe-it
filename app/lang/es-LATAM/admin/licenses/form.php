8d92fd4d181a3c2533395ecc06ceecb3f1a6bab8
thomas1216 <jarby1211@gmail.com>
2015-07-21 11:38:48 -0400

    Create form.php

---
+++ app/lang/es-LATAM/admin/licenses/form.php
@@ -0,0 +1,27 @@
+<?php
+
+return array(
+
+    'asset'             => 'Equipo',
+    'checkin'           => 'Quita',
+    'cost'              => 'Precio Compra',
+    'create'            => 'Nueva Licencia',
+    'date'              => 'Fecha Compra',
+    'depreciation'      => 'Depreciación',
+    'expiration'        => 'Fecha de vencimiento',
+    'maintained'        => 'Mantenido',
+    'name'              => 'Nombre del Programa',
+    'no_depreciation'   => 'No se deprecia',
+    'notes'             => 'Notas',
+    'order'             => 'N. Factura',
+    'purchase_order'    => 'Número de orden de compra',
+    'remaining_seats'   => 'Instalaciones Restantes',
+    'seats'             => 'Instalaciones',
+    'serial'            => 'N. Serie',
+    'supplier'          => 'Proveedor',
+    'termination_date'  => 'Fecha de Terminación',
+    'to_email'          => 'Registrado a Email',
+    'to_name'           => 'Registrado a Nombre',
+    'update'            => 'Actualizar Licencia',
+    'checkout_help'     => 'Puedes asignar la licencia a un equipo a a un usuario. Puedes seleccionarlos a ambos, pero el dueño de la licencia debe ser el mismo usuario al que asignes el equipo.'
+);
