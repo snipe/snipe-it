9d2a1979602637ed4d3b833769cda335878d3549
thomas1216 <jarby1211@gmail.com>
2015-07-21 11:30:34 -0400

    Create message.php

---
+++ app/lang/es-LATAM/admin/depreciations/message.php
@@ -0,0 +1,25 @@
+<?php
+
+return array(
+
+    'does_not_exist' => 'La depreciación de clase no existe.',
+    'assoc_users'	 => 'Esta Depreciación está asociada a uno o más modelos y no puede ser eliminada.',
+
+
+    'create' => array(
+        'error'   => 'Depreciación no creada, intentalo de nuevo. :(',
+        'success' => 'Depreciación creada correctamente. :)'
+    ),
+
+    'update' => array(
+        'error'   => 'Depreciación no actualizada, intentalo de nuevo.',
+        'success' => 'Depreciación actualizada correctamente.'
+    ),
+
+    'delete' => array(
+        'confirm'   => 'Seguro que desea eliminar esta clase de depreciación?',
+        'error'   => 'Depreciación no eliminada, intentalo de nuevo.',
+        'success' => 'Depreciación eliminada correctamente.'
+    )
+
+);
