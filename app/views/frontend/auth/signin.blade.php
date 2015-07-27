c78355e39adc812f299db7c47e850e00b1679f3f
snipe <snipe@snipe.net>
2015-07-26 21:58:03 -0700

    Cleaned up login screen layout

--- app/views/frontend/auth/signin.blade.php
+++ app/views/frontend/auth/signin.blade.php
@@ -11,52 +11,55 @@ Account Sign in ::
 
 <div class="row header">
     <div class="col-md-12">
-        <h3>Sign in into your account</h3>
+        <h3>Sign in</h3>
     </div>
 </div>
 
-<div class="col-md-6 col-md-offset-1">
+<div class="col-md-11 col-md-offset-1">
 
     <form method="post" action="{{ route('signin') }}" class="form-horizontal">
         <!-- CSRF Token -->
         <input type="hidden" name="_token" value="{{ csrf_token() }}" />
 
             <!-- username -->
-            <div class="form-group{{ $errors->first('username', ' error') }}">
-                <label for="username" class="col-md-5 col-sm-12 control-label">Username</label>
-                    <div class="col-md-7 col-sm-12">
+            <div class="form-group {{ $errors->first('username', ' error') }}">
+                <label for="username" class="col-md-2 col-sm-12 control-label">Username</label>
+                    <div class="col-md-5 col-sm-12">
                         <input class="form-control" type="username" name="username" id="username" value="{{{ Input::old('username') }}}" />
                         {{ $errors->first('username', '<br><span class="alert-msg"><i class="fa fa-times"></i> :message</span>') }}
                     </div>
             </div>
 
             <!-- Password -->
-            <div class="form-group{{ $errors->first('password', ' error') }}">
-                <label for="password" class="col-md-5 col-sm-12 control-label">Password</label>
-                    <div class="col-md-7 col-sm-12">
+            <div class="form-group {{ $errors->first('password', ' error') }}">
+                <label for="password" class="col-md-2 col-sm-12 control-label">Password</label>
+                    <div class="col-md-5 col-sm-12">
                         <input class="form-control" type="password" name="password" id="password" value="{{{ Input::old('password') }}}" />
                         {{ $errors->first('password', '<br><span class="alert-msg"><i class="fa fa-times"></i> :message</span>') }}
                     </div>
             </div>
 
             <!-- Form Actions -->
-            <div class="col-md-7 col-sm-12 col-xs-12 col-md-offset-5">
+            <div class="col-md-12 col-sm-12 col-xs-12 col-md-offset-2">
 
-	    		<div class="form-group checkbox col-md-4 col-sm-12 col-xs-7">
-	    			<label>
-	    				 	{{ Form::checkbox('remember-me', '1', Input::old('remember-me'), array('style'=>'margin-left: 0px; margin-right: 5px;')) }}
-	    				 	Remember me
-	    			</label>
-	    		</div>
-	    		<div class="col-md-4 col-sm-6 col-xs-6 pull-right">
+                <div class="col-md-3 col-sm-12 col-xs-12" style="padding-left: 0px;">
+                    <div class="checkbox">
+                        <label>
+                          {{ Form::checkbox('remember-me', '1', Input::old('remember-me')) }} Remember me
+                        </label>
+                    </div>
+                </div>
+
+	    		<div class="col-md-2 col-sm-12 col-xs-12 text-right">
 	                 <button type="submit" class="btn btn-success"><i class="fa fa-check icon-white"></i> Sign in</button>
 	            </div>
-
             </div>
-            <div class="col-md-7 col-sm-12 col-xs-6 col-md-offset-5 text-right" style="padding-top: 15px;">
-	                <a href="{{ route('forgot-password') }}">I forgot my password</a>
-	            </div>
 
     </form>
 </div>
+
+<div class="col-md-6 col-sm-12 col-xs-12 col-md-offset-2 text-right" style="padding-top: 40px; padding-right: 60px">
+      <a href="{{ route('forgot-password') }}">I forgot my password</a>
+</div>
+
 @stop
