39da217a9ea1f300bc14f1615b2365027dab72dc
snipe <snipe@snipe.net>
2015-07-29 22:47:24 -0700

    Fixed table nane for asset_logs in migration

--- app/database/migrations/2015_07_29_230054_add_thread_id_to_asset_logs_table.php
+++ app/database/migrations/2015_07_29_230054_add_thread_id_to_asset_logs_table.php
@@ -15,7 +15,7 @@
         public function up()
         {
 
-            Schema::table( 'action_logs', function ( Blueprint $table ) {
+            Schema::table( 'asset_logs', function ( Blueprint $table ) {
 
                 $table->integer( 'thread_id', false, true );
                 $table->index( 'thread_id' );
@@ -30,7 +30,7 @@
         public function down()
         {
 
-            Schema::table( 'action_logs', function ( Blueprint $table ) {
+            Schema::table( 'asset_logs', function ( Blueprint $table ) {
 
                 $table->dropIndex( 'thread_id' );
                 $table->dropColumn( 'thread_id' );
