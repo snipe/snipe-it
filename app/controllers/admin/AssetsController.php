c513007b36b2bb8d6d09c9a25fbbf5e3ac71760c
madd15 <joshdeakin@adam.com.au>
2015-07-30 13:44:32 +0930

    Add sort to table, missing most columns still

--- app/controllers/admin/AssetsController.php
+++ app/controllers/admin/AssetsController.php
@@ -1089,17 +1089,32 @@ class AssetsController extends AdminController
           $assets->where('order_number','=',e(Input::get('order_number')));
       }
 
-      $assets = $assets->orderBy('asset_tag', 'ASC');
-
      if (Input::has('search')) {
          $assets = $assets->TextSearch(Input::get('search'));
      }
+     
+     if (Input::has('sort') && Input::has('order')){
+        $order = Input::get('order');
+        $sort = Input::get('sort');
+
+        switch ($sort)
+        {
+            case 'model':
+                $assets = $assets->OrderModels($order);
+                break;
+            default:
+                $assets = $assets->orderBy($sort, $order);
+                break;
+        }
+     } else {
+        $assets = $assets->orderBy('asset_tag', 'ASC');
+     }
+     
      $assetCount = $assets->count();
      $assets = $assets->skip(Input::get('offset'))->take(Input::get('limit'))->get();
 
 
       $rows = array();
-      $i=0;
       foreach ($assets as $asset) {
         if ($asset->deleted_at=='') {
             $actions = '<div style=" white-space: nowrap;"><a href="'.route('clone/hardware', $asset->id).'" class="btn btn-info btn-sm" title="Clone asset"><i class="fa fa-files-o"></i></a> <a href="'.route('update/hardware', $asset->id).'" class="btn btn-warning btn-sm"><i class="fa fa-pencil icon-white"></i></a> <a data-html="false" class="btn delete-asset btn-danger btn-sm" data-toggle="modal" href="'.route('delete/hardware', $asset->id).'" data-content="'.Lang::get('admin/hardware/message.delete.confirm').'" data-title="'.Lang::get('general.delete').' '.htmlspecialchars($asset->asset_tag).'?" onClick="return false;"><i class="fa fa-trash icon-white"></i></a></div>';
@@ -1133,7 +1148,6 @@ class AssetsController extends AdminController
             'change'    => $inout,
             'actions'   => $actions
             );
-        $i++;
       }
 
       $data = array('total'=>$assetCount, 'rows'=>$rows);
