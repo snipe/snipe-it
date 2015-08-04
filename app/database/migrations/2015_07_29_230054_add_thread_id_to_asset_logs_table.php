eb00acddfdf12ed1bb41d0a07741927964614d17
snipe <snipe@snipe.net>
2015-08-04 14:55:10 -0700

    Trying to fix the broken migration

--- app/database/migrations/2015_07_29_230054_add_thread_id_to_asset_logs_table.php
+++ app/database/migrations/2015_07_29_230054_add_thread_id_to_asset_logs_table.php
@@ -1,40 +1,36 @@
 <?php
 
-    use Illuminate\Database\Schema\Blueprint;
-    use Illuminate\Database\Migrations\Migration;
-    use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
-    class AddThreadIdToAssetLogsTable extends Migration
+class AddThreadIdToAssetLogsTable extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('asset_logs', function(Blueprint $table)
+        {
+            $table->integer('thread_id')->nullable()->default(NULL);
+            $table->index('thread_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
     {
 
-        /**
-         * Run the migrations.
-         *
-         * @return void
-         */
-        public function up()
-        {
-
-            Schema::table('asset_logs', function ( Blueprint $table ) {
-
-                $table->integer('thread_id')->nullable()->default(NULL);
-                $table->index('thread_id');
-            } );
-        }
-
-        /**
-         * Reverse the migrations.
-         *
-         * @return void
-         */
-        public function down()
-        {
-
-            Schema::table('asset_logs', function ( Blueprint $table ) {
-
-                $table->dropIndex('thread_id');
-                $table->dropColumn('thread_id');
-            } );
-        }
-
+        Schema::table('asset_logs', function ( Blueprint $table ) {
+            $table->dropIndex('thread_id');
+            $table->dropColumn('thread_id');
+        } );
     }
+
+}
