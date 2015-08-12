de238f155172dc6ae3490ba031a507696a035f09
snipe <snipe@snipe.net>
2015-08-11 19:00:08 -0700

    Set testing hostname

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,8 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-    'local'		 	=> array('homestead','AlisonMBP','snipe-it.dev'),
+    'testing'		=> array('snipe-it.dev'),
+    'local'		 	=> array('homestead','AlisonMBP'),
     'staging' 		=> array('stagingweb01'),
     'production' 	=> array('www.yourserver.com')
 ));
