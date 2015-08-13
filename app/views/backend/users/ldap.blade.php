a531ea34bbf8c6c5b65fa85aa4b3d7e274e435fa
Aladin Alaily <aladin@aladin.ca>
2015-08-13 13:27:24 -0400

    Cleaned up the code

--- app/views/backend/users/ldap.blade.php
+++ app/views/backend/users/ldap.blade.php
@@ -57,96 +57,17 @@ $( document ).ready(function() {
                 <p>Note: Email notification for users is disabled for this installation.</p>
             @endif
 
-			<!--
-			<div class="form-group {{ $errors->has('connection_string') ? 'has-error' : '' }}">
-                <label class="col-md-3 control-label" for="connection_string">LDAP Connection String <i class='fa fa-asterisk'></i></label>
-                <div class="col-md-7">
-                    <input class="form-control" type="text" name="connection_string" id="connection_string" value="" />
-                    {{ $errors->first('connection_string', '<br><span class="alert-msg">:message</span>') }}
-                </div>
-            </div>
-
-			<div class="form-group {{ $errors->has('ldap_username') ? 'has-error' : '' }}">
-                <label class="col-md-3 control-label" for="ldap_username">Username <i class='fa fa-asterisk'></i></label>
-                <div class="col-md-7">
-                    <input class="form-control" type="text" name="ldap_username" id="ldap_username" value="" />
-                    {{ $errors->first('ldap_username', '<br><span class="alert-msg">:message</span>') }}
-                </div>
-            </div>
-
-			<div class="form-group {{ $errors->has('ldap_password') ? 'has-error' : '' }}">
-                <label class="col-md-3 control-label" for="ldap_password">Password <i class='fa fa-asterisk'></i></label>
-                <div class="col-md-7">
-                    <input class="form-control" type="password" name="ldap_password" id="ldap_password" value="" />
-                    {{ $errors->first('ldap_password', '<br><span class="alert-msg">:message</span>') }}
-                </div>
-            </div>
-			-->
-			
-			<!--
-            <div class="form-group {{ $errors->first('user_import_csv', 'has-error') }}">
-                <label for="first_name" class="col-sm-2 control-label">@lang('admin/users/general.usercsv')</label>
-				<div class="col-sm-5">
-					<input type="file" name="user_import_csv" id="user_import_csv">
-				</div>
-            </div>
-			-->
-            <!-- Has Headers -->
-			<!--
-			<div class="form-group">
-				<div class="col-sm-2 ">
-				</div>
-				<div class="col-sm-5">
-					{{ Form::checkbox('has_headers', '1', Input::old('has_headers')) }} This CSV has a header row
-				</div>
-			</div>
-			-->
-			<!-- Email user -->
-			<!--
-			<div class="form-group">
-				<div class="col-sm-2 ">
-				</div>
-				<div class="col-sm-5">
-					{{ Form::checkbox('email_user', '1', Input::old('email_user')) }} Email these users their credentials? (Only possible where email address is included with user data.)
-				</div>
-			</div>
-			-->
-			<!-- Activate -->
-			<!--
-			<div class="form-group">
-				<div class="col-sm-2 ">
-				</div>
-				<div class="col-sm-5">
-					{{ Form::checkbox('activate', '1', Input::old('activate')) }} Activate user?
-				</div>
-			</div>
-			-->
-
-
         </div>
     </div>
 
-    <!-- Form Actions -->
-	<!--
     <div class="form-group">
-        <div class="col-sm-offset-2 col-sm-4">
-            <a class="btn btn-link" href="{{ route('users') }}">@lang('button.cancel')</a>
-            <button type="submit" class="btn btn-default">@lang('button.submit')</button>
+	<label class="col-md-3 control-label"></label>
+        <div class="col-md-7">
+            <button type="submit" class="btn btn-warning" id="sync">
+                <i class="fa fa-refresh icon-white"></i> Synchronize
+            </button>
         </div>
     </div>
-	-->
-	<div class="form-group">
-		<label class="col-md-3 control-label"></label>
-			<div class="col-md-7">
-				<!--
-				<a class="btn btn-link" href="{{ route('users') }}">@lang('button.cancel')</a>
-				<button type="reset" class="btn">Reset</button> -->
-				<button type="submit" class="btn btn-warning" id="sync">
-					<i class="fa fa-refresh icon-white"></i> Synchronize
-				</button>
-
-			</div>
-		</div>
 </form>
 
 @if (Session::get('summary'))
@@ -158,20 +79,20 @@ $( document ).ready(function() {
             <th>Email</th><th>Notes</th>
 	</tr>
 	<?php
-				$summary = Session::get('summary');
-				foreach( $summary as $entry ) {
-					echo "<tr>";
-					echo "<td>" . $entry['username'] . "</td>";
-					echo "<td>" . $entry['pycyin'] . "</td>";
-					echo "<td>" . $entry['firstname'] . "</td>";
-					echo "<td>" . $entry['lastname'] . "</td>";
-					echo "<td>" . $entry['mail'] . "</td>";
-					echo "<td>" . $entry['note'] . "</td>";
-					echo "</tr>";
-				}
-				?>
-				</table>
+            $summary = Session::get('summary');
+            foreach( $summary as $entry ) {
+                echo "<tr>";
+		echo "<td>" . $entry['username'] . "</td>";
+		echo "<td>" . $entry['pycyin'] . "</td>";
+		echo "<td>" . $entry['firstname'] . "</td>";
+		echo "<td>" . $entry['lastname'] . "</td>";
+		echo "<td>" . $entry['mail'] . "</td>";
+		echo "<td>" . $entry['note'] . "</td>";
+		echo "</tr>";
+            }
+	?>
+    </table>
 			
-			@endif
+@endif
 
 @stop
