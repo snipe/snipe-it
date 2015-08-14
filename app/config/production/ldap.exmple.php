bebd569490799ef7255cdbc73b25ee6930f5fe92
Aladin Alaily <aladin@aladin.ca>
2015-08-14 00:23:23 -0400

    updated the sample config doc

--- app/config/production/ldap.exmple.php
+++ app/config/production/ldap.exmple.php
@@ -41,7 +41,7 @@ return array(
     | The base where the search for users will be executed.
     |
     */
-	'basedn'   => "ou=People,dc=yorku,dc=ca",
+	'basedn'   => "",
 	
 
 	/*
@@ -52,6 +52,27 @@ return array(
     | The search filter for the LDAP query.
     |
     */
-	'filter' => ""
+	'filter' => "",
 
+    
+	/*
+    |--------------------------------------------------------------------------
+    | LDAP field names that will be retrieved to create a user.
+    | 
+    | Using the username as an example:
+    | If I set 'result.username' => 'my-org-username', the code will connect to
+    | LDAP as follows (where $results[$i] represents a row in the LDAP query:
+    | $username-to-insert-in-snipe-it = $results[$i]["my-org-username"][0]
+    |
+    | Note: all these fields are required.
+    |--------------------------------------------------------------------------
+    |
+    | The search filter for the LDAP query.
+    |
+    */    
+    'result.username' => "",
+    'result.emp.num'  => "",
+    'result.last.name'  => "",
+    'result.first.name' => "",
+    'result.email' => ""
 );
