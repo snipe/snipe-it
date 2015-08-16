8d5033c4e5d7e5b08aff636f1879bc0e00350169
Vincent Sposato <vincent.sposato@gmail.com>
2015-08-15 20:33:47 -0400

    A tweak to make this work with an app environment variable

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -1,73 +1,76 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Create The Application
-|--------------------------------------------------------------------------
-|
-| The first thing we will do is create a new Laravel application instance
-| which serves as the "glue" for all the components of Laravel, and is
-| the IoC container for the system binding all of the various parts.
-|
-*/
+    /*
+    |--------------------------------------------------------------------------
+    | Create The Application
+    |--------------------------------------------------------------------------
+    |
+    | The first thing we will do is create a new Laravel application instance
+    | which serves as the "glue" for all the components of Laravel, and is
+    | the IoC container for the system binding all of the various parts.
+    |
+    */
 
-$app = new Illuminate\Foundation\Application;
+    $app = new Illuminate\Foundation\Application;
 
-/*
-|--------------------------------------------------------------------------
-| Detect The Application Environment
-|--------------------------------------------------------------------------
-|
-| Laravel takes a dead simple approach to your application environments
-| so you can just specify a machine name or HTTP host that matches a
-| given environment, then we will automatically detect it for you.
-|
-*/
+    /*
+    |--------------------------------------------------------------------------
+    | Detect The Application Environment
+    |--------------------------------------------------------------------------
+    |
+    | Laravel takes a dead simple approach to your application environments
+    | so you can just specify a machine name or HTTP host that matches a
+    | given environment, then we will automatically detect it for you.
+    |
+    */
+    if ($_SERVER[ 'APP_ENV' ] == 'travis-ci') {
+        $env = 'travis-ci';
+    } else {
+        $env = $app->detectEnvironment( [
+            'local'      => [ 'homestead', 'AlisonMBP' ],
+            'travis-ci'  => [ 'travis-linux-*', 'worker-linux-docker-*' ],
+            'staging'    => [ 'stagingweb01' ],
+            'production' => [ 'www.yourserver.com' ]
+        ] );
+    }
 
-$env = $app->detectEnvironment(array(
-    'local'		 	=> array('homestead','AlisonMBP'),
-    'travis-ci'		 	=> array('travis-linux-*', 'worker-linux-docker-*'),
-    'staging' 		=> array('stagingweb01'),
-    'production' 	=> array('www.yourserver.com')
-));
+    /*
+    |--------------------------------------------------------------------------
+    | Bind Paths
+    |--------------------------------------------------------------------------
+    |
+    | Here we are binding the paths configured in paths.php to the app. You
+    | should not be changing these here. If you need to change these you
+    | may do so within the paths.php file and they will be bound here.
+    |
+    */
 
-/*
-|--------------------------------------------------------------------------
-| Bind Paths
-|--------------------------------------------------------------------------
-|
-| Here we are binding the paths configured in paths.php to the app. You
-| should not be changing these here. If you need to change these you
-| may do so within the paths.php file and they will be bound here.
-|
-*/
+    $app->bindInstallPaths( require __DIR__ . '/paths.php' );
 
-$app->bindInstallPaths(require __DIR__.'/paths.php');
+    /*
+    |--------------------------------------------------------------------------
+    | Load The Application
+    |--------------------------------------------------------------------------
+    |
+    | Here we will load the Illuminate application. We'll keep this is in a
+    | separate location so we can isolate the creation of an application
+    | from the actual running of the application with a given request.
+    |
+    */
 
-/*
-|--------------------------------------------------------------------------
-| Load The Application
-|--------------------------------------------------------------------------
-|
-| Here we will load the Illuminate application. We'll keep this is in a
-| separate location so we can isolate the creation of an application
-| from the actual running of the application with a given request.
-|
-*/
+    $framework = $app[ 'path.base' ] . '/vendor/laravel/framework/src';
 
-$framework = $app['path.base'].'/vendor/laravel/framework/src';
+    require $framework . '/Illuminate/Foundation/start.php';
 
-require $framework.'/Illuminate/Foundation/start.php';
+    /*
+    |--------------------------------------------------------------------------
+    | Return The Application
+    |--------------------------------------------------------------------------
+    |
+    | This script returns the application instance. The instance is given to
+    | the calling script so we can separate the building of the instances
+    | from the actual running of the application and sending responses.
+    |
+    */
 
-/*
-|--------------------------------------------------------------------------
-| Return The Application
-|--------------------------------------------------------------------------
-|
-| This script returns the application instance. The instance is given to
-| the calling script so we can separate the building of the instances
-| from the actual running of the application and sending responses.
-|
-*/
-
-return $app;
+    return $app;
