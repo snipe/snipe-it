d35f8e624496ab820ab03cb65a63f9760f3190e9
madd15 <joshdeakin@adam.com.au>
2015-08-18 14:13:59 +0930

    Change to Bootstrap Table

--- app/views/backend/accessories/view.blade.php
+++ app/views/backend/accessories/view.blade.php
@@ -25,62 +25,26 @@
 </div>
 
 <div class="user-profile">
-<div class="row profile">
-<div class="col-md-9 bio">
-
-
-                            <!-- checked out accessories table -->
-                            @if ($accessory->users->count() > 0)
-                             {{ Datatable::table()
-                ->addColumn(Lang::get('general.user'), 
-                            Lang::get('table.actions'))
-                ->setOptions(
-                        array(
-                            'sAjaxSource'=>route('api.accessories.view', $accessory->id),
-                            'dom' =>'T<"clear">lfrtip',
-                            'tableTools' => array(
-                                'sSwfPath'=> Config::get('app.url').'/assets/swf/copy_csv_xls_pdf.swf',
-                                'aButtons'=>array(
-                                    array(
-                                        'sExtends'=>'copy',
-                                    ),
-                                    'print',
-                                    array(
-                                        'sExtends'=>'collection',
-                                        'sButtonText'=>'Export',
-                                        'aButtons'=>array(
-                                            array(
-                                                'sExtends'=>'csv',
-                                            ),
-                                            array(
-                                                'sExtends'=>'xls',
-                                            ),
-                                            array(
-                                                'sExtends'=>'pdf',
-                                            ),
-                                        ),
-                                    ),
-                                ) 
-                            ),
-                            'columnDefs'=> array(
-                                array('bSortable'=>false,'targets'=>array(1)),
-                                array('width'=>'auto','targets'=>array(1)),
-                                ),
-                            'order'=>array(array(0,'asc')),
-                        )
-                    )
-                ->render() }}
-
-                            @else
-                            <div class="col-md-9">
-                                <div class="alert alert-info alert-block">
-                                    <i class="fa fa-info-circle"></i>
-                                    @lang('general.no_results')
-                                </div>
-                            </div>
-                            @endif
-
-                        </div>
+    <div class="row profile">
+        <div class="col-md-9 bio">
+            @if ($accessory->users->count() > 0)
+                <table name="accessory_users" id="table" data-url="{{route('api.accessories.view', $accessory->id)}}">
+                    <thead>
+                        <tr>
+                            <th data-switchable="false" data-searchable="false" data-sortable="false" data-field="name">{{Lang::get('general.user')}}</th>
+                            <th data-switchable="false" data-searchable="false" data-sortable="false" data-field="actions">{{Lang::get('table.actions')}}</th>
+                        </tr>
+                    </thead>
+                </table>
+            @else
+                <div class="col-md-9">
+                    <div class="alert alert-info alert-block">
+                        <i class="fa fa-info-circle"></i>
+                        @lang('general.no_results')
+                    </div>
+                </div>
+            @endif
+        </div>
 
 <!-- side address column -->
 <div class="col-md-3 col-xs-12 address pull-right">
@@ -89,5 +53,34 @@
     <p>@lang('admin/accessories/general.about_accessories_text') </p>
 
 </div>
+
+<script type="text/javascript">
+    $('#table').bootstrapTable({
+        classes: 'table table-hover table-no-bordered',
+        undefinedText: 'undefined',
+        iconsPrefix: 'fa',
+        showRefresh: true,
+        search: false,
+        pageSize: {{{ Setting::getSettings()->per_page }}},
+        pagination: true,
+        sidePagination: 'server',
+        sortable: true,
+        mobileResponsive: true,
+        showExport: true,
+        showColumns: false,
+        maintainSelected: true,
+        paginationFirstText: "@lang('general.first')",
+        paginationLastText: "@lang('general.last')",
+        paginationPreText: "@lang('general.previous')",
+        paginationNextText: "@lang('general.next')",
+        pageList: ['10','25','50','100','150','200'],
+        icons: {
+            paginationSwitchDown: 'fa-caret-square-o-down',
+            paginationSwitchUp: 'fa-caret-square-o-up',
+            columns: 'fa-columns',
+            refresh: 'fa-refresh'
+        },
+    });
+</script>
 
 @stop
