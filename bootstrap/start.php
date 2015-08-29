c870933125c470245fbf5aaba6b73a2da51e2581
Vincent Sposato <vincent.sposato@gmail.com>
2015-08-28 20:01:29 -0400

    More tweaks to get environment working

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -23,18 +23,18 @@
     | given environment, then we will automatically detect it for you.
     |
     */
-    if (isset($_SERVER[ 'APP_ENV' ]) && $_SERVER[ 'APP_ENV' ] == 'travis-ci') {
-        $env =  $app->detectEnvironment(function() {
-            return 'travis-ci';
-        });
-    } else {
+//    if (isset($_SERVER[ 'APP_ENV' ]) && $_SERVER[ 'APP_ENV' ] == 'travis-ci') {
+//        $env =  $app->detectEnvironment(function() {
+//            return 'travis-ci';
+//        });
+//    } else {
         $env = $app->detectEnvironment( [
             'local'      => [ 'homestead', 'AlisonMBP' ],
-            'travis-ci'  => [ 'travis-linux-*', 'worker-linux-docker-*' ],
+            'travis-ci'  => getenv('APP_ENV') == 'travis-ci',
             'staging'    => [ 'stagingweb01' ],
             'production' => [ 'www.yourserver.com' ]
         ] );
-    }
+//    }
 
     /*
     |--------------------------------------------------------------------------
