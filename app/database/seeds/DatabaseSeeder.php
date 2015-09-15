c6b6ccc81447e5625479ef457d16c5ebc24037ef
snipe <snipe@snipe.net>
2015-09-15 03:31:20 -0700

    More seeders

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -22,6 +22,8 @@ class DatabaseSeeder extends Seeder
         $this->call('LicensesSeeder');
         $this->call('LicenseSeatsSeeder');
         $this->call('ActionlogSeeder');
+        $this->call('AccessoriesSeeder');
+        $this->call('AssetMaintenancesSeeder');
     }
 
 }
