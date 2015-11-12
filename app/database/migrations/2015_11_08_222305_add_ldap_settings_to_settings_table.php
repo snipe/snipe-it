12912b3e2734d9ae1e9d1a26c84c037dde045272
Walter <merid14@gmail.com>
2015-11-11 23:43:02 -0500

    adjusting the migration

--- app/database/migrations/2015_11_08_222305_add_ldap_settings_to_settings_table.php
+++ app/database/migrations/2015_11_08_222305_add_ldap_settings_to_settings_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddCustomCssToSettings extends Migration {
+class AddLdapFieldsToSettings extends Migration {
 
 	/**
 	 * Run the migrations.
