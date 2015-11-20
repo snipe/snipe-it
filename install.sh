a0f2ad5d546b98a4a59f5c54e3a9382627838023
Walter <merid14@gmail.com>
2015-11-19 21:32:58 -0500

    point to dev

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-wget https://raw.githubusercontent.com/merid14/snipe-it/master/snipeit.sh
+wget https://raw.githubusercontent.com/merid14/snipe-it/develop/snipeit.sh
 chmod 744 snipeit.sh
-./.snipeit.sh "$@" 2>&1 | tee /var/log/snipeit-install.log
+./snipeit.sh "$@" 2>&1 | tee /var/log/snipeit-install.log
 sudo ./snipeit.sh
