fb59ad9fe634640f797dcf67640961e385b7e680
Wahlstedt, Walter <wwahlstedt@unionky.edu>
2015-11-23 15:15:08 -0500

    remove force root

--- install.sh
+++ install.sh
@@ -1,7 +1,3 @@
 wget https://raw.githubusercontent.com/merid14/snipe-it/develop/snipeit.sh
 chmod 744 snipeit.sh
-# ensure running as root
-if [ "$(id -u)" != "0" ]; then
-  exec sudo "$0" "$@"
-fi
-./snipeit.sh 2>&1 | sudo tee -a /var/log/snipeit-install.log
+sudo ./snipeit.sh 2>&1 | sudo tee -a /var/log/snipeit-install.log
