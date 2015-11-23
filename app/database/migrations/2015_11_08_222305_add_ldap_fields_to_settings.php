7a07c07e2906452f37c2ea5314568b34150391e3
Walter <merid14@gmail.com>
2015-11-23 16:16:05 -0500

    removed password insert to database

--- app/database/migrations/2015_11_08_222305_add_ldap_fields_to_settings.php
+++ app/database/migrations/2015_11_08_222305_add_ldap_fields_to_settings.php
@@ -17,7 +17,7 @@ class AddLdapFieldsToSettings extends Migration {
 			$table->string('ldap_enabled')->nullable()->default(NULL);
 			$table->string('ldap_server')->nullable()->default(NULL);
 			$table->string('ldap_uname')->nullable()->default(NULL);
-			$table->longText('ldap_pword')->nullable()->default('eyJpdiI6IllDdUU1NUtXWGd1Tzg0dGdvY0JtU3JUS2VEcDhEbEs1UFB4cFh3STJEVVk9IiwidmFsdWUiOiJ2UFwvN1VIZ3hJZmJLdHNyejlIK0RVT0d5VUgyYVFIeEpFT0xyUExJQjRIWT0iLCJtYWMiOiI3MTIzNTliYzczMWJlNTViZmRiNDg0MjI5MDhkMzdhN2RkNmUxMmFiZDlmYWNmNGY3YzcxYzQ4MTY1NDIwN2UyIn0=');
+			$table->longText('ldap_pword')->nullable()->default('NULL');
 			$table->string('ldap_basedn')->nullable()->default(NULL);
 			$table->string('ldap_filter')->nullable()->default('cn=*');
 			$table->string('ldap_username_field')->nullable()->default('samaccountname');
