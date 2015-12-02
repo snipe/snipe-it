179f96c8e81e606a9ef949700b4255ab8ada5736
snipe <snipe@snipe.net>
2015-12-01 18:16:44 -0800

    More strings for #1397

--- app/lang/en/admin/custom_fields/general.php
+++ app/lang/en/admin/custom_fields/general.php
@@ -1,15 +1,16 @@
 <?php
 
 return array(
-    'custom_fields'		 => 'Custom Fields',
-    'field'		         => 'Field',
-    'fieldset'      	 => 'fieldset',
-    'fieldset_name'    => 'Fieldset Name',
-    'field_name'       => 'Field Name',
-    'field_element'    => 'Form Element',
-    'field_format'     => 'Format',
+    'custom_fields'		        => 'Custom Fields',
+    'field'		                => 'Field',
+    'fieldset'      	        => 'fieldset',
+    'fieldset_name'           => 'Fieldset Name',
+    'field_name'              => 'Field Name',
+    'field_element'           => 'Form Element',
+    'field_format'            => 'Format',
     'field_custom_format'     => 'Custom Format',
-    'required'   		   => 'Required',
-    'create_fieldset'  => 'Create New Fieldset',
-    'create_field'     => 'Create New Custom Field',
+    'required'   		          => 'Required',
+    'order'   		            => 'Order',
+    'create_fieldset'         => 'Create New Fieldset',
+    'create_field'            => 'Create New Custom Field',
 );
