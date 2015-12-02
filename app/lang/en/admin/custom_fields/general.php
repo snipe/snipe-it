5f9d5ae8363b7f56fc2b393cd398ef91fcd91cc9
snipe <snipe@snipe.net>
2015-12-01 17:55:42 -0800

    More laguage strings for #1397

--- app/lang/en/admin/custom_fields/general.php
+++ app/lang/en/admin/custom_fields/general.php
@@ -5,6 +5,11 @@ return array(
     'field'		         => 'Field',
     'fieldset'      	 => 'fieldset',
     'fieldset_name'    => 'Fieldset Name',
+    'field_name'       => 'Field Name',
+    'field_element'    => 'Form Element',
+    'field_format'     => 'Format',
+    'field_custom_format'     => 'Custom Format',
     'required'   		   => 'Required',
     'create_fieldset'  => 'Create New Fieldset',
+    'create_field'     => 'Create New Custom Field',
 );
