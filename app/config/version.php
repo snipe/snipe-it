aff7b99d91a109aa6f6d8e2aae5dc811a93cd3a0
snipe <snipe@snipe.net>
2015-12-02 16:31:57 -0800

    Nicer add/edit formatting putting required fields at the top

--- app/config/version.php
+++ app/config/version.php
@@ -1,5 +1,5 @@
 <?php
 return array (
-  'app_version' => 'v2.0.6-78',
-  'hash_version' => 'v2.0.6-78-gf1b0085',
+  'app_version' => 'v2.0.6-79',
+  'hash_version' => 'v2.0.6-79-g3dc7919',
 );
--- app/views/backend/models/custom_fields_form.blade.php
+++ app/views/backend/models/custom_fields_form.blade.php
@@ -1,13 +1,14 @@
 @if($model->fieldset)
-<h1>Custom Fields</h1>
-@foreach($model->fieldset->fields AS $field)
-  <div class="form-group">
-    <label for="idunno" class="col-md-2 control-label">{{{ $field->name }}}</label>
-    <div class="col-md-7 col-sm-12">
-        <input type="text" value="{{{ Input::old('fields[{{$field->db_column_name()}}]',(isset($asset) ? $asset->{$field->db_column_name()} : "")) }}}" name="fields[{{{ $field->db_column_name() }}}]">
+  @foreach($model->fieldset->fields AS $field)
+    <div class="form-group">
+      <label for="idunno" class="col-md-2 control-label">{{{ $field->name }}}
+        @if ($field->pivot->required=='1')
+          *
+        @endif
+      </label>
+      <div class="col-md-7 col-sm-12">
+          <input type="text" value="{{{ Input::old('fields[{{$field->db_column_name()}}]',(isset($asset) ? $asset->{$field->db_column_name()} : "")) }}}" class="form-control" name="fields[{{{ $field->db_column_name() }}}]">
+      </div>
     </div>
-  </div>
-@endforeach
-@else
-No Custom Fields Defined
+  @endforeach
 @endif
