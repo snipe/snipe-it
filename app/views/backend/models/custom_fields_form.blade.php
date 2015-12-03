8cdb032a14b92b79f52514d0c806d602cc47db12
Brady Wetherington <uberbrady@gmail.com>
2015-12-02 17:57:28 -0800

    Fixes error messages, and re-filling old input on failed asset create

--- app/views/backend/models/custom_fields_form.blade.php
+++ app/views/backend/models/custom_fields_form.blade.php
@@ -7,8 +7,14 @@
         @endif
       </label>
       <div class="col-md-7 col-sm-12">
-          <input type="text" value="{{{ Input::old('fields[{{$field->db_column_name()}}]',(isset($asset) ? $asset->{$field->db_column_name()} : "")) }}}" id="{{ $field->db_column_name() }}" class="form-control" name="fields[{{{ $field->db_column_name() }}}]">
-          {{ $errors->first($field->db_column_name(), '<br><span class="alert-msg"><i class="fa fa-times"></i> :message</span>') }}
+          <input type="text" value="{{{ Input::old('fields.'.$field->db_column_name(),(isset($asset) ? $asset->{$field->db_column_name()} : "")) }}}" id="{{ $field->db_column_name() }}" class="form-control" name="fields[{{{ $field->db_column_name() }}}]">
+          <?php
+          $errormessage=$errors->first($field->db_column_name());
+          if($errormessage) {
+            $errormessage=preg_replace('/ snipeit /','',$errormessage);
+            print('<br><span class="alert-msg"><i class="fa fa-times"></i> '.$errormessage.'</span>');
+          }
+          ?>
       </div>
     </div>
   @endforeach
