cac254ded6685ef1e087b803e28cd14ff93047ca
snipe <snipe@snipe.net>
2015-12-15 04:15:28 -0800

    Fixes #1371 - problem saving new models with non-latin characters

--- app/models/Model.php
+++ app/models/Model.php
@@ -10,7 +10,7 @@ class Model extends Elegant
 
     // Declare the rules for the form validation
     protected $rules = array(
-        'name'   		=> 'required|alpha_space|min:2|max:255|unique:models,deleted_at,NULL',
+        'name'   		=> 'required|alpha_space|min:2|max:255|unique:models,name,NULL,deleted_at',
         'modelno'   		=> 'alpha_space|min:1|max:255',
         'category_id'   	=> 'required|integer',
         'manufacturer_id'   => 'required|integer',
@@ -42,7 +42,7 @@ class Model extends Elegant
     {
         return $this->belongsTo('Manufacturer','manufacturer_id');
     }
-    
+
     public function fieldset()
     {
         return $this->belongsTo('CustomFieldset','fieldset_id');
