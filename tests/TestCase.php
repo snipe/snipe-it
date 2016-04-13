7236559b44adac3352487758fc8f25296c659c19
snipe <snipe@snipe.net>
2016-04-13 04:07:03 -0700

    Try adding a defaul url to override the Webdriver "url is blank" bullshit

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,6 +7,7 @@ class TestCase extends Illuminate\Foundation\Testing\TestCase
      *
      * @var string
      */
+    protected $baseUrl = 'http://localhost:8000';
 
     /**
      * Creates the application.
