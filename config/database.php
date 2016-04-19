e4b5d426db4e935a8bf872786fad0246baec1646
snipe <snipe@snipe.net>
2016-04-19 00:47:12 -0700

    Set DB prefix in env

--- config/database.php
+++ config/database.php
@@ -66,7 +66,7 @@ return [
             'password'  => env('DB_PASSWORD', ''),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
-            'prefix'    => '',
+            'prefix'    => env('DB_PREFIX', null),
             'strict'    => false,
             'engine'    => null,
         ],
