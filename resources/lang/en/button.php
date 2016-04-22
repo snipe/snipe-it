ed8c72b94635c6a0be32fd83d0e2c9520040bce3
snipe <snipe@snipe.net>
2016-04-21 21:53:13 -0700

    Checkin and Delete User string

--- resources/lang/en/button.php
+++ resources/lang/en/button.php
@@ -2,14 +2,15 @@
 
 return array(
 
-    'actions' 	=> 'Actions',
-    'add'    	=> 'Add New',
-    'cancel'    => 'Cancel',
-    'delete'  	=> 'Delete',
-    'edit'    	=> 'Edit',
-    'restore' 	=> 'Restore',
-    'request'   => 'Request',
-    'submit'  	=> 'Submit',
-    'upload'    => 'Upload',
+    'actions' 	                => 'Actions',
+    'add'    	                => 'Add New',
+    'cancel'                    => 'Cancel',
+    'checkin_and_delete'  	    => 'Checkin & Delete User',
+    'delete'  	                => 'Delete',
+    'edit'    	                => 'Edit',
+    'restore' 	                => 'Restore',
+    'request'                   => 'Request',
+    'submit'  	                => 'Submit',
+    'upload'                    => 'Upload',
 
 );
