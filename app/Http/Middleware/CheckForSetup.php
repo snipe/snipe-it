1a14abed05fb91fd1efdc6ccbede53ecc5f4ae1a
snipe <snipe@snipe.net>
2016-05-14 15:05:20 -0700

    Check that the table exists
    Should probably find a way to handle this that doesn't require a DB call

--- app/Http/Middleware/CheckForSetup.php
+++ app/Http/Middleware/CheckForSetup.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Config;
 use Route;
+use Schema;
 use App\Models\User;
 
 class CheckForSetup
@@ -13,12 +14,22 @@ class CheckForSetup
     {
 
         try {
-            $usercount = User::withTrashed()->count();
-            if (($usercount > 0) && (Route::is('setup*'))) {
-                return redirect(config('app.url'));
+
+            $users_table_exists = Schema::hasTable('users');
+            $settings_table_exists = Schema::hasTable('settings');
+
+            if ($users_table_exists && $settings_table_exists) {
+                $usercount = User::withTrashed()->count();
+                if (($usercount > 0) && (Route::is('setup*'))) {
+                    return redirect(config('app.url'));
+                } else {
+                    return $next($request);
+                }
             } else {
                 return $next($request);
             }
+
+
         } catch (\Exception $e) {
             return $next($request);
         }
