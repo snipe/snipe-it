0ea5b090af192fe344154df24644c8a9f558be3b
snipe <snipe@snipe.net>
2016-05-17 21:15:48 -0700

    Fixes issue with HTTPS key not found on non-HTTPS installs

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -50,7 +50,7 @@ class SettingsController extends Controller
             $start_settings['db_error'] = $e->getMessage();
         }
 
-        $protocol = $_SERVER['HTTPS'] == "on" ? 'https://' : 'http://';
+        $protocol = array_key_exists('HTTPS',$_SERVER) && ( $_SERVER['HTTPS'] == "on") ? 'https://' : 'http://';
 
 
         $pageURL = $protocol;
