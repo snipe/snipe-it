e4ad5b3d4a62dbb18e2935ed314bbeafe3d33d6c
Daniel Meltzer <parallelgrapefruit@gmail.com>
2016-05-17 23:31:57 -0500

    Replace the assorted concat() methods with a full_name method in the User model and adjust queries accordingly

--- app/Http/Controllers/LicensesController.php
+++ app/Http/Controllers/LicensesController.php
@@ -435,8 +435,7 @@ class LicensesController extends Controller
         }
 
         // Get the dropdown of users and then pass it to the checkout view
-         $users_list = array('' => 'Select a User') + DB::table('users')->select(DB::raw('concat(last_name,", ",first_name," (",username,")") as full_name, id'))->whereNull('deleted_at')->orderBy('last_name', 'asc')->orderBy('first_name', 'asc')->lists('full_name', 'id');
-
+        $users_list = Helper::usersList();
 
         // Left join to get a list of assets and some other helpful info
         $asset = DB::table('assets')
