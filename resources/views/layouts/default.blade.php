b796e72faf3a86fc18d26a6aef933d59a6832eb1
snipe <snipe@snipe.net>
2016-05-20 16:01:23 -0700

    Remove old search

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -322,18 +322,6 @@
       <aside class="main-sidebar">
         <!-- sidebar: style can be found in sidebar.less -->
         <section class="sidebar">
-
-          <!-- search form -->
-          <!--   <form action="#" method="get" class="sidebar-form">
-            <div class="input-group">
-              <input type="text" name="q" class="form-control" placeholder="Search...">
-              <span class="input-group-btn">
-                <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i></button>
-              </span>
-            </div>
-          </form>
-          -->
-          <!-- /.search form -->
           <!-- sidebar menu: : style can be found in sidebar.less -->
           <ul class="sidebar-menu">
             @if (Auth::user()->hasAccess('admin'))
