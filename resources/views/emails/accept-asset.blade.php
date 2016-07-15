616f922306db2396b42b452e5dca8d2047d91386
snipe <snipe@snipe.net>
2016-07-14 19:50:00 -0700

    Fixes #2254

--- resources/views/emails/accept-asset.blade.php
+++ resources/views/emails/accept-asset.blade.php
@@ -1,91 +1,91 @@
 @extends('emails/layouts/default')
 
 @section('content')
-<p>Hello {{ $first_name }},</p>
+	<p>Hello {{ $first_name }},</p>
 
 
-<p>A new item has been checked out under your name, details are below.</p>
+	<p>A new item has been checked out under your name, details are below.</p>
 
-<table>
-	<tr>
-		<td>
-			Asset Name:
-		</td>
-		<td>
-			<strong>{{ $item_name }}</strong>
-		</td>
-	</tr>
-	@if isset($item_tag)
+	<table>
 		<tr>
 			<td>
-				Asset Tag:
+				Asset Name:
 			</td>
 			<td>
-				<strong>{{ $item_tag }}</strong>
+				<strong>{{ $item_name }}</strong>
 			</td>
 		</tr>
-	@endif
-    @if isset($item_serial)
+		@if (isset($item_tag))
+			<tr>
+				<td>
+					Asset Tag:
+				</td>
+				<td>
+					<strong>{{ $item_tag }}</strong>
+				</td>
+			</tr>
+		@endif
+		@if (isset($item_serial))
+			<tr>
+				<td>
+					Serial:
+				</td>
+				<td>
+					<strong>{{ $item_serial }}</strong>
+				</td>
+			</tr>
+		@endif
 		<tr>
 			<td>
-				Serial:
+				Checkout Date:
 			</td>
 			<td>
-				<strong>{{ $item_serial }}</strong>
+				<strong>{{ $checkout_date }}</strong>
 			</td>
 		</tr>
-	@endif
-	<tr>
-		<td>
-			Checkout Date:
-		</td>
-		<td>
-			<strong>{{ $checkout_date }}</strong>
-		</td>
-	</tr>
-	@if isset($expected_checkin)
-		<tr>
-			<td>
-				Expected Checkin Date:
-			</td>
-			<td>
-				<strong>{{ $expected_checkin }}</strong>
-			</td>
-		</tr>
-	@endif
-	@if isset($note)
-		<tr>
-			<td>
-				Additional Notes:
-			</td>
-			<td>
-				<strong>{{ $note }}</strong>
-			</td>
-		</tr>
-	@endif
-</table>
+		@if (isset($expected_checkin))
+			<tr>
+				<td>
+					Expected Checkin Date:
+				</td>
+				<td>
+					<strong>{{ $expected_checkin }}</strong>
+				</td>
+			</tr>
+		@endif
+		@if (isset($note))
+			<tr>
+				<td>
+					Additional Notes:
+				</td>
+				<td>
+					<strong>{{ $note }}</strong>
+				</td>
+			</tr>
+		@endif
+	</table>
 
-@if (($require_acceptance==1) && ($eula!=''))
+	@if (($require_acceptance==1) && ($eula!=''))
 
-	Please read the terms of use below, and click on the link at the bottom to confirm that you read and agree to the terms of use, and have received the asset.
+		Please read the terms of use below, and click on the link at the bottom to confirm that you read and agree to the terms of use, and have received the asset.
 
-@elseif (($require_acceptance==1) && ($eula==''))
+	@elseif (($require_acceptance==1) && ($eula==''))
 
-	Please click on the link at the bottom to confirm that you have received the asset.
+		Please click on the link at the bottom to confirm that you have received the asset.
 
-@elseif (($require_acceptance==0) && ($eula!=''))
+	@elseif (($require_acceptance==0) && ($eula!=''))
 
-	Please read the terms of use below.
+		Please read the terms of use below.
 
-@endif
+		@endif
 
-</p>
+		</p>
 
-<p><blockquote>{!! $eula !!}</blockquote></p>
+		<p><blockquote>{!! $eula !!}</blockquote></p>
 
-@if ($require_acceptance==1)
-<p><strong><a href="{{ config('app.url') }}/account/accept-asset/{{ $log_id }}">I have read and agree to the terms of use, and have received this item.</a></strong></p>
-@endif
+		@if ($require_acceptance==1)
+			<p><strong><a href="{{ config('app.url') }}/account/accept-asset/{{ $log_id }}">I have read and agree to the terms of use, and have received this item.</a></strong></p>
+		@endif
 
-<p>{{ \App\Models\Setting::getSettings()->site_name }}</p>
+		<p>{{ \App\Models\Setting::getSettings()->site_name }}</p>
 @stop
