9a9b214c1d1340101431beb1728d6718d8c0808a
snipe <snipe@snipe.net>
2016-07-26 06:07:47 -0700

    Fixes #2319

--- database/migrations/2014_11_04_231416_update_group_field_for_reporting.php
+++ database/migrations/2014_11_04_231416_update_group_field_for_reporting.php
@@ -18,9 +18,9 @@ class UpdateGroupFieldForReporting extends Migration {
 		// 	//
 		// });
 
-    DB::update('update groups set permissions = ? where id = ?', ['{"admin":1,"users":1,"reports":1}', 1]);
+    DB::update('update '.DB::getTablePrefix().'groups set permissions = ? where id = ?', ['{"admin":1,"users":1,"reports":1}', 1]);
 
-    DB::update('update groups set permissions = ? where id = ?', ['{"users":1,"reports":1}', 2]);
+    DB::update('update '.DB::getTablePrefix().'groups set permissions = ? where id = ?', ['{"users":1,"reports":1}', 2]);
 
       // DB::statement('UPDATE '.$prefix.'groups SET permissions="{\"admin\":1,\"users\":1,\"reports\":1}" where id=1');
       // DB::statement('UPDATE '.$prefix.'groups SET permissions="{\"users\":1,\"reports\":1}" where id=2');
