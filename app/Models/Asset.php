139325d8bacf3602bc7c885aaf9d5f9dd62a87eb
snipe <snipe@snipe.net>
2016-08-01 10:17:46 -0700

    Small tweaks for bad data

--- app/Models/Asset.php
+++ app/Models/Asset.php
@@ -376,7 +376,10 @@ class Asset extends Depreciable
     {
 
         if ($this->name == '') {
-            return $this->model->name.' ('.$this->asset_tag.')';
+            if ($this->model) {
+                return $this->model->name.' ('.$this->asset_tag.')';
+            }
+            return $this->asset_tag;
         } else {
             return $this->name;
         }
