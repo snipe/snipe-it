122f0b9cba837c5d979102a9105007c1dc81234f
snipe <snipe@snipe.net>
2016-08-25 18:35:28 -0700

    Custom fields form in asset edit view

--- resources/views/models/custom_fields_form.blade.php
+++ resources/views/models/custom_fields_form.blade.php
@@ -1,13 +1,32 @@
 @if($model->fieldset)
   @foreach($model->fieldset->fields AS $field)
-    <div class="form-group {{ $errors->has($field->db_column_name()) ? ' has-error' : '' }}">
-      <label for="{{ $field->db_column_name() }}" class="col-md-3 control-label">{{ $field->name }}
-        @if ($field->pivot->required=='1')
-          *
-        @endif
-      </label>
-      <div class="col-md-7 col-sm-12">
-          <input type="text" value="{{ Input::old($field->db_column_name(),(isset($asset) ? $asset->{$field->db_column_name()} : "")) }}" id="{{ $field->db_column_name() }}" class="form-control" name="{{ $field->db_column_name() }}">
+    <div class="form-group{{ $errors->has($field->db_column_name()) ? ' has-error' : '' }}">
+      <label for="{{ $field->db_column_name() }}" class="col-md-3 control-label">{{ $field->name }} </label>
+      <div class="col-md-7 col-sm-12{{ ($field->pivot->required=='1') ? ' required' : '' }}">
+
+          @if ($field->element!='text')
+
+              @if ($field->element=='listbox')
+                  {{ Form::select($field->db_column_name(), $field->formatFieldValuesAsArray(), Input::old($field->db_column_name(), $asset->{$field->db_column_name()}), ['class'=>'format select2 form-control']) }}
+              @elseif ($field->element=='checkbox')
+
+                  @foreach ($field->formatFieldValuesAsArray() as $key => $value)
+
+                      <div>
+                          <label>
+                              <input type="checkbox" value="1" name="{{ $key }}[]" class="minimal" {{ Input::old($field->db_column_name()) == '1' ? ' checked="checked"' : '' }}> {{ $value }}
+                          </label>
+                      </div>
+                  @endforeach
+
+              @endif
+
+
+          @else
+            <input type="text" value="{{ Input::old($field->db_column_name(),(isset($asset) ? $asset->{$field->db_column_name()} : "")) }}" id="{{ $field->db_column_name() }}" class="form-control" name="{{ $field->db_column_name() }}">
+
+          @endif
+
           <?php
           $errormessage=$errors->first($field->db_column_name());
           if ($errormessage) {
