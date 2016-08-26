a92ed97b4eebd0845ae33e7627c2f6c8fba0a419
snipe <snipe@snipe.net>
2016-08-25 21:06:43 -0700

    Definitely not beta ;)

--- config/version.php
+++ config/version.php
@@ -1,5 +1,5 @@
 <?php
 return array (
-  'app_version' => 'v3.4.0-beta',
-  'hash_version' => 'v3.4.0-beta-49-g122f0b9',
+  'app_version' => 'v3.4.0-alpha',
+  'hash_version' => 'v3.4.0-alpha-49-g122f0b9',
 );
