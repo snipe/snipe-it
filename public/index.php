eee64d5509325f96204aa5719f7139ba8c3e6b51
snipe <snipe@snipe.net>
2016-09-01 12:53:38 -0700

    Set the public path for users who can’t use public as a directory (shared hosting)

--- public/index.php
+++ public/index.php
@@ -36,6 +36,11 @@ include '../c3.php';
 
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
+// set the public path to this directory
+$app->bind('path.public', function() {
+    return __DIR__;
+});
+
 /*
 |--------------------------------------------------------------------------
 | Run The Application
