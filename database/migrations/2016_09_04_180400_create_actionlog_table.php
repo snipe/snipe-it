5846f23d8b6e12dc8968e3f5784628f21e7e9f0d
snipe <snipe@snipe.net>
2016-09-14 18:59:14 -0700

    Fix stupid migration? Maybe? Might be more trouble later

--- database/migrations/2016_09_04_180400_create_actionlog_table.php
+++ database/migrations/2016_09_04_180400_create_actionlog_table.php
@@ -12,27 +12,29 @@ class CreateActionlogTable extends Migration
      */
     public function up()
     {
-        Schema::create('action_logs', function (Blueprint $table) {
-            $table->increments('id');
-            $table->integer('user_id')->nullable();
-            $table->string('action_type');
-            $table->integer('target_id')->nullable();  // Was checkedout_to
-            $table->integer('target_type')->nullable(); // For polymorphic thingies
-            $table->integer('location_id')->nullable();
-            $table->text('note')->nullable();
-            $table->text('filename')->nullable();
-            $table->string('item_type');
-            $table->integer('item_id');  // Replaces asset_id, accessory_id, etc.
-            $table->date('expected_checkin')->nullable()->default(null);
-            $table->integer('accepted_id')->nullable();
-            $table->timestamps();
-            $table->softDeletes();
+        if (!Schema::hasTable('action_logs')) {
+            Schema::create('action_logs', function (Blueprint $table) {
+                $table->increments('id');
+                $table->integer('user_id')->nullable();
+                $table->string('action_type');
+                $table->integer('target_id')->nullable();  // Was checkedout_to
+                $table->integer('target_type')->nullable(); // For polymorphic thingies
+                $table->integer('location_id')->nullable();
+                $table->text('note')->nullable();
+                $table->text('filename')->nullable();
+                $table->string('item_type');
+                $table->integer('item_id');  // Replaces asset_id, accessory_id, etc.
+                $table->date('expected_checkin')->nullable()->default(null);
+                $table->integer('accepted_id')->nullable();
+                $table->timestamps();
+                $table->softDeletes();
 
-            $table->integer('thread_id')
-                ->nullable()
-                ->default(null);
-            $table->index('thread_id');
-        });
+                $table->integer('thread_id')
+                    ->nullable()
+                    ->default(null);
+                $table->index('thread_id');
+            });
+        }
     }
 
     /**
@@ -42,6 +44,6 @@ class CreateActionlogTable extends Migration
      */
     public function down()
     {
-        Schema::drop('action_logs');
+        Schema::dropIfExists('action_logs');
     }
 }
