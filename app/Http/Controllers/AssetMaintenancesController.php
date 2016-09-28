b90c0de82264d958bcf1bad3e74bf57a51e617cd
snipe <snipe@snipe.net>
2016-09-27 19:07:45 -0700

    Fix companyable reult in maintenances

--- app/Http/Controllers/AssetMaintenancesController.php
+++ app/Http/Controllers/AssetMaintenancesController.php
@@ -141,7 +141,7 @@ class AssetMaintenancesController extends Controller
                 'completion_date'     => $maintenance->completion_date,
                 'user_id'       => ($maintenance->admin) ? (string)link_to('/admin/users/'.$maintenance->admin->id.'/view', $maintenance->admin->fullName()) : '',
                 'actions'       => $actions,
-                'companyName'   => ($maintenance->asset) ? $maintenance->asset->company->name : ''
+                'companyName'   => ($maintenance->asset->company) ? $maintenance->asset->company->name : ''
             );
         }
 
