8cb4954f8a4e54349aa48a98c3b14d7f0e686ee8
snipe <snipe@snipe.net>
2016-09-28 14:58:01 -0700

    Apply encrypted fields patch from develop

--- resources/views/models/custom_fields_form.blade.php
+++ resources/views/models/custom_fields_form.blade.php
@@ -27,11 +27,11 @@
           @else
             <!-- Text field -->
 
-            @can('admin')
+            @if (($field->field_encrypted=='0') || (Gate::allows('admin')))
             <input type="text" value="{{ Input::old($field->db_column_name(),(isset($asset) ? \App\Helpers\Helper::gracefulDecrypt($field, $asset->{$field->db_column_name()}) : "")) }}" id="{{ $field->db_column_name() }}" class="form-control" name="{{ $field->db_column_name() }}" placeholder="Enter {{ strtolower($field->format) }} text">
                 @else
                     <input type="text" value="{{ strtoupper(trans('admin/custom_fields/general.encrypted')) }}" class="form-control disabled" disabled>
-            @endcan
+            @endif
 
           @endif
 
