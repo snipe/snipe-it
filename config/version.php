85ddb9cfffe913ac3fe0ea567bccb3c5b671c81b
snipe <snipe@snipe.net>
2016-09-29 22:37:54 -0700

    Bumped version

--- config/version.php
+++ config/version.php
@@ -1,5 +1,5 @@
 <?php
 return array (
-  'app_version' => 'v3.5-beta',
-  'hash_version' => 'v3.5-29-g6dc143d',
+  'app_version' => 'v3.5.0-beta2',
+  'hash_version' => 'v3.5.0-beta2-50-gb92d26f',
 );
