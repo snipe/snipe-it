3415056bee456f90ade1f1e132d5346c7dd606b6
snipe <snipe@snipe.net>
2016-10-29 05:00:11 -0700

    Display feature disabled text if in demo mode

--- resources/views/settings/edit.blade.php
+++ resources/views/settings/edit.blade.php
@@ -77,7 +77,14 @@
                     {{ Form::label('two_factor_enabled', trans('admin/settings/general.two_factor_enabled_text')) }}
                   </div>
                   <div class="col-md-9">
+
                     {!! Form::two_factor_options('two_factor_enabled', Input::old('two_factor_enabled', $setting->two_factor_enabled), 'select2') !!}
+                    <p class="help-block">{{ trans('admin/settings/general.two_factor_enabled_warning') }}</p>
+
+                    @if (config('app.lock_passwords'))
+                    <p class="help-block">{{ trans('general.feature_disabled') }}</p>
+                    @endif
+
                     {!! $errors->first('two_factor_enabled', '<span class="alert-msg">:message</span>') !!}
                   </div>
                 </div>
