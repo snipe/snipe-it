4409a75751509818bd2c84ac647099a41881633d
Brady Wetherington <uberbrady@gmail.com>
2016-10-28 21:51:02 -0700

    refactor to clean up LDAP login, and make the login method easier to handle.

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -63,6 +63,48 @@ class AuthController extends Controller
         return View::make('auth.login');
     }
 
+    private function login_via_ldap(Request $request)
+    {
+        LOG::debug("Binding user to LDAP.");
+        $ldap_user = Ldap::findAndBindUserLdap($request->input('username'), $request->input('password'));
+        if(!$ldap_user) {
+          throw new Exception("Could not find user in LDAP directory");
+        }
+
+        // Check if the user exists in the database
+        $user = User::where('username', '=', Input::get('username'))->whereNull('deleted_at')->first();
+        LOG::debug("Local auth lookup complete");
+
+        // The user does not exist in the database. Try to get them from LDAP.
+        // If user does not exist and authenticates sucessfully with LDAP we
+        // will create it on the fly and sign in with default permissions
+        if (!$user) {
+            LOG::debug("Local user ".Input::get('username')." does not exist");
+            LOG::debug("Creating local user ".Input::get('username'));
+
+            if ($newuser = Ldap::createUserFromLdap($userattr)) { //this handles passwords on its own
+                LOG::debug("Local user created.");
+            } else {
+                LOG::debug("Could not create local user.");
+                throw new Exception("Could not create local user");
+            }
+            // If the user exists and they were imported from LDAP already
+        } else {
+            LOG::debug("Local user ".Input::get('username')." exists in database. Updating existing user against LDAP.");
+
+            $ldap_attr = Ldap::parseAndMapLdapAttributes($ldap_user);
+
+            if (Setting::getSettings()->ldap_pw_sync=='1') {
+                $user->password = bcrypt($request->input('password'));
+            }
+
+            $user->email = $ldap_attr['email'];
+            $user->first_name = $ldap_attr['firstname'];
+            $user->last_name = $ldap_attr['lastname'];
+            $user->save();
+        } // End if(!user)
+        return $user;
+    }
 
 
     /**
@@ -77,94 +119,28 @@ class AuthController extends Controller
         if ($validator->fails()) {
             return redirect()->back()->withInput()->withErrors($validator);
         }
-
+        $user = null;
         // Should we even check for LDAP users?
         if (Setting::getSettings()->ldap_enabled=='1') {
-
             LOG::debug("LDAP is enabled.");
-            // Check if the user exists in the database
-            $user = User::where('username', '=', Input::get('username'))->whereNull('deleted_at')->first();
-            LOG::debug("Local auth lookup complete");
-
             try {
-                Ldap::findAndBindUserLdap($request->input('username'), $request->input('password'));
-                LOG::debug("Binding user to LDAP.");
-            } catch (\Exception $e) {
-                LOG::debug("User ".Input::get('username').' did not authenticate successfully against LDAP.');
-                //$ldap_error = $e->getMessage();
-                // return redirect()->back()->withInput()->with('error',$e->getMessage());
+              $user=$this->login_via_ldap($request);
+              Auth::login($user, true);
+            } catch (Exception $e) {
+              if(Setting::getSettings()->ldap_pw_sync!='1') {
+                return redirect()->back()->withInput()->with('error',$e->getMessage());
+              }
             }
-
-
-            // The user does not exist in the database. Try to get them from LDAP.
-            // If user does not exist and authenticates sucessfully with LDAP we
-            // will create it on the fly and sign in with default permissions
-            if (!$user) {
-                LOG::debug("Local user ".Input::get('username')." does not exist");
-
-                try {
-
-                    if ($userattr = Ldap::findAndBindUserLdap($request->input('username'), $request->input('password'))) {
-                        LOG::debug("Creating local user ".Input::get('username'));
-
-                        if ($newuser = Ldap::createUserFromLdap($userattr)) {
-                            LOG::debug("Local user created.");
-                        } else {
-                            LOG::debug("Could not create local user.");
-                        }
-
-                    } else {
-                        LOG::debug("User did not authenticate correctly against LDAP. No local user was created.");
-                    }
-
-                } catch (\Exception $e) {
-                    return redirect()->back()->withInput()->with('error',$e->getMessage());
-                }
-
-            // If the user exists and they were imported from LDAP already
-            } else {
-
-                LOG::debug("Local user ".Input::get('username')." exists in database. Authenticating existing user against LDAP.");
-
-                if ($ldap_user = Ldap::findAndBindUserLdap($request->input('username'), $request->input('password'))) {
-                    $ldap_attr = Ldap::parseAndMapLdapAttributes($ldap_user);
-
-                    LOG::debug("Valid LDAP login. Updating the local data.");
-
-                    if (Setting::getSettings()->ldap_pw_sync=='1') {
-                        $user->password = bcrypt($request->input('password'));
-                    }
-
-                    $user->email = $ldap_attr['email'];
-                    $user->first_name = $ldap_attr['firstname'];
-                    $user->last_name = $ldap_attr['lastname'];
-                    $user->save();
-
-                    if (Setting::getSettings()->ldap_pw_sync!='1') {
-                        Auth::login($user, true);
-                        // Redirect to the users page
-                        return redirect()->to('/home')->with('success', trans('auth/message.signin.success'));
-                    }
-
-                } else {
-                    LOG::debug("User ".Input::get('username')." did not authenticate correctly against LDAP. Local user was not updated.");
-                }// End LDAP auth
-
-            } // End if(!user)
-
-        // NO LDAP enabled - just try to login the user normally
         }
-
-
-        LOG::debug("Authenticating user against database.");
-        // Try to log the user in
-        if (!Auth::attempt(Input::only('username', 'password'), Input::get('remember-me', 0))) {
-            LOG::debug("Local authentication failed.");
-            // throw new Cartalyst\Sentry\Users\UserNotFoundException();
-            return redirect()->back()->withInput()->with('error', trans('auth/message.account_not_found'));
+        if(!$user) {
+          LOG::debug("Authenticating user against database.");
+          // Try to log the user in
+          if (!Auth::attempt(Input::only('username', 'password'), Input::get('remember-me', 0))) {
+              LOG::debug("Local authentication failed.");
+              // throw new Cartalyst\Sentry\Users\UserNotFoundException();
+              return redirect()->back()->withInput()->with('error', trans('auth/message.account_not_found'));
+          }
         }
-
-
 
         // Get the page we were before
         $redirect = \Session::get('loginRedirect', 'home');
@@ -174,9 +150,6 @@ class AuthController extends Controller
 
         // Redirect to the users page
         return redirect()->to($redirect)->with('success', trans('auth/message.signin.success'));
-
-        // Ooops.. something went wrong
-        return redirect()->back()->withInput()->withErrors($this->messageBag);
     }
 
     /**
