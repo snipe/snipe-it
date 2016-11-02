ab79a27e2fc2d126240daebddc6bab889d99d8a8
Deleu Tanguy <tanguy.deleu@student.ehb.be>
2016-11-02 15:32:37 +0100

    added api token migration for users

---
+++ database/migrations/2016_11_02_065833_add_auth_token_to_users.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddAuthTokenToUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function ($table) {
+            $table->string('api_token', 60)->unique();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function ($table) {
+            $table->dropColumn('api_token');
+        });
+    }
+}
