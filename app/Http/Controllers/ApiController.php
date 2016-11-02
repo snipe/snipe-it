fdfbde935527b2a163902631b11e88b357d7b33f
Deleu Tanguy <tanguy.deleu@student.ehb.be>
2016-11-02 15:42:18 +0100

    added getAllAssets json response

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Asset;
+use GuzzleHttp\Psr7\Response;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
@@ -11,5 +14,10 @@ class ApiController extends Controller
     public function __construct()
     {
         $this->middleware('auth:api');
+    }
+
+    public function getAllAssets()
+    {
+        return response()->json(Asset::all());
     }
 }
