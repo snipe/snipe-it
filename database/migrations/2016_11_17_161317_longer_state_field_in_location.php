e8f45555ff0b036e7e38502862b16d1423ce6264
snipe <snipe@snipe.net>
2016-11-17 16:18:39 -0800

    Make length match validation

--- database/migrations/2016_11_17_161317_longer_state_field_in_location.php
+++ database/migrations/2016_11_17_161317_longer_state_field_in_location.php
@@ -13,7 +13,7 @@ class LongerStateFieldInLocation extends Migration
     public function up()
     {
         Schema::table('locations', function ($table) {
-            $table->string('state', 20)->nullable()->default(null)->change();
+            $table->string('state', 32)->nullable()->default(null)->change();
         });
     }
 
