16cfdbaa93ebb8c95cd18c81a93de894ed389313
snipe <snipe@snipe.net>
2016-12-01 02:04:43 -0800

    Redirect the lost password success back to login

--- app/Http/Controllers/Auth/PasswordController.php
+++ app/Http/Controllers/Auth/PasswordController.php
@@ -46,4 +46,15 @@ class PasswordController extends Controller
     public function getEmailSubject(){
         return property_exists($this, 'subject') ? $this->subject : \Lang::get('mail.reset_link');
     }
+
+    /**
+     * Get the response for after the reset link has been successfully sent.
+     *
+     * @param  string  $response
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    protected function getSendResetLinkEmailSuccessResponse($response)
+    {
+        return redirect()->route('login')->with('status', trans($response));
+    }
 }
