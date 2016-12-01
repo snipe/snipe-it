5a7ea0e90830754b8ca9e3f4f8554098cf460313
snipe <snipe@snipe.net>
2016-12-01 02:59:11 -0800

    Updated hash

--- config/version.php
+++ config/version.php
@@ -1,7 +1,7 @@
 <?php
 return array (
   'app_version' => 'v3.6.0',
-  'build_version' => '68',
-  'hash_version' => 'ge62f3f9',
-  'full_hash' => 'v3.6.0-68-ge62f3f9',
+  'build_version' => '70',
+  'hash_version' => 'g59617cc',
+  'full_hash' => 'v3.6.0-70-g59617cc',
 );
