a7a9f027fbd038d3086355b19933275a9f6beee7
snipe <snipe@snipe.net>
2016-12-01 03:00:18 -0800

    Automatically updated version hash

--- config/version.php
+++ config/version.php
@@ -1,7 +1,7 @@
 <?php
 return array (
   'app_version' => 'v3.6.0',
-  'build_version' => '72',
-  'hash_version' => 'gc3b4342',
-  'full_hash' => 'v3.6.0-72-gc3b4342',
+  'build_version' => '73',
+  'hash_version' => 'ge587b2d',
+  'full_hash' => 'v3.6.0-73-ge587b2d',
 );
