99e79a02d68f7bfa50261e403d3dd9c1c959aed8
snipe <snipe@snipe.net>
2016-12-01 03:08:14 -0800

    Revert "Halp! Stuck in pre-commit loop hell. :("
    This reverts commit 47c77fe042a66a6986e13e3fb621a1338bd1c590.

--- config/version.php
+++ config/version.php
@@ -1,7 +1,7 @@
 <?php
 return array (
-  'app_version' => 'v3.6.0',
-  'build_version' => '75',
-  'hash_version' => 'ga2ffe77',
-  'full_hash' => 'v3.6.0-75-ga2ffe77',
+  'app_version' => 'v3.6.1-pre',
+  'build_version' => '73',
+  'hash_version' => 'ge587b2d',
+  'full_hash' => 'v3.6.1-pre-73-ge587b2d',
 );
