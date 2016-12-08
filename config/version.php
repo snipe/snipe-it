32c5a258a7463706ff25f6a8eefe5d17c382e926
snipe <snipe@snipe.net>
2016-12-07 17:14:48 -0800

    Updated hash

--- config/version.php
+++ config/version.php
@@ -3,5 +3,5 @@ return array (
   'app_version' => 'v3.6.2',
   'build_version' => '10',
   'hash_version' => 'g927a12f',
-  'full_hash' => 'v3.6.1-10-g927a12f',
+  'full_hash' => 'v3.6.2-10-g899a991',
 );
