69031d42154ecc2b66a0fd153a1eaaf891b02703
snipe <snipe@snipe.net>
2016-12-14 05:07:02 -0800

    Put notifiable back in

--- config/app.php
+++ config/app.php
@@ -196,7 +196,7 @@ return [
         Illuminate\Foundation\Providers\FoundationServiceProvider::class,
         Illuminate\Hashing\HashServiceProvider::class,
         Illuminate\Mail\MailServiceProvider::class,
-        //Illuminate\Notifications\NotificationServiceProvider::class,
+        Illuminate\Notifications\NotificationServiceProvider::class,
         Illuminate\Pagination\PaginationServiceProvider::class,
         Illuminate\Pipeline\PipelineServiceProvider::class,
         Illuminate\Queue\QueueServiceProvider::class,
