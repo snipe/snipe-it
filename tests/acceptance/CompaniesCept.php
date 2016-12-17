87f801573bbf8164d6721b384b48589ce0219265
Daniel Meltzer <parallelgrapefruit@gmail.com>
2016-12-16 19:26:41 -0600

    Port acceptance tests to new urls.

--- tests/acceptance/CompaniesCept.php
+++ tests/acceptance/CompaniesCept.php
@@ -5,12 +5,12 @@ AcceptanceTester::test_login($I);
 $I->am('logged in user');
 $I->wantTo('ensure that the company listing page loads without errors');
 $I->lookForwardTo('seeing it load without errors');
-$I->amOnPage('/admin/settings/companies');
+$I->amOnPage('/companies');
 $I->waitForElement('.table', 5); // secs
 $I->seeNumberOfElements('table[name="companies"] tr', [5,30]);
 $I->seeInTitle('Companies');
 $I->see('Companies');
-$I->seeInPageSource('admin/settings/companies/create');
+$I->seeInPageSource('companies/create');
 $I->dontSee('Companies', '.page-header');
 $I->see('Companies', 'h1.pull-left');
 
@@ -18,7 +18,7 @@ $I->see('Companies', 'h1.pull-left');
 $I->wantTo('ensure that the create company form loads without errors');
 $I->lookForwardTo('seeing it load without errors');
 $I->click(['link' => 'Create New']);
-$I->amOnPage('/admin/settings/companies/create');
+$I->amOnPage('/companies/create');
 $I->dontSee('Create Company', '.page-header');
 $I->see('Create Company', 'h1.pull-left');
 $I->dontSee('&lt;span class=&quot;');
--- tests/acceptance/ConsumablesCept.php
+++ tests/acceptance/ConsumablesCept.php
@@ -4,12 +4,12 @@ AcceptanceTester::test_login($I);
 $I->am('logged in user');
 $I->wantTo('ensure that the consumables listing page loads without errors');
 $I->lookForwardTo('seeing it load without errors');
-$I->amOnPage('/admin/consumables');
+$I->amOnPage('/consumables');
 $I->waitForElement('.table', 5); // secs
 $I->seeNumberOfElements('table[name="consumables"] tr', [5,30]);
 $I->seeInTitle('Consumables');
 $I->see('Consumables');
-$I->seeInPageSource('admin/consumables/create');
+$I->seeInPageSource('/consumables/create');
 $I->dontSee('Consumables', '.page-header');
 $I->see('Consumables', 'h1.pull-left');
 
@@ -17,7 +17,7 @@ $I->see('Consumables', 'h1.pull-left');
 $I->wantTo('ensure that the create consumables form loads without errors');
 $I->lookForwardTo('seeing it load without errors');
 $I->click(['link' => 'Create New']);
-$I->amOnPage('/admin/consumables/create');
+$I->amOnPage('/consumables/create');
 $I->dontSee('Create Consumable', '.page-header');
 $I->see('Create Consumable', 'h1.pull-left');
 $I->dontSee('&lt;span class=&quot;');
