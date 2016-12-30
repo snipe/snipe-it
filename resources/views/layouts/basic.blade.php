16bd78fb3ff419dda6e345f2e7bda2b1ed4464ad
snipe <snipe@snipe.net>
2016-12-29 17:26:21 -0800

    Removed stray poop

--- resources/views/layouts/basic.blade.php
+++ resources/views/layouts/basic.blade.php
@@ -61,7 +61,6 @@
     @if (($snipeSettings) && ($snipeSettings->logo!=''))
         <center>
             <img class="logo" style="padding-top: 20px; padding-bottom: 10px;" src="{{ url('/') }}/uploads/{{ $snipeSettings->logo }}">
-            poop
         </center>
     @endif
   <!-- Content -->
