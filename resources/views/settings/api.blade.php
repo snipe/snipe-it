a02f406fa68e7ebf7d15a8fe997fe8a37f0f651a
snipe <snipe@snipe.net>
2017-01-11 03:58:09 -0800

    Removed personal access token from settings api page

--- resources/views/settings/api.blade.php
+++ resources/views/settings/api.blade.php
@@ -8,12 +8,7 @@
 
 {{-- Page content --}}
 @section('content')
-
-
+    
         <passport-clients></passport-clients>
         <passport-authorized-clients></passport-authorized-clients>
-        <passport-personal-access-tokens></passport-personal-access-tokens>
-
-
-
 @stop
