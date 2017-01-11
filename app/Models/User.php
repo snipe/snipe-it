b7bbc44052a457b9597aecc5cac060c78bece029
snipe <snipe@snipe.net>
2017-01-10 23:21:47 -0800

    Removed unneeded use statement

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,7 +3,6 @@ namespace App\Models;
 
 use App\Presenters\Presentable;
 use Illuminate\Auth\Authenticatable;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Auth\Passwords\CanResetPassword;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
