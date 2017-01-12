534e40feb80143bdf3a7adfe25c1eb4a89b71679
snipe <snipe@snipe.net>
2017-01-12 03:55:54 -0800

    Fixed bad fieldset call

--- app/Http/Controllers/Api/AssetsController.php
+++ app/Http/Controllers/Api/AssetsController.php
@@ -291,7 +291,7 @@ class AssetsController extends Controller
                 $asset->company_id = Company::getIdForCurrentUser($request->get('company_id')) : '';
 
             if ($request->has('model_id')) {
-                if ($model = AssetModel::find($request->get('model_id'))) {
+                if (($model = AssetModel::find($request->get('model_id'))) && (isset($model->fieldset))) {
                     foreach ($model->fieldset->fields as $field) {
                         if ($request->has(CustomField::name_to_db_name($field->name))) {
                             $asset->{CustomField::name_to_db_name($field->name)} = e($request->input(CustomField::name_to_db_name($field->name)));
