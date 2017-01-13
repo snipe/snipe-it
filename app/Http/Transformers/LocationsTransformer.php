f54fcb88e27678b8b906ed2d4e5026f3684ea08b
snipe <snipe@snipe.net>
2017-01-13 01:43:51 -0800

    Fixed formatting

--- app/Http/Transformers/LocationsTransformer.php
+++ app/Http/Transformers/LocationsTransformer.php
@@ -7,12 +7,10 @@ use Illuminate\Database\Eloquent\Collection;
 class LocationsTransformer
 {
 
-    public function transformLocations( Collection $locations)
+    public function transformLocations (Collection $locations)
     {
         $locations_array = array();
-        foreach ($locations as $location) {
             $locations_array[] = self::transformLocation($locations);
-        }
         return $users_array;
     }
 
