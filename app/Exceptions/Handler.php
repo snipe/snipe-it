cedf8fe8e503b3ad0b55cdf3e5ae0929f421703e
Daniel Meltzer <parallelgrapefruit@gmail.com>
2017-01-25 22:28:34 -0600

    Add support for generic exception reporting if app.debug is enabled.

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -75,6 +75,10 @@ class Handler extends ExceptionHandler
 
                 }
             }
+            // Try to parse 500 Errors ina  bit nicer way when debug is enabled.
+            if (config('app.debug')) {
+                return response()->json(Helper::formatStandardApiResponse('error', null, "An Error has occured! " . $e->getMessage()), 500);
+            }
 
         }
 
