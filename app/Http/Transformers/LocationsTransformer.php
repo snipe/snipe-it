c7003897ae8d0b41437bc0483a4fb1fc271299a6
snipe <snipe@snipe.net>
2017-01-26 20:07:46 -0800

    Fixed Locations transformer for assigned assets

--- app/Http/Transformers/LocationsTransformer.php
+++ app/Http/Transformers/LocationsTransformer.php
@@ -7,11 +7,13 @@ use Illuminate\Database\Eloquent\Collection;
 class LocationsTransformer
 {
 
-    public function transformLocations (Collection $locations)
+    public function transformLocations (Collection $locations, $total)
     {
-        $locations_array = array();
-            $locations_array[] = self::transformLocation($locations);
-        return $locations_array;
+        $array = array();
+        foreach ($locations as $location) {
+            $array[] = self::transformLocation($location);
+        }
+        return (new DatatablesTransformer)->transformDatatables($array, $total);
     }
 
     public function transformLocation (Location $location = null)
@@ -23,6 +25,8 @@ class LocationsTransformer
                 'address' => e($location->address),
                 'city' => e($location->city),
                 'state' => e($location->state),
+                'assets_checkedout' => $location->assets()->count(),
+                'assets_default'    => $location->assignedassets()->count(),
                 'country' => e($location->country),
             ];
             return $transformed;
