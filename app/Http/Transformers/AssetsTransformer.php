90aacb51b0f992d6d9b66adc4901e22cf1a489a9
Andrea Bergamasco <abergamasco@gmail.com>
2017-03-13 14:27:25 +0100

    Refactored AssetsTransformer
    Casted all ids to int, escaped all text values,

--- app/Http/Transformers/AssetsTransformer.php
+++ app/Http/Transformers/AssetsTransformer.php
@@ -7,11 +7,9 @@ use App\Http\Transformers\UsersTransformer;
 use Gate;
 use App\Helpers\Helper;
 
-
 class AssetsTransformer
 {
-
-    public function transformAssets (Collection $assets, $total)
+    public function transformAssets(Collection $assets, $total)
     {
         $array = array();
         foreach ($assets as $asset) {
@@ -21,67 +19,83 @@ class AssetsTransformer
     }
 
 
-    public function transformAsset (Asset $asset)
+    public function transformAsset(Asset $asset)
     {
-
         $array = [
-            'id' => $asset->id,
+            'id' => (int) $asset->id,
             'name' => e($asset->name),
             'asset_tag' => e($asset->asset_tag),
             'serial' => e($asset->serial),
-            'model' => ($asset->model) ? ['id' => $asset->model->id,'name'=> e($asset->model->name)] : '',
+            'model' => ($asset->model) ? [
+                'id' => (int) $asset->model->id,
+                'name'=> e($asset->model->name)
+            ] : null,
             'model_number' => ($asset->model) ? e($asset->model->model_number) : null,
-            'status_label' => ($asset->assetstatus) ? ['id' => $asset->assetstatus->id,'name'=> e($asset->assetstatus->name)] : null,
-            'category' => ($asset->model->category) ? ['id' => $asset->model->category->id,'name'=> e($asset->model->category->name)]  : null,
-            'manufacturer' => ($asset->model->manufacturer) ? ['id' => $asset->model->manufacturer->id,'name'=> e($asset->model->manufacturer->name)] : null,
-            'notes' => $asset->notes,
-            'order_number' => $asset->order_number,
-            'company' => ($asset->company) ? ['id' => $asset->company->id,'name'=> e($asset->company->name)] : null,
-            'location' => ($asset->assetLoc) ? ['id' => $asset->assetLoc->id,'name'=> e($asset->assetLoc->name)]  : null,
-            'rtd_location' => ($asset->defaultLoc) ? ['id' => $asset->defaultLoc->id,'name'=> e($asset->defaultLoc->name)]  : null,
+            'status_label' => ($asset->assetstatus) ? [
+                'id' => (int) $asset->assetstatus->id,
+                'name'=> e($asset->assetstatus->name)
+            ] : null,
+            'category' => ($asset->model->category) ? [
+                'id' => (int) $asset->model->category->id,
+                'name'=> e($asset->model->category->name)
+            ]  : null,
+            'manufacturer' => ($asset->model->manufacturer) ? [
+                'id' => (int) $asset->model->manufacturer->id,
+                'name'=> e($asset->model->manufacturer->name)
+            ] : null,
+            'notes' => e($asset->notes),
+            'order_number' => e($asset->order_number),
+            'company' => ($asset->company) ? [
+                'id' => (int) $asset->company->id,
+                'name'=> e($asset->company->name)
+            ] : null,
+            'location' => ($asset->assetLoc) ? [
+                'id' => (int) $asset->assetLoc->id,
+                'name'=> e($asset->assetLoc->name)
+            ]  : null,
+            'rtd_location' => ($asset->defaultLoc) ? [
+                'id' => (int) $asset->defaultLoc->id,
+                'name'=> e($asset->defaultLoc->name)
+            ]  : null,
             'image' => ($asset->getImageUrl()) ? $asset->getImageUrl() : null,
-            'assigned_to' => ($asset->assigneduser) ? ['id' => $asset->assigneduser->id, 'name' => $asset->assigneduser->getFullNameAttribute(), 'first_name'=> e( $asset->assigneduser->first_name), 'last_name'=> e( $asset->assigneduser->last_name)]  : null,
-            'warranty' =>  ($asset->warranty_months > 0) ? e($asset->warranty_months).' '.trans('admin/hardware/form.months') : null,
-            'warranty_expires' => ($asset->warranty_months > 0) ?  $asset->present()->warrantee_expires() : null,
+            'assigned_to' => ($asset->assigneduser) ? [
+                'id' => (int) $asset->assigneduser->id,
+                'name' => e($asset->assigneduser->getFullNameAttribute()),
+                'first_name'=> e($asset->assigneduser->first_name),
+                'last_name'=> e($asset->assigneduser->last_name)
+            ]  : null,
+            'warranty' =>  ($asset->warranty_months > 0) ? e($asset->warranty_months . ' ' . trans('admin/hardware/form.months')) : null,
+            'warranty_expires' => ($asset->warranty_months > 0) ?  Helper::getFormattedDateObject($asset->warranty_expires, 'date') : null,
             'created_at' => Helper::getFormattedDateObject($asset->created_at, 'datetime'),
             'updated_at' => Helper::getFormattedDateObject($asset->updated_at, 'datetime'),
             'purchase_date' => Helper::getFormattedDateObject($asset->purchase_date, 'date'),
             'last_checkout' => Helper::getFormattedDateObject($asset->last_checkout, 'datetime'),
             'expected_checkin' => Helper::getFormattedDateObject($asset->expected_checkin, 'date'),
-            'purchase_cost' => $asset->purchase_cost,
-            'user_can_checkout' => $asset->availableForCheckout(),
-
+            'purchase_cost' => Helper::formatCurrencyOutput($asset->purchase_cost),
+            'user_can_checkout' => (bool) $asset->availableForCheckout(),
         ];
 
-
         $permissions_array['available_actions'] = [
-            'checkout' => Gate::allows('checkout', Asset::class) ? true : false,
-            'checkin' => Gate::allows('checkin', Asset::class) ? true : false,
-            'update' => Gate::allows('update', Asset::class) ? true : false,
-            'delete' => Gate::allows('delete', Asset::class) ? true : false,
+            'checkout' => (bool) Gate::allows('checkout', Asset::class),
+            'checkin' => (bool) Gate::allows('checkin', Asset::class),
+            'update' => (bool) Gate::allows('update', Asset::class),
+            'delete' => (bool) Gate::allows('delete', Asset::class),
         ];
 
         $array += $permissions_array;
 
-
-
         if ($asset->model->fieldset) {
-
-         foreach($asset->model->fieldset->fields as $field) {
-             $fields_array = [$field->name => $asset->{$field->convertUnicodeDbSlug()}];
-             $array += $fields_array;
-         }
-
+            foreach ($asset->model->fieldset->fields as $field) {
+                $fields_array = [$field->name => $asset->{$field->convertUnicodeDbSlug()}];
+                $array += $fields_array;
+            }
         }
-
 
         return $array;
     }
 
-    public function transformAssetsDatatable($assets) {
+    public function transformAssetsDatatable($assets)
+    {
         return (new DatatablesTransformer)->transformDatatables($assets);
     }
-
-
-
 }
