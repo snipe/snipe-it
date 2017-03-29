8d33666180d8a20698f4220bc9ee67bfd6a64489
Karol <karol.pawelski@hotmail.com>
2017-03-30 00:28:09 +0200

    update

--- resources/lang/en/admin/setup/general.php
+++ resources/lang/en/admin/setup/general.php
@@ -4,8 +4,8 @@ return array(
 'create_user'			=> 'Create a User',
 'main_info_title'	=> 'Pre-Flight Check',
 'main_info_text'	=> 'This page will do a system check to make sure your configuration looks correct. We will add your first user on the next page.',
-'url_correct_text' => 'That URL looks right! Good job!{{ $start_settings['''url_config'''] }} ',
-'url_incorrect_text' => 'Uh oh! Snipe-IT thinks your URL is {{ $start_settings['''url_config'''] }}, but your real URL is {{ $start_settings['''real_url'''] }} Please update your <code>APP_URL</code> settings in your  <code>.env</code> file',
+'url_correct_text' => 'That URL looks right! Good job!{{ $start_settings[''url_config''] }} ',
+'url_incorrect_text' => 'Uh oh! Snipe-IT thinks your URL is {{ $start_settings[''url_config''] }}, but your real URL is {{ $start_settings[''real_url''] }} Please update your <code>APP_URL</code> settings in your  <code>.env</code> file',
 
 
 
