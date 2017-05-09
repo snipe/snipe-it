07882110fa357761f1c2f595d616a278988ff6e6
snipe <snipe@snipe.net>
2017-05-09 00:38:45 -0700

    Cleanup dashboard controller
    We ajax this data in now

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -30,49 +30,13 @@ class DashboardController extends Controller
         // Show the page
         if (Auth::user()->hasAccess('admin')) {
 
-            $asset_stats['total'] = Asset::Hardware()->count();
-
-            $asset_stats['rtd']['total'] = Asset::Hardware()->RTD()->count();
-
-            if ($asset_stats['rtd']['total'] > 0) {
-                $asset_stats['rtd']['percent'] = round(($asset_stats['rtd']['total']/$asset_stats['total']) * 100);
-            } else {
-                $asset_stats['rtd']['percent'] = 0;
-            }
+            $asset_stats=null;
 
 
-            $asset_stats['pending']['total'] = Asset::Hardware()->Pending()->count();
 
-            if ($asset_stats['pending']['total'] > 0) {
-                $asset_stats['pending']['percent'] = round(($asset_stats['pending']['total']/$asset_stats['total']) * 100);
-            } else {
-                $asset_stats['pending']['percent'] = 0;
-            }
-
-
-            $asset_stats['deployed']['total'] = Asset::Hardware()->Deployed()->count();
-
-            if ($asset_stats['deployed']['total'] > 0) {
-                 $asset_stats['deployed']['percent'] = round(($asset_stats['deployed']['total']/$asset_stats['total']) * 100);
-            } else {
-                $asset_stats['deployed']['percent'] = 0;
-            }
-
-
-            $asset_stats['undeployable']['total'] = Asset::Hardware()->Undeployable()->count();
-
-            if ($asset_stats['undeployable']['total'] > 0) {
-                $asset_stats['undeployable']['percent'] = round(($asset_stats['undeployable']['total']/$asset_stats['total']) * 100);
-            } else {
-                $asset_stats['undeployable']['percent'] = 0;
-            }
-
-            $asset_stats['archived']['total'] = Asset::Hardware()->Archived()->count();
-
-            if ($asset_stats['archived']['total'] > 0) {
-                $asset_stats['archived']['percent'] = round(($asset_stats['archived']['total']/$asset_stats['total']) * 100);
-            } else {
-                $asset_stats['archived']['percent'] = 0;
+            if ((!file_exists(storage_path().'/oauth-private.key')) || (!file_exists(storage_path().'/oauth-public.key'))) {
+                \Artisan::call('passport:install');
+                \Artisan::call('migrate', ['--force' => true]);
             }
 
 
