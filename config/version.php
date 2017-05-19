c526ffbf683e5220a60e26db5edfbf1902bd62bb
snipe <snipe@snipe.net>
2017-05-18 22:20:52 -0700

    Bumped version

--- config/version.php
+++ config/version.php
@@ -2,6 +2,6 @@
 return array (
   'app_version' => 'v3.6.5',
   'build_version' => '1',
-  'hash_version' => 'gee2c67a',
-  'full_hash' => 'v3.6.5-gee2c67a',
+  'hash_version' => 'gc8796cf',
+  'full_hash' => 'v3.6.5-1-gc8796cf',
 );
