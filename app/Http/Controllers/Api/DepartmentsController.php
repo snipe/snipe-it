6c623866c072f23fe6088021a81df4706e575128
snipe <snipe@snipe.net>
2017-05-23 01:08:55 -0700

    Additional API methods for departments

--- app/Http/Controllers/Api/DepartmentsController.php
+++ app/Http/Controllers/Api/DepartmentsController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Department;
 use App\Http\Transformers\DepartmentsTransformer;
+use App\Helpers\Helper;
+use Auth;
 
 class DepartmentsController extends Controller
 {
@@ -25,7 +27,7 @@ class DepartmentsController extends Controller
             'id',
             'name',
             'location_id'
-        ])->with('users')->withCount('assets')->withCount('users');
+        ])->with('users')->withCount('users');
 
         if ($request->has('search')) {
             $departments = $departments->TextSearch($request->input('search'));
@@ -41,5 +43,42 @@ class DepartmentsController extends Controller
         $departments = $departments->skip($offset)->take($limit)->get();
         return (new DepartmentsTransformer)->transformDepartments($departments, $total);
 
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @author [A. Gianotto] [<snipe@snipe.net>]
+     * @since [v4.0]
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->authorize('create', Department::class);
+        $department = new Department;
+        $department->fill($request->all());
+        $department->user_id = Auth::user()->id;
+
+        if ($department->save()) {
+            return response()->json(Helper::formatStandardApiResponse('success', $department, trans('admin/departments/message.create.success')));
+        }
+        return response()->json(Helper::formatStandardApiResponse('error', null, $department->getErrors()));
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @author [A. Gianotto] [<snipe@snipe.net>]
+     * @since [v4.0]
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $this->authorize('view', Department::class);
+        $department = Department::findOrFail($id);
+        return (new DepartmentsTransformer)->transformDepartment($department);
     }
 }
