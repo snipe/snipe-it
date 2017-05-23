5e0cc93ba2870c31ef7ff97334931675739d5bf2
snipe <snipe@snipe.net>
2017-05-23 14:33:19 -0700

    Use new formatters in bootstrap table

--- resources/views/reports/activity.blade.php
+++ resources/views/reports/activity.blade.php
@@ -19,17 +19,18 @@
                 data-toolbar="#toolbar"
                 class="table table-striped snipe-table"
                 id="table"
-                data-url="{{ route('api.activity.list') }}"
+                data-url="{{ route('api.activity.index') }}"
                 data-cookie="true"
                 data-cookie-id-table="activityReportTable">
                     <thead>
                         <tr>
-                            <th class="col-sm-1" data-field="admin">{{ trans('general.admin') }}</th>
-                            <th class="col-sm-1" data-field="action_type">{{ trans('general.action') }}</th>
-                            <th class="col-sm-1" data-field="item_type">{{ trans('general.type') }}</th>
-                            <th class="col-sm-1" data-field="item">{{ trans('general.item') }}</th>
-                            <th class="col-sm-1" data-field="target">To</th>
-                            <th class="col-sm-1" data-field="created_at">{{ trans('general.date') }}</th>
+                            <th data-field="icon" style="width: 40px;" class="hidden-xs" data-formatter="iconFormatter"></th>
+                            <th class="col-sm-3" data-field="created_at" data-formatter="dateDisplayFormatter">{{ trans('general.date') }}</th>
+                            <th class="col-sm-2" data-field="admin" data-formatter="usersLinkObjFormatter">{{ trans('general.admin') }}</th>
+                            <th class="col-sm-2" data-field="action_type">{{ trans('general.action') }}</th>
+                            <th class="col-sm-1" data-field="item.type">{{ trans('general.type') }}</th>
+                            <th class="col-sm-3" data-field="item" data-formatter="polymorphicItemFormatter">{{ trans('general.item') }}</th>
+                            <th class="col-sm-2" data-field="target" data-formatter="polymorphicItemFormatter">To</th>
                             <th class="col-sm-1" data-field="note">{{ trans('general.notes') }}</th>
                         </tr>
                     </thead>
