748aba3c48abf0e28a851754c042b7f18b43f15a
snipe <snipe@snipe.net>
2017-05-31 05:23:54 -0700

    Add assets dashboard feature
    - also a really broken sample graph

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -17,7 +17,7 @@
     <!-- small box -->
     <div class="small-box bg-teal">
       <div class="inner">
-        <h3>{{ number_format(\App\Models\Asset::assetcount()) }}</h3>
+        <h3>{{ number_format($counts['asset']) }}</h3>
         <p>{{ trans('general.total_assets') }}</p>
       </div>
       <div class="icon">
@@ -33,7 +33,7 @@
     <!-- small box -->
     <div class="small-box bg-maroon">
       <div class="inner">
-        <h3>{{ number_format(\App\Models\License::assetcount()) }}</h3>
+        <h3>{{ number_format($counts['license']) }}</h3>
         <p>{{ trans('general.total_licenses') }}</p>
       </div>
       <div class="icon">
@@ -45,11 +45,12 @@
     </div>
   </div><!-- ./col -->
 
+
   <div class="col-lg-3 col-xs-6">
     <!-- small box -->
     <div class="small-box bg-orange">
       <div class="inner">
-        <h3> {{ number_format(\App\Models\Accessory::count()) }}</h3>
+        <h3> {{ number_format($counts['accessory']) }}</h3>
           <p>{{ trans('general.total_accessories') }}</p>
       </div>
       <div class="icon">
@@ -65,7 +66,7 @@
     <!-- small box -->
     <div class="small-box bg-purple">
       <div class="inner">
-        <h3> {{ number_format(\App\Models\Consumable::count()) }}</h3>
+        <h3> {{ number_format($counts['consumable']) }}</h3>
           <p>{{ trans('general.total_consumables') }}</p>
       </div>
       <div class="icon">
@@ -78,6 +79,201 @@
   </div><!-- ./col -->
 </div>
 
+@if ($counts['grand_total'] == 0)
+
+    <div class="row">
+        <div class="col-md-12">
+            <div class="box">
+                <div class="box-header with-border">
+                    <h3 class="box-title">This is your dashboard. There are many like it, but this one is yours.</h3>
+                </div>
+                <!-- /.box-header -->
+                <div class="box-body">
+                    <div class="row">
+                        <div class="col-md-12">
+
+                            <div class="progress">
+                                <div class="progress-bar progress-bar-yellow" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%">
+                                    <span class="sr-only">60% Complete (warning)</span>
+                                </div>
+                            </div>
+
+
+                            <p><strong>It looks like you haven't added anything yet, so we don't have anything awesome to display. Get started by adding some assets, accessories, consumables, or licenses now!</strong></p>
+
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-3">
+                            @can('create', \App\Models\Asset::class)
+                            <a class="btn bg-teal" style="width: 100%" href="{{ route('hardware.create') }}">New Asset</a>
+                            @endcan
+                        </div>
+                        <div class="col-md-3">
+                            @can('create', \App\Models\License::class)
+                                <a class="btn bg-maroon" style="width: 100%" href="{{ route('licenses.create') }}">New License</a>
+                            @endcan
+                        </div>
+                        <div class="col-md-3">
+                            @can('create', \App\Models\Accessory::class)
+                                <a class="btn bg-orange" style="width: 100%" href="{{ route('accessories.create') }}">New Accessory</a>
+                            @endcan
+                        </div>
+                        <div class="col-md-3">
+                            @can('create', \App\Models\Consumable::class)
+                                <a class="btn bg-purple" style="width: 100%" href="{{ route('consumables.create') }}">New Consumable</a>
+                            @endcan
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@else
+
+<div class="row">
+    <div class="col-md-9">
+        <div class="box">
+            <div class="box-header with-border">
+                <h3 class="box-title">Monthly Recap Report</h3>
+
+                <div class="box-tools pull-right">
+                    <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                    </button>
+                </div>
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body">
+                <div class="row">
+                    <div class="col-md-8">
+                        <p class="text-center">
+                            <strong>Sales: 1 Jan, 2014 - 30 Jul, 2014</strong>
+                        </p>
+
+                        <div class="chart">
+                            <canvas id="salesChart" style="height:200px"></canvas>
+                        </div>
+                        <!-- /.chart-responsive -->
+                    </div>
+                    <!-- /.col -->
+                    <div class="col-md-4">
+                        <p class="text-center">
+                            <strong>Goal Completion</strong>
+                        </p>
+
+                        <div class="progress-group">
+                            <span class="progress-text">Add Products to Cart</span>
+                            <span class="progress-number"><b>160</b>/200</span>
+
+                            <div class="progress sm">
+                                <div class="progress-bar progress-bar-aqua" style="width: 80%"></div>
+                            </div>
+                        </div>
+                        <!-- /.progress-group -->
+                        <div class="progress-group">
+                            <span class="progress-text">Complete Purchase</span>
+                            <span class="progress-number"><b>310</b>/400</span>
+
+                            <div class="progress sm">
+                                <div class="progress-bar progress-bar-red" style="width: 80%"></div>
+                            </div>
+                        </div>
+                        <!-- /.progress-group -->
+                        <div class="progress-group">
+                            <span class="progress-text">Visit Premium Page</span>
+                            <span class="progress-number"><b>480</b>/800</span>
+
+                            <div class="progress sm">
+                                <div class="progress-bar progress-bar-green" style="width: 80%"></div>
+                            </div>
+                        </div>
+                        <!-- /.progress-group -->
+                        <div class="progress-group">
+                            <span class="progress-text">Send Inquiries</span>
+                            <span class="progress-number"><b>250</b>/500</span>
+
+                            <div class="progress sm">
+                                <div class="progress-bar progress-bar-yellow" style="width: 80%"></div>
+                            </div>
+                        </div>
+                        <!-- /.progress-group -->
+                    </div>
+                    <!-- /.col -->
+                </div>
+                <!-- /.row -->
+            </div>
+            <!-- ./box-body -->
+            <div class="box-footer">
+                <div class="row">
+                    <div class="col-sm-3 col-xs-6">
+                        <div class="description-block border-right">
+                            <span class="description-percentage text-green"><i class="fa fa-caret-up"></i> 17%</span>
+                            <h5 class="description-header">$35,210.43</h5>
+                            <span class="description-text">TOTAL REVENUE</span>
+                        </div>
+                        <!-- /.description-block -->
+                    </div>
+                    <!-- /.col -->
+                    <div class="col-sm-3 col-xs-6">
+                        <div class="description-block border-right">
+                            <span class="description-percentage text-yellow"><i class="fa fa-caret-left"></i> 0%</span>
+                            <h5 class="description-header">$10,390.90</h5>
+                            <span class="description-text">TOTAL COST</span>
+                        </div>
+                        <!-- /.description-block -->
+                    </div>
+                    <!-- /.col -->
+                    <div class="col-sm-3 col-xs-6">
+                        <div class="description-block border-right">
+                            <span class="description-percentage text-green"><i class="fa fa-caret-up"></i> 20%</span>
+                            <h5 class="description-header">$24,813.53</h5>
+                            <span class="description-text">TOTAL PROFIT</span>
+                        </div>
+                        <!-- /.description-block -->
+                    </div>
+                    <!-- /.col -->
+                    <div class="col-sm-3 col-xs-6">
+                        <div class="description-block">
+                            <span class="description-percentage text-red"><i class="fa fa-caret-down"></i> 18%</span>
+                            <h5 class="description-header">1200</h5>
+                            <span class="description-text">GOAL COMPLETIONS</span>
+                        </div>
+                        <!-- /.description-block -->
+                    </div>
+                </div>
+                <!-- /.row -->
+            </div>
+            <!-- /.box-footer -->
+        </div>
+        <!-- /.box -->
+    </div>
+    <!-- /.col -->
+    <div class="col-md-3">
+        <div class="box box-default">
+            <div class="box-header with-border">
+                <h3 class="box-title">{{ trans('general.assets') }}</h3>
+                <div class="box-tools pull-right">
+                    <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                    </button>
+                </div>
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="chart-responsive">
+                            <canvas id="statusPieChart" height="150"></canvas>
+                        </div> <!-- ./chart-responsive -->
+                    </div> <!-- /.col -->
+                </div> <!-- /.row -->
+            </div><!-- /.box-body -->
+        </div> <!-- /.box -->
+
+    </div>
+</div>
+<!-- /.row -->
+
 <!-- recent activity -->
 <div class="row">
   <div class="col-md-9">
@@ -85,7 +281,8 @@
       <div class="box-header with-border">
         <h3 class="box-title">{{ trans('general.recent_activity') }}</h3>
         <div class="box-tools pull-right">
-          <a href="{{ route('reports/activity') }}"><i class="fa fa-ellipsis-h"></i></a>
+            <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+            </button>
         </div>
       </div><!-- /.box-header -->
       <div class="box-body">
@@ -97,6 +294,7 @@
                 name="activityReport"
                 id="table"
                 data-sort-order="desc"
+                data-height="400"
                 data-url="{{ route('api.activity.index', ['limit' => 25]) }}">
                 <thead>
                   <tr>
@@ -112,30 +310,14 @@
 
             </div><!-- /.responsive -->
           </div><!-- /.col -->
+          <div class="col-md-12 text-center" style="padding-top: 10px;">
+            <a href="{{ route('reports/activity') }}" class="btn btn-primary btn-sm" style="width: 100%">View All</a>
+          </div>
         </div><!-- /.row -->
       </div><!-- ./box-body -->
     </div><!-- /.box -->
   </div>
   <div class="col-md-3">
-      <div class="box box-default">
-          <div class="box-header with-border">
-              <h3 class="box-title">{{ trans('general.assets') }}</h3>
-              <div class="box-tools pull-right">
-                  <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
-                  </button>
-              </div>
-          </div>
-          <!-- /.box-header -->
-          <div class="box-body">
-              <div class="row">
-                  <div class="col-md-12">
-                      <div class="chart-responsive">
-                          <canvas id="statusPieChart" height="150"></canvas>
-                      </div> <!-- ./chart-responsive -->
-                  </div> <!-- /.col -->
-              </div> <!-- /.row -->
-          </div><!-- /.box-body -->
-      </div> <!-- /.box -->
 
     <!-- Categories -->
     <div class="box box-default">
@@ -154,7 +336,9 @@
                       class="table table-striped snipe-table"
                       name="categorySummary"
                       id="table"
-                      data-url="{{ route('api.categories.index', ['limit' => 15]) }}">
+                      data-height="400"
+                      data-show-footer="true"
+                      data-url="{{ route('api.categories.index') }}">
                   <thead>
                   <tr>
                       <th class="col-sm-2" data-field="name" data-formatter="categoriesLinkFormatter">{{ trans('general.name') }}</th>
@@ -163,20 +347,85 @@
                   </thead>
               </table>
           </div> <!-- /.col -->
+            <div class="col-md-12 text-center" style="padding-top: 10px;">
+                <a href="{{ route('reports/activity') }}" class="btn btn-primary btn-sm" style="width: 100%">View All</a>
+            </div>
         </div> <!-- /.row -->
+
       </div><!-- /.box-body -->
     </div> <!-- /.box -->
   </div>
 </div> <!--/row-->
+
+@endif
+
 
 @stop
 
 @section('moar_scripts')
 @include ('partials.bootstrap-table', ['simple_view' => true])
 
+@if ($snipeSettings->load_remote=='1')
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script>
+@else
+    <script src="{{ asset('assets/js/plugins/chartjs/Chart.min.js') }}"></script>
+@endif
 
-<script src="{{ asset('assets/js/plugins/chartjs/Chart.min.js') }}"></script>
+
 <script>
+
+
+
+
+        /* ChartJS
+         * -------
+         */
+
+        // -----------------------
+        // - MONTHLY SALES CHART -
+        // -----------------------
+
+
+
+        var ctx = document.getElementById('salesChart').getContext("2d")
+        var myChart = new Chart(ctx, {
+            type: 'line'
+
+
+
+        });
+
+
+        $.ajax({
+            type: 'GET',
+            url: '{{  route('api.statuslabels.assets.bytype') }}',
+            headers: {
+                "X-Requested-With": 'XMLHttpRequest',
+                "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr('content')
+            },
+
+            dataType: 'json',
+            success: function (data) {
+                var ctx = new Chart(ctx,{
+                    type: 'line',
+                    data: data,
+                    options: lineOptions
+                });
+            },
+            error: function (data) {
+                window.location.reload(true);
+            }
+        });
+
+
+
+
+
+  // ---------------------------
+  // - END MONTHLY SALES CHART -
+  // ---------------------------
+
+
     var pieChartCanvas = $("#statusPieChart").get(0).getContext("2d");
     var pieChart = new Chart(pieChartCanvas);
     var ctx = document.getElementById("statusPieChart");
