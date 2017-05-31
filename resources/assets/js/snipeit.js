b7976046271ceb5fab010cb6463a181a3d1ed602
snipe <snipe@snipe.net>
2017-05-31 13:26:48 -0700

    Fixed &%#!@@!ing ES that prevented npm run production from working

--- resources/assets/js/snipeit.js
+++ resources/assets/js/snipeit.js
@@ -172,7 +172,7 @@ $(document).ready(function () {
      {
         // Vue collision: Avoid overriding a vue select2 instance
         // by checking to see if the item has already been select2'd.
-        $('select.select2:not(".select2-hidden-accessible")').each((i,obj) => {
+        $('select.select2:not(".select2-hidden-accessible")').each(function (i,obj) {
             {
                 $(obj).select2();
             }
