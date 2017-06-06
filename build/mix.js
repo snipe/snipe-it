1f7266274d7c41ab4864139107dd95e3e42d5075
snipe <snipe@snipe.net>
2017-06-06 00:06:01 -0700

    Updated manifest

--- build/mix.js
+++ build/mix.js
@@ -63,18 +63,17 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 58);
+/******/ 	return __webpack_require__(__webpack_require__.s = 57);
 /******/ })
 /************************************************************************/
 /******/ ({
 
-/***/ 58:
+/***/ 57:
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(9);
-__webpack_require__(7);
 __webpack_require__(8);
-module.exports = __webpack_require__(6);
+__webpack_require__(6);
+module.exports = __webpack_require__(7);
 
 
 /***/ }),
@@ -89,18 +88,11 @@ module.exports = __webpack_require__(6);
 /***/ 7:
 /***/ (function(module, exports) {
 
-// removed by extract-text-webpack-plugin
+throw new Error("Module build failed: ModuleBuildError: Module build failed: Can't resolve './resources/assets/less/overrides.less' in '/Users/agianotto/Sites/snipe-it/snipe-it/resources/assets/less'\n @ /Users/agianotto/Sites/snipe-it/snipe-it/resources/assets/less/app.less (line 9, column 0)\n near lines:\n   @import '../../../node_modules/bootstrap-colorpicker/src/less/colorpicker';\n   @import 'resources/assets/less/overrides.less';\n   \n    at runLoaders (/Users/agianotto/Sites/snipe-it/snipe-it/node_modules/webpack/lib/NormalModule.js:192:19)\n    at /Users/agianotto/Sites/snipe-it/snipe-it/node_modules/loader-runner/lib/LoaderRunner.js:364:11\n    at /Users/agianotto/Sites/snipe-it/snipe-it/node_modules/loader-runner/lib/LoaderRunner.js:230:18\n    at context.callback (/Users/agianotto/Sites/snipe-it/snipe-it/node_modules/loader-runner/lib/LoaderRunner.js:111:13)\n    at /Users/agianotto/Sites/snipe-it/snipe-it/node_modules/less-loader/index.js:68:16\n    at /Users/agianotto/Sites/snipe-it/snipe-it/node_modules/less/lib/less/render.js:26:35\n    at /Users/agianotto/Sites/snipe-it/snipe-it/node_modules/less/lib/less/parse.js:62:33\n    at Object.finish [as _finish] (/Users/agianotto/Sites/snipe-it/snipe-it/node_modules/less/lib/less/parser/parser.js:180:28)\n    at Object._onSequencerEmpty (/Users/agianotto/Sites/snipe-it/snipe-it/node_modules/less/lib/less/visitors/import-visitor.js:35:14)\n    at ImportSequencer.tryRun (/Users/agianotto/Sites/snipe-it/snipe-it/node_modules/less/lib/less/visitors/import-sequencer.js:50:14)\n    at /Users/agianotto/Sites/snipe-it/snipe-it/node_modules/less/lib/less/visitors/import-sequencer.js:19:25\n    at fileParsedFunc (/Users/agianotto/Sites/snipe-it/snipe-it/node_modules/less/lib/less/import-manager.js:50:17)\n    at /Users/agianotto/Sites/snipe-it/snipe-it/node_modules/less/lib/less/import-manager.js:113:21\n    at finish (/Users/agianotto/Sites/snipe-it/snipe-it/node_modules/less/lib/less/parser/parser.js:180:28)\n    at Object.parse (/Users/agianotto/Sites/snipe-it/snipe-it/node_modules/less/lib/less/parser/parser.js:191:24)\n    at loadFileCallback (/Users/agianotto/Sites/snipe-it/snipe-it/node_modules/less/lib/less/import-manager.js:112:64)\n    at /Users/agianotto/Sites/snipe-it/snipe-it/node_modules/less/lib/less/import-manager.js:123:17\n    at /Users/agianotto/Sites/snipe-it/snipe-it/node_modules/less-loader/index.js:123:5\n    at next (/Users/agianotto/Sites/snipe-it/snipe-it/node_modules/webpack/lib/dependencies/LoaderPlugin.js:52:15)\n    at compilation.addModuleDependencies (/Users/agianotto/Sites/snipe-it/snipe-it/node_modules/webpack/lib/dependencies/LoaderPlugin.js:29:12)\n    at _combinedTickCallback (internal/process/next_tick.js:73:7)\n    at process._tickCallback (internal/process/next_tick.js:104:9)");
 
 /***/ }),
 
 /***/ 8:
-/***/ (function(module, exports) {
-
-// removed by extract-text-webpack-plugin
-
-/***/ }),
-
-/***/ 9:
 /***/ (function(module, exports) {
 
 
