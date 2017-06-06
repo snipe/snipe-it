240a07b108455b04bae4d87f23461409cb940127
snipe <snipe@snipe.net>
2017-06-06 02:32:04 -0700

    Formatting for error pages

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -13,9 +13,9 @@
 
 
 <div class="row">
-  <div class="col-md-12">
+  <div class="col-md-8 col-md-offset-2">
 
-    <div class="error-page" style="padding-top: 200px">
+    <div style="padding-top: 200px">
       <img src="{{ url('/') }}/img/sad-panda.png" style="width: 200px; height: 200px;" class="pull-left">
             <div class="error-content">
               <h3><i class="fa fa-warning text-yellow"></i> 403 Forbidden.</h3>
