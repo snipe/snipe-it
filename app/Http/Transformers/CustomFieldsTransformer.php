212460a141051833e5862e40244befc7590cd83e
Joshua Gibson <gibsonjoshua55@gmail.com>
2017-06-23 16:06:59 -0400

    Adds basic GET api support for CustomFieldsets
    Currently there is not support for getting what fields a given fieldset contains
    from the API.  This commit creates a new API Controller for CustomFieldsets as
    well as Transformers for CustomFields CustomFieldsets.  Additionally, the api
    route has been updated so that a show method can be access from
    http://myapp/api/v1/fieldsets/{id}

---
+++ app/Http/Transformers/CustomFieldsTransformer.php
@@ -0,0 +1,30 @@
+<?php
+namespace App\Http\Transformers;
+
+use App\Models\CustomField;
+use Illuminate\Database\Eloquent\Collection;
+use App\Helpers\Helper;
+use Gate;
+
+class CustomFieldsTransformer
+{
+
+    public function transformCustomFields (Collection $fields, $total)
+    {
+        $array = array();
+        foreach ($fields as $field) {
+            $array[] = self::transformCustomField($field);
+        }
+        return (new DatatablesTransformer)->transformDatatables($array, $total);
+    }
+
+    public function transformCustomField (CustomField $field)
+    {
+        $array = [
+            'name' => $field->name,
+            'db_column_name' => $field->db_column_name(),
+            'format'   =>  $field->format
+        ];
+        return $array;
+    }
+}
