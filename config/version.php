a7a10455aeff1ac1230213788474072d5e0bc7b5
snipe <snipe@snipe.net>
2017-08-25 13:27:58 -0700

    Bumped version

--- config/version.php
+++ config/version.php
@@ -1,7 +1,7 @@
 <?php
 return array (
   'app_version' => 'v4.0',
-  'build_version' => 'beta 2',
-  'hash_version' => '46',
-  'full_hash' => 'v4.0-beta-246-g73ce5f9',
+  'build_version' => 'beta3',
+  'hash_version' => '22',
+  'full_hash' => 'v4.0-beta3-22-gbd02b9e',
 );
