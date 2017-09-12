e7d72beb88484f6fd70cb265f6ffd7804aaa1954
snipe <snipe@snipe.net>
2017-09-12 13:08:43 -0700

    Also check for $snipeSettings in the first place
    Since the preflight also uses this basic blade

--- resources/views/layouts/basic.blade.php
+++ resources/views/layouts/basic.blade.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>{{ ($snipeSettings->site_name) ? $snipeSettings->site_name : 'Snipe-IT' }}</title>
+    <title>{{ ($snipeSettings) && ($snipeSettings->site_name) ? $snipeSettings->site_name : 'Snipe-IT' }}</title>
 
 
     <!-- Select2 -->
