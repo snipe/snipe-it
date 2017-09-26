abc2f7e789334944e832a13ebfc9c986a09f1765
snipe <snipe@snipe.net>
2017-09-25 21:57:16 -0700

    Bumped build number

--- config/version.php
+++ config/version.php
@@ -1,7 +1,7 @@
 <?php
 return array (
   'app_version' => 'v4.0.1',
-  'build_version' => '12',
+  'build_version' => '13',
   'hash_version' => 'g0715791',
-  'full_hash' => 'v4.0.1-12-g0715791',
+  'full_hash' => 'v4.0.1-13-g0715791',
 );
