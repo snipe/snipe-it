0fb4ff77f45687f8751c0ced62a05f6878a0f2f6
snipe <snipe@snipe.net>
2017-09-27 16:39:20 -0700

    Restoring older SnipeModel attribute setters
    In case needed by API - needs investigation

--- app/Models/SnipeModel.php
+++ app/Models/SnipeModel.php
@@ -8,6 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class SnipeModel extends Model
 {
+    // Setters that are appropriate across multiple models.
+    public function setPurchaseDateAttribute($value)
+    {
+        if ($value == '') {
+            $value = null;
+        }
+        $this->attributes['purchase_date'] = $value;
+        return;
+    }
 
     /**
      * @param $value
@@ -19,6 +28,70 @@ class SnipeModel extends Model
             $value = null;
         }
         $this->attributes['purchase_cost'] = $value;
+        return;
+    }
+
+    public function setLocationIdAttribute($value)
+    {
+        if ($value == '') {
+            $value = null;
+        }
+        $this->attributes['location_id'] = $value;
+        return;
+    }
+
+    public function setCategoryIdAttribute($value)
+    {
+        if ($value == '') {
+            $value = null;
+        }
+        $this->attributes['category_id'] = $value;
+        // dd($this->attributes);
+        return;
+    }
+
+    public function setSupplierIdAttribute($value)
+    {
+        if ($value == '') {
+            $value = null;
+        }
+        $this->attributes['supplier_id'] = $value;
+        return;
+    }
+
+    public function setDepreciationIdAttribute($value)
+    {
+        if ($value == '') {
+            $value = null;
+        }
+        $this->attributes['depreciation_id'] = $value;
+        return;
+    }
+
+    public function setManufacturerIdAttribute($value)
+    {
+        if ($value == '') {
+            $value = null;
+        }
+        $this->attributes['manufacturer_id'] = $value;
+        return;
+    }
+
+    public function setMinAmtAttribute($value)
+    {
+        if ($value == '') {
+            $value = null;
+        }
+        $this->attributes['min_amt'] = $value;
+        return;
+    }
+
+    public function setParentIdAttribute($value)
+    {
+        if ($value == '') {
+            $value = null;
+        }
+        $this->attributes['parent_id'] = $value;
         return;
     }
 
