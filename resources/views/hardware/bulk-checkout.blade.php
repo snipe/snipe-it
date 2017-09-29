aab409dec22af87380a8444059db6800875618c8
snipe <snipe@snipe.net>
2017-09-28 22:22:21 -0700

    Fixes #4061 - bulk checkout error

--- resources/views/hardware/bulk-checkout.blade.php
+++ resources/views/hardware/bulk-checkout.blade.php
@@ -82,13 +82,14 @@
               {!! $errors->first('selected_asset', '<span class="alert-msg"><i class="fa fa-times"></i> :message</span>') !!}
             </div>
           </div>
-        </form>
+
       </div> <!--./box-body-->
       <div class="box-footer">
         <a class="btn btn-link" href="{{ URL::previous() }}"> {{ trans('button.cancel') }}</a>
         <button type="submit" class="btn btn-success pull-right"><i class="fa fa-check icon-white"></i> {{ trans('general.checkout') }}</button>
       </div>
     </div>
+      </form>
   </div> <!--/.col-md-7-->
 
   <!-- right column -->
