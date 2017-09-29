2ea91266c0f7955caad925b4d2907f1305a77030
snipe <snipe@snipe.net>
2017-09-29 14:26:00 -0700

    Init lightbox

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -681,6 +681,10 @@
         $(function () {
             $('[data-toggle="tooltip"]').tooltip();
         })
+        $(document).on('click', '[data-toggle="lightbox"]', function(event) {
+            event.preventDefault();
+            $(this).ekkoLightbox();
+        });
     </script>
 
     @if ((Session::get('topsearch')=='true') || (Request::is('/')))
