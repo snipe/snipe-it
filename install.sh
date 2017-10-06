cc22350d49094b71caafbff73a431f085c836f2f
tiagom62 <tmmeireles@outlook.com>
2017-10-05 23:17:08 -0400

    Check if sudo is available.

--- install.sh
+++ install.sh
@@ -1,7 +1,13 @@
 # ensure running as root
 if [ "$(id -u)" != "0" ]; then
-  exec sudo "$0" "$@"
+    #Debian doesnt have sudo if root has a password.
+    if ! hash sudo 2>/dev/null; then
+        exec su -c "$0" "$@"
+    else
+        exec sudo "$0" "$@"
+    fi
 fi
+
 wget https://raw.githubusercontent.com/snipe/snipe-it/master/snipeit.sh
 chmod 744 snipeit.sh
 sudo ./snipeit.sh 2>&1 | sudo tee -a /var/log/snipeit-install.log
