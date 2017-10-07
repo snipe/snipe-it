1608edbb28591e6d728ca5db34461f79b0d6dda0
snipe <snipe@snipe.net>
2017-10-07 03:18:57 -0700

    Added ID reference numbers

--- database/seeds/AssetModelSeeder.php
+++ database/seeds/AssetModelSeeder.php
@@ -10,30 +10,30 @@ class AssetModelSeeder extends Seeder
         AssetModel::truncate();
 
         // Laptops
-        factory(AssetModel::class, 1)->states('mbp-13-model')->create();
-        factory(AssetModel::class, 1)->states('mbp-air-model')->create();
-        factory(AssetModel::class, 1)->states('surface-model')->create();
-        factory(AssetModel::class, 1)->states('xps13-model')->create();
-        factory(AssetModel::class, 1)->states('spectre-model')->create();
-        factory(AssetModel::class, 1)->states('zenbook-model')->create();
-        factory(AssetModel::class, 1)->states('yoga-model')->create();
+        factory(AssetModel::class, 1)->states('mbp-13-model')->create(); // 1
+        factory(AssetModel::class, 1)->states('mbp-air-model')->create(); // 2
+        factory(AssetModel::class, 1)->states('surface-model')->create(); // 3
+        factory(AssetModel::class, 1)->states('xps13-model')->create(); // 4
+        factory(AssetModel::class, 1)->states('spectre-model')->create(); // 5
+        factory(AssetModel::class, 1)->states('zenbook-model')->create(); // 6
+        factory(AssetModel::class, 1)->states('yoga-model')->create(); // 7
 
         // Desktops
-        factory(AssetModel::class, 1)->states('macpro-model')->create();
-        factory(AssetModel::class, 1)->states('lenovo-i5-model')->create();
-        factory(AssetModel::class, 1)->states('optiplex-model')->create();
+        factory(AssetModel::class, 1)->states('macpro-model')->create(); // 8
+        factory(AssetModel::class, 1)->states('lenovo-i5-model')->create(); // 9
+        factory(AssetModel::class, 1)->states('optiplex-model')->create(); // 10
 
         // Conference Phones
-        factory(AssetModel::class, 1)->states('polycom-model')->create();
-        factory(AssetModel::class, 1)->states('polycomcx-model')->create();
+        factory(AssetModel::class, 1)->states('polycom-model')->create(); // 11
+        factory(AssetModel::class, 1)->states('polycomcx-model')->create(); // 12
 
         // Tablets
-        factory(AssetModel::class, 1)->states('ipad-model')->create();
-        factory(AssetModel::class, 1)->states('tab3-model')->create();
+        factory(AssetModel::class, 1)->states('ipad-model')->create(); // 13
+        factory(AssetModel::class, 1)->states('tab3-model')->create(); // 14
 
         // Phones
-        factory(AssetModel::class, 1)->states('iphone6s-model')->create();
-        factory(AssetModel::class, 1)->states('iphone7-model')->create();
+        factory(AssetModel::class, 1)->states('iphone6s-model')->create(); // 15
+        factory(AssetModel::class, 1)->states('iphone7-model')->create(); // 16
 
 
     }
