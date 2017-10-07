ecfe1a5442b5e2f18115601633e1a8c8ca3c25b1
snipe <snipe@snipe.net>
2017-10-07 05:30:53 -0700

    Depreciations factory

--- database/seeds/DepreciationSeeder.php
+++ database/seeds/DepreciationSeeder.php
@@ -7,6 +7,8 @@ class DepreciationSeeder extends Seeder
     public function run()
     {
         Depreciation::truncate();
-        factory(Depreciation::class, 5)->create();
+        factory(Depreciation::class, 1)->states('computer')->create(); // 1
+        factory(Depreciation::class, 1)->states('display')->create(); // 2
+        factory(Depreciation::class, 1)->states('mobile-phones')->create(); // 3
     }
 }
