ea6140e786a611b36e4fb919ce1b9681fbd9aa40
snipe <snipe@snipe.net>
2017-10-07 06:16:53 -0700

    Components factory and seeder

--- database/seeds/ComponentSeeder.php
+++ database/seeds/ComponentSeeder.php
@@ -8,6 +8,9 @@ class ComponentSeeder extends Seeder
     {
         Component::truncate();
         DB::table('components_assets')->truncate();
-        factory(Component::class, 10)->create();
+        factory(Component::class, 1)->states('ram-crucial4')->create(); // 1
+        factory(Component::class, 1)->states('ram-crucial8')->create(); // 1
+        factory(Component::class, 1)->states('ssd-crucial120')->create(); // 1
+        factory(Component::class, 1)->states('ssd-crucial240')->create(); // 1
     }
 }
