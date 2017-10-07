20be6706487d446fc680fdc6b6545e63ab8352da
snipe <snipe@snipe.net>
2017-10-07 04:43:55 -0700

    Fixed ids for license

--- database/factories/LicenseFactory.php
+++ database/factories/LicenseFactory.php
@@ -11,8 +11,6 @@
 
 $factory->define(App\Models\License::class, function (Faker\Generator $faker) {
 
-
-    
     return [
         'user_id' => 1,
         'serial' => $faker->uuid,
@@ -29,6 +27,7 @@ $factory->state(App\Models\License::class, 'photoshop', function ($faker) {
     $data =  [
         'name' => 'Photoshop',
         'manufacturer_id' => 9,
+        'purchase_cost' => '299.99',
         'seats' => 10,
     ];
 
@@ -48,12 +47,13 @@ $factory->state(App\Models\License::class, 'acrobat', function ($faker) {
     $data =  [
         'name' => 'Acrobat',
         'manufacturer_id' => 9,
+        'purchase_cost' => '29.99',
         'seats' => 10,
     ];
 
     for ($x = 0; $x < $data['seats']; $x++) {
         $seat = new App\Models\LicenseSeat;
-        $seat->license_id = 1;
+        $seat->license_id = 2;
         $seat->save();
     }
 
@@ -65,12 +65,13 @@ $factory->state(App\Models\License::class, 'indesign', function ($faker) {
     $data =  [
         'name' => 'InDesign',
         'manufacturer_id' => 9,
+        'purchase_cost' => '199.99',
         'seats' => 10,
     ];
 
     for ($x = 0; $x < $data['seats']; $x++) {
         $seat = new App\Models\LicenseSeat;
-        $seat->license_id = 1;
+        $seat->license_id = 3;
         $seat->save();
     }
 
@@ -83,12 +84,13 @@ $factory->state(App\Models\License::class, 'office', function ($faker) {
     $data =  [
         'name' => 'Office',
         'manufacturer_id' => 2,
+        'purchase_cost' => '49.99',
         'seats' => 20,
     ];
 
     for ($x = 0; $x < $data['seats']; $x++) {
         $seat = new App\Models\LicenseSeat;
-        $seat->license_id = 1;
+        $seat->license_id = 4;
         $seat->save();
     }
 
