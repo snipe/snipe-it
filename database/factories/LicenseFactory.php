61a634bc1a5162622c0614b75ff6ff5083b1de56
snipe <snipe@snipe.net>
2017-10-07 04:34:51 -0700

    No idea why this worked locally before, but…

--- database/factories/LicenseFactory.php
+++ database/factories/LicenseFactory.php
@@ -13,7 +13,7 @@ $factory->define(App\Models\License::class, function (Faker\Generator $faker) {
 
 
     
-    $data = [
+    return [
         'user_id' => 1,
         'location_id' => rand(1,5),
         'serial' => $faker->uuid,
@@ -22,50 +22,78 @@ $factory->define(App\Models\License::class, function (Faker\Generator $faker) {
         'order_number' => $faker->numberBetween(1000000, 50000000),
     ];
 
+    
+});
+
+// 1
+$factory->state(App\Models\License::class, 'photoshop', function ($faker) {
+    $data =  [
+        'name' => 'Photoshop',
+        'manufacturer_id' => 9,
+        'seats' => 10,
+    ];
+
     for ($x = 0; $x < $data['seats']; $x++) {
-        $seat = new LicenseSeat;
+        $seat = new App\Models\LicenseSeat;
         $seat->license_id = 1;
         $seat->save();
     }
     
     return $data;
-});
 
-// 1
-$factory->state(App\Models\License::class, 'photoshop', function ($faker) {
-    return [
-        'name' => 'Photoshop',
-        'manufacturer_id' => 9,
-        'seats' => 10,
-    ];
 });
 
 // 2
 $factory->state(App\Models\License::class, 'acrobat', function ($faker) {
-    return [
+
+    $data =  [
         'name' => 'Acrobat',
         'manufacturer_id' => 9,
         'seats' => 10,
     ];
+
+    for ($x = 0; $x < $data['seats']; $x++) {
+        $seat = new App\Models\LicenseSeat;
+        $seat->license_id = 1;
+        $seat->save();
+    }
+
+    return $data;
 });
 
 // 3
 $factory->state(App\Models\License::class, 'indesign', function ($faker) {
-    return [
+    $data =  [
         'name' => 'InDesign',
         'manufacturer_id' => 9,
-        'seats' => 5,
+        'seats' => 10,
     ];
+
+    for ($x = 0; $x < $data['seats']; $x++) {
+        $seat = new App\Models\LicenseSeat;
+        $seat->license_id = 1;
+        $seat->save();
+    }
+
+    return $data;
 });
 
 
 // 4
 $factory->state(App\Models\License::class, 'office', function ($faker) {
-    return [
+    $data =  [
         'name' => 'Office',
         'manufacturer_id' => 2,
         'seats' => 20,
     ];
+
+    for ($x = 0; $x < $data['seats']; $x++) {
+        $seat = new App\Models\LicenseSeat;
+        $seat->license_id = 1;
+        $seat->save();
+    }
+
+    return $data;
 });
 
 
