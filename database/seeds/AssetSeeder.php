7f02ff12cf6b78122a365ee143c43f9bef22da76
snipe <snipe@snipe.net>
2017-10-07 03:19:17 -0700

    Asset seeder

--- database/seeds/AssetSeeder.php
+++ database/seeds/AssetSeeder.php
@@ -8,6 +8,28 @@ class AssetSeeder extends Seeder
   public function run()
   {
       Asset::truncate();
-      factory(Asset::class, 100)->create();
+      //factory(Asset::class, 100)->create();
+      factory(Asset::class, 10)->states('laptop-mbp')->create();
+      factory(Asset::class, 10)->states('laptop-air')->create();
+      factory(Asset::class, 5)->states('laptop-surface')->create();
+      factory(Asset::class, 5)->states('laptop-xps')->create();
+      factory(Asset::class, 5)->states('laptop-spectre')->create();
+      factory(Asset::class, 5)->states('laptop-zenbook')->create();
+      factory(Asset::class, 3)->states('laptop-yoga')->create();
+
+      factory(Asset::class, 3)->states('desktop-macpro')->create();
+      factory(Asset::class, 3)->states('desktop-lenovo-i5')->create();
+      factory(Asset::class, 10)->states('desktop-optiplex')->create();
+
+      factory(Asset::class, 5)->states('conf-polycom')->create();
+      factory(Asset::class, 2)->states('conf-polycomcx')->create();
+
+      factory(Asset::class, 12)->states('tablet-ipad')->create();
+      factory(Asset::class, 4)->states('tablet-tab3')->create();
+
+      factory(Asset::class, 27)->states('phone-iphone6s')->create();
+      factory(Asset::class, 40)->states('phone-iphone7')->create();
+
+
   }
 }
