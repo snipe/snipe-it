0decd184dea5cd5d2bd978a1a067a9c3572bd2ae
snipe <snipe@snipe.net>
2017-10-25 21:28:36 -0700

    Added manufacturers API support for image

--- app/Http/Controllers/Api/SuppliersController.php
+++ app/Http/Controllers/Api/SuppliersController.php
@@ -23,7 +23,7 @@ class SuppliersController extends Controller
         $allowed_columns = ['id','name','address','phone','contact','fax','email','image','assets_count','licenses_count', 'accessories_count'];
         
         $suppliers = Supplier::select(
-                array('id','name','address','address2','city','state','country','fax', 'phone','email','contact','created_at','updated_at','deleted_at')
+                array('id','name','address','address2','city','state','country','fax', 'phone','email','contact','created_at','updated_at','deleted_at','image')
             )->withCount('assets')->withCount('licenses')->withCount('accessories')->whereNull('deleted_at');
 
 
