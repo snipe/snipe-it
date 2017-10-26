dc9a908de7577a37c89851cd8b2e65ba4cddcd81
snipe <snipe@snipe.net>
2017-10-25 18:13:03 -0700

    Fixing laravel mix() fuckery, hopefully for the last time

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,8 +1,6 @@
 
 const { mix } = require('laravel-mix');
 
-mix.setPublicPath('build'); //this throws everything to root dir 'build'
-
 
 // This generates a file called app.css, which we use
 // later on to build all.css
@@ -17,21 +15,27 @@ mix
     .less('resources/assets/less/app.less', 'css')
     .less('resources/assets/less/overrides.less', 'css')
 .styles([
-    'build/css/app.css',
+    './resources/assets/css/app.css',
     'public/css/AdminLTE.css',
     'resources/assets/css/font-awesome/font-awesome.min.css',
     './bower_components/iCheck/skins/minimal/minimal.css',
     './node_modules/bootstrap-datepicker/dist/css/bootstrap-datepicker.standalone.css',
     'public/css/bootstrap-tables-sticky-header.css',
-    'build/css/overrides.css'
-], 'public/css/dist/all.css')
+    'public/css/overrides.css'
+],
+    './public/css/dist/all.css')
+
+// jQuery is loaded from vue.js webpack process
+// This compiles the vue.js file in the build directory
+// for later concatenation in the scripts() section below.
 .js(
-    // jQuery is loaded from vue.js webpack process
-    './resources/assets/js/vue.js', //this is Snipe-IT's initializer for Vue.js
-    'build'
-).sourceMaps().scripts([
+
+    'resources/assets/js/vue.js', // Snipe-IT's initializer for Vue.js
+    './public/js/build'
+).sourceMaps()
+.scripts([
     './node_modules/jquery-ui/jquery-ui.js',
-    'build/vue.js', //this is the modularized nifty Vue.js thing we just built, above!
+    './public/build/vue.js', //this is the modularized nifty Vue.js thing we just built, above!
     './node_modules/tether/dist/js/tether.min.js',
     './node_modules/jquery-slimscroll/jquery.slimscroll.js',
     './node_modules/jquery.iframe-transport/jquery.iframe-transport.js',
@@ -44,6 +48,9 @@ mix
     './resources/assets/js/app.js', //this is part of AdminLTE
     './resources/assets/js/snipeit.js', //this is the actual Snipe-IT JS
     './resources/assets/js/snipeit_modals.js'
-],'public/js/dist/all.js').version();
+],
+    './public/js/dist/all.js');
 
+mix.copy('./public/css/dist/all.css', './public/css/build/all.css').copy('./public/js/dist/all.js', './public/js/build/all.js');
 
+mix.version();
