76c4c19b3ee847f87b385e66df07114e2c1c5ea8
snipe <snipe@snipe.net>
2017-10-26 21:51:53 -0700

    Fixed small issues with select2 partials

--- resources/views/partials/forms/edit/asset-select.blade.php
+++ resources/views/partials/forms/edit/asset-select.blade.php
@@ -1,8 +1,13 @@
 <!-- Asset -->
-<div id="assigned_asset" class="form-group{{ $errors->has($fieldname) ? ' has-error' : '' }}">
+<div id="{{ $fieldname }}" class="form-group{{ $errors->has($fieldname) ? ' has-error' : '' }}">
     {{ Form::label($fieldname, $translated_name, array('class' => 'col-md-3 control-label')) }}
     <div class="col-md-7 required">
         <select class="js-data-ajax" data-endpoint="hardware" name="{{ $fieldname }}" style="width: 100%" id="assigned_asset_select">
+            @if (Input::old($fieldname))
+                <option value="{{ Input::old($fieldname) }}" selected="selected">
+                    {{ \App\Models\Asset::select('name')->find(Input::old($fieldname))->name }}
+                </option>
+            @endif
         </select>
     </div>
     {!! $errors->first($fieldname, '<div class="col-md-8 col-md-offset-3"><span class="alert-msg"><i class="fa fa-times"></i> :message</span></div>') !!}
--- resources/views/partials/forms/edit/location-select.blade.php
+++ resources/views/partials/forms/edit/location-select.blade.php
@@ -1,17 +1,21 @@
 <!-- Location -->
-<div id="assigned_location" class="form-group{{ $errors->has($fieldname) ? ' has-error' : '' }}">
+<div id="{{ $fieldname }}" class="form-group{{ $errors->has($fieldname) ? ' has-error' : '' }}">
+
     {{ Form::label($fieldname, $translated_name, array('class' => 'col-md-3 control-label')) }}
-    <div class="col-md-7 required">
-        <select class="js-data-ajax" data-endpoint="locations" name="{{ $fieldname }}" style="width: 100%" id="assigned_location_select">
+    <div class="col-md-7{{  ((isset($required) && ($required =='true'))) ?  ' required' : '' }}">
+        <select class="js-data-ajax" data-endpoint="locations" name="{{ $fieldname }}" style="width: 100%" id="{{ $fieldname }}_location_select">
         </select>
     </div>
 
     <div class="col-md-1 col-sm-1 text-left">
         @can('create', \App\Models\Location::class)
-            <a href='{{ route('modal.location') }}' data-toggle="modal"  data-target="#createModal" data-dependency="location" data-select='assigned_location_select' class="btn btn-sm btn-default">New</a>
+            <a href='{{ route('modal.location') }}' data-toggle="modal"  data-target="#createModal" data-dependency="location" data-select='{{ $fieldname }}_location_select' class="btn btn-sm btn-default">New</a>
         @endcan
     </div>
 
     {!! $errors->first($fieldname, '<div class="col-md-8 col-md-offset-3"><span class="alert-msg"><i class="fa fa-times"></i> :message</span></div>') !!}
 
 </div>
+
+
+
