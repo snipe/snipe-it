49afd325a9fba7e1a32a9164164efef5afdb9e11
snipe <snipe@snipe.net>
2017-10-28 01:48:50 -0700

    Add more data from seeders to check for 1001 queries

--- database/seeds/ActionlogSeeder.php
+++ database/seeds/ActionlogSeeder.php
@@ -7,7 +7,7 @@ class ActionlogSeeder extends Seeder
   public function run()
   {
     Actionlog::truncate();
-      factory(Actionlog::class, 'asset-checkout-user',5)->create();
-      factory(Actionlog::class, 'asset-checkout-location',5)->create();
+      factory(Actionlog::class, 'asset-checkout-user',300)->create();
+      factory(Actionlog::class, 'asset-checkout-location',100)->create();
   }
 }
