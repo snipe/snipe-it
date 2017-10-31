7f1b7be41645585cb8d6f7c626e2afac245928bd
snipe <snipe@snipe.net>
2017-10-30 21:26:25 -0700

    Rolling back prepending models to sort

--- app/Http/Controllers/Api/AssetModelsController.php
+++ app/Http/Controllers/Api/AssetModelsController.php
@@ -55,7 +55,7 @@ class AssetModelsController extends Controller
                 $assetmodels->OrderManufacturer($order);
                 break;
             default:
-                $assetmodels->orderBy('models'.$sort, $order);
+                $assetmodels->orderBy($sort, $order);
                 break;
         }
 
