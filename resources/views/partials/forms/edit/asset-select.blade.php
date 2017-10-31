379274defff1f372b4e51bf567f69503e5cd7829
snipe <snipe@snipe.net>
2017-10-31 04:48:40 -0700

    Check for multiple variable (for bulk checkout)

--- resources/views/partials/forms/edit/asset-select.blade.php
+++ resources/views/partials/forms/edit/asset-select.blade.php
@@ -2,7 +2,7 @@
 <div id="{{ $fieldname }}" class="form-group{{ $errors->has($fieldname) ? ' has-error' : '' }}">
     {{ Form::label($fieldname, $translated_name, array('class' => 'col-md-3 control-label')) }}
     <div class="col-md-7 required">
-        <select class="js-data-ajax" data-endpoint="hardware" name="{{ $fieldname }}" style="width: 100%" id="assigned_asset_select">
+        <select class="js-data-ajax select2" data-endpoint="hardware" name="{{ $fieldname }}" style="width: 100%" id="assigned_asset_select"{{ (isset($multiple)) ? ' multiple="multiple"' : '' }}>
             @if ($asset_id = Input::old($fieldname, (isset($item)) ? $item->{$fieldname} : ''))
                 <option value="{{ $asset_id }}" selected="selected">
                     {{ \App\Models\Asset::find($asset_id)->present()->fullName }}
