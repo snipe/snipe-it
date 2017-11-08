1123272ae818cb5619b54aa036e08625ed124c0a
snipe <snipe@snipe.net>
2017-11-07 23:12:10 -0800

    Bumped hash

--- config/version.php
+++ config/version.php
@@ -1,7 +1,7 @@
 <?php
 return array (
   'app_version' => 'v4.1.3',
-  'build_version' => '94',
-  'hash_version' => 'gb6a14d2',
-  'full_hash' => 'v4.1.3-beta2-94-gb6a14d2',
+  'build_version' => '48',
+  'hash_version' => 'g74aa562',
+  'full_hash' => 'v4.1.3-48-g74aa562',
 );
