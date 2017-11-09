65353fa42233efa2dc88daef101ee149afdd2522
snipe <snipe@snipe.net>
2017-11-08 18:06:51 -0800

    Nicer styling for purchase cost in edit form
    Made it more consistent with the warranty months foeld below it

--- resources/views/partials/forms/edit/purchase_cost.blade.php
+++ resources/views/partials/forms/edit/purchase_cost.blade.php
@@ -1,17 +1,20 @@
 <!-- Purchase Cost -->
 <div class="form-group {{ $errors->has('purchase_cost') ? ' has-error' : '' }}">
-   <label for="purchase_cost" class="col-md-3 control-label">{{ trans('general.purchase_cost') }} </label>
-   <div class="col-md-2">
-       <div class="input-group">
-           <span class="input-group-addon">
+    <label for="purchase_cost" class="col-md-3 control-label">{{ trans('general.purchase_cost') }}</label>
+    <div class="col-md-9">
+        <div class="input-group col-md-3" style="padding-left: 0px;">
+            <input class="form-control" type="text" name="purchase_cost" id="purchase_cost" value="{{ Input::old('purchase_cost', \App\Helpers\Helper::formatCurrencyOutput($item->purchase_cost)) }}" />
+            <span class="input-group-addon">
                 @if (isset($currency_type))
-                {{ $currency_type }}
+                    {{ $currency_type }}
                 @else
-               {{ $snipeSettings->default_currency }}
-               @endif
-           </span>
-           <input class="col-md-2 form-control" type="text" name="purchase_cost" id="purchase_cost" value="{{ Input::old('purchase_cost', \App\Helpers\Helper::formatCurrencyOutput($item->purchase_cost)) }}" />
-           {!! $errors->first('purchase_cost', '<span class="alert-msg"><i class="fa fa-times"></i> :message</span>') !!}
-       </div>
-   </div>
+                    {{ $snipeSettings->default_currency }}
+                @endif
+            </span>
+        </div>
+        <div class="col-md-9" style="padding-left: 0px;">
+            {!! $errors->first('purchase_cost', '<span class="alert-msg"><i class="fa fa-times"></i> :message</span>') !!}
+        </div>
+    </div>
+
 </div>
