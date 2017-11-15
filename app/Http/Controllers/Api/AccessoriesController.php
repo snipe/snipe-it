ba38b841cb0f0532c3aa3fc477d9f9895402ca1e
snipe <snipe@snipe.net>
2017-11-14 16:47:21 -0800

    Constrain accessory by category ID if one is passed

--- app/Http/Controllers/Api/AccessoriesController.php
+++ app/Http/Controllers/Api/AccessoriesController.php
@@ -34,6 +34,10 @@ class AccessoriesController extends Controller
             $accessories->where('company_id','=',$request->input('company_id'));
         }
 
+        if ($request->has('category_id')) {
+            $accessories->where('category_id','=',$request->input('category_id'));
+        }
+
         if ($request->has('manufacturer_id')) {
             $accessories->where('manufacturer_id','=',$request->input('manufacturer_id'));
         }
