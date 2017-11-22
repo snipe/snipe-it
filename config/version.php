4a6c18532b88769e41dca09fe7bd88b6cbfc9b6b
snipe <snipe@snipe.net>
2017-11-21 22:35:02 -0800

    Auto-bumped hash/version

--- config/version.php
+++ config/version.php
@@ -1,8 +1,8 @@
 <?php
 return array (
   'app_version' => 'v4.1.6-pre',
-  'full_app_version' => 'v4.1.6-pre - build 2762-g1d6320a',
-  'build_version' => '2762',
+  'full_app_version' => 'v4.1.6-pre - build 2763-g1d6320a',
+  'build_version' => '2763',
   'prerelease_version' => '',
   'hash_version' => 'g1d6320a',
   'full_hash' => 'v4.1.5-2-g1d6320a',
