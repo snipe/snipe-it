0c9e41e1fae40431f5034a068512fce74873c1a1
snipe <snipe@snipe.net>
2017-11-21 19:01:47 -0800

    Updated versioning script to handle githooks better

--- config/version.php
+++ config/version.php
@@ -1,7 +1,11 @@
 <?php
 return array (
-  'app_version' => 'v4.1.5',
-  'build_version' => '187',
-  'hash_version' => 'gc0293a7',
-  'full_hash' => 'v4.1.5-beta2-187-gc0293a7',
+  'app_version' => 'v4.1.6-pre',
+  'full_app_version' => 'v4.1.6-pre - build 2736-g1d6320a',
+  'build_version' => '2736',
+  'prerelease_version' => '',
+  'hash_version' => 'g1d6320a',
+  'full_hash' => 'v4.1.5-2-g1d6320a',
+  'branch' => 'develop',
+  'next_version' => '',
 );
