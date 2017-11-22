43c1e893c0b665a682208f823c588120dd9da622
snipe <snipe@snipe.net>
2017-11-21 20:13:51 -0800

    Fixed #4494 - use audit settings for asset audit pre-populaton

--- app/Http/Controllers/AssetsController.php
+++ app/Http/Controllers/AssetsController.php
@@ -1238,8 +1238,9 @@ class AssetsController extends Controller
 
     public function audit($id)
     {
+        $settings = Setting::getSettings();
         $this->authorize('audit', Asset::class);
-        $dt = Carbon::now()->addMonths(12)->toDateString();
+        $dt = Carbon::now()->addMonths($settings->audit_interval)->toDateString();
         $asset = Asset::findOrFail($id);
         return view('hardware/audit')->with('asset', $asset)->with('next_audit_date', $dt)->with('locations_list');
     }
