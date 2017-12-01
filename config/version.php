80109071a288cac4a29db0a409ff04d2ff970538
snipe <snipe@snipe.net>
2017-12-01 14:54:02 -0800

    Fixed version number

--- config/version.php
+++ config/version.php
@@ -1,10 +1,10 @@
 <?php
 return array (
-  'app_version' => 'v4.1.7-pre',
-  'full_app_version' => 'v4.1.7-pre - build 2811-gb934d2e',
-  'build_version' => '2811',
-  'prerelease_version' => '',
-  'hash_version' => 'gb934d2e',
-  'full_hash' => 'v4.1.6-76-gb934d2e',
-  'branch' => 'develop',
+    'app_version' => 'v4.1.6',
+    'full_app_version' => 'v4.1.6 - build 2994-g83c8449',
+    'build_version' => '2994',
+    'prerelease_version' => '',
+    'hash_version' => 'g83c8449',
+    'full_hash' => 'v4.1.5-76-gb934d2e',
+    'branch' => 'master',
 );
