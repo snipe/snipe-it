035f7a529269288caabedf1e11e1c8e11750fd4d
snipe <snipe@snipe.net>
2017-12-07 19:24:03 -0800

    Formatted version

--- config/version.php
+++ config/version.php
@@ -1,7 +1,7 @@
 <?php
 return array (
   'app_version' => 'v4.1.8-pre',
-  'full_app_version' => 'v4.1.8-pre - build 3068-gceca76b',
+  'full_app_version' => 'v4.1.8-pre - build 3068 (gceca76b)',
   'build_version' => '3068',
   'prerelease_version' => '',
   'hash_version' => 'gceca76b',
