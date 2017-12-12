2952497a60bfcc79a704c2c83f770462415d7a46
snipe <snipe@snipe.net>
2017-12-12 11:42:26 -0800

    Removed bower components directory
    We use npm/webpack now

--- bower_components/jquery-ui/ui/minified/form.js
+++
@@ -1,4 +0,0 @@
-/*! jQuery UI - v1.12.1 - 2016-09-15
-* http://jqueryui.com
-* Copyright jQuery Foundation and other contributors; Licensed  */
-!function(a){"function"==typeof define&&define.amd?define(["jquery","./version"],a):a(jQuery)}(function(a){return a.fn.form=function(){return"string"==typeof this[0].form?this.closest("form"):a(this[0].form)}});
