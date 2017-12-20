1f36e7997f1611ad86f478fc8454a3a0f51cab6f
snipe <snipe@snipe.net>
2017-12-20 03:31:43 -0800

    Bumped version

--- config/version.php
+++ config/version.php
@@ -1,7 +1,7 @@
 <?php
 return array (
-  'app_version' => 'v4.1.9-pre',
-  'full_app_version' => 'v4.1.9-pre - build 3128-ge5a7e66',
+  'app_version' => 'v4.1.9',
+  'full_app_version' => 'v4.1.9 - build 3128-ge5a7e66',
   'build_version' => '3128',
   'prerelease_version' => '',
   'hash_version' => 'ge5a7e66',
