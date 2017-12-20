33b5c26da57ec3b1a48d2d677ffcb59bedad3d18
snipe <snipe@snipe.net>
2017-12-19 22:16:31 -0800

    Bumped hashcount

--- config/version.php
+++ config/version.php
@@ -1,10 +1,10 @@
 <?php
 return array (
   'app_version' => 'v4.1.9-pre',
-  'full_app_version' => 'v4.1.9-pre - build 3117-',
-  'build_version' => '3117',
+  'full_app_version' => 'v4.1.9-pre - build 3128-ge5a7e66',
+  'build_version' => '3128',
   'prerelease_version' => '',
-  'hash_version' => 'geb827cd',
-  'full_hash' => 'v4.1.7-69-geb827cd',
+  'hash_version' => 'ge5a7e66',
+  'full_hash' => 'v4.1.8-11-ge5a7e66',
   'branch' => 'master',
 );
