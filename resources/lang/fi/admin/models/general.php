5fe800903540cdfc68f4b6ab46411deda8fa48da
atuc2 <30864603+atuc2@users.noreply.github.com>
2018-01-05 23:00:39 +0200

    Localization fix

--- resources/lang/fi/admin/models/general.php
+++ resources/lang/fi/admin/models/general.php
@@ -1,18 +1,18 @@
 <?php
 
 return array(
-    'about_models_title'     => 'Tietoja varallismalleista',
-    'about_models_text'           => 'Asset-mallit ovat tapa ryhmitellä identtiset varat. "MBP 2013", "IPhone 6s" jne.',
-    'deleted'  					        => 'Tämä malli on poistettu. <a href="/hardware/models/:model_id/restore">Vaihda tästä palauttaaksesi sen</a>.',
-    'bulk_delete'               => 'Bulk Delete Asset Models',
-    'bulk_delete_help'               => 'Use the checkboxes below to confirm the deletion of the selected asset models. Asset models that have assets associated with them cannot be deleted until the assets are associated with a different model.',
-    'bulk_delete_warn'          => 'You are about to delete :model_count asset models.',
+    'about_models_title'     => 'Tietoja malleista',
+    'about_models_text'           => 'Omaisuusmallit ovat tapa ryhmitellä identtiset varat. "MBP 2013", "IPhone 6s" jne.',
+    'deleted'  					        => 'Tämä malli on poistettu. <a href="/hardware/models/:model_id/restore">Paina tästä palauttaaksesi sen</a>.',
+    'bulk_delete'               => 'Massa poista omaisuusmalleja',
+    'bulk_delete_help'               => 'Käytä alla olevaan valintalaatikkoa vahvistaaksesi valitun omaisuusmallin poiston. Omaisuusmallit joihin on yhditetty nimikkeitä ei voida poistaa ennen kuin nimikkeet ovat osoitettu toiselle mallille.',
+    'bulk_delete_warn'          => 'Olet poistamassa :model_count omaisuusmallia.',
     'restore'                   => 'Palauta malli',
     'requestable'               => 'Käyttäjät voivat pyytää tätä mallia',
 	'show_mac_address'			      => 'Näytä MAC-osoitekenttä tämän mallin laitteille',
     'view_deleted'              => 'Näytä poistettu',
     'view_models'               => 'Näytä mallit',
-    'fieldset'                  => 'fieldset',
+    'fieldset'                  => 'kenttäsarja',
     'no_custom_field'           => 'Ei muokattuja kenttiä',
 
 );
