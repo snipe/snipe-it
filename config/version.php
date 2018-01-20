913e6a57095efa63f5e210fb5ca14d024133c72f
snipe <snipe@snipe.net>
2018-01-20 03:59:42 -0800

    Bumped hash

--- config/version.php
+++ config/version.php
@@ -1,10 +1,10 @@
 <?php
 return array (
   'app_version' => 'v4.1.10-pre',
-  'full_app_version' => 'v4.1.10-pre - build 3219-gf38dc37',
-  'build_version' => '3219',
+  'full_app_version' => 'v4.1.10-pre - build 3238-g4ca8272',
+  'build_version' => '3238',
   'prerelease_version' => '',
-  'hash_version' => 'gf38dc37',
-  'full_hash' => 'v4.1.10-pre-89-gf38dc37',
+  'hash_version' => 'g4ca8272',
+  'full_hash' => 'v4.1.10-pre-108-g4ca8272',
   'branch' => 'develop',
 );
