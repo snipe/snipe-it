c6e14caa311890ac9147854c649e46cb266521ba
snipe <snipe@snipe.net>
2018-03-22 14:36:36 -0700

    Removed e() from array_smart_custom_field_fetch

--- app/Importer/Importer.php
+++ app/Importer/Importer.php
@@ -200,7 +200,7 @@ abstract class Importer
     public function array_smart_custom_field_fetch(array $array, $key)
     {
         $index_name = strtolower($key->name);
-        return array_key_exists($index_name, $array) ? e(trim($array[$index_name])) : false;
+        return array_key_exists($index_name, $array) ? trim($array[$index_name]) : false;
     }
 
     protected function log($string)
