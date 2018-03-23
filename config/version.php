c4c137dc08c5643ee82611282f45c1e0b030ee9a
snipe <snipe@snipe.net>
2018-03-22 20:47:09 -0700

    Bumped to 4.2.0

--- config/version.php
+++ config/version.php
@@ -1,10 +1,10 @@
 <?php
 return array (
-  'app_version' => 'v4.1.15',
-  'full_app_version' => 'v4.1.15 - build 3479-gbf74bb1',
+  'app_version' => 'v4.2.0',
+  'full_app_version' => 'v4.2.0 - build 3479-gbf74bb1',
   'build_version' => '3479',
   'prerelease_version' => '',
   'hash_version' => 'gbf74bb1',
-  'full_hash' => 'v4.1.15-3479-gbf74bb1',
+  'full_hash' => 'v4.2.0-3479-gbf74bb1',
   'branch' => 'develop',
 );
