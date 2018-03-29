8462b0140e2eab420c66ebfaa087b1c1b5b333e0
snipe <snipe@snipe.net>
2018-03-29 04:29:42 -0700

    Refactored BS table formatter for requested assets

--- resources/views/partials/bootstrap-table.blade.php
+++ resources/views/partials/bootstrap-table.blade.php
@@ -301,11 +301,12 @@
     }
 
 
+    // This is only used by the requestable assets section
     function assetRequestActionsFormatter (row, value) {
-        if (value.available_actions.request == true)  {
+        if (value.available_actions.cancel == true)  {
+            return '<form action="{{ url('/') }}/account/request-asset/'+ value.id + '" method="GET"><button class="btn btn-danger btn-sm" data-tooltip="true" title="Cancel this item request">{{ trans('button.cancel') }}</button></form>';
+        } else if (value.available_actions.request == true)  {
             return '<form action="{{ url('/') }}/account/request-asset/'+ value.id + '" method="GET"><button class="btn btn-primary btn-sm" data-tooltip="true" title="Request this item">{{ trans('button.request') }}</button></form>';
-        } else if (value.available_actions.cancel == true)  {
-            return '<form><a href="{{ url('/') }}/' + row + '/request" class="btn btn-danger btn-sm" data-tooltip="true" title="Request this item">{{ trans('button.cancel') }}</a></form>';
         }
 
     }
