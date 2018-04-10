ff033ca285c9a709bc7d7bb5abe823ea9a91a4fc
Matthew Bowker <github@matthewbowker.net>
2018-04-10 17:58:45 -0600

    Begin work on querying affected assets

--- app/Models/Insurance.php
+++ app/Models/Insurance.php
@@ -5,11 +5,23 @@ namespace App\Models;
 
 class Insurance extends Agreement
 {
+    private function getAffectedAsset() {
+        return [];
+    }
     public function __construct($attributes = []) {
         parent::__construct($attributes);
         $this->table = "insurance";
 
         $this->rules = array_merge($this->rules, ["policy" => "string", "policy_number"=>"string"]);
+    }
+
+    public function getAffected($type = "asset") {
+        $data = [];
+        switch($type) {
+            case "asset":
+                $data = $this->getAffectedAsset();
+                break;
+        }
     }
 
 }
