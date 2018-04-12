261b2a78272c7b0f48930db331084e65d23f354c
snipe <snipe@snipe.net>
2018-04-11 20:20:02 -0700

    Reverting relative path in spatie - doesn’t work via the web - needs more work

--- config/backup.php
+++ config/backup.php
@@ -27,14 +27,12 @@ return [
                  * The list of directories and files that will be included in the backup.
                  */
                 'include' => [
-                    'public/uploads',
-                    'storage/private_uploads',
-                    'storage/oauth-private.key',
-                    'storage/oauth-public.key',
-                    (env('BACKUP_ENV')=='true') ? '.env' : '.env.example',
-
-
-
+                    public_path('uploads'),
+                    storage_path('private_uploads'),
+                    storage_path('oauth-private.key'),
+                    storage_path('oauth-public.key'),
+                    (env('BACKUP_ENV')=='true') ? base_path('.env') : base_path('.env.example'),
+                    
                 ],
 
                 /*
