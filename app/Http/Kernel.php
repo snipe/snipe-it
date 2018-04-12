72e3483cddca258e3964e130bec3cac07c30e981
snipe <snipe@snipe.net>
2018-04-11 17:46:38 -0700

    Delete unused code

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -25,7 +25,6 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\CheckForSetup::class,
         \App\Http\Middleware\TrustProxies::class,
         \App\Http\Middleware\CheckForDebug::class,
-       // \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
     ];
 
     /**
