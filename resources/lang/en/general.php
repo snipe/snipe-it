0fc0aa39b1c9befbb3a60c05036f64c3ab48b46b
snipe <snipe@snipe.net>
2018-05-01 21:33:51 -0700

    Use flexible language string for confirm delete

--- resources/lang/en/general.php
+++ resources/lang/en/general.php
@@ -68,6 +68,7 @@
     'debug_warning'         => 'Warning!',
     'debug_warning_text'    => 'This application is running in production mode with debugging enabled. This can expose sensitive data if your application is accessible to the outside world. Disable debug mode by setting the <code>APP_DEBUG</code> value in your <code>.env</code> file to <code>false</code>.',
     'delete'  				=> 'Delete',
+    'delete_confirm'  		=> 'Are you sure you wish to delete :item?',
     'deleted'  				=> 'Deleted',
     'delete_seats'  		=> 'Deleted Seats',
     'departments'           => 'Departments',
