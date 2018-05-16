9d2a4bc32cf65897faad5c8e3af4871c02dc2def
snipe <snipe@snipe.net>
2018-05-16 14:57:57 -0700

    Increment counters for checkin;/checkout

--- app/Models/Loggable.php
+++ app/Models/Loggable.php
@@ -55,9 +55,17 @@ trait Loggable
         if ($log->target_type == Location::class) {
             $log->location_id = $target->id;
         } elseif ($log->target_type == Asset::class) {
-            $log->location_id = $target->rtd_location_id;
+            $log->location_id = $target->location_id;
         } else {
             $log->location_id = $target->location_id;
+        }
+
+        if ($log->item_type == Asset::class) {
+
+            if ($asset = Asset::find($log->item_id)) {
+                \Log::debug('Increment the checkout count for asset: '.$log->item_id);
+                $asset->increment('checkout_counter', 1);
+            }
         }
 
         $log->note = $note;
@@ -121,8 +129,17 @@ trait Loggable
             $log->item_type = License::class;
             $log->item_id = $this->license_id;
         } else {
+
             $log->item_type = static::class;
             $log->item_id = $this->id;
+
+            if (static::class == Asset::class) {
+                if ($asset = Asset::find($log->item_id)) {
+                    \Log::debug('Increment the checkin count for asset: '.$log->item_id);
+                    $asset->increment('checkin_counter', 1);
+                }
+            }
+
         }
 
 
