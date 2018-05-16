0dca30e3dcc41d0118c2784443e96c241d9a60d7
snipe <snipe@snipe.net>
2018-05-16 15:33:43 -0700

    Added request increment/decrementer

--- app/Http/Controllers/ViewAssetsController.php
+++ app/Http/Controllers/ViewAssetsController.php
@@ -181,6 +181,8 @@ class ViewAssetsController extends Controller
         // If it's already requested, cancel the request.
         if ($asset->isRequestedBy(Auth::user())) {
             $asset->cancelRequest();
+            $asset->decrement('requests_counter', 1);
+            
             $logaction->logaction('request canceled');
             $settings->notify(new RequestAssetCancelationNotification($data));
             return redirect()->route('requestable-assets')
@@ -188,8 +190,8 @@ class ViewAssetsController extends Controller
         } else {
 
             $logaction->logaction('requested');
-
             $asset->request();
+            $asset->increment('requests_counter', 1);
             $settings->notify(new RequestAssetNotification($data));
 
 
