649563457d50089b666933e96fab1af8c0283b7f
Brian Monroe <briancmonroe@gmail.com>
2018-05-28 14:30:18 -0700

    Added notes field to acessories and consumable checkout pages. Resolves request #5607. (#5608)

--- resources/views/consumables/checkout.blade.php
+++ resources/views/consumables/checkout.blade.php
@@ -66,7 +66,14 @@
                 </div>
               </div>
             @endif
-
+          <!-- Note -->
+          <div class="form-group {{ $errors->has('note') ? 'error' : '' }}">
+            <label for="note" class="col-md-3 control-label">{{ trans('admin/hardware/form.notes') }}</label>
+            <div class="col-md-7">
+              <textarea class="col-md-6 form-control" id="note" name="note">{{ Input::old('note', $consumable->note) }}</textarea>
+              {!! $errors->first('note', '<span class="alert-msg"><i class="fa fa-times"></i> :message</span>') !!}
+            </div>
+          </div>
         </div> <!-- .box-body -->
         <div class="box-footer">
           <a class="btn btn-link" href="{{ URL::previous() }}">{{ trans('button.cancel') }}</a>
