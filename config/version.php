fde4a595108af273ba80fc58d0a4b4c0928cb755
snipe <snipe@snipe.net>
2018-06-21 07:40:37 -0700

    Bumped version

--- config/version.php
+++ config/version.php
@@ -1,10 +1,10 @@
 <?php
 return array (
-  'app_version' => 'v4.4.1',
-  'full_app_version' => 'v4.4.1 - build 3636-gece916e12',
-  'build_version' => '3636',
+  'app_version' => 'v4.4.2',
+  'full_app_version' => 'v4.4.2 - build 3666-gaa5003d29',
+  'build_version' => '3666',
   'prerelease_version' => '',
-  'hash_version' => 'gece916e12',
-  'full_hash' => 'v4.4.1-54-gece916e12',
-  'branch' => 'master',
+  'hash_version' => 'gaa5003d29',
+  'full_hash' => 'v4.4.1-28-gaa5003d29',
+  'branch' => 'develop',
 );
