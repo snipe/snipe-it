a44bd9abe0a137f60f3dac6e9f72af1d844885f5
snipe <snipe@snipe.net>
2018-07-05 18:02:25 -0700

    Disallow deleting category if there are licenses

--- app/Http/Transformers/CategoriesTransformer.php
+++ app/Http/Transformers/CategoriesTransformer.php
@@ -41,7 +41,7 @@ class CategoriesTransformer
 
             $permissions_array['available_actions'] = [
                 'update' => Gate::allows('update', Category::class) ? true : false,
-                'delete' => (Gate::allows('delete', Category::class) && ($category->assets_count == 0) && ($category->accessories_count == 0) && ($category->consumables_count == 0) && ($category->components_count == 0)) ? true : false,
+                'delete' => (Gate::allows('delete', Category::class) && ($category->assets_count == 0) && ($category->accessories_count == 0) && ($category->consumables_count == 0) && ($category->components_count == 0) && ($category->licenses_count == 0)) ? true : false,
             ];
 
             $array += $permissions_array;
