f011abcc505bbdb746961b4a672ea5618faaa55f
Till Deeke <privat@tilldeeke.de>
2018-07-11 12:53:36 +0200

    removes special handling of custom fieldsets in base policy
    I just realised that this code duplicates the logic from the custom fieldset policy.
    Since we are checking for the authorization of custom fields anyway, we can just use the columnName for the fields.

--- app/Policies/CustomFieldsetPolicy.php
+++ app/Policies/CustomFieldsetPolicy.php
@@ -9,8 +9,9 @@ class CustomFieldsetPolicy extends SnipePermissionsPolicy
     protected function columnName()
     {
         /**
-         * Authorization for custom fieldsets gets proxied down to custom fields
-         * in SnipePermissionsPolicy.
+         * Proxy the authorization for custom fieldsets down to custom fields.
+         * This allows us to use the existing permissions in use and have more 
+         * semantically correct authorization checks for custom fieldsets.
          *
          * See: https://github.com/snipe/snipe-it/pull/5795
          */
--- app/Policies/SnipePermissionsPolicy.php
+++ app/Policies/SnipePermissionsPolicy.php
@@ -45,24 +45,6 @@ abstract class SnipePermissionsPolicy
         if ($user->hasAccess('admin')) {
             return true;
         }
-
-        /**
-         * Proxy the authorization for custom fieldsets down to custom fields.
-         * This allows us to use the existing permissions in use and have more 
-         * semantically correct authorization checks for custom fieldsets.
-         *
-         * See: https://github.com/snipe/snipe-it/pull/5795
-         */
-        if ($item instanceof CustomFieldset) {
-
-            // The permission is saved as "{column}.edit", but named "update" in the 
-            // authorization checks.
-            if ($ability == 'update') {
-                $ability = 'edit';
-            }
-
-            return $user->hasAccess('customfields.' . $ability);
-        }
     }
 
     public function index(User $user)
