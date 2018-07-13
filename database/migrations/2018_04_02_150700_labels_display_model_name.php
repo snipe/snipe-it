b4542d4d42ba6457c2c8c53b5cb7f28ae3c4259c
snipe <snipe@snipe.net>
2018-07-12 21:49:44 -0700

    Fix migration for models on labels

--- database/migrations/2018_04_02_150700_labels_display_model_name.php
+++ database/migrations/2018_04_02_150700_labels_display_model_name.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class LabelsDisplayCompanyName extends Migration
+class LabelsDisplayModelName extends Migration
 {
     /**
      * Run the migrations.
