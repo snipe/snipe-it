bd581d01a35adc2cfbf32f0d8747c8f70730d09c
snipe <snipe@snipe.net>
2018-07-19 10:39:12 -0700

    Use full row width for image field

--- resources/views/partials/forms/edit/image-upload.blade.php
+++ resources/views/partials/forms/edit/image-upload.blade.php
@@ -1,6 +1,6 @@
 <div class="form-group {{ $errors->has('image') ? 'has-error' : '' }}">
     <label class="col-md-3 control-label" for="image">{{ trans('general.image_upload') }}</label>
-    <div class="col-md-5">
+    <div class="col-md-9">
         <label class="btn btn-default">
             {{ trans('button.select_file')  }}
             <input type="file" name="image" id="uploadFile" data-maxsize="{{ \App\Helpers\Helper::file_upload_max_size() }}" accept="image/gif,image/jpeg,image/png,image/svg" style="display:none">
