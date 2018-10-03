db385e024bee4b57b07a614668982b80e08b96ce
snipe <snipe@snipe.net>
2018-10-03 13:04:25 -0700

    Possible proxy issue fix

--- config/trustedproxy.php
+++ config/trustedproxy.php
@@ -63,11 +63,12 @@ return [
      * We may also want to add something like:
      * \Illuminate\Http\Request::HEADER_CLIENT_PROTO => 'X_FORWARDED_SCHEME',
      */
+    // These are defaults already set in the config:
     'headers' => [
+        (defined('Illuminate\Http\Request::HEADER_FORWARDED') ? Illuminate\Http\Request::HEADER_FORWARDED : 'forwarded') => 'FORWARDED',
         \Illuminate\Http\Request::HEADER_CLIENT_IP    => 'X_FORWARDED_FOR',
         \Illuminate\Http\Request::HEADER_CLIENT_HOST  => 'X_FORWARDED_HOST',
         \Illuminate\Http\Request::HEADER_CLIENT_PROTO => 'X_FORWARDED_PROTO',
         \Illuminate\Http\Request::HEADER_CLIENT_PORT  => 'X_FORWARDED_PORT',
-
     ]
 ];
