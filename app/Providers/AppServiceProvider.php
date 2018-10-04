de76e8db5fdf06544c7b387e3cda94cb86f0f0ba
snipe <snipe@snipe.net>
2018-10-04 12:11:27 -0700

    Re-enable rollbar

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -56,9 +56,9 @@ class AppServiceProvider extends ServiceProvider
         $log_level = config('app.log_level');
 
         if (($this->app->environment('production'))  && (config('services.rollbar.access_token'))){
-            //$this->app->register(\Rollbar\Laravel\RollbarServiceProvider::class);
+            $this->app->register(\Rollbar\Laravel\RollbarServiceProvider::class);
         }
-
+        
         foreach ($monolog->getHandlers() as $handler) {
             $handler->setLevel($log_level);
         }
