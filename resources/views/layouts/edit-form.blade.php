3843764d15c9069aeac0964d466aa329bdea646d
snipe <snipe@snipe.net>
2018-10-04 20:24:53 -0700

    Added save button at top of form as well

--- resources/views/layouts/edit-form.blade.php
+++ resources/views/layouts/edit-form.blade.php
@@ -23,36 +23,38 @@
 
 <div class="row">
     <div class="col-md-8 col-md-offset-2">
+        <form id="create-form" class="form-horizontal" method="post" action="{{ (isset($formAction)) ? $formAction : \Request::url()  }}" autocomplete="off" role="form" enctype="multipart/form-data">
+
         <div class="box box-default">
             <div class="box-header with-border">
-            <h3 class="box-title">
+            <h3 class="box-title" style="min-height: 20px;">
             @if ($item->id)
             {{ $item->display_name }}
             @endif
             </h3>
+                <div class="box-tools pull-right">
                 @if (isset($helpText))
-                    <div class="box-tools pull-right">
-                        <button class="slideout-menu-toggle btn btn-box-tool btn-box-tool-lg" data-toggle="tooltip" title="Help"><i class="fa fa-question"></i></button>
-                    </div>
+                    <button class="slideout-menu-toggle btn btn-box-tool btn-box-tool-lg" data-toggle="tooltip" title="Help"><i class="fa fa-question"></i></button>
                 @endif
+                 @include('partials.forms.edit.submit-button')
+                </div>
             </div><!-- /.box-header -->
 
             <div class="box-body">
-                <form id="create-form" class="form-horizontal" method="post" action="{{ (isset($formAction)) ? $formAction : \Request::url()  }}" autocomplete="off" role="form" enctype="multipart/form-data">
 
                     @if ($item->id)
                     {{ method_field('PUT') }}
                     @endif
 
-
                     <!-- CSRF Token -->
                     {{ csrf_field() }}
                     @yield('inputFields')
                     @include('partials.forms.edit.submit')
-                </form>
+
             </div>
         </div>
     </div>
+    </form>
 
     @if ((isset($helpText)) && (isset($helpTitle)))
     <div class="slideout-menu">
