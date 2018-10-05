6f71abd0e0bea68afdc45e8a64aa0164768dd4a0
snipe <snipe@snipe.net>
2018-10-05 00:45:58 -0700

    Fixed improper html nesting

--- resources/views/layouts/edit-form.blade.php
+++ resources/views/layouts/edit-form.blade.php
@@ -21,11 +21,16 @@
 
 @section('content')
 
+<!-- row -->
 <div class="row">
+    <!-- col-md-8 -->
     <div class="col-md-8 col-md-offset-2">
+
         <form id="create-form" class="form-horizontal" method="post" action="{{ (isset($formAction)) ? $formAction : \Request::url()  }}" autocomplete="off" role="form" enctype="multipart/form-data">
 
+        <!-- box -->
         <div class="box box-default">
+            <!-- box-header -->
             <div class="box-header with-border">
 
             <h3 class="box-title" style="min-height: 20px;">
@@ -48,6 +53,7 @@
                 @endif
             </div><!-- /.box-header -->
 
+            <!-- box-body -->
             <div class="box-body">
 
                     @if ($item->id)
@@ -59,10 +65,11 @@
                     @yield('inputFields')
                     @include('partials.forms.edit.submit')
 
-            </div>
-        </div>
-    </div>
-    </form>
-</div>
+            </div> <!-- ./box-body -->
+        </div> <!-- box -->
+        </form>
+    </div> <!-- col-md-8 -->
+
+</div><!-- ./row -->
 
 @stop
