476b58632b0c9cdecab29bbf84f9adce934c64aa
snipe <snipe@snipe.net>
2018-10-05 04:34:47 -0700

    Handle array of asset tags
    TODO: figure out how to display validation errors more sanely

--- resources/views/hardware/edit.blade.php
+++ resources/views/hardware/edit.blade.php
@@ -23,7 +23,7 @@
       <!-- we are editing an existing asset -->
       @if  ($item->id)
           <div class="col-md-7 col-sm-12{{  (\App\Helpers\Helper::checkIfRequired($item, 'asset_tag')) ? ' required' : '' }}">
-          <input class="form-control" type="text" name="asset_tag" id="asset_tag" value="{{ Input::old('asset_tag', $item->asset_tag) }}" data-validation="required">
+          <input class="form-control" type="text" name="asset_tags[1]" id="asset_tag" value="{{ Input::old('asset_tag', $item->asset_tag) }}" data-validation="required">
           </div>
       @else
           <!-- we are creating a new asset - let people use more than one asset tag -->
