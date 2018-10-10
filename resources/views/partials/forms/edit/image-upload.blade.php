c7596e77419c66d9c1f7eeb81a06a5eeb436af9b
snipe <snipe@snipe.net>
2018-10-09 17:31:52 -0700

    Fixed image not uploading on asset create

--- resources/views/partials/forms/edit/image-upload.blade.php
+++ resources/views/partials/forms/edit/image-upload.blade.php
@@ -10,7 +10,7 @@
         <p class="help-block" id="upload-file-status">{{ trans('general.image_filetypes_help', ['size' => \App\Helpers\Helper::file_upload_max_size_readable()]) }}</p>
         {!! $errors->first('image', '<span class="alert-msg">:message</span>') !!}
     </div>
-    <div class="col-md-4">
+    <div class="col-md-4 col-md-offset-3">
         <img id="imagePreview" style="max-width: 200px;">
     </div>
 </div>
