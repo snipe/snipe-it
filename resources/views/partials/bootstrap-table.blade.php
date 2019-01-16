c055e3af21a5cb2f4bfb8575f6cbb4e023515bfe
snipe <snipe@snipe.net>
2019-01-16 02:19:35 -0800

    Only try to return the asset tag link if a valid asset id has been passed

--- resources/views/partials/bootstrap-table.blade.php
+++ resources/views/partials/bootstrap-table.blade.php
@@ -435,7 +435,11 @@
     }
 
     function assetTagLinkFormatter(value, row) {
-        return '<a href="{{ url('/') }}/hardware/' + row.asset.id + '"> ' + row.asset.asset_tag + '</a>';
+        if ((row.asset) && (row.asset.id)) {
+            return '<a href="{{ url('/') }}/hardware/' + row.asset.id + '"> ' + row.asset.asset_tag + '</a>';
+        }
+        return '';
+
     }
 
     function assetNameLinkFormatter(value, row) {
