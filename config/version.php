c0669150fbdcb6c8cf8896cc043bb215781987c9
snipe <snipe@snipe.net>
2019-02-12 23:48:31 -0800

    Bumped point version

--- config/version.php
+++ config/version.php
@@ -1,10 +1,10 @@
 <?php
 return array (
-  'app_version' => 'v4.6.8',
-  'full_app_version' => 'v4.6.8 - build 3959-g194d0733d',
-  'build_version' => '3959',
+  'app_version' => 'v4.6.9',
+  'full_app_version' => 'v4.6.9 - build 3968-gf3c12f38b',
+  'build_version' => '3968',
   'prerelease_version' => '',
-  'hash_version' => 'g194d0733d',
-  'full_hash' => 'v4.6.8-14-g194d0733d',
+  'hash_version' => 'gf3c12f38b',
+  'full_hash' => 'v4.6.9-8-gf3c12f38b',
   'branch' => 'master',
 );
