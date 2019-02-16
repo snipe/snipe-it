e2834fab90b9808580845249f6e2491f9c13980c
snipe <snipe@snipe.net>
2019-02-16 11:48:20 -0800

    Bumped minor version

--- config/version.php
+++ config/version.php
@@ -1,10 +1,10 @@
 <?php
 return array (
-  'app_version' => 'v4.6.11',
-  'full_app_version' => 'v4.6.11 - build 3980-gb26fbf986',
-  'build_version' => '3980',
+  'app_version' => 'v4.6.12',
+  'full_app_version' => 'v4.6.12 - build 3982-gd687e1d76',
+  'build_version' => '3982',
   'prerelease_version' => '',
-  'hash_version' => 'gb26fbf986',
-  'full_hash' => 'v4.6.11-4-gb26fbf986',
+  'hash_version' => 'gd687e1d76',
+  'full_hash' => 'v4.6.12-1-gd687e1d76',
   'branch' => 'master',
 );
