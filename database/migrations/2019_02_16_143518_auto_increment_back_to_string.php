d687e1d762675f61e2bb11804bd654e4342d5fce
snipe <snipe@snipe.net>
2019-02-16 11:37:05 -0800

    Fixed #6725 - revert auro_increment_prefix to string
    This was mis-migrated as boolean.
    
    It’s essentially a duplicate migration, as it is fixed in-place on the existing migration, and then a new migratio was created to handle anyone who already upgraded.

--- database/migrations/2019_02_16_143518_auto_increment_back_to_string.php
+++ database/migrations/2019_02_16_143518_auto_increment_back_to_string.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class ChangeAutoIncrementPrefixToNullable extends Migration
+class AutoIncrementBackToString extends Migration
 {
     /**
      * Run the migrations.
