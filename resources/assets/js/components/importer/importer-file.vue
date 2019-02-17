636d396a3c73966065c2a6219e37ff5206107c7f
smb <sb@sbuehl.com>
2019-02-17 16:33:29 +0100

    Remove duplicate style attribute causing webpack to fail

--- resources/assets/js/components/importer/importer-file.vue
+++ resources/assets/js/components/importer/importer-file.vue
@@ -82,9 +82,8 @@
                      <br><br>
                  </div>
 
-                <div class="alert col-md-12" style="padding-top: 20px;"
+                <div class="alert col-md-12" style="padding-top: 20px; text-align:left; "
                      :class="alertClass"
-                     style="text-align:left"
                      v-if="statusText">
                     {{ this.statusText }}
                 </div>
