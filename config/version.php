53db96edadd556aa75b37bd56261059a561c2234
snipe <snipe@snipe.net>
2019-02-22 13:24:45 -0800

    Bumped minor version

--- config/version.php
+++ config/version.php
@@ -1,10 +1,10 @@
 <?php
 return array (
-  'app_version' => 'v4.6.12',
-  'full_app_version' => 'v4.6.12 - build 3982-gd687e1d76',
-  'build_version' => '3982',
+  'app_version' => 'v4.6.13',
+  'full_app_version' => 'v4.6.13 - build 3985-g5f3147cf3',
+  'build_version' => '3985',
   'prerelease_version' => '',
-  'hash_version' => 'gd687e1d76',
-  'full_hash' => 'v4.6.12-1-gd687e1d76',
+  'hash_version' => 'g5f3147cf3',
+  'full_hash' => 'v4.6.13-2-g5f3147cf3',
   'branch' => 'master',
 );
