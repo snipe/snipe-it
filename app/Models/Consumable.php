3b41aa52c88da47e2a4734efa43fc9a21f551d91
snipe <snipe@snipe.net>
2019-03-05 23:20:16 -0800

    Added ability to search consumables by item number
    Fixes ch1086

--- app/Models/Consumable.php
+++ app/Models/Consumable.php
@@ -76,7 +76,7 @@ class Consumable extends SnipeModel
      * 
      * @var array
      */
-    protected $searchableAttributes = ['name', 'order_number', 'purchase_cost', 'purchase_date'];
+    protected $searchableAttributes = ['name', 'order_number', 'purchase_cost', 'purchase_date', 'item_no'];
 
     /**
      * The relations and their attributes that should be included when searching the model.
