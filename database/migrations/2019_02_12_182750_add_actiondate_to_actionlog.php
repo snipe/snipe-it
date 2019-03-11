6e28902401a8bb6fe224899ab0a882e7452993d2
Ivan Nieto Vivanco <inietov@gmail.com>
2019-03-11 13:01:09 -0600

    Updating the action_date taking the value from the action_log table

--- database/migrations/2019_02_12_182750_add_actiondate_to_actionlog.php
+++ database/migrations/2019_02_12_182750_add_actiondate_to_actionlog.php
@@ -16,6 +16,12 @@ class AddActionDateToActionlog extends Migration
         Schema::table('action_logs', function (Blueprint $table) {
             $table->date('action_date')->nullable()->default(null);
         });
+
+        $results = DB::table('action_logs')->select('created_at', 'id')->where('action_type', 'checkout')->orWhere('action_type', 'checkin from')->get();
+
+        foreach($results as $result){
+            $update = DB::update('update '.DB::getTablePrefix().'action_logs set action_date=? where id=?', [$result->created_at, $result->id]);
+        }
     }
 
     /**
