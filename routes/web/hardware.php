ab060288faa07d1d10dd77a5ca420d52171194a7
snipe <snipe@snipe.net>
2019-05-05 19:13:37 -0400

    Controller/API methods for due/overdue audits
    We could probably skip this and just handle it via view in the routes…

--- routes/web/hardware.php
+++ routes/web/hardware.php
@@ -29,6 +29,16 @@ Route::group(
             'uses' => 'AssetsController@scan'
         ]);
 
+        Route::get('audit/due', [
+            'as' => 'assets.audit.due',
+            'uses' => 'AssetsController@dueForAudit'
+        ]);
+
+        Route::get('audit/overdue', [
+            'as' => 'assets.audit.overdue',
+            'uses' => 'AssetsController@overdueForAudit'
+        ]);
+
         Route::get('audit/{id}', [
             'as' => 'asset.audit.create',
             'uses' => 'AssetsController@audit'
