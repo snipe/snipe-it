7f86f929010c58f39eeb3033ee3df404351a5b34
snipe <snipe@snipe.net>
2019-05-13 02:16:47 -0700

    Cleaned up comments, added more clarification to what the script does

--- app/Console/Commands/ImportLocations.php
+++ app/Console/Commands/ImportLocations.php
@@ -39,8 +39,7 @@ class ImportLocations extends Command
      */
     public function handle()
     {
-        // Import parent location names first if they don't exist
-        // 2019-05-13-021051-locationscsv
+
 
         if (!ini_get("auto_detect_line_endings")) {
             ini_set("auto_detect_line_endings", '1');
@@ -48,26 +47,28 @@ class ImportLocations extends Command
 
         $filename = $this->argument('filename');
         $csv = Reader::createFromPath(storage_path('private_uploads/imports/').$filename, 'r');
-
         $this->info('Attempting to process: '.storage_path('private_uploads/imports/').$filename);
         $csv->setOffset(1); //because we don't want to insert the header
         $results = $csv->fetchAssoc();
 
+        // Import parent location names first if they don't exist
         foreach ($results as $parent_index => $parent_row) {
 
             $parent_name = trim($parent_row['Parent Name']);
             // First create any parents if they don't exist
 
             if ($parent_name!='') {
+
+                // Save parent location name
+                // This creates a sort of name-stub that we'll update later on in this script
                 $parent_location = Location::firstOrCreate(array('name' => $parent_name));
                 $this->info('Parent for '.$parent_row['Name'].' is '.$parent_name.'. Attempting to save '.$parent_name.'.');
 
-
-                // Save parent location name
+                // Check if the record was updated or created.
+                // This is mostly for clearer debugging.
                 if ($parent_location->exists) {
                         $this->info('- Parent location '.$parent_name.' already exists.');
                 } else {
-
                     $this->info('- Parent location '.$parent_name.' was created.');
                 }
 
@@ -77,8 +78,11 @@ class ImportLocations extends Command
 
         }
 
+        // Loop through ALL records and add/update them if there are additional fields
+        // besides name
         foreach ($results as $index => $row) {
 
+            // Set the location attributes to save
             $location = Location::firstOrNew(array('name' => trim($row['Name'])));
             $location->name = trim($row['Name']);
             $location->currency = trim($row['Currency']);
@@ -92,21 +96,26 @@ class ImportLocations extends Command
 
             $this->info('Checking location: '.$location->name);
 
-
+            // If a parent name nis provided, we created it earlier in the script,
+            // so let's grab that ID
             if ($parent_name) {
                 $parent = Location::where('name', '=', $parent_name)->first();
                 $location->parent_id = $parent->id;
                 $this->info('Parent ID: '.$parent->id);
             }
 
+            // Make sure the more advanced (non-name) fields pass validation
             if (($location->isValid()) && ($location->save())) {
 
+                // Check if the record was updated or created.
+                // This is mostly for clearer debugging.
                 if ($location->exists) {
                     $this->info('Location ' . $location->name . ' already exists. Updating...');
                 } else {
                     $this->info('- Location '.$location->name.' was created. ');
                 }
 
+            // If there's a validation error, display that
             } else {
                 $this->error('- Non-parent Location '.$location->name.' could not be created: '.$location->getErrors() );
             }
