33b59d7bed2a2dc24a285ec29530af327c5c108d
snipe <snipe@snipe.net>
2019-05-15 16:45:07 -0700

    Bumped version

--- config/version.php
+++ config/version.php
@@ -1,10 +1,10 @@
 <?php
 return array (
-  'app_version' => 'v4.6.16',
-  'full_app_version' => 'v4.6.16 - build 4018-gce16eae50',
-  'build_version' => '4018',
+  'app_version' => 'v4.6.17',
+  'full_app_version' => 'v4.6.17 - build 4026-g888bdbdb6',
+  'build_version' => '4026',
   'prerelease_version' => '',
-  'hash_version' => 'gce16eae50',
-  'full_hash' => 'v4.6.16-6-gce16eae50',
-  'branch' => 'features/6204_email_audit-alerts',
+  'hash_version' => 'g888bdbdb6',
+  'full_hash' => 'v4.6.17-7-g888bdbdb6',
+  'branch' => 'master',
 );
