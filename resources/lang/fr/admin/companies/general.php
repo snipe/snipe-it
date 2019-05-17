71006ea0edb68b52e0a0e14100e149d0247b3e71
Emmanuel Andry <eandry@free.fr>
2019-05-17 16:05:10 +0200

    Update general.php

--- resources/lang/fr/admin/companies/general.php
+++ resources/lang/fr/admin/companies/general.php
@@ -2,5 +2,5 @@
 return [
     'about_companies_title'            => 'A propos des entreprises',
     'about_companies_text'                  => 'Les entreprises peuvent être utilisées comme simple champ d\'identification, ou bien afin de limité la visibilité des items, utilisateurs etc... Si le support complet de l\'entreprise est activé dans les paramètres d\'administration.',
-    'select_company' => 'Sélectionnez une compagnie',
+    'select_company' => 'Sélectionnez une entreprise',
 ];
