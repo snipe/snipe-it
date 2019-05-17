7202988a8c2262a391a9bc6ad9944c87d5f0fda1
Emmanuel Andry <eandry@free.fr>
2019-05-17 15:25:05 +0200

    Update general.php

--- resources/lang/fr/admin/accessories/general.php
+++ resources/lang/fr/admin/accessories/general.php
@@ -2,7 +2,7 @@
 
 return array(
     'about_accessories_title' 			=> 'À propos des accessoires',
-    'about_accessories_text'  			=> 'Les accessoires sont des items que vous assignerai à des utilisateurs mais qui n\'ont pas de numéro de série (ou que vous ne désirez pas en faire un suivi). Par exemple, une souris ou un clavier.',
+    'about_accessories_text'  			=> 'Les accessoires sont des items que vous assignerez à des utilisateurs mais qui n\'ont pas de numéro de série (ou que vous ne désirez pas en faire un suivi). Par exemple, une souris ou un clavier.',
     'accessory_category' 				=> 'Catégorie d\'accessoire',
     'accessory_name'  					=> 'Nom de l\'accessoire',
     'checkout'  							=> 'Attribuer l\'accessoire',
@@ -14,7 +14,7 @@ return array(
     'require_acceptance'				=> 'L\'utilisateur doit confirmer qu\'il accepte les items dans cette catégorie.',
     'no_default_eula'					=> 'Aucune licence d\'utilisation par défaut trouvée. Ajoutez-en une dans la section "Réglages".',
     'total'  							=> 'Total',
-    'remaining'  						=> 'Dispo',
+    'remaining'  						=> 'Disponible',
     'update'  							=> 'Mise à jour d\'accessoires',
     'use_default_eula'					=> 'Utiliser la <a href="#" data-toggle="modal" data-target="#eulaModal">license primaire par défaut </a> à la place.',
     'use_default_eula_disabled'			=> '<del>Utilisez la license par défaut à la place.</del> Aucune licence d\'utilisation par défaut trouvée. Ajoutez-en une dans la section "Préférences".',
