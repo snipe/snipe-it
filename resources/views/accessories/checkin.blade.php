e037fd1e969e9daf92886ff6c8b42569d9b4dd0e
Ryan Meyers <ryanmeyersweb@gmail.com>
2019-05-22 15:44:43 -0500

    Fix accessory checkin

--- resources/views/accessories/checkin.blade.php
+++ resources/views/accessories/checkin.blade.php
@@ -30,9 +30,7 @@
                         </div><!-- /.box-header -->
                     @endif
 
-                    <div class="box-body">
-
-                        <form class="form-horizontal" method="post" action="" autocomplete="off">
+                            <div class="box-body">
                             <!-- CSRF Token -->
                             <input type="hidden" name="_token" value="{{ csrf_token() }}" />
 
@@ -54,12 +52,28 @@
                                             {!! $errors->first('note', '<span class="alert-msg"><i class="fa fa-times"></i> :message</span>') !!}
                                         </div>
                                     </div>
+                            <!-- Checkout/Checkin Date -->
+                            <div class="form-group{{ $errors->has('checkin_at') ? ' has-error' : '' }}">
+                                {{ Form::label('checkin_at', trans('admin/hardware/form.checkin_date'), array('class' => 'col-md-3 control-label')) }}
+                                <div class="col-md-8">
+                                    <div class="input-group col-md-5 required">
+                                        <div class="input-group date" data-provide="datepicker" data-date-format="yyyy-mm-dd" data-date-end-date="0d" data-autoclose="true">
+                                            <input type="text" class="form-control" placeholder="{{ trans('general.select_date') }}" name="checkin_at" id="checkin_at" value="{{ Input::old('checkin_at', date('Y-m-d')) }}">
+                                            <span class="input-group-addon"><i class="fa fa-calendar"></i></span>
+                                        </div>
+                                        {!! $errors->first('checkin_at', '<span class="alert-msg"><i class="fa fa-times"></i> :message</span>') !!}
+                                    </div>
+                                </div>
+                            </div>
+
                               </div>
                         <div class="box-footer">
                             <a class="btn btn-link" href="{{ URL::previous() }}">{{ trans('button.cancel') }}</a>
                             <button type="submit" class="btn btn-success pull-right"><i class="fa fa-check icon-white"></i>
                                 {{ trans('general.checkin') }}</button>
                         </div>
+
+
                 </div> <!-- .box.box-default -->
             </form>
         </div> <!-- .col-md-9-->
