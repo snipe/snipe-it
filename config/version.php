03cf3b5431c75753dbe6fa5cfc023f33355c230f
snipe <snipe@snipe.net>
2019-05-28 19:12:42 -0700

    Bumped release again :(

--- config/version.php
+++ config/version.php
@@ -1,10 +1,10 @@
 <?php
 return array (
-  'app_version' => 'v4.7.1',
+  'app_version' => 'v4.7.2',
   'full_app_version' => 'v4.7.1 - build 4037',
   'build_version' => '4037',
   'prerelease_version' => '',
   'hash_version' => '',
-  'full_hash' => 'v4.7.1',
+  'full_hash' => 'v4.7.2',
   'branch' => 'master',
 );
