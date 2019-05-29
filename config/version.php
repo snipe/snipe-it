db76090e10edf1470519525182bbe76a46ae740b
snipe <snipe@snipe.net>
2019-05-28 19:12:53 -0700

    Missed one

--- config/version.php
+++ config/version.php
@@ -1,7 +1,7 @@
 <?php
 return array (
   'app_version' => 'v4.7.2',
-  'full_app_version' => 'v4.7.1 - build 4037',
+  'full_app_version' => 'v4.7.2 - build 4037',
   'build_version' => '4037',
   'prerelease_version' => '',
   'hash_version' => '',
