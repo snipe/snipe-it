6966c132d0f1480ea5a049d255d5cf5c80b1c74d
snipe <snipe@snipe.net>
2019-05-31 12:17:09 -0700

    Update language strings

--- resources/lang/sv-SE/admin/hardware/general.php
+++ resources/lang/sv-SE/admin/hardware/general.php
@@ -3,7 +3,7 @@
 return array(
     'about_assets_title'           => 'Om tillgångar',
     'about_assets_text'            => 'Tillgångar är poster som spåras med serienummer eller tillgångstagg. De tenderar att vara mer värdefulla saker där identifiering av en viss sak är viktig.',
-	'archived'  				=> 'arkiveras',
+	'archived'  				=> 'Arkiverade',
     'asset'  					=> 'Tillgång',
     'bulk_checkout'             => 'Checkout tillgångar',
     'checkin'  					=> 'Checkin Asset',
@@ -16,7 +16,7 @@ return array(
     'requestable'               => 'Requestable',
     'requested'				    => 'Begärda',
     'restore'  					=> 'Återställ tillgången',
-    'pending'  					=> 'Avvaktan',
+    'pending'  					=> 'Väntande',
     'undeployable'  			=> 'Undeployable',
     'view'  					=> 'Visa tillgång',
 );
