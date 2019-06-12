2bfa05fd2d9bc5af0286beb3522b614555a6e8b3
snipe <snipe@snipe.net>
2019-06-12 16:24:55 -0700

    Back in time fix FOR #7145 for new installs on MySQL 8+

--- database/migrations/2019_06_12_184327_rename_groups_table.php
+++ database/migrations/2019_06_12_184327_rename_groups_table.php
@@ -13,7 +13,21 @@ class RenameGroupsTable extends Migration
      */
     public function up()
     {
-        Schema::rename('groups', 'permission_groups');
+        // We check to see if this table exists before attempting the migration since
+        // upgraded installs would have this table, but new installs wouldn't.
+        // We had to change the name of the table in the older migrations
+        // to handle a MySQl 8+ compatibility issue related to reserved words.
+        // Without going back in time in migrations, this would fail since the groups table
+        // would never be allowed to be created in the first place on MySql 8+.
+        //
+        // So... if an upgrade, let's rename that table.
+        // If a new install, the migration was already changed, so the table isn't
+        // called that anymore and we can skip this migration.
+
+        if (Schema::hasTable('groups')) {
+            Schema::rename('groups', 'permission_groups');
+        }
+
     }
 
     /**
@@ -23,6 +37,8 @@ class RenameGroupsTable extends Migration
      */
     public function down()
     {
-        Schema::rename('permission_groups', 'groups');
+        if (Schema::hasTable('permission_groups')) {
+            Schema::rename('permission_groups', 'groups');
+        }
     }
 }
