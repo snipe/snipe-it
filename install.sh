6bb6a10ce9ec5a1b4c0191eb4b7c105ad21e5c20
luism123mb <36648228+luism123mb@users.noreply.github.com>
2019-06-18 15:42:41 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,8 @@ if [ "$(id -u)" != "0" ]; then
     else
         exec sudo "$0" "$@"
     fi
+fi
+
 wget https://raw.githubusercontent.com/luism123mb/snipe-it/master/snipeit.sh
 chmod 744 snipeit.sh
 ./snipeit.sh 2>&1 | tee -a /var/log/snipeit-install.log
