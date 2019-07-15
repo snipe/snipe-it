d409be6d43579b840ac3056b755cf7643c35edc6
Ivan Nieto <inietov@gmail.com>
2019-07-15 15:02:44 -0500

    Fix #6910: Add logic to manipulate the eloquent query. (#7006)
    * Added company_id to consumables_users table
    
    * Added logic to manage when a pivot table doesn't have the column company_id trough a join with users
    
    * Remove a migration that tries to fix this problem, but is not longer necessary

--- app/Http/Controllers/Api/ConsumablesController.php
+++ app/Http/Controllers/Api/ConsumablesController.php
@@ -165,7 +165,7 @@ class ConsumablesController extends Controller
     {
         $consumable = Consumable::with(array('consumableAssignments'=>
         function ($query) {
-            $query->orderBy('created_at', 'DESC');
+            $query->orderBy($query->getModel()->getTable().'.created_at', 'DESC');
         },
         'consumableAssignments.admin'=> function ($query) {
         },
