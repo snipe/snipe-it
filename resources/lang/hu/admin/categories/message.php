7493cd843ca2faf4e460d4016fd2a0d8d0c76040
bcs78 <bcs78@protonmail.com>
2019-10-03 13:18:22 +0200

    Update message.php
    fixing translation errors

--- resources/lang/hu/admin/categories/message.php
+++ resources/lang/hu/admin/categories/message.php
@@ -3,22 +3,22 @@
 return array(
 
     'does_not_exist' => 'A kategória nem létezik.',
-    'assoc_models'	 => 'Ez a kategória jelenleg legalább egy modellhez kapcsolódik, és nem törölhető. Kérjük, frissítse a modelleket, hogy ne hivatkozzon erre a kategóriára, és próbálja újra.',
-    'assoc_items'	 => 'Ez a kategória jelenleg legalább egy: asset_type-hez van társítva, és nem törölhető. Kérjük, frissítse a: asset_type-t, hogy ne hivatkozzon erre a kategóriára és próbálja újra.',
+    'assoc_models'	 => 'Legalább egy modell tartozik ehhez a kategóriához, ezért nem törölhető. Kérjük, módosítsa a kapcsolódó modelleket, hogy ne hivatkozzon erre a kategóriára, majd próbálja újra.',
+    'assoc_items'	 => 'Legalább egy :asset_type van társítva ehhez a kategóriához, ezért nem törölhető. Kérjük, módosítsa az eszközt, hogy ne hivatkozzon erre a kategóriára, mnajd próbálja újra.',
 
     'create' => array(
-        'error'   => 'Nem sikerült a kategória létrehozása, kérjük, próbálja újra.',
+        'error'   => 'Nem sikerült a kategória létrehozása. Kérjük, próbálja újra.',
         'success' => 'Sikeresen létrehozta a kategóriát.'
     ),
 
     'update' => array(
-        'error'   => 'Nem sikerült a kategória módosítása, kérjük, próbálja újra',
+        'error'   => 'Nem sikerült a kategória módosítása. Kérjük, próbálja újra',
         'success' => 'Sikeresen módosította a kategóriát.'
     ),
 
     'delete' => array(
         'confirm'   => 'Biztos benne, hogy törölni szeretné a kategóriát?',
-        'error'   => 'A kategória törlése közben probléma merült fel, kérjük, próbálja újra.',
+        'error'   => 'A kategória törlése közben probléma merült fel. Kérjük, próbálja újra.',
         'success' => 'Sikeresen törölte a kategóriát.'
     )
 
