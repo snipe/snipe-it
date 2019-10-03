552eaf40e2653a431af28fa4c8c0c9c307d0bcf9
bcs78 <bcs78@protonmail.com>
2019-10-03 12:25:55 +0200

    Update message.php
    Fixing errors. Adding missing 2FA translations.

--- resources/lang/hu/auth/message.php
+++ resources/lang/hu/auth/message.php
@@ -7,11 +7,23 @@ return array(
     'account_not_activated'  => 'Ez a felhasználó nincs aktiválva.',
     'account_suspended'      => 'Ez a felhasználói fiók fel van függesztve.',
     'account_banned'         => 'Ez a felhasználó ki van tiltva.',
-    'throttle'               => 'Túl sok sikertelen bejelentkezési kísérlet. Kérjük, próbáld újra körül: perc perc (s).',
+    'throttle'               => 'Túl sok sikertelen bejelentkezési kísérlet. Kérjük, :seconds másodperc múlva próbálja újra.',
 
+    'two_factor' => array(
+        'already_enrolled'      => 'Ez az eszköz már regisztrálva van.',
+        'success'               => 'Sikeresen bejelentkezett.',
+        'code_required'         => 'Két lépcsős azonosítás szükséges.',
+        'invalid_code'          => 'A megadott kód érvényletelen.',
+    ),
+    
     'signin' => array(
         'error'   => 'Bejelentkezés közben probléma lépett fel, kérjük próbálja újra.',
         'success' => 'Sikeresen bejelentkezett.',
+    ),
+    
+    'logout' => array(
+        'error'   => 'Kijelentkezés közben probléma lépett fel, kérjük próbálja újra.',
+        'success' => 'Sikeresen kijelentkezett.',
     ),
 
     'signup' => array(
@@ -19,15 +31,15 @@ return array(
         'success' => 'A felhasználói fiók sikeresen elkészült.',
     ),
 
-        'forgot-password' => array(
-            'error'   => 'A jelszó visszaállító kód kiküldése közben probléma lépett fel, kérjük próbálja újra.',
-            'success' => 'A jelszó visszaállító emailt sikeresen elküldtük.',
-        ),
+    'forgot-password' => array(
+        'error'   => 'A jelszó visszaállító email kiküldése közben probléma lépett fel, kérjük próbálja újra.',
+        'success' => 'A jelszó visszaállító emailt sikeresen elküldtük.',
+    ),
 
-        'forgot-password-confirm' => array(
-            'error'   => 'Jelszó helyreállítás közben probléma lépett fel, kérjük próbálja újra.',
-            'success' => 'A jelszavát sikeresen helyreállította.',
-        ),
+    'forgot-password-confirm' => array(
+        'error'   => 'Jelszó helyreállítás közben probléma lépett fel, kérjük próbálja újra.',
+        'success' => 'A jelszavát sikeresen helyreállította.',
+    ),
 
     'activate' => array(
         'error'   => 'A felhasználói fiók aktiválása közben probléma lépett fel, kérjük próbálja újra!',
