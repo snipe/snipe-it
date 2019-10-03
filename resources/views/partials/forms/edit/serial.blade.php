61cb134732e7aaf0d34db51d9b74e07f3694fa26
Cahyadi Nugraha <cahyadinugraha18@gmail.com>
2019-10-03 11:02:31 +0700

    Added unique class identifier (rather than using ID)

--- resources/views/partials/forms/edit/serial.blade.php
+++ resources/views/partials/forms/edit/serial.blade.php
@@ -1,5 +1,5 @@
 <!-- Serial -->
-<div class="form-group {{ $errors->has('serial') ? ' has-error' : '' }}">
+<div class="form-group form-serial-group {{ $errors->has('serial') ? ' has-error' : '' }}">
     <label for="{{ $fieldname }}" class="col-md-3 control-label">{{ trans('admin/hardware/form.serial') }} </label>
     <div class="col-md-7 col-sm-12{{  (\App\Helpers\Helper::checkIfRequired($item, 'serial')) ? ' required' : '' }}">
         <input class="form-control" type="text" name="{{ $fieldname }}" id="serial" value="{{ Input::old('serial', $item->serial) }}" />
