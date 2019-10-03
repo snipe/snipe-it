af988f8562305adfd73a9cc8ac039fd848446444
bcs78 <bcs78@protonmail.com>
2019-10-03 13:14:05 +0200

    Update general.php

--- resources/lang/hu/admin/accessories/general.php
+++ resources/lang/hu/admin/accessories/general.php
@@ -10,13 +10,13 @@ return array(
     'create'  							=> 'Tartozék létrehozása',
     'edit'  							=> 'Tartozék szerkesztése',
     'eula_text'							=> 'Kategóriához tartozó felhasználási feltételek',
-    'eula_text_help'					=> 'Itt adhatja meg a kategóriához tartozó eszközök felhasználási feltételeit. Ha ehhez a kategóriához nem akar külön feltételeket megadni, akkor jelölje be a négyzetet lent, hogy az alapértelmezettet használja.',
-    'require_acceptance'				=> 'A felhasználónak kötelező elfogadnia az eszköz átvételét ebben a kategóriában.',
-    'no_default_eula'					=> 'Nincsen alapértelmezett felhasználási feltétel felrögzítve. Adjon hozzá egyet a Beállításokban!',
+    'eula_text_help'					=> 'Itt adhatja meg a kategóriához tartozó eszközök felhasználási szabályait. Ha ehhez a kategóriához nem akar külön szabályokat megadni, akkor jelölje be a négyzetet lent, hogy az alapértelmezettet használja.',
+    'require_acceptance'				=> 'A felhasználónak el kell fogadnia a kategóriához tartozó eszköz átvételét.',
+    'no_default_eula'					=> 'Nincsen alapértelmezett felhasználási szabály felrögzítve. Adjon hozzá egyet a Beállításokban!',
     'total'  							=> 'Összesen',
     'remaining'  						=> 'Maradék',
     'update'  							=> 'Tartozék frissítése',
-    'use_default_eula'					=> 'Használja inkább az <a href="#" data-toggle="modal" data-target="#eulaModal">alapértelmezett felhasználási feltételeket</a>-t.',
-    'use_default_eula_disabled'			=> '<del>Használja inkább az alapértelmezett felhasználási feltételeket.</del> Nincs alapértelmezett felhasználási feltétel felrögzítve. Kérem adjon hozzá egyet a Beállításokban!',
+    'use_default_eula'					=> 'Használja inkább az <a href="#" data-toggle="modal" data-target="#eulaModal">alapértelmezett felhasználási szabályt</a>.',
+    'use_default_eula_disabled'			=> '<del>Használja inkább az alapértelmezett felhasználási szabályt.</del> Nincs alapértelmezett felhasználási szabály felrögzítve. Kérem adjon hozzá egyet a Beállításokban!',
 
 );
