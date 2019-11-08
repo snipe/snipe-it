478628100e8bbdbffad9c5a2b6d8e7410aabfc0b
jacobsen9026 <jacobsen9026@gmail.com>
2019-11-08 08:38:04 -0500

    Add Checkout on Audit Setting
    Save checkout on audit setting when saving. Default value 0.
    
    The database needs the checkout_on_audit column added to the Settings table during install. I am unsure how.

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -327,6 +327,7 @@ class SettingsController extends Controller
 
         $setting->full_multiple_companies_support = $request->input('full_multiple_companies_support', '0');
         $setting->unique_serial                   = $request->input('unique_serial', '0');
+	$setting->checkout_on_audit = $request->input('checkout_on_audit','0');
         $setting->show_images_in_email            = $request->input('show_images_in_email', '0');
         $setting->show_archived_in_list           = $request->input('show_archived_in_list', '0');
         $setting->dashboard_message               = $request->input('dashboard_message');
