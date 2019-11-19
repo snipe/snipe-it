0a820a59938437d8b6b9905811c9a83db3004501
snipe <snipe@snipe.net>
2019-11-18 22:30:21 -0800

    New translations general.php (Turkish)

--- tr/admin/asset_maintenances/general.php
+++ tr/admin/asset_maintenances/general.php
@@ -1,14 +1,14 @@
 <?php
 
     return [
-        'asset_maintenances' => 'Asset Maintenances',
-        'edit'               => 'Edit Asset Maintenance',
-        'delete'             => 'Delete Asset Maintenance',
-        'view'               => 'View Asset Maintenance Details',
-        'repair'             => 'Repair',
-        'maintenance'        => 'Maintenance',
-        'upgrade'            => 'Upgrade',
-        'calibration'        => 'Calibration',
-        'software_support'   => 'Software Support',
-        'hardware_support'   => 'Hardware Support',
+        'asset_maintenances' => 'Varlık Bakımları',
+        'edit'               => 'Varlık Bakımını Düzenle',
+        'delete'             => 'Varlık Bakımını Sil',
+        'view'               => 'Varlık Bakım Detaylarını Gör',
+        'repair'             => 'Onarım',
+        'maintenance'        => 'Bakım',
+        'upgrade'            => 'İyileştirme',
+        'calibration'        => 'Kalibrasyon',
+        'software_support'   => 'Yazılım Desteği',
+        'hardware_support'   => 'Donanım Desteği',
     ];
