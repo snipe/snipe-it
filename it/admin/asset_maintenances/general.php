a54f9eb7f671c2f939c0eda4cbb7e231c9547232
snipe <snipe@snipe.net>
2019-11-19 02:11:41 -0800

    New translations general.php (Italian)

--- it/admin/asset_maintenances/general.php
+++ it/admin/asset_maintenances/general.php
@@ -1,14 +1,14 @@
 <?php
 
     return [
-        'asset_maintenances' => 'Asset Maintenances',
-        'edit'               => 'Edit Asset Maintenance',
-        'delete'             => 'Delete Asset Maintenance',
-        'view'               => 'View Asset Maintenance Details',
-        'repair'             => 'Repair',
-        'maintenance'        => 'Maintenance',
-        'upgrade'            => 'Upgrade',
-        'calibration'        => 'Calibration',
-        'software_support'   => 'Software Support',
-        'hardware_support'   => 'Hardware Support',
+        'asset_maintenances' => 'Manutenzione Prodotto',
+        'edit'               => 'Modifica Manutenzione Prodotto',
+        'delete'             => 'Elimina Manutenzione Prodotto',
+        'view'               => 'Visualizza Dettagli Manutenzione Prodotto',
+        'repair'             => 'Ripara',
+        'maintenance'        => 'Manutenzione',
+        'upgrade'            => 'Aggiorna',
+        'calibration'        => 'Calibrazione',
+        'software_support'   => 'Supporto software',
+        'hardware_support'   => 'Supporto hardware',
     ];
