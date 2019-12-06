00ce5f57c76a712925b657b0b08b1cf10424caf1
snipe <snipe@snipe.net>
2019-12-06 11:39:58 -0800

    New translations general.php (Afrikaans)

---
+++ af/admin/hardware/general.php
@@ -0,0 +1,20 @@
+<?php
+
+return array(
+    'archived'  				=> 'Archived',
+    'asset'  					=> 'Asset',
+    'bulk_checkout'             => 'Checkout Assets',
+    'checkin'  					=> 'Checkin Asset',
+    'checkout'  				=> 'Checkout Asset',
+    'clone'  					=> 'Clone Asset',
+    'deployable'  				=> 'Deployable',
+    'deleted'  					=> 'This asset has been deleted. <a href="/hardware/:asset_id/restore">Click here to restore it</a>.',
+    'edit'  					=> 'Edit Asset',
+    'model_deleted'  			=> 'This Assets model has been deleted. You must restore the model before you can restore the Asset.<br/> <a href="/hardware/models/:model_id/restore">Click here to restore the model</a>.',
+    'requestable'               => 'Requestable',
+    'requested'				    => 'Requested',
+    'restore'  					=> 'Restore Asset',
+    'pending'  					=> 'Pending',
+    'undeployable'  			=> 'Undeployable',
+    'view'  					=> 'View Asset',
+);
