7236eab5616cb1674cab7e875d931d183fbe89d1
snipe <snipe@snipe.net>
2019-12-06 11:40:10 -0800

    New translations general.php (Afrikaans)

---
+++ af/admin/kits/general.php
@@ -0,0 +1,16 @@
+<?php
+
+return array(
+    'about_kits'                        => 'About Predefined Kits',
+    'checkout'                          => 'Checkout Kit ',
+    'create_success'                    => 'Kit was successfully created.',
+    'create'                            => 'Create Predefined Kit',
+    'update'                            => 'Update Predefined Kit',
+    'delete_success'                    => 'Kit was successfully deleted.',
+    'update_success'                    => 'Kit was successfully updated.',
+    'none_models'                       => 'There are not enough available assets for :model to checkout. :qty are required. ',
+    'none_licenses'                     => 'There are not enough available seats for :license to checkout. :qty are required. ',
+    'none_consumables'                  => 'There are not enough available units of :consumable to checkout. :qty are required. ',
+    'none_accessory'                    => 'There are not enough available units of :accessory to checkout. :qty are required. ',
+
+);
