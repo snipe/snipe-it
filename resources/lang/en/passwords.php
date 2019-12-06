ecd9d576d681678dd6a96d834d59d17d298f2cc0
Godfrey Martinez <godfrey@Grokability-Godfrey.local>
2019-12-05 17:26:49 -0800

    added a proper response for password errors

--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -3,5 +3,8 @@
 return [
     'sent'	        => 'Your password link has been sent!',
     'user'			=> 'No matching active user found with that email.',
+    'token'         => 'The password reset token you have provided is invalid',
+    'password'      => 'Passwords must be at least six characters and match the confirmation.',
+    'reset'         => 'Your password has been reset'
 ];
 
