a437bcc9517010bbc1c57f5e5b5d71cdfcf71308
snipe <snipe@snipe.net>
2020-03-05 17:30:51 -0800

    Added fieldname variable to blade partial for image upload

--- resources/views/partials/forms/edit/image-upload.blade.php
+++ resources/views/partials/forms/edit/image-upload.blade.php
@@ -1,9 +1,9 @@
-<div class="form-group {{ $errors->has('image') ? 'has-error' : '' }}">
+<div class="form-group {{ $errors->has((isset($fieldname) ? $fieldname : 'image')) ? 'has-error' : '' }}">
     <label class="col-md-3 control-label" for="image">{{ trans('general.image_upload') }}</label>
     <div class="col-md-9">
         <label class="btn btn-default">
             {{ trans('button.select_file')  }}
-            <input type="file" name="image" class="js-uploadFile" id="uploadFile" data-maxsize="{{ \App\Helpers\Helper::file_upload_max_size() }}" accept="image/gif,image/jpeg,image/png,image/svg" style="display:none; max-width: 90%">
+            <input type="file" name="{{ (isset($fieldname) ? $fieldname : 'image')  }}" class="js-uploadFile" id="uploadFile" data-maxsize="{{ \App\Helpers\Helper::file_upload_max_size() }}" accept="image/gif,image/jpeg,image/png,image/svg,image/svg+xml,image/bmp" style="display:none; max-width: 90%">
         </label>
         <span class='label label-default' id="uploadFile-info"></span>
 
