e72ebfb94b207a42e488636d3440a5758ce00fca
snipe <snipe@snipe.net>
2020-03-28 03:42:45 -0700

    Added placeholder text for modal header

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -814,7 +814,7 @@
             <div class="modal-content">
                 <div class="modal-header">
                     <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                    <h4 class="modal-title" id="myModalLabel"></h4>
+                    <h4 class="modal-title" id="myModalLabel">&nbps;</h4>
                 </div>
                 <div class="modal-body"></div>
                 <div class="modal-footer">
