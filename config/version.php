be79a1f3d62ce6c96e4de509bc0f292f5a11882e
snipe <snipe@snipe.net>
2020-04-02 19:27:41 -0700

    Bumped hash

--- config/version.php
+++ config/version.php
@@ -1,10 +1,10 @@
 <?php
 return array (
   'app_version' => 'v4.9.0',
-  'full_app_version' => 'v4.9.0 - build 4210-g8b2f8ef3c',
-  'build_version' => '4210',
+  'full_app_version' => 'v4.9.0 - build 4223-g031adc3be',
+  'build_version' => '4223',
   'prerelease_version' => '',
-  'hash_version' => 'g8b2f8ef3c',
-  'full_hash' => 'v4.9.0-21-g8b2f8ef3c',
+  'hash_version' => 'g031adc3be',
+  'full_hash' => 'v4.9.0-9-g031adc3be',
   'branch' => 'master',
 );
