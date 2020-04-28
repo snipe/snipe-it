61a216a531493f88fc29254e6cc2ea228b2afd46
Daniel Meltzer <dmeltzer.devel@gmail.com>
2020-04-28 12:14:23 -0400

    Rearrange seeder order so that users validate properly on first seed run.  This hopefully fixes the actionlog seeder error.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -23,11 +23,11 @@ class DatabaseSeeder extends Seeder
 
         $this->call(CompanySeeder::class);
         $this->call(CategorySeeder::class);
+        $this->call(LocationSeeder::class);
         $this->call(UserSeeder::class);
         $this->call(DepreciationSeeder::class);
         $this->call(DepartmentSeeder::class);
         $this->call(ManufacturerSeeder::class);
-        $this->call(LocationSeeder::class);
         $this->call(SupplierSeeder::class);
         $this->call(AssetModelSeeder::class);
         $this->call(DepreciationSeeder::class);
