d24eb5fc5c6cb9e4c991e1d1034f8eebc0314bc5
user.email <andresgutierrez535@gmail.com>
2020-06-17 21:51:27 -0400

    change throttle rate from 120/min to 100000/min

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -45,7 +45,7 @@ class Kernel extends HttpKernel
 
         'api' => [
             \Barryvdh\Cors\HandleCors::class,
-            'throttle:120,1',
+            'throttle:100000,1',
             'auth:api',
         ],
     ];
