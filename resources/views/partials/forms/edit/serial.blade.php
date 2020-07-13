8fd4529fad2cb7f02272ade483e7bc37e3a40539
Ivan Nieto <inietov@gmail.com>
2020-07-13 13:49:21 -0500

    Change the form input name for serials in the view

--- resources/views/partials/forms/edit/serial.blade.php
+++ resources/views/partials/forms/edit/serial.blade.php
@@ -2,7 +2,7 @@
 <div class="form-group {{ $errors->has('serial') ? ' has-error' : '' }}">
     <label for="{{ $fieldname }}" class="col-md-3 control-label">{{ trans('admin/hardware/form.serial') }} </label>
     <div class="col-md-7 col-sm-12{{  (\App\Helpers\Helper::checkIfRequired($item, 'serial')) ? ' required' : '' }}">
-        <input class="form-control" type="text" name="serial" id="serial" value="{{ old('serial', $item->serial) }}" />
+        <input class="form-control" type="text" name="{{ $fieldname }}" id="{{ $fieldname }}" value="{{ old('serial', $item->serial) }}" />
         {!! $errors->first('serial', '<span class="alert-msg" aria-hidden="true"><i class="fa fa-times" aria-hidden="true"></i> :message</span>') !!}
     </div>
 </div>
