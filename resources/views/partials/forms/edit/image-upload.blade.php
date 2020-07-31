502b3c50a9742fc7af8888c216f84476f7847666
snipe <snipe@snipe.net>
2020-07-31 14:36:50 -0700

    Fixed upload preview (fixed regression from #6736)
    This change appears to have been stomped on in one of the merges. :(

--- resources/views/partials/forms/edit/image-upload.blade.php
+++ resources/views/partials/forms/edit/image-upload.blade.php
@@ -6,15 +6,15 @@
 
         <label class="btn btn-default" aria-hidden="true">
             {{ trans('button.select_file')  }}
-            <input type="file" name="image" id="uploadFile" data-maxsize="{{ \App\Helpers\Helper::file_upload_max_size() }}" accept="image/gif,image/jpeg,image/png,image/svg" style="display:none" aria-label="image" aria-hidden="true">
+            <input type="file" name="image" class="js-uploadFile" id="uploadFile" data-maxsize="{{ \App\Helpers\Helper::file_upload_max_size() }}" accept="image/gif,image/jpeg,image/png,image/svg" style="display:none; max-width: 90%" aria-label="image" aria-hidden="true">
         </label>
         <span class='label label-default' id="uploadFile-info"></span>
 
-        <p class="help-block" id="upload-file-status">{{ trans('general.image_filetypes_help', ['size' => \App\Helpers\Helper::file_upload_max_size_readable()]) }}</p>
+        <p class="help-block" id="uploadFile-status">{{ trans('general.image_filetypes_help', ['size' => \App\Helpers\Helper::file_upload_max_size_readable()]) }}</p>
         {!! $errors->first('image', '<span class="alert-msg" aria-hidden="true">:message</span>') !!}
     </div>
     <div class="col-md-4 col-md-offset-3" aria-hidden="true">
-        <img id="imagePreview" style="max-width: 200px; display: none;" alt="Uploaded image thumbnail">
+        <img id="uploadFile-imagePreview" style="max-width: 200px; display: none;" alt="Uploaded image thumbnail">
     </div>
 </div>
 
