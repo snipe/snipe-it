5b68a321a6f401d75effd045ada8e97879d734e5
snipe <snipe@snipe.net>
2020-09-08 18:15:45 -0700

    Added comapny formatter to asset maintenance report - fixes [ch15119]

--- resources/views/reports/asset_maintenances.blade.php
+++ resources/views/reports/asset_maintenances.blade.php
@@ -35,7 +35,7 @@
                         }'>
                 <thead>
                 <tr>
-                    <th data-field="company" data-sortable="false" data-visible="false">{{ trans('admin/companies/table.title') }}</th>
+                    <th data-field="company" data-sortable="false" data-visible="false" data-formatter="companiesLinkObjFormatter">{{ trans('admin/companies/table.title') }}</th>
                     <th data-sortable="true" data-field="id" data-visible="false">{{ trans('general.id') }}</th>
                     <th data-sortable="false" data-field="asset_name" data-formatter="assetNameLinkFormatter">{{ trans('admin/asset_maintenances/table.asset_name') }}</th>
                     <th data-sortable="false" data-field="supplier" data-formatter="suppliersLinkObjFormatter">{{ trans('general.supplier') }}</th>
