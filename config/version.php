24a52eaeaec36eab6610236e53795abd00579bdb
snipe <snipe@snipe.net>
2020-09-12 10:04:36 -0700

    Bumped hash

--- config/version.php
+++ config/version.php
@@ -1,10 +1,10 @@
 <?php
 return array (
   'app_version' => 'v5.0.0-beta-3',
-  'full_app_version' => 'v5.0.0-beta-3 - build 4352-gec723a3da',
-  'build_version' => '4190',
+  'full_app_version' => 'v5.0.0-beta-3 - build 4482-g5b68a321a',
+  'build_version' => '4482',
   'prerelease_version' => '',
-  'hash_version' => 'gec723a3da',
-  'full_hash' => 'v5.0.0-beta-3-gec723a3da',
+  'hash_version' => 'g5b68a321a',
+  'full_hash' => 'v5.0.0-beta-3-g5b68a321a',
   'branch' => 'develop',
 );
