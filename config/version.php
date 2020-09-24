b0e9875a50ff58fd1aa065b1443b4fbf8baed553
snipe <snipe@snipe.net>
2020-09-24 14:20:29 -0700

    Counting is hard. :(

--- config/version.php
+++ config/version.php
@@ -1,10 +1,10 @@
 <?php
 return array (
-  'app_version' => 'v5.0.0-RC4',
-  'full_app_version' => 'v5.0.0-RC4 - build 4483-',
+  'app_version' => 'v5.0.0-beta-5 RC',
+  'full_app_version' => 'v5.0.0-beta-5 RC - build 4483-',
   'build_version' => '4483',
   'prerelease_version' => '',
   'hash_version' => '',
-  'full_hash' => 'v5.0.0-RC4',
+  'full_hash' => 'v5.0.0-beta-5 RC',
   'branch' => 'develop',
 );
