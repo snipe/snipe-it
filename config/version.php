4dbf1f58e37f9d0bb74dcc2f9e4ab28274b6fdf7
snipe <snipe@snipe.net>
2020-10-06 19:11:34 -0700

    Bumped RC to GM

--- config/version.php
+++ config/version.php
@@ -1,10 +1,10 @@
 <?php
 return array (
-  'app_version' => 'v5.0.0-beta-5 RC',
-  'full_app_version' => 'v5.0.0-beta-5 RC - build 4483-',
+  'app_version' => 'v5.0.0-beta-6-GM',
+  'full_app_version' => 'v5.0.0-beta-6-GM - build 4483-',
   'build_version' => '4483',
   'prerelease_version' => '',
   'hash_version' => '',
-  'full_hash' => 'v5.0.0-beta-5 RC',
+  'full_hash' => 'v5.0.0-beta-6-GM',
   'branch' => 'develop',
 );
