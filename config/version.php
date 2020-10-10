2dc027e479c7e47399b8b823acdd31e625d6d96c
snipe <snipe@snipe.net>
2020-10-09 17:29:31 -0700

    Bumped GM version

--- config/version.php
+++ config/version.php
@@ -1,10 +1,10 @@
 <?php
 return array (
-  'app_version' => 'v5.0.0-beta-6-GM',
-  'full_app_version' => 'v5.0.0-beta-6-GM - build 4483-',
-  'build_version' => '4483',
+  'app_version' => 'v5.0.0-beta-7-GM',
+  'full_app_version' => 'v5.0.0-beta-7-GM - build 4484-g33cf4896d',
+  'build_version' => '4484',
   'prerelease_version' => '',
-  'hash_version' => '',
-  'full_hash' => 'v5.0.0-beta-6-GM',
+  'hash_version' => 'g33cf4896d',
+  'full_hash' => 'v5.0.0-beta-7-GM-1-g33cf4896d',
   'branch' => 'develop',
 );
