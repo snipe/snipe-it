a62e8413ba6a0f6155d6ce4825d6ad34436f3a23
snipe <snipe@snipe.net>
2020-10-19 16:36:25 -0700

    Updated version

--- config/version.php
+++ config/version.php
@@ -1,10 +1,10 @@
 <?php
 return array (
-  'app_version' => 'v5.0.0-beta-7-GM',
-  'full_app_version' => 'v5.0.0-beta-7-GM - build 4484-g33cf4896d',
+  'app_version' => 'v5.0.0',
+  'full_app_version' => 'v5.0.0 - build 4484-g33cf4896d',
   'build_version' => '4484',
   'prerelease_version' => '',
   'hash_version' => 'g33cf4896d',
-  'full_hash' => 'v5.0.0-beta-7-GM-1-g33cf4896d',
+  'full_hash' => 'v5.0.0-1-g33cf4896d',
   'branch' => 'develop',
 );
