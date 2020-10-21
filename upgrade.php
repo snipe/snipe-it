137982187601bdba858bcd5aedea9274d5726f72
snipe <snipe@snipe.net>
2020-10-21 12:02:25 -0700

    Removed accompanying method, since we don’t use it

--- upgrade.php
+++ upgrade.php
@@ -194,34 +194,3 @@ echo "your upgraded Snipe-IT.\n";
 echo "--------------------------------------------------------\n\n";
 
 
-/**
- * Recursively move files from one directory to another
- *
- * @param String $src - Source of files being moved
- * @param String $dest - Destination of files being moved
- */
-function rmove($src, $dest){
-
-    // If source is not a directory stop processing
-    if(!is_dir($src)) return false;
-
-    // If the destination directory does not exist create it
-    if(!is_dir($dest)) {
-        if(!mkdir($dest)) {
-            // If the destination directory could not be created stop processing
-            return false;
-        }
-    }
-
-    // Open the source directory to read in files
-    $i = new DirectoryIterator($src);
-    foreach($i as $f) {
-        if($f->isFile()) {
-            rename($f->getRealPath(), "$dest/" . $f->getFilename());
-        } else if(!$f->isDot() && $f->isDir()) {
-            rmove($f->getRealPath(), "$dest/$f");
-            unlink($f->getRealPath());
-        }
-    }
-    unlink($src);
-}
