0c2800c7ddabc73ff9c4d5f37bc483bda7b23ba0
snipe <snipe@snipe.net>
2020-11-02 21:26:13 -0800

    Removed debugging line

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -167,7 +167,6 @@ class ProfileController extends Controller
 
             // First let's see if that option is enabled in the settings
             if (strpos(Setting::passwordComplexityRulesSaving('store'), 'disallow_same_pwd_as_user_fields')) {
-                \Log::debug('disallow_same_pwd_as_user_fields is ON');
                 if (($request->input('password') == $user->username) ||
                     ($request->input('password') == $user->email) ||
                     ($request->input('password') == $user->first_name) ||
