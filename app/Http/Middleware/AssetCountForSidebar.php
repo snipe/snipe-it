973eacf6c3f03ab27562a5a99af32a131f029f40
snipe <snipe@snipe.net>
2020-11-24 13:51:02 -0800

    Small fixes for SAML
    The SAML routes are in a service provide (sigh), so they did not have the `web` middleware group assigned to it.
    
    I also added some additional checks so that the setup blade won’t fail (the migrations wouldn’t have been run yet, so outside of a try/catch, it would return an error since those tables don’t exist.)

--- app/Http/Middleware/AssetCountForSidebar.php
+++ app/Http/Middleware/AssetCountForSidebar.php
@@ -17,16 +17,41 @@ class AssetCountForSidebar
      */
     public function handle($request, Closure $next)
     {
-        $total_rtd_sidebar = Asset::RTD()->count();
-        $total_deployed_sidebar = Asset::Deployed()->count();
-        $total_archived_sidebar = Asset::Archived()->count();
-        $total_pending_sidebar = Asset::Pending()->count();
-        $total_undeployable_sidebar = Asset::Undeployable()->count();
-        view()->share('total_rtd_sidebar', $total_rtd_sidebar);
-        view()->share('total_deployed_sidebar', $total_deployed_sidebar);
-        view()->share('total_archived_sidebar', $total_archived_sidebar);
-        view()->share('total_pending_sidebar', $total_pending_sidebar);
-        view()->share('total_undeployable_sidebar', $total_undeployable_sidebar);
+        try
+        {
+            $total_rtd_sidebar = Asset::RTD()->count();
+            view()->share('total_rtd_sidebar', $total_rtd_sidebar);
+        } catch (\Exception $e) {
+            \Log::debug($e);
+        }
+
+        try {
+            $total_deployed_sidebar = Asset::Deployed()->count();
+            view()->share('total_deployed_sidebar', $total_deployed_sidebar);
+        } catch (\Exception $e) {
+            \Log::debug($e);
+        }
+
+        try {
+            $total_archived_sidebar = Asset::Archived()->count();
+            view()->share('total_archived_sidebar', $total_archived_sidebar);
+        } catch (\Exception $e) {
+            \Log::debug($e);
+        }
+
+        try {
+            $total_pending_sidebar = Asset::Pending()->count();
+            view()->share('total_pending_sidebar', $total_pending_sidebar);
+        } catch (\Exception $e) {
+            \Log::debug($e);
+        }
+
+        try {
+            $total_undeployable_sidebar = Asset::Undeployable()->count();
+            view()->share('total_undeployable_sidebar', $total_undeployable_sidebar);
+        } catch (\Exception $e) {
+            \Log::debug($e);
+        }
 
         return $next($request);
     }
