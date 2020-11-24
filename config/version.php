e9bfb157bb59de33addd04aba21bea7b6633a2c0
snipe <snipe@snipe.net>
2020-11-24 13:53:28 -0800

    Bumped version. Again.

--- config/version.php
+++ config/version.php
@@ -1,10 +1,10 @@
 <?php
 return array (
-  'app_version' => 'v5.0.8',
-  'full_app_version' => 'v5.0.8 - build 5616-8a38b9d',
+  'app_version' => 'v5.0.9',
+  'full_app_version' => 'v5.0.9 - build 5616-973eacf6c3',
   'build_version' => '5616',
   'prerelease_version' => '',
-  'hash_version' => '8a38b9d',
-  'full_hash' => 'v5.0.8-87-8a38b9d',
+  'hash_version' => '973eacf6c3',
+  'full_hash' => 'v5.0.9-87-973eacf6c3',
   'branch' => 'develop',
 );
