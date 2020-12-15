fb37dbed927e7c813ef3898fac1c1e934d8a9aad
Alexander Chibrikin <alek13.me@gmail.com>
2020-12-15 04:45:00 +0300

    use supported package for slack (#8867)
    * Updated version.php to master
    
    * use supported package for slack
    
    Co-authored-by: snipe <snipe@snipe.net>

--- config/version.php
+++ config/version.php
@@ -4,7 +4,7 @@ return array (
   'full_app_version' => 'v5.0.11 - build 5695-gfd4ee6027',
   'build_version' => '5695',
   'prerelease_version' => '',
-  'hash_version' => 'gfd4ee6027',
-  'full_hash' => 'v5.0.11-13-gfd4ee6027',
-  'branch' => 'develop',
+  'hash_version' => 'gf9b1fdc36',
+  'full_hash' => 'v5.0.10-21-gf9b1fdc36',
+  'branch' => 'master',
 );
