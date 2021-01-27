9ec0e0f81725fed0db933604d462d449a7382f88
snipe <snipe@snipe.net>
2021-01-27 14:29:13 -0800

    Split the view into tabs so we can combine asset depreciations and license depreciations

--- resources/views/depreciations/view.blade.php
+++ resources/views/depreciations/view.blade.php
@@ -3,8 +3,7 @@
 {{-- Page title --}}
 @section('title')
 
-    {{ trans('general.depreciation') }}
-    : {{ $depreciation->name }}
+    {{ trans('general.depreciation') }}: {{ $depreciation->name }}
 
     @parent
 @stop
@@ -25,28 +24,66 @@
 @section('content')
 
     <div class="row">
-        <div class="col-md-12">
-            <div class="box box-default">
-                <div class="box-body">
-                    <div class="row">
-                        <div class="col-md-12">
-                            <div class="table table-responsive">
-                                <table
-                                        name="location_users"
-                                        id="table-users"
-                                        class="table table-striped snipe-table"
-                                        data-url="{{ route('api.assets.index',['depreciation'=> $depreciation->id]) }}"
-                                        data-cookie="true"
-                                        data-click-to-select="true"
-                                        data-cookie-id-table="department_usersDetailTable">
-                                    <thead>
+        <div class="col-md-9">
 
-                                </table>
+
+            <!-- Custom Tabs -->
+            <div class="nav-tabs-custom">
+                <ul class="nav nav-tabs">
+                    <li class="active"><a href="#assets" data-toggle="tab">{{ trans('general.assets') }}</a></li>
+                    <li><a href="#licenses" data-toggle="tab">{{ trans('general.licenses') }}</a></li>
+                    </ul>
+
+                <div class="tab-content">
+
+                    <div class="tab-pane active" id="assets">
+
+
+                               sldkgjlksgj
+                    </div> <!-- end tab-pane -->
+
+                    <!-- tab-pane -->
+                    <div class="tab-pane" id="licenses">
+                        <div class="row">
+                            <div class="col-md-12">
+
+                                <div class="table-responsive">
+
+                                    <table
+                                            data-columns="{{ \App\Presenters\LicensePresenter::dataTableLayoutSeats() }}"
+                                            data-cookie-id-table="seatsTable"
+                                            data-id-table="seatsTable"
+                                            id="seatsTable"
+                                            data-pagination="true"
+                                            data-search="false"
+                                            data-side-pagination="server"
+                                            data-show-columns="true"
+                                            data-show-export="true"
+                                            data-show-refresh="true"
+                                            data-sort-order="asc"
+                                            data-sort-name="name"
+                                            class="table table-striped snipe-table"
+                                            data-url="{{ route('api.assets.index',['depreciation'=> $depreciation->id]) }}"
+                                            data-export-options='{
+                        "fileName": "export-depreciations-{{ date('Y-m-d') }}",
+                        "ignoreColumn": ["actions","image","change","checkbox","checkincheckout","icon"]
+                        }'>
+                                    </table>
+
+                                </div>
+
                             </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+
+                        </div> <!--/.row-->
+                    </div> <!-- /.tab-pane -->
+
+                </div> <!-- /.tab-content -->
+            </div> <!-- nav-tabs-custom -->
+
+
+        </div>
+        <div class="col-md-3">
+            <h2>Depreciation info:</h2>
         </div>
     </div>
 
