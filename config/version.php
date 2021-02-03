fdde844ce5933ad101a0d95158535d2cc0f8e4b0
snipe <snipe@snipe.net>
2021-02-02 21:48:42 -0800

    Bumped version to 5.1.0

--- config/version.php
+++ config/version.php
@@ -1,10 +1,10 @@
 <?php
 return array (
-  'app_version' => 'v5.0.13-pre',
-  'full_app_version' => 'v5.0.13-pre - build 5749-gd19c6ab8e',
-  'build_version' => '5749',
+  'app_version' => 'v5.1.0',
+  'full_app_version' => 'v5.1.0 - build 5772-g124343911',
+  'build_version' => '5772',
   'prerelease_version' => '',
-  'hash_version' => 'gd19c6ab8e',
-  'full_hash' => 'v5.0.13-pre-23-gd19c6ab8e',
-  'branch' => 'master',
+  'hash_version' => 'g124343911',
+  'full_hash' => 'v5.1.0-46-g124343911',
+  'branch' => 'develop',
 );
