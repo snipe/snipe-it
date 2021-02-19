a5731a3088fb934fa507292392cccddfc13c278b
snipe <snipe@snipe.net>
2021-02-19 10:09:22 -0800

    Updated hash

--- config/version.php
+++ config/version.php
@@ -1,10 +1,10 @@
 <?php
 return array (
-  'app_version' => 'v5.1.1',
-  'full_app_version' => 'v5.1.1 - build 5811-',
-  'build_version' => '5811',
+  'app_version' => 'v5.1.2-pre',
+  'full_app_version' => 'v5.1.2-pre - build 5831-g6449d0aaf',
+  'build_version' => '5831',
   'prerelease_version' => '',
-  'hash_version' => '',
-  'full_hash' => 'v5.1.1',
+  'hash_version' => 'g6449d0aaf',
+  'full_hash' => 'v5.1.2-pre-17-g6449d0aaf',
   'branch' => 'develop',
 );
