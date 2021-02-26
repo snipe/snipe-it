6373ef3283a414d1688423df49518a2813a42d9e
snipe <snipe@snipe.net>
2021-02-25 16:53:37 -0800

    Bumped version

--- config/version.php
+++ config/version.php
@@ -1,10 +1,10 @@
 <?php
 return array (
-  'app_version' => 'v5.1.2-pre',
-  'full_app_version' => 'v5.1.2-pre - build 5831-g6449d0aaf',
-  'build_version' => '5831',
+  'app_version' => 'v5.1.2',
+  'full_app_version' => 'v5.1.2 - build 5847-g00a7c1e9e',
+  'build_version' => '5847',
   'prerelease_version' => '',
-  'hash_version' => 'g6449d0aaf',
-  'full_hash' => 'v5.1.2-pre-17-g6449d0aaf',
+  'hash_version' => 'g00a7c1e9e',
+  'full_hash' => 'v5.1.2-33-g00a7c1e9e',
   'branch' => 'develop',
 );
