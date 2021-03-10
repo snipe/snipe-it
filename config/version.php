b0abdc922647a171fc38d3e5e804681f06462c2f
snipe <snipe@snipe.net>
2021-03-09 16:19:21 -0800

    Bumped point version

--- config/version.php
+++ config/version.php
@@ -1,10 +1,10 @@
 <?php
 return array (
   'app_version' => 'v5.1.3',
-  'full_app_version' => 'v5.1.3 - build 5847-g00a7c1e9e',
-  'build_version' => '5847',
+  'full_app_version' => 'v5.1.3 - build 5862-gc3871c98d',
+  'build_version' => '5862',
   'prerelease_version' => '',
-  'hash_version' => 'g00a7c1e9e',
-  'full_hash' => 'v5.1.3-33-g00a7c1e9e',
-  'branch' => 'develop',
+  'hash_version' => 'gc3871c98d',
+  'full_hash' => 'v5.1.3-12-gc3871c98d',
+  'branch' => 'master',
 );
