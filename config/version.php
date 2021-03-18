afb7fcfa3ef00ebdaca0e63d3287cac6f5f72077
snipe <snipe@snipe.net>
2021-03-17 20:02:54 -0700

    Bumped hash
    Signed-off-by: snipe <snipe@snipe.net>

--- config/version.php
+++ config/version.php
@@ -1,10 +1,10 @@
 <?php
 return array (
-  'app_version' => 'v5.1.2',
-  'full_app_version' => 'v5.1.2 - build 5847-g00a7c1e9e',
+  'app_version' => 'v5.1.3',
+  'full_app_version' => 'v5.1.3 - build 5847-g00a7c1e9e',
   'build_version' => '5847',
   'prerelease_version' => '',
   'hash_version' => 'g00a7c1e9e',
-  'full_hash' => 'v5.1.2-33-g00a7c1e9e',
+  'full_hash' => 'v5.1.3-33-g00a7c1e9e',
   'branch' => 'develop',
 );
