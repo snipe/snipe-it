2bc698585c4f8bebe28595d5372a44e7eddc84c3
Johannes Drexl <johannes.drexl@nfon.com>
2021-03-22 12:51:00 +0100

    Fixing #9224

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -78,7 +78,7 @@ final class Company extends SnipeModel
             $company_id = null;
         }
 
-        $table = ($table_name) ? DB::getTablePrefix().$table_name."." : '';
+        $table = ($table_name) ? $table_name."." : '';
 
         if(\Schema::hasColumn($query->getModel()->getTable(), $column)){
              return $query->where($table.$column, '=', $company_id); 
