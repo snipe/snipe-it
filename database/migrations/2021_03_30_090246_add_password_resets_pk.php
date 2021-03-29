6982505474494d5319f6d1b93207406728f7990b
Jelle Sebreghts <sebreghts.jelle@gmail.com>
2021-03-29 14:57:22 +0200

    Add primary key to migrations table if it doesn't exist yet
    Some mysql clusters (like Percona-XtraDB-Cluster) prohibit the use of DML
    commands on a table. It's also just a good practice to add it.
    
    See https://github.com/laravel/framework/pull/15770

---
+++ database/migrations/2021_03_30_090246_add_password_resets_pk.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddMigrationsPk extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('migrations', function (Blueprint $table) {
+            // Add the new id and batch columns to the migrations table if they
+            // don't exist yet
+            if (!Schema::hasColumn('migrations', 'id')) {
+                $table->increments('id');
+            }
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+    }
+}
