618e4439e272c8bafd66918c253231a9398d7070
snipe <snipe@snipe.net>
2021-03-31 14:02:10 -0700

    Fixed validation to include textarea
    Signed-off-by: snipe <snipe@snipe.net>

--- app/Models/CustomField.php
+++ app/Models/CustomField.php
@@ -359,7 +359,7 @@ class CustomField extends Model
             "name" => "required|unique:custom_fields",
             "element" => [
                 "required",
-                Rule::in(['text', 'listbox'])
+                Rule::in(['text', 'listbox',  'textara', 'checkbox', 'radio'])
             ],
             'format' => [
                 Rule::in(array_merge(array_keys(CustomField::PREDEFINED_FORMATS), CustomField::PREDEFINED_FORMATS))
