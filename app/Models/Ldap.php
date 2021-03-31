62a6fe2c5f1e39fa98da49d55702619be4197a5f
andreaci <andreaci@users.noreply.github.com>
2021-03-31 16:05:40 +0200

    Set default filter on LDAP search
    Without this parameter, search fails.

--- app/Models/Ldap.php
+++ app/Models/Ldap.php
@@ -264,6 +264,10 @@ class Ldap extends Model
             if ($filter != '' && substr($filter, 0, 1) != '(') { // wrap parens around NON-EMPTY filters that DON'T have them, for back-compatibility with AdLdap2-based filters
                 $filter = "($filter)";
             }
+            else if ($filter == '') {
+				$filter = "(cn=*)";
+            }
+            
             $search_results = ldap_search($ldapconn, $base_dn, $filter);
 
             if (!$search_results) {
