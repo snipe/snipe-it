8000e18c121ae3f0277cbd40f3fee2893a8af65e
Steven Lietaer <nevets82@gmail.com>
2021-04-23 19:19:04 +0200

    Removed unused file;

--- resources/lang/en/admin/locations/general.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-return array(
-    'edit'              => 'Edit Location',
-    'info'              => 'Info',
-    'include_child_locations' => 'Include Child Locations',
-    'exclude_child_locations' => 'Exclude Child Locations',
-    );
