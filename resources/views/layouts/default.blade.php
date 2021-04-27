23376c317ecd29c4a9a9c147f31b127ad8e8b15e
snipe <snipe@snipe.net>
2021-04-26 17:22:02 -0700

    Use correct URL for css for subdirs
    Signed-off-by: snipe <snipe@snipe.net>

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -30,7 +30,7 @@
     {{-- stylesheets --}}
     <link rel="stylesheet" href="{{ url(mix('css/dist/all.css')) }}">
     @if (($snipeSettings) && ($snipeSettings->allow_user_skin==1) && Auth::check() && Auth::user()->present()->skin != '')
-        <link rel="stylesheet" href="{{ mix('css/dist/skins/skin-'.Auth::user()->present()->skin.'.min.css') }}">
+        <link rel="stylesheet" href="{{ url(mix('css/dist/skins/skin-'.Auth::user()->present()->skin.'.min.css')) }}">
     @else
     <link rel="stylesheet" href="{{ url(mix('css/dist/skins/skin-'.($snipeSettings->skin!='' ? $snipeSettings->skin : 'blue').'.css')) }}">
     @endif
