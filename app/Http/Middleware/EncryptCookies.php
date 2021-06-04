fe03974689a327ac8044429654d75bc9da3040c8
snipe <snipe@snipe.net>
2021-06-03 23:56:53 -0700

    Updated Excrypot cookies middleware
    Signed-off-by: snipe <snipe@snipe.net>

--- app/Http/Middleware/EncryptCookies.php
+++ app/Http/Middleware/EncryptCookies.php
@@ -2,26 +2,18 @@
 
 namespace App\Http\Middleware;
 
-use Illuminate\Cookie\Middleware\EncryptCookies as BaseEncrypter;
+use Illuminate\Cookie\Middleware\EncryptCookies as Middleware;
 
-class EncryptCookies extends BaseEncrypter
+class EncryptCookies extends Middleware
 {
-
     /**
      * The names of the cookies that should not be encrypted.
      *
      * @var array
      */
     protected $except = [
-        //
+        
     ];
 
-    /**
-     * Indicates if cookies should be serialized.
-     *
-     * @var bool
-     */
     protected static $serialize = null;
-
-
 }
