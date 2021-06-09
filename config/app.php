3cca98dd32bffb3e1ea1a82a00b1b9e4e9c9d4f5
snipe <snipe@snipe.net>
2021-06-09 15:00:59 -0700

    Updated composer
    Signed-off-by: snipe <snipe@snipe.net>

--- config/app.php
+++ config/app.php
@@ -371,6 +371,7 @@ return [
     'aliases' => [
 
         'App' => Illuminate\Support\Facades\App::class,
+        'Arr' => Illuminate\Support\Arr::class,
         'Artisan' => Illuminate\Support\Facades\Artisan::class,
         'Auth' => Illuminate\Support\Facades\Auth::class,
         'Blade' => Illuminate\Support\Facades\Blade::class,
