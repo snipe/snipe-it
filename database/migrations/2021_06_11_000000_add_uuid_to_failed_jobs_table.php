f54a0f88b9b063776eea9b169e9cc5debe4eafb7
Laravel Shift <shift@laravelshift.com>
2021-06-11 15:21:58 +0000

    Add `uuid` to `failed_jobs` table

---
+++ database/migrations/2021_06_11_000000_add_uuid_to_failed_jobs_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Str;
+
+class AddUuidToFailedJobsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('failed_jobs', function (Blueprint $table) {
+            $table->string('uuid')->after('id')->nullable()->unique();
+        });
+
+        DB::table('failed_jobs')->whereNull('uuid')->cursor()->each(function ($job) {
+            DB::table('failed_jobs')
+                ->where('id', $job->id)
+                ->update(['uuid' => (string)Str::uuid()]);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('failed_jobs', function (Blueprint $table) {
+            $table->dropColumn('uuid');
+        });
+    }
+}
