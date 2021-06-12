226b208f7c8daeecf4e59f53aa28359ab5e3e76c
snipe <snipe@snipe.net>
2021-06-11 22:19:10 -0700

    Reset handler to stock for now
    Signed-off-by: snipe <snipe@snipe.net>

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -13,14 +13,7 @@ class Handler extends ExceptionHandler
      * @var array
      */
     protected $dontReport = [
-        \Illuminate\Auth\AuthenticationException::class,
-        \Illuminate\Auth\Access\AuthorizationException::class,
-        \Symfony\Component\HttpKernel\Exception\HttpException::class,
-        \Illuminate\Database\Eloquent\ModelNotFoundException::class,
-        \Illuminate\Session\TokenMismatchException::class,
-        \Illuminate\Validation\ValidationException::class,
-        \Intervention\Image\Exception\NotSupportedException::class,
-        \League\OAuth2\Server\Exception\OAuthServerException::class,
+        //
     ];
 
     /**
@@ -41,49 +34,8 @@ class Handler extends ExceptionHandler
      */
     public function register()
     {
-        
-
         $this->reportable(function (Throwable $e) {
-
-            // CSRF token mismatch error
-            if ($e instanceof \Illuminate\Session\TokenMismatchException) {
-                return redirect()->back()->with('error', trans('general.token_expired'));
-            }
-
-
-            // Handle Ajax requests that fail because the model doesn't exist
-            if ($request->ajax() || $request->wantsJson()) {
-                
-
-                if ($e instanceof \Illuminate\Database\Eloquent\ModelNotFoundException) {
-                    $className = last(explode('\\', $e->getModel()));
-                    return response()->json(Helper::formatStandardApiResponse('error', null, $className . ' not found'), 200);
-                }
-
-                if ($this->isHttpException($e)) {
-
-                    $statusCode = $e->getStatusCode();
-
-                    switch ($e->getStatusCode()) {
-                        case '404':
-                        return response()->json(Helper::formatStandardApiResponse('error', null, $statusCode . ' endpoint not found'), 404);
-                        case '405':
-                            return response()->json(Helper::formatStandardApiResponse('error', null, 'Method not allowed'), 405);
-                        default:
-                            return response()->json(Helper::formatStandardApiResponse('error', null, $statusCode), 405);
-
-                    }
-                }
-            }
-
-
-            if ($this->isHttpException($e) && (isset($statusCode)) && ($statusCode == '404' )) {
-                return response()->view('layouts/basic', [
-                    'content' => view('errors/404')
-                ],$statusCode);
-            }
-
-            return parent::render($request, $e);
-            });
+            //
+        });
     }
 }
