dce1dd41c8b821a7bdf26588524db7a48b800709
Brady Wetherington <bwetherington@grokability.com>
2021-06-28 16:38:05 -0700

    Regenerate assets after repairing Vue integration

--- resources/assets/js/vue.js
+++ resources/assets/js/vue.js
@@ -13,27 +13,27 @@ require('./bootstrap');
  */
 Vue.component(
     'passport-clients',
-    require('./components/passport/Clients.vue')
+    require('./components/passport/Clients.vue').default
 );
 
 Vue.component(
     'passport-authorized-clients',
-    require('./components/passport/AuthorizedClients.vue')
+    require('./components/passport/AuthorizedClients.vue').default
 );
 
 Vue.component(
     'passport-personal-access-tokens',
-    require('./components/passport/PersonalAccessTokens.vue')
+    require('./components/passport/PersonalAccessTokens.vue').default
 );
 
 Vue.component(
     'importer',
-    require('./components/importer/importer.vue')
+    require('./components/importer/importer.vue').default
 );
 
 Vue.component(
     'fieldset-default-values',
-    require('./components/forms/asset-models/fieldset-default-values.vue')
+    require('./components/forms/asset-models/fieldset-default-values.vue').default
 );
 
 // Commented out currently to avoid trying to load vue everywhere.
