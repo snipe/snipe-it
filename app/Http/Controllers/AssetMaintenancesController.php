d917ae51b708051331add163a7b72faedadc0e50
Ivan Nieto Vivanco <inietov@gmail.com>
2021-08-31 04:01:20 -0500

    Remove e() function from other input fields

--- app/Http/Controllers/AssetMaintenancesController.php
+++ app/Http/Controllers/AssetMaintenancesController.php
@@ -209,9 +209,9 @@ class AssetMaintenancesController extends Controller
             return static::getInsufficientPermissionsRedirect();
         }
 
-        $assetMaintenance->supplier_id = e($request->input('supplier_id'));
-        $assetMaintenance->is_warranty = e($request->input('is_warranty'));
-        $assetMaintenance->cost =  Helper::ParseFloat(e($request->input('cost')));
+        $assetMaintenance->supplier_id = $request->input('supplier_id');
+        $assetMaintenance->is_warranty = $request->input('is_warranty');
+        $assetMaintenance->cost =  Helper::ParseFloat($request->input('cost'));
         $assetMaintenance->notes = $request->input('notes');
 
         $asset = Asset::find(request('asset_id'));
