dd36774da3cd31acc908e6a0bf88bb26924b7100
Godfrey M <godmartinz@gmail.com>
2021-08-31 16:49:06 -0700

    updated repo, added all fields to Depreciation Reports Presenter

---
+++ app/Http/Transformers/DepreciationReportTransformer.php
@@ -0,0 +1,51 @@
+<?php
+namespace App\Http\Transformers;
+
+use App\Helpers\Helper;
+use App\Models\Asset;
+use Gate;
+use Illuminate\Database\Eloquent\Collection;
+
+class DepreciationsReportTransformer
+{
+
+    public function transformDepreciations (Collection $depreciations)
+    {
+
+        $reportsArray = array();
+        foreach ($depreciations as $depreciatedAsset)  {
+            $reportsArray[] = self::transformDepreciationReport($depreciatedAsset);
+
+        }
+        return (new DatatablesTransformer)->transformDatatables(($reportsArray));
+    }
+
+    public function transformDepreciationReport ($depreciatedAsset)
+    {
+        $array = [
+            'company_name'          => e($depreciatedAsset->company->name),
+            'id'                    => (int) $depreciatedAsset->id,
+            'model'                 => $depreciatedAsset->model->category->name,
+            'deleted_at'            => $depreciatedAsset->deleted_at,
+            'asset_tag'             => $depreciatedAsset->asset_tag,
+            'asset_name'            => $depreciatedAsset->model->name,
+            'serial'                => $depreciatedAsset->serial,
+            'depreciation_type'     => $depreciatedAsset->model->depreciation->name,
+            'depreciation_length'   => $depreciatedAsset->model->depreciation->months,
+            'asset_status'          => $depreciatedAsset->assetstatus->name,
+            'asset_checkedOut'      => $depreciatedAsset->checkedOutToUser(),
+            'asset_assigned'        => $depreciatedAsset->assigned,
+            'asset_fullNameAtt'     => $depreciatedAsset->assigned->getFullNameAttribute(),
+            'asset_assigned_name'   => $depreciatedAsset->assigned->name,
+            'location'              => $depreciatedAsset->location->name,
+            'default_loc'           => $depreciatedAsset->defaultloc->name,
+            'purchase_date'         => Carbon\Carbon::parse($depreciatedAsset->purchase_date)->format('Y-m-d'),
+            'eol'                   => $depreciatedAsset->present()->eol_date(),
+            'purchase_cost'         => $depreciatedAsset->purchase_cost,
+            'location_currency'     => $depreciatedAsset->location->currency,
+            'current_value'         => $depreciationAsset->getDepreciatedValue(),
+
+        ];
+        return $array;
+    }
+}
---
+++ app/Presenters/DepreciationReportPresenter.php
@@ -0,0 +1,164 @@
+<?php
+
+namespace App\Presenters;
+
+/**
+ * Class DepreciationPresenter
+ * @package App\Presenters
+ */
+class DepreciationPresenter extends Presenter
+{
+    /**
+     * Json Column Layout for bootstrap table
+     * @return string
+     */
+    public static function dataTableLayout()
+    {
+
+        $layout = [
+            [
+                "field" => "company_name",
+                "searchable" => false,
+                "sortable" => true,
+                "switchable" => true,
+                "title" => trans('admin/companies/table.title'),
+                "visible" => false
+            ], [
+                "field" => "id",
+                "searchable" => true,
+                "sortable" => true,
+                "title" => trans('admin/categories/general.category_name'),
+                "visible" => true,
+
+            ],
+
+            [
+                "field" => "asset_tag",
+                "searchable" => true,
+                "sortable" => true,
+                "title" =>  trans('admin/hardware/table.asset_tag'),
+                "visible" => true,
+            ],
+            [
+                "field" => "asset_name",
+                "searchable" => false,
+                "sortable" => false,
+                "switchable" => false,
+                "title" => trans('general.name'),
+                "visible" => false,
+
+            ],
+            [
+                "field" => "serial",
+                "searchable" => false,
+                "sortable" => false,
+                "switchable" => false,
+                "title" => trans('admin/hardware/table.serial'),
+                "visible" => true,
+
+            ],
+            [
+                "field" => "depreciation_type",
+                "searchable" => false,
+                "sortable" => false,
+                "switchable" => false,
+                "title" => trans('admin/depreciations/general.depreciation_name'),
+                "visible" => true,
+
+            ],
+            [
+                "field" => "depreciation_length",
+                "searchable" => false,
+                "sortable" => false,
+                "switchable" => false,
+                "title" => trans('admin/depreciations/general.number_of_months'),
+                "visible" => true,
+
+            ],
+            [
+                "field" => "asset_status",
+                "searchable" => false,
+                "sortable" => false,
+                "switchable" => false,
+                "title" => trans('admin/hardware/table.status'),
+                "visible" => true,
+
+            ],
+            [
+                "field" => "asset_checkedOut",
+                "searchable" => false,
+                "sortable" => false,
+                "switchable" => false,
+                "title" => trans('admin/hardware/table.checkoutto'),
+                "visible" => true,
+
+            ],
+            [
+                "field" => "location",
+                "searchable" => false,
+                "sortable" => false,
+                "switchable" => false,
+                "title" => trans('admin/hardware/table.location'),
+                "visible" => false,
+
+            ],
+            [
+                "field" => "purchase_date",
+                "searchable" => false,
+                "sortable" => false,
+                "switchable" => false,
+                "title" => trans('admin/hardware/table.purchase_date'),
+                "visible" => true,
+
+            ],
+            [
+                "field" => "eol",
+                "searchable" => false,
+                "sortable" => false,
+                "switchable" => false,
+                "title" => trans('admin/hardware/table.eol'),
+                "visible" => true,
+
+            ],
+            [
+                "field" => "purchase_cost",
+                "searchable" => false,
+                "sortable" => false,
+                "switchable" => false,
+                "title" => trans('admin/hardware/table.purchase_cost'),
+                "visible" => true,
+
+            ],
+            [
+                "field" => "current_value",
+                "searchable" => false,
+                "sortable" => false,
+                "switchable" => false,
+                "title" => trans('admin/hardware/table.book_value'),
+                "visible" => true,
+
+            ],
+            [
+                "field" => "monthly_depreciation",
+                "searchable" => false,
+                "sortable" => false,
+                "switchable" => false,
+                "title" => trans('admin/hardware/table.monthly_depreciation'),
+                "visible" => true,
+
+            ],
+            [
+                "field" => "diff",
+                "searchable" => false,
+                "sortable" => false,
+                "switchable" => false,
+                "title" => trans('admin/hardware/table.diff'),
+                "visible" => true,
+
+            ],
+        ];
+
+        return json_encode($layout);
+    }
+
+}
