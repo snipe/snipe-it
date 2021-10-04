9b48732cd21f8245a510cadc4e1ddf247cfca55c
snipe <snipe@snipe.net>
2021-10-04 12:52:48 -0700

    Force revalidation headers when user logs out
    Signed-off-by: snipe <snipe@snipe.net>

---
+++ app/Http/Middleware/PreventBackHistory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class PreventBackHistory
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $response = $next($request);
+        return $response->header('Cache-Control','no-cache, no-store, max-age=0, must-revalidate')
+            ->header('Pragma','no-cache')
+            ->header('Expires','Sun, 02 Jan 1990 00:00:00 GMT');
+    }
+}
