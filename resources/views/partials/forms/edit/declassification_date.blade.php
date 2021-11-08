afee92f50a43f68b73f0bbb74d0e97addf281ea9
Robert Hart <robertdhart99@gmail.com>
2021-11-07 21:34:50 -0500

    got the date selection field in asset page to work on vm

--- resources/views/partials/forms/edit/declassification_date.blade.php
+++ resources/views/partials/forms/edit/declassification_date.blade.php
@@ -1,6 +1,6 @@
 <!-- Declassification date -->
 <div class="form-group{{ $errors->has('declassification_date') ? ' has-error' : '' }}">
-    <label for="declassification_date" class="col-md-3 control-label">{{ trans('general.declassification_date') }}</label>
+    <label for="declassification_date" class="col-md-3 control-label">{{ trans('Declassification Date') }}</label>
     <div class="input-group col-md-3">
         <div class="input-group date" data-provide="datepicker" data-date-format="yyyy-mm-dd"  data-autoclose="true">
             <input type="text" class="form-control" placeholder="{{ trans('general.select_date') }}" name="declassification_date" id="declassification_date" value="{{ old('declassification_date', ($item->declassification_date) ? $item->declassification_date->format('Y-m-d') : '') }}">
