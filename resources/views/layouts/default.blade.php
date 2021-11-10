ec2a3b0f35af3654ca076d87c60bad5b913cd0dd
snipe <snipe@snipe.net>
2021-11-10 01:44:34 -0800

    Updated label names
    Signed-off-by: snipe <snipe@snipe.net>

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -853,12 +853,12 @@
     </div>
 
 
-    <div class="modal modal-warning fade" id="restoreConfirmModal" tabindex="-1" role="dialog" aria-labelledby="restoreConfirmModalLabel" aria-hidden="true">
+    <div class="modal modal-warning fade" id="restoreConfirmModal" tabindex="-1" role="dialog" aria-labelledby="confirmModalLabel" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
                     <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                    <h4 class="modal-title" id="restoreConfirmModalLabel">&nbsp;</h4>
+                    <h4 class="modal-title" id="confirmModalLabel">&nbsp;</h4>
                 </div>
                 <div class="modal-body"></div>
                 <div class="modal-footer">
