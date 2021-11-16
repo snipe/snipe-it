5ba9cfa521b3ede529a60f53d7238e57038084a1
Daniel Cullen <DanCullen@live.com>
2021-11-15 22:14:30 -0800

    boxes

--- resources/views/partials/forms/edit/holder.blade.php
+++ resources/views/partials/forms/edit/holder.blade.php
@@ -3,7 +3,7 @@
     <label for="holder" class="col-md-3 control-label">{{ trans('Holder') }}</label>
     <div class="input-group col-md-3">
         <div class="input-group date" data-provide="datepicker" data-date-format="yyyy-mm-dd"  data-autoclose="true">
-            <input type="text" class="form-control" placeholder="{{ trans('general.select_date') }}" name="holder" id="holder" value="{{ old('holder', ($item->holder) ? $item->holder->format('Y-m-d') : '') }}">
+            <input type="checkbox" class="form-control" placeholder="{{ trans('general.select_date') }}" name="holder" id="holder" value="{{ old('holder', ($item->holder) ? $item->holder->format('Y-m-d') : '') }}">
             <span class="input-group-addon"><i class="fa fa-calendar" aria-hidden="true"></i></span>
         </div>
         {!! $errors->first('Holder', '<span class="alert-msg" aria-hidden="true"><i class="fa fa-times" aria-hidden="true"></i> :message</span>') !!}
