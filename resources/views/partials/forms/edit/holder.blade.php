614029f2ee1b9b6e3275db77935737aaf2775939
Daniel Cullen <DanCullen@live.com>
2021-11-15 20:06:32 -0800

    g

--- resources/views/partials/forms/edit/holder.blade.php
+++ resources/views/partials/forms/edit/holder.blade.php
@@ -1,3 +1,5 @@
 <!-- Holder -->
 <div class="form-group{{ $errors->has('holder') ? ' has-error' : '' }}">
     <label for="holder" class="col-md-3 control-label">{{ trans('Holder') }}</label>
+    </div>
+    
