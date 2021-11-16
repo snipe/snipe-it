8fedbb3ff2fcd6744044225a97bbce0358eb03d3
Daniel Cullen <DanCullen@live.com>
2021-11-15 20:13:29 -0800

    edit.blade

--- resources/views/hardware/edit.blade.php
+++ resources/views/hardware/edit.blade.php
@@ -50,6 +50,7 @@
 
     @include ('partials.forms.edit.derived_from', ['translated_name' => trans('admin/hardware/form.derived_from')])
 
+    @include (patials.forms.edit.Holder', ['translated_name' => trans('admin/hardware/form.Holder'),'fieldname' => 'Holder']))
     @include ('partials.forms.edit.declassification_date', ['translated_name' => trans('admin/hardware/form.declassification_date'),'fieldname' => 'declassification_date'])
     @include ('partials.forms.edit.model-select', ['translated_name' => trans('admin/hardware/form.model'), 'fieldname' => 'model_id', 'field_req' => true])
 
