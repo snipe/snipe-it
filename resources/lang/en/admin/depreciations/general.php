6917d591852541a063429b9803a1ab8dc748280c
Wächtler, Yannick <waeya@t-online.de>
2021-11-19 12:39:19 +0100

    Added localized strings for reports

--- resources/lang/en/admin/depreciations/general.php
+++ resources/lang/en/admin/depreciations/general.php
@@ -10,4 +10,7 @@ return [
     'number_of_months'  					=> 'Number of Months',
     'update'  					            => 'Update Depreciation',
     'depreciation_min'                      => 'Minimum Value after Depreciation',
+    'no_depreciations_warning'               => '<strong>Warning: </strong>
+                      You do not currently have any depreciations set up.
+                      Please set up at least one depreciation to view the depreciation report.',
 ];
