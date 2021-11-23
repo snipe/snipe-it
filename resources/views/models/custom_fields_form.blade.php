d95d3dc2829d19d0685493cbdc134d9355e5fd1f
Ivan Nieto Vivanco <inietov@gmail.com>
2021-11-23 17:21:46 -0600

    Add the call to defaultValue() function in custom fields with date format

--- resources/views/models/custom_fields_form.blade.php
+++ resources/views/models/custom_fields_form.blade.php
@@ -48,7 +48,7 @@
 
                         <div class="input-group col-md-4" style="padding-left: 0px;">
                             <div class="input-group date" data-provide="datepicker" data-date-format="yyyy-mm-dd"  data-autoclose="true">
-                                <input type="text" class="form-control" placeholder="{{ trans('general.select_date') }}" name="{{ $field->db_column_name() }}" id="{{ $field->db_column_name() }}" value="{{ old($field->db_column_name(),(isset($item) ? \App\Helpers\Helper::gracefulDecrypt($field, $item->{$field->db_column_name()}) : "")) }}">
+                                <input type="text" class="form-control" placeholder="{{ trans('general.select_date') }}" name="{{ $field->db_column_name() }}" id="{{ $field->db_column_name() }}" value="{{ old($field->db_column_name(),(isset($item) ? \App\Helpers\Helper::gracefulDecrypt($field, $item->{$field->db_column_name()}) : $field->defaultValue($model->id))) }}">
                                 <span class="input-group-addon"><i class="fa fa-calendar" aria-hidden="true"></i></span>
                             </div>
                         </div>
