d96e95abd662ac2b1e5704e1bf17df1b47c0c2aa
snipe <snipe@snipe.net>
2021-12-01 13:46:21 -0800

    Small mods to configs, removed old faker, added new
    Signed-off-by: snipe <snipe@snipe.net>

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,7 +4,7 @@ namespace Database\Factories;
 
 use App\Models\Company;
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Facades\Hash;
+
 
 
 class UserFactory extends Factory
