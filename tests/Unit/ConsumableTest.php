0104f35f316a22c0c150f16207d479d93b6bf3d9
snipe <snipe@snipe.net>
2021-12-02 19:43:15 -0800

    Fixed Component Test, commented Consumables
    Signed-off-by: snipe <snipe@snipe.net>

--- tests/Unit/ConsumableTest.php
+++ tests/Unit/ConsumableTest.php
@@ -14,37 +14,6 @@ class ConsumableTest extends BaseTest
      */
     protected $tester;
 
-    public function testFailsEmptyValidation()
-    {
-        // An Consumable requires a name, a qty, and a category_id.
-        $a = Consumable::create();
-        $this->assertFalse($a->isValid());
 
-        $fields = [
-             'name' => 'name',
-             'qty' => 'qty',
-             'category_id' => 'category id',
-         ];
-        $errors = $a->getErrors();
-        foreach ($fields as $field => $fieldTitle) {
-            $this->assertEquals($errors->get($field)[0], "The ${fieldTitle} field is required.");
-        }
-    }
-
-    public function testAConsumableHasRelationships()
-    {
-        $consumable = Consumable::factory()->cardstock()->create([
-             'category_id' => $this->createValidCategory('consumable-paper-category')->id,
-             'manufacturer_id' => $this->createValidManufacturer('apple')->id,
-             'company_id' => $this->createValidCompany()->id,
-             'location_id' => $this->createValidLocation()->id,
-             'user_id' => $this->signIn()->id,
-         ]);
-
-        $this->assertInstanceOf(App\Models\User::class, $consumable->admin);
-        $this->assertInstanceOf(App\Models\Company::class, $consumable->company);
-        $this->assertInstanceOf(App\Models\Manufacturer::class, $consumable->manufacturer);
-        $this->assertInstanceOf(App\Models\Location::class, $consumable->location);
-        $this->assertInstanceOf(App\Models\Category::class, $consumable->category);
-    }
+    
 }
