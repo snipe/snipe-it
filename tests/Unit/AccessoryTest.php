c2709be4a1e23ac90e076c40c54d8cc5410408da
snipe <snipe@snipe.net>
2021-12-02 16:14:23 -0800

    Removed pointless tests
    Signed-off-by: snipe <snipe@snipe.net>

--- tests/Unit/AccessoryTest.php
+++ tests/Unit/AccessoryTest.php
@@ -15,59 +15,7 @@ class AccessoryTest extends BaseTest
      */
     protected $tester;
 
-    public function testFailsEmptyValidation()
-    {
-        // An Accessory requires a name, a qty, and a category_id.
-        $a = Accessory::create();
-        $this->assertFalse($a->isValid());
-        $fields = [
-            'name' => 'name',
-            'qty' => 'qty',
-            'category_id' => 'category id',
-        ];
-        $errors = $a->getErrors();
-        foreach ($fields as $field => $fieldTitle) {
-            $this->assertEquals($errors->get($field)[0], "The ${fieldTitle} field is required.");
-        }
-    }
-
-    public function testFailsMinValidation()
-    {
-        // An Accessory name has a min length of 3
-        // An Accessory has a min qty of 1
-        // An Accessory has a min amount of 0
-        $a = Accessory::factory()->make([
-            'name' => 'a',
-            'qty' => 0,
-            'min_amt' => -1,
-        ]);
-        $fields = [
-            'name' => 'name',
-            'qty' => 'qty',
-            'min_amt' => 'min amt',
-        ];
-        $this->assertFalse($a->isValid());
-        $errors = $a->getErrors();
-        foreach ($fields as $field => $fieldTitle) {
-            $this->assertStringContainsString("The ${fieldTitle} must be at least", $errors->get($field)[0]);
-        }
-    }
-
-    public function testCategoryIdMustExist()
-    {
-        $category = $this->createValidCategory('accessory-keyboard-category', ['category_type' => 'accessory']);
-        $accessory = Accessory::factory()->appleBtKeyboard()->make(['category_id' => $category->id]);
-        $this->createValidManufacturer('apple');
-
-        $accessory->save();
-        $this->assertTrue($accessory->isValid());
-        $newId = $category->id + 1;
-        $accessory = Accessory::factory()->appleBtKeyboard()->make(['category_id' => $newId]);
-        $accessory->save();
-
-        $this->assertFalse($accessory->isValid());
-        $this->assertStringContainsString('The selected category id is invalid.', $accessory->getErrors()->get('category_id')[0]);
-    }
+    
 
     public function testAnAccessoryBelongsToACompany()
     {
