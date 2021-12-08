80d36cd72bda8adfa985b19617e62e0cc55ae52c
snipe <snipe@snipe.net>
2021-12-08 15:53:05 -0800

    Added slack settings request
    Signed-off-by: snipe <snipe@snipe.net>

---
+++ app/Http/Requests/SlackSettingsRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+class SlackSettingsRequest extends Request
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'slack_endpoint' => 'url|required_with:slack_channel|starts_with:https://hooks.slack.com|nullable',
+            'slack_channel'                       => 'required_with:slack_endpoint|starts_with:#|nullable',
+            'slack_botname'                       => 'string|nullable',
+
+        ];
+    }
+
+    public function response(array $errors)
+    {
+        return $this->redirector->back()->withInput()->withErrors($errors, $this->errorBag);
+    }
+}
