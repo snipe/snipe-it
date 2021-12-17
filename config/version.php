04f4bb83e96218a4be9700625dc69d9a553ef6fe
snipe <snipe@snipe.net>
2021-12-16 20:37:22 -0800

    Bumped version
    Signed-off-by: snipe <snipe@snipe.net>

--- config/version.php
+++ config/version.php
@@ -1,10 +1,10 @@
 <?php
 return array (
-  'app_version' => 'v5.3.5',
-  'full_app_version' => 'v5.3.5 - build 6558-gc988d8427',
-  'build_version' => '6558',
+  'app_version' => 'v5.3.6',
+  'full_app_version' => 'v5.3.6 - build 6567-g9b2dd6522',
+  'build_version' => '6567',
   'prerelease_version' => '',
-  'hash_version' => 'gc988d8427',
-  'full_hash' => 'v5.3.5-2-gc988d8427',
+  'hash_version' => 'g9b2dd6522',
+  'full_hash' => 'v5.3.6-8-g9b2dd6522',
   'branch' => 'master',
 );
