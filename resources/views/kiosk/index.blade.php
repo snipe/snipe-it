aaa148668b09cacb8de4b575bd03002e4045e815
Alex Janes <ajanes@adagiohealth.org>
2021-12-20 16:49:01 -0500

    first version of a kiosk mode

--- resources/views/kiosk/index.blade.php
+++ resources/views/kiosk/index.blade.php
@@ -2,13 +2,24 @@
 
 {{-- Page title --}}
 @section('title')
-    Kiosk
+    Kiosk Menu
     @parent
 @stop
 
 @section('content')
-    Check-In
-    Check-Out
-    Audit
-    Return to SnipeIT
+    <div class="row">
+        <div class="col-md-6">
+            <div class="box box-default">
+                <div class="box-header with-border">
+                    <h2 class="box-title"> Options </h2>
+                </div>
+                <div class="box-body">
+                    <a href="{{ route('kiosk/checkin') }}">Check-In</a>
+                    <a href="{{ route('kiosk/checkout') }}">Check-Out</a>
+                    <a href="{{ route('kiosk/audit') }}">Audit</a>
+                    <a href="{{ route('home') }}">Return to SnipeIT</a>
+                </div>
+            </div>
+        </div>
+    </div>
 @stop
