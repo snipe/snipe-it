4e8ae8a162403478d749a5f607daabf809161285
Brady Wetherington <bwetherington@grokability.com>
2022-01-13 16:32:40 -0800

    Fix extraneous wildcard query in requestable asset search

--- app/Http/Controllers/ViewAssetsController.php
+++ app/Http/Controllers/ViewAssetsController.php
@@ -57,10 +57,10 @@ class ViewAssetsController extends Controller
      */
     public function getRequestableIndex()
     {
-        $assets = Asset::with('model', 'defaultLoc', 'location', 'assignedTo', 'requests')->Hardware()->RequestableAssets();
+        $asset_count = Asset::with('model', 'defaultLoc', 'location', 'assignedTo', 'requests')->Hardware()->RequestableAssets()->count();
         $models = AssetModel::with('category', 'requests', 'assets')->RequestableModels()->get();
 
-        return view('account/requestable-assets', compact('assets', 'models'));
+        return view('account/requestable-assets', compact('asset_count', 'models'));
     }
 
     public function getRequestItem(Request $request, $itemType, $itemId = null)
