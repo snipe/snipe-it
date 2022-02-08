413e44be2f544057626bd4376afe9d58e86aa906
Ivan Nieto Vivanco <inietov@gmail.com>
2022-02-08 11:58:23 -0600

    Add custom date to checkin actionlogs and show it in the history of the asset tab

--- app/Models/Loggable.php
+++ app/Models/Loggable.php
@@ -115,6 +115,7 @@ trait Loggable
 
         $log->location_id = null;
         $log->note = $note;
+        $log->action_date = $action_date;
 
         if (Auth::user()) {
             $log->user_id = Auth::user()->id;
