2a71690aafebe53c6f583070bf866017181cdf5a
snipe <snipe@snipe.net>
2022-02-09 15:17:57 -0800

    Added license_name to unescaper
    Signed-off-by: snipe <snipe@snipe.net>

--- app/Console/Commands/FixDoubleEscape.php
+++ app/Console/Commands/FixDoubleEscape.php
@@ -40,7 +40,7 @@ class FixDoubleEscape extends Command
 
         $tables = [
             '\App\Models\Asset' => ['name'],
-            '\App\Models\License' => ['name'],
+            '\App\Models\License' => ['name', 'license_name'],
             '\App\Models\Consumable' => ['name'],
             '\App\Models\Accessory' => ['name'],
             '\App\Models\Component' => ['name'],
