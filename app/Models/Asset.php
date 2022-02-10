f36de6c670cf0dec396495998ec9a3fdd9dd9331
Ivan Nieto Vivanco <inietov@gmail.com>
2022-02-10 13:53:49 -0600

    Validates if model and model->category exist before return the checkin_email property

--- app/Models/Asset.php
+++ app/Models/Asset.php
@@ -830,7 +830,9 @@ class Asset extends Depreciable
      */
     public function checkin_email()
     {
-        return $this->model->category->checkin_email;
+        if (($this->model) && ($this->model->category)) {
+            return $this->model->category->checkin_email;
+        }
     }
 
     /**
