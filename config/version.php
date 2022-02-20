784bf6f22372ba8b5189372699f7976f3b386e3e
snipe <snipe@snipe.net>
2022-02-20 13:30:24 -0800

    Bumped version
    Signed-off-by: snipe <snipe@snipe.net>

--- config/version.php
+++ config/version.php
@@ -1,10 +1,10 @@
 <?php
 return array (
-  'app_version' => 'v6.0.0-RC2',
-  'full_app_version' => 'v6.0.0-RC2 - build 6627-g2815e0d36',
+  'app_version' => 'v6.0.0-RC3',
+  'full_app_version' => 'v6.0.0-RC3 - build 6627-g2815e0d36',
   'build_version' => '6627',
   'prerelease_version' => '',
   'hash_version' => 'g2815e0d36',
-  'full_hash' => 'v6.0.0-RC2-6-g2815e0d36',
+  'full_hash' => 'v6.0.0-RC3-6-g2815e0d36',
   'branch' => 'develop',
 );
