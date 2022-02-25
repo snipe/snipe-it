336d8e657442de4d1a5819d2962c587a214b9a8c
snipe <snipe@snipe.net>
2022-02-24 18:17:15 -0800

    Updated version

--- config/version.php
+++ config/version.php
@@ -1,10 +1,10 @@
 <?php
 return array (
-  'app_version' => 'v5.3.10',
-  'full_app_version' => 'v5.3.10 - build 6684-g05c081977',
-  'build_version' => '6684',
+  'app_version' => 'v5.4.0',
+  'full_app_version' => 'v5.4.0 - build 6685-dfbff274839',
+  'build_version' => '6685',
   'prerelease_version' => '',
-  'hash_version' => 'g05c081977',
-  'full_hash' => 'v5.3.10-1-g05c081977',
+  'hash_version' => 'dfbff274839',
+  'full_hash' => 'v5.4.0-1-dfbff274839',
   'branch' => 'master',
 );
