530a76881e94f4913aa0c7e42c94b3fd8b27a50b
snipe <snipe@snipe.net>
2022-03-02 14:19:42 -0800

    Added language strings
    Signed-off-by: snipe <snipe@snipe.net>

--- resources/lang/en/admin/hardware/form.php
+++ resources/lang/en/admin/hardware/form.php
@@ -1,11 +1,13 @@
 <?php
 
 return [
+  'bulk_checkin'		=> 'Confirm Bulk Checkin Assets',
     'bulk_delete'		=> 'Confirm Bulk Delete Assets',
   'bulk_delete_help'	=> 'Review the assets for bulk deletion below. Once deleted, these assets can be restored, but they will no longer be associated with any users they are currently assigned to.',
   'bulk_delete_warn'	=> 'You are about to delete :asset_count assets.',
     'bulk_update'		=> 'Bulk Update Assets',
     'bulk_update_help'	=> 'This form allows you to update multiple assets at once. Only fill in the fields you need to change. Any fields left blank will remain unchanged. ',
+    'bulk_checkin_warn' => 'You are about to checkin :asset_count assets.',
     'bulk_update_warn'	=> 'You are about to edit the properties of :asset_count assets.',
     'checkedout_to'		=> 'Checked Out To',
     'checkout_date'		=> 'Checkout Date',
