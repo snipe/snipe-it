172942878b9d080665a11f1469e71630407c76ab
snipe <snipe@snipe.net>
2022-03-02 14:20:13 -0800

    Added checkin as option in dropdown actions
    Signed-off-by: snipe <snipe@snipe.net>

--- resources/views/hardware/index.blade.php
+++ resources/views/hardware/index.blade.php
@@ -76,6 +76,7 @@
                       <label for="bulk_actions"><span class="sr-only">{{ trans('button.bulk_actions') }}</span></label>
                       <select name="bulk_actions" class="form-control select2" aria-label="bulk_actions">
                         <option value="edit">{{ trans('button.edit') }}</option>
+                        <option value="checkin">{{ trans('general.checkin') }}</option>
                         <option value="delete">{{ trans('button.delete') }}</option>
                         <option value="labels">{{ trans_choice('button.generate_labels', 2) }}</option>
                       </select>
