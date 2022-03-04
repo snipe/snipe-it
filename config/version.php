afd83311aa7b54431b821c2d048b1ff8930b731e
snipe <snipe@snipe.net>
2022-03-03 20:58:54 -0800

    Fixed version number (vV)
    Signed-off-by: snipe <snipe@snipe.net>

--- config/version.php
+++ config/version.php
@@ -1,6 +1,6 @@
 <?php
 return array (
-  'app_version' => 'vV5.4.1',
+  'app_version' => 'v5.4.1',
   'full_app_version' => 'vV5.4.1 - build 6746-g3e22dce11',
   'build_version' => '6746',
   'prerelease_version' => '',
