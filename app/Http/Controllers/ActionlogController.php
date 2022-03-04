f840652395318bdcbc2511a1651d005e8319b742
snipe <snipe@snipe.net>
2022-03-04 03:18:28 -0800

    Suppress errors for missing signatures
    Signed-off-by: snipe <snipe@snipe.net>

--- app/Http/Controllers/ActionlogController.php
+++ app/Http/Controllers/ActionlogController.php
@@ -9,10 +9,21 @@ class ActionlogController extends Controller
 {
     public function displaySig($filename)
     {
+        // PHP doesn't let you handle file not found errors well with 
+        // file_get_contents, so we set the error reporting for just this class
+        error_reporting(0);
+
         $this->authorize('view', \App\Models\Asset::class);
-        $file = config('app.private_uploads') . '/signatures/' . $filename;
+        $file = config('app.private_uploads').'/signatures/'.$filename;
         $filetype = Helper::checkUploadIsImage($file);
-        $contents = file_get_contents($file);
-        return Response::make($contents)->header('Content-Type', $filetype);
+
+        $contents = file_get_contents($file, false, stream_context_create(['http' => ['ignore_errors' => true]]));
+        if ($contents === false) {
+            \Log::warn('File '.$file.' not found');
+            return false;
+        } else {
+            return Response::make($contents)->header('Content-Type', $filetype);
+        }
+       
     }
 }
