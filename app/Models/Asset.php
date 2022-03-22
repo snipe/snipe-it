8c92198636c9440f0828ee2e4a67ffd0a5858f24
snipe <snipe@snipe.net>
2022-03-22 13:47:43 +0000

    Possible fix for boolean field check
    Signed-off-by: snipe <snipe@snipe.net>

--- app/Models/Asset.php
+++ app/Models/Asset.php
@@ -185,15 +185,18 @@ class Asset extends Depreciable
             $model = AssetModel::find($this->model_id);
 
             if (($model) && ($model->fieldset)) {
+
+                foreach ($model->fieldset->fields as $field){
+                    if($field->format == 'BOOLEAN'){
+                        $this->{$field->db_column} = filter_var($this->{$field->db_column}, FILTER_VALIDATE_BOOLEAN);
+                    }
+                }
+
                 $this->rules += $model->fieldset->validation_rules();
             }
         }
 
-        foreach ($this->model->fieldset->fields as $field){
-            if($field->format == 'BOOLEAN'){
-                $this->{$field->db_column} = filter_var($this->{$field->db_column}, FILTER_VALIDATE_BOOLEAN);
-            }
-        }
+
 
         return parent::save($params);
     }
