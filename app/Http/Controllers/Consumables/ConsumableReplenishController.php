6df755651e5c0d7444804d27dfdab4cb3e89a955
Achmad Fienan Rahardianto <veenone@gmail.com>
2022-03-22 20:58:35 +0700

    adding new files adding database migrations/2022_03_22_114700_create_consumables_stock.php

---
+++ app/Http/Controllers/Consumables/ConsumableReplenishController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers\Consumables;
+
+use App\Events\ConsumableCheckedOut;
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\ConsumablesController;
+use App\Models\Consumable;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Input;
+use Illuminate\Validation\Rule;
+use DB;
+
+class ConsumableReplenishController extends Controller
+{
+    /**
+     * Return a view to replenish a consumable .
+     *
+     * @author [A. Gianotto] [<snipe@snipe.net>]
+     * @see ConsumableCheckoutController::store() method that stores the data.
+     * @since [v1.0]
+     * @param int $consumableId
+     * @return \Illuminate\Contracts\View\View
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
+    public function create($consumableId)
+    {
+        if (is_null($consumable = Consumable::find($consumableId))) {
+            return redirect()->route('consumables.index')->with('error', trans('admin/consumables/message.does_not_exist'));
+        }
+        $this->authorize('replenish', $consumable);
+
+        return view('consumables/replenish', compact('consumable'));
+    }
+
+    /**
+     * Saves the replenish information
+     *
+     * @author [A. Gianotto] [<snipe@snipe.net>]
+     * @see ConsumableCheckoutController::create() method that returns the form.
+     * @since [v1.0]
+     * @param int $consumableId
+     * @return \Illuminate\Http\RedirectResponse
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
+    public function store(Request $request, $consumableId)
+    {
+        if (is_null($consumable = Consumable::find($consumableId))) {
+            return redirect()->route('consumables.index')->with('error', trans('admin/consumables/message.not_found'));
+        }
+
+        $request->validate([
+            "totalnum" => "required|regex:/^[0-9]*$/|min:1"
+          ],[
+            'totalnum.min' =>  trans('admin/consumables/message.below'),
+            'totalnum.required' => trans('admin/consumables/message.required'),
+            'totalnum.regex' => trans('admin/consumables/message.numeric'),
+          ]);
+
+        $this->authorize('replenish', $consumable);
+
+        $admin_user = Auth::user();
+
+        // Update the consumable data
+        $consumable->assigned_to = e($request->input('assigned_to'));
+
+        $consumable->replenishusers()->attach($consumable->id, [
+            'consumable_id' => $consumable->id,            
+            'user_id' => $admin_user->id,
+            'initial_qty' => $consumable->qty,            
+            'total_replenish' => e($request->input('totalnum')),
+            'replenishnote' => e($request->input('replenishnote'))
+        ]);
+
+        $addedquantity = $consumable->qty + e($request->input('totalnum'));
+
+        DB::table('consumables')
+        ->where('id',$consumable->id)
+        ->update(['qty'=> $addedquantity]);
+
+        event(new ConsumableCheckedOut($consumable, Auth::user(), $consumable->qty, $request->input('note'), $request->input('totalnum')));
+
+        // Redirect to the new consumable page
+        return redirect()->route('consumables.index')->with('success', trans('admin/consumables/message.checkout.success'));
+    }
+}
---
+++ resources/views/consumables/replenish.blade.php
@@ -0,0 +1,98 @@
+@extends('layouts/default')
+
+{{-- Page title --}}
+@section('title')
+     {{ trans('admin/consumables/general.replenish') }}
+@parent
+@stop
+
+{{-- Page content --}}
+@section('content')
+
+<div class="row">
+  <div class="col-md-9">
+
+    <form class="form-horizontal" method="post" action="" autocomplete="off">
+      <!-- CSRF Token -->
+      <input type="hidden" name="_token" value="{{ csrf_token() }}" />
+
+      <div class="box box-default">
+
+        @if ($consumable->id)
+          <div class="box-header with-border">
+            <div class="box-heading">
+              <h2 class="box-title">{{ $consumable->name }}   ({{ $consumable->numRemaining()  }} out of {{ $consumable->qty}}  {{ trans('admin/consumables/general.remaining') }}) </h2>
+            </div>
+          </div><!-- /.box-header -->
+        @endif
+
+        <div class="box-body">
+          @if ($consumable->name)
+          <!-- consumable name -->
+          <div class="form-group">
+            <label class="col-sm-3 control-label">{{ trans('admin/consumables/general.consumable_name') }}</label>
+            <div class="col-md-6">
+              <p class="form-control-static">{{ $consumable->name }}</p>
+            </div>
+          </div>
+          @endif
+
+          <!-- number of consumables -->         
+            <div class="form-group {{ $errors->has('total') ? ' has-error' : '' }}">
+              <label for="total" class="col-md-3 control-label">{{ trans('admin/consumables/general.total') }}
+                <i class='icon-asterisk'></i></label>
+              <div class="col-md-5" >
+                    <input class="form-control" type="text" name="totalnum" id="totalnum" style="width: 70px; float:left;" value="{{ old('totalnum') ?? 1 }}" > &nbsp;&nbsp;                    
+                    <div style="float:right;" class="col-ld-1">
+                    {!! $errors->first('totalnum', '<span class="alert-msg" aria-hidden="true" width=100% position=absolute><i class="fa fa-times" aria-hidden="true"></i> :message</span>') !!}    
+                    </div>
+                    </input>               
+                             
+              </div>
+            </div>
+          
+
+            @if ($consumable->requireAcceptance() || $consumable->getEula() || ($snipeSettings->slack_endpoint!=''))
+              <div class="form-group notification-callout">
+                <div class="col-md-8 col-md-offset-3">
+                  <div class="callout callout-info">
+
+                    @if ($consumable->category->require_acceptance=='1')
+                      <i class="far fa-envelope"></i>
+                      {{ trans('admin/categories/general.required_acceptance') }}
+                      <br>
+                    @endif
+
+                    @if ($consumable->getEula())
+                      <i class="far fa-envelope"></i>
+                      {{ trans('admin/categories/general.required_eula') }}
+                        <br>
+                    @endif
+
+                    @if ($snipeSettings->slack_endpoint!='')
+                        <i class="fab fa-slack"></i>
+                        A slack message will be sent
+                    @endif
+                  </div>
+                </div>
+              </div>
+            @endif
+          <!-- Note -->
+          <div class="form-group {{ $errors->has('replenishnote') ? 'error' : '' }}">
+            <label for="note" class="col-md-3 control-label">{{ trans('admin/hardware/form.notes') }}</label>
+            <div class="col-md-7">
+              <textarea class="col-md-6 form-control" id="replenishnote" name="replenishnote">{{ old('note', $consumable->checkoutnote) }}</textarea>	      
+              {!! $errors->first('note', '<span class="alert-msg" aria-hidden="true"><i class="fa fa-times" aria-hidden="true"></i> :message</span>') !!}
+            </div>
+          </div>
+        </div> <!-- .box-body -->
+        <div class="box-footer">
+          <a class="btn btn-link" href="{{ route('consumables.index') }}">{{ trans('button.cancel') }}</a>
+          <button type="submit" class="btn btn-primary pull-right"><i class="fas fa-check icon-white" aria-hidden="true"></i> {{ trans('general.update') }}</button>
+       </div>
+      </div>
+    </form>
+
+  </div>
+</div>
+@stop
