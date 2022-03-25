0dd05eb1f1c6edad1f761c47d51db1c665fda951
Achmad Fienan Rahardianto <veenone@gmail.com>
2022-03-25 09:56:28 +0700

    missed to add new assignment file

---
+++ app/Models/ConsumableReplenishAssignment.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ConsumableReplenishAssignment extends Model
+{
+    use CompanyableTrait;
+
+    protected $table = 'consumables_stock';
+
+    public function consumable()
+    {
+        return $this->belongsTo(\App\Models\Consumable::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(\App\Models\User::class, 'user_id');
+    }
+
+    public function admin()
+    {
+        return $this->belongsTo(\App\Models\User::class, 'user_id');
+    }
+}
