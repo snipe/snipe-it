f7cd21a00720cee868161a56d111230f6e707d48
snipe <snipe@snipe.net>
2022-03-29 22:59:52 +0100

    Sigh.
    Signed-off-by: snipe <snipe@snipe.net>

--- config/version.php
+++ config/version.php
@@ -1,10 +1,10 @@
 <?php
 return array (
-  'app_version' => 'v6.0.0-RC-6',
-  'full_app_version' => 'v6.0.0-RC-6 - build 6785-gab18ceb2f',
-  'build_version' => '6785',
+  'app_version' => 'v6.0.0-RC-7',
+  'full_app_version' => 'v6.0.0-RC-7 - build 6787-gd56552a8a',
+  'build_version' => '6787',
   'prerelease_version' => '',
-  'hash_version' => 'gab18ceb2f',
-  'full_hash' => 'v6.0.0-RC-6-gab18ceb2f',
+  'hash_version' => 'gd56552a8a',
+  'full_hash' => 'v6.0.0-RC-7-34-gd56552a8a',
   'branch' => 'develop',
 );
