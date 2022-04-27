4be6288ae31b2b45307a2475b45bc31d1d905af7
Christian Weirich <christian.weirich@trizelos.com>
2022-04-27 15:26:33 +0200

    Possibility to set region for AWS (SES)

--- config/services.php
+++ config/services.php
@@ -32,9 +32,9 @@ return [
     ],
 
     'ses' => [
-        'key'    => env('SES_KEY'),
-        'secret' => env('SES_SECRET'),
-        'region' => 'us-east-1',
+        'key' => env('AWS_ACCESS_KEY_ID'),
+        'secret' => env('AWS_SECRET_ACCESS_KEY'),
+        'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
     'stripe' => [
