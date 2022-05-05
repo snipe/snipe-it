31f2b73e5ada0cbc96fd160b42a7eb758f764d48
snipe <snipe@snipe.net>
2022-05-05 10:55:29 -0700

    Bumped version to GM
    Signed-off-by: snipe <snipe@snipe.net>

--- config/version.php
+++ config/version.php
@@ -1,10 +1,10 @@
 <?php
 return array (
-  'app_version' => 'v6.0.0-RC-8',
-  'full_app_version' => 'v6.0.0-RC-8 - build 6825-g173ec44b9',
+  'app_version' => 'v6.0.0-GM',
+  'full_app_version' => 'v6.0.0-GM - build 6825-g173ec44b9',
   'build_version' => '6825',
   'prerelease_version' => '',
   'hash_version' => 'g173ec44b9',
-  'full_hash' => 'v6.0.0-RC-8-10-g173ec44b9',
+  'full_hash' => 'v6.0.0-GM-10-g173ec44b9',
   'branch' => 'develop',
 );
