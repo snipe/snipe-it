a43b54cc8d61f9b772cd7045ffc525d6cc0ecd74
snipe <snipe@snipe.net>
2022-05-06 02:13:56 -0700

    Bumped hash for clarity
    Signed-off-by: snipe <snipe@snipe.net>

--- config/version.php
+++ config/version.php
@@ -1,10 +1,10 @@
 <?php
 return array (
   'app_version' => 'v6.0.0-GM',
-  'full_app_version' => 'v6.0.0-GM - build 6825-g173ec44b9',
-  'build_version' => '6825',
+  'full_app_version' => 'v6.0.0-GM - build 6845-g6304709c7',
+  'build_version' => '6845',
   'prerelease_version' => '',
-  'hash_version' => 'g173ec44b9',
-  'full_hash' => 'v6.0.0-GM-10-g173ec44b9',
+  'hash_version' => 'g6304709c7',
+  'full_hash' => 'v6.0.0-GM-15-g6304709c7',
   'branch' => 'develop',
 );
