39ee825ba223f33782c20d75dc78cd872adf47ee
snipe <snipe@snipe.net>
2022-05-06 01:32:20 -0700

    Fixed html lang specification
    Signed-off-by: snipe <snipe@snipe.net>

--- resources/views/users/print.blade.php
+++ resources/views/users/print.blade.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="en">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <title>Assigned to {{ $show_user->present()->fullName() }}</title>
