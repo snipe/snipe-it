5d659c811ffcbc7ce427cf0c7d86a638fd728526
Godfrey M <godmartinz@gmail.com>
2022-05-10 15:24:12 -0700

    removes dead spaces and unused name spaces

--- app/Http/Controllers/Assets/AssetCheckinController.php
+++ app/Http/Controllers/Assets/AssetCheckinController.php
@@ -6,14 +6,13 @@ use App\Events\CheckoutableCheckedIn;
 use App\Helpers\Helper;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\AssetCheckinRequest;
-use App\Models\Actionlog;
 use App\Models\Asset;
 use App\Models\CheckoutAcceptance;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\View;
-use App\Observers\AssetObserver;
+
 
 class AssetCheckinController extends Controller
 {
@@ -133,7 +132,6 @@ class AssetCheckinController extends Controller
 
         // Was the asset updated?
         if ($asset->save()) {
-
             event(new CheckoutableCheckedIn($asset, $target, Auth::user(), $request->input('note'), $checkin_at));
 
             if ((isset($user)) && ($backto == 'user')) {
@@ -145,5 +143,4 @@ class AssetCheckinController extends Controller
         // Redirect to the asset management page with error
         return redirect()->route('hardware.index')->with('error', trans('admin/hardware/message.checkin.error').$asset->getErrors());
     }
-
 }
