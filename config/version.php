816437b4d80bcdfe2d5fbbc5a242b932d12ff3d6
snipe <snipe@snipe.net>
2022-05-10 17:28:11 -0700

    Updated hash
    Signed-off-by: snipe <snipe@snipe.net>

--- config/version.php
+++ config/version.php
@@ -1,10 +1,10 @@
 <?php
 return array (
   'app_version' => 'v6.0.0-GM',
-  'full_app_version' => 'v6.0.0-GM - build 6845-g6304709c7',
-  'build_version' => '6845',
+  'full_app_version' => 'v6.0.0-GM - build 6857-gf4650c65d',
+  'build_version' => '6857',
   'prerelease_version' => '',
-  'hash_version' => 'g6304709c7',
-  'full_hash' => 'v6.0.0-GM-15-g6304709c7',
+  'hash_version' => 'gf4650c65d',
+  'full_hash' => 'v6.0.0-GM-27-gf4650c65d',
   'branch' => 'develop',
 );
