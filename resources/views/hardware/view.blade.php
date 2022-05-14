5f1f6baee9b1fee41558900c13ee9f46d354071f
snipe <snipe@snipe.net>
2022-05-14 10:19:12 -0700

    Fixed unrelated typo
    Signed-off-by: snipe <snipe@snipe.net>

--- resources/views/hardware/view.blade.php
+++ resources/views/hardware/view.blade.php
@@ -104,7 +104,7 @@
                     <li class="active">
                         <a href="#details" data-toggle="tab">
                           <span class="hidden-lg hidden-md">
-                          <i class="fas fa-info-circle fa-2x"x></i>
+                          <i class="fas fa-info-circle fa-2x"></i>
                           </span>
                           <span class="hidden-xs hidden-sm">{{ trans('admin/users/general.info') }}</span>
                         </a>
