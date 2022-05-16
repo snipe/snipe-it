7573a03cc5313062f0689bda2321556550ddca27
snipe <snipe@snipe.net>
2022-05-16 15:32:57 -0700

    Bumped minor version
    Signed-off-by: snipe <snipe@snipe.net>

--- config/version.php
+++ config/version.php
@@ -1,10 +1,10 @@
 <?php
 return array (
-  'app_version' => 'v6.0.0',
-  'full_app_version' => 'v6.0.0 - build 6860-g722e88a47',
-  'build_version' => '6860',
+  'app_version' => 'v6.0.1',
+  'full_app_version' => 'v6.0.1 - build 7750-gb2e7572fe',
+  'build_version' => '7750',
   'prerelease_version' => '',
-  'hash_version' => 'g722e88a47',
-  'full_hash' => 'v6.0.0-30-g722e88a47',
-  'branch' => 'master',
+  'hash_version' => 'gb2e7572fe',
+  'full_hash' => 'v6.0.1-29-gb2e7572fe',
+  'branch' => 'develop',
 );
