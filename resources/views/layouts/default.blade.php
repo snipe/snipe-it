eaeb74c5761a09f633c72c302db982f26b17d9b7
Godfrey M <godmartinz@gmail.com>
2022-05-18 13:13:38 -0700

    readded comment

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -385,6 +385,7 @@
       </a>
        <!-- Sidebar toggle button-->
       </header>
+        <!-- Left side column. contains the logo and sidebar -->
       <aside class="main-sidebar">
         <!-- sidebar: style can be found in sidebar.less -->
         <section class="sidebar">
