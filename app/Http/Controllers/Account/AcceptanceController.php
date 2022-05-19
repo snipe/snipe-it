ad8143b0bd99786d9f2585dcf17bbc9db6298a2b
snipe <snipe@snipe.net>
2022-05-19 11:28:14 -0700

    Added comments
    Signed-off-by: snipe <snipe@snipe.net>

--- app/Http/Controllers/Account/AcceptanceController.php
+++ app/Http/Controllers/Account/AcceptanceController.php
@@ -124,7 +124,7 @@ class AcceptanceController extends Controller
             // The item was accepted, check for a signature
             if ($request->filled('signature_output')) {
                 $sig_filename = 'siglog-'.Str::uuid().'-'.date('Y-m-d-his').'.png';
-                $data_uri = e($request->input('signature_output'));
+                $data_uri = $request->input('signature_output');
                 $encoded_image = explode(',', $data_uri);
                 $decoded_image = base64_decode($encoded_image[1]);
                 Storage::put('private_uploads/signatures/'.$sig_filename, (string) $decoded_image);
@@ -136,18 +136,19 @@ class AcceptanceController extends Controller
                 $pdf_view_route ='account.accept.accept-asset-eula';
                 $asset_model = AssetModel::find($item->model_id);
                 $display_model = $asset_model->name;
-                $assigned_to = User::find($item->assigned_to);
+                $assigned_to = User::find($item->assigned_to)->present()->fullName;
 
             } elseif ($acceptance->checkoutable_type== 'App\Models\Accessory') {
                 $pdf_view_route ='account.accept.accept-accessory-eula';
                 $accessory = Accessory::find($item->id);
                 $display_model = $accessory->name;
-                $assigned_to = User::find($item->assigned_to);
+                $assigned_to = User::find($item->assignedTo);
 
             }
 
             /**
-             * Gather the data for the PDF
+             * Gather the data for the PDF. We fire this whether there is a signature required or not,
+             * since we want the moment-in-time proof of what the EULA was when they accepted it.
              */
             $branding_settings = SettingsController::getPDFBranding();
             $data = [
@@ -159,12 +160,13 @@ class AcceptanceController extends Controller
                 'accepted_date' => Carbon::parse($acceptance->accepted_at)->format($branding_settings->date_display_format),
                 'assigned_to' => $assigned_to,
                 'company_name' => $branding_settings->site_name,
-                'signature' => storage_path() . '/private_uploads/signatures/' . $sig_filename,
+                'signature' => '',
                 'logo' => public_path() . '/uploads/' . $branding_settings->logo,
                 'date_settings' => $branding_settings->date_display_format,
             ];
 
             if ($pdf_view_route!='') {
+                \Log::debug($pdf_filename.' is the filename, and the route was specified.');
                 $pdf = Pdf::loadView($pdf_view_route, $data);
                 Storage::put('private_uploads/eula-pdfs/' .$pdf_filename, $pdf->output());
             }
