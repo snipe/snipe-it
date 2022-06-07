740f27198fda435920371e01bfa9e566d905d931
Ivan Nieto Vivanco <inietov@gmail.com>
2022-06-07 18:00:46 -0500

    Evaluate if a custom field is a Date to present it with the correct format

--- app/Http/Transformers/AssetsTransformer.php
+++ app/Http/Transformers/AssetsTransformer.php
@@ -98,6 +98,14 @@ class AssetsTransformer
                     $decrypted = Helper::gracefulDecrypt($field, $asset->{$field->convertUnicodeDbSlug()});
                     $value = (Gate::allows('superadmin')) ? $decrypted : strtoupper(trans('admin/custom_fields/general.encrypted'));
 
+                    if ($field->format == 'DATE'){
+                        if (Gate::allows('superadmin')){
+                            $value = Helper::getFormattedDateObject($value)['formatted'];
+                        } else {
+                           $value = strtoupper(trans('admin/custom_fields/general.encrypted'));
+                        }
+                    }
+
                     $fields_array[$field->name] = [
                             'field' => e($field->convertUnicodeDbSlug()),
                             'value' => e($value),
@@ -105,14 +113,18 @@ class AssetsTransformer
                         ];
 
                 } else {
+                    $value = $asset->{$field->convertUnicodeDbSlug()};
+
+                    if ($field->format == 'DATE'){
+                        $value = Helper::getFormattedDateObject($value)['formatted'];
+                    }
                     $fields_array[$field->name] = [
                         'field' => e($field->convertUnicodeDbSlug()),
-                        'value' => e($asset->{$field->convertUnicodeDbSlug()}),
+                        'value' => e($value),
                         'field_format' => $field->format,
                     ];
-
-
                 }
+
                 $array['custom_fields'] = $fields_array;
             }
         } else {
