9994dedb64cea78952749e001ead1d8938c7d57f
snipe <snipe@snipe.net>
2022-06-13 12:16:40 -0700

    New translations general.php (Turkish) Fixed: New translations general.php from Crowdin

--- resources/lang/tr-TR/general.php
+++ resources/lang/tr-TR/general.php
@@ -1,23 +1,7 @@
 <?php
 
-return array(
-    'asset_categories' 					=> 'Asset Categories',
-    'category_name'  					=> 'Category Name',
-    'checkin_email'                     => 'Send email to user on checkin/checkout.',
-    'checkin_email_notification'        => 'This user will be sent an email on checkin/checkout.',
-    'clone'                             => 'Clone Category',
-    'create'  							=> 'Create Category',
-    'edit'                              => 'Edit Category',
-    'eula_text'							=> 'Category EULA',
-    'eula_text_help'					=> 'This field allows you to customize your EULAs for specific types of assets. If you only have one EULA for all of your assets, you can check the box below to use the primary default.',
-    'name'                              => 'Category Name',
-    'require_acceptance'				=> 'Require users to confirm acceptance of assets in this category.',
-    'required_acceptance'				=> 'This user will be emailed with a link to confirm acceptance of this item.',
-    'required_eula'						=> 'This user will be emailed a copy of the EULA',
-    'no_default_eula'					=> 'No primary default EULA found. Add one in Settings.',
-    'update'  							=> 'Update Category',
-    'use_default_eula'					=> 'Use the <a href="#" data-toggle="modal" data-target="#eulaModal">primary default EULA</a> instead.',
-    'use_default_eula_disabled'			=> '<del>Use the primary default EULA instead.</del> No primary default EULA is set. Please add one in Settings.',
-    'use_default_eula_column'			=> 'Use default EULA',
-
-);
+return [
+    'select_company' => 'Select Company',
+    'about_companies' => 'About Companies',
+    'about_companies_description' => '        You can use companies as a simple informative field, or you can use them to restrict asset visibility and availability to users with a specific company by enabling Full Company Support in your Admin Settings.',
+];
