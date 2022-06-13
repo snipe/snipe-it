4d09d40e1ddced8e8eed08b1890cf51adaf41787
snipe <snipe@snipe.net>
2022-06-13 12:21:32 -0700

    New translations table.php (Norwegian) Fixed: New translations table.php from Crowdin

--- resources/lang/no-NO/table.php
+++ resources/lang/no-NO/table.php
@@ -1,9 +1,5 @@
 <?php
+
 return array(
-    'companies' => 'Companies',
-    'create'    => 'Create Company',
-    'title'     => 'Company',
-    'update'    => 'Update Company',
-    'name'      => 'Company Name',
-    'id'        => 'ID',
+    'title'      				=> 'Component Name',
 );
