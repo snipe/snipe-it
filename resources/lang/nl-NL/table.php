98cb1fa822e4d3d94bb199830186c0b10defa7d5
snipe <snipe@snipe.net>
2022-06-13 16:56:09 -0700

    New translations table.php (Dutch) Fixed: New translations table.php from Crowdin

--- resources/lang/nl-NL/table.php
+++ resources/lang/nl-NL/table.php
@@ -1,9 +1,30 @@
 <?php
 
-return array(
+return [
 
-    'id'         => 'Id',
-    'name'       => 'Name',
-    'users'      => '# of Users',
-
-);
+    'asset_tag'   	=> 'Asset Tag',
+    'asset_model'       => 'Model',
+    'book_value'  	=> 'Current Value',
+    'change' 		=> 'In/Out',
+    'checkout_date' => 'Checkout Date',
+    'checkoutto' 	=> 'Checked Out',
+    'current_value' => 'Current Value',
+    'diff' 			=> 'Diff',
+    'dl_csv' 		=> 'Download CSV',
+    'eol' 			=> 'EOL',
+    'id'      		=> 'ID',
+    'location' 		=> 'Location',
+    'purchase_cost'	=> 'Cost',
+    'purchase_date'	=> 'Purchased',
+    'serial'   		=> 'Serial',
+    'status'   		=> 'Status',
+    'title'      	=> 'Asset ',
+    'image'		    => 'Device Image',
+    'days_without_acceptance' => 'Days Without Acceptance',
+    'monthly_depreciation' => 'Monthly Depreciation',
+    'assigned_to'    => 'Assigned To',
+    'requesting_user' => 'Requesting User',
+    'requested_date' => 'Requested Date',
+    'changed'       => 'Changed',
+    'icon' => 'Icon',
+];
