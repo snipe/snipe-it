0dcd484f55b001c03635e8515ab94bddd36df0d5
snipe <snipe@snipe.net>
2022-06-13 16:49:27 -0700

    New translations message.php (Vietnamese) Fixed: New translations message.php from Crowdin

--- resources/lang/vi-VN/message.php
+++ resources/lang/vi-VN/message.php
@@ -2,56 +2,24 @@
 
 return array(
 
-    'field' => array(
-        'invalid'   => 'That field does not exist.',
-        'already_added'   => 'Field already added',
+    'does_not_exist' => 'Depreciation class does not exist.',
+    'assoc_users'	 => 'This depreciation is currently associated with one or more models and cannot be deleted. Please delete the models, and then try deleting again. ',
 
-        'create' => array(
-            'error'   => 'Field was not created, please try again.',
-            'success' => 'Field created successfully.',
-            'assoc_success' => 'Field successfully added to fieldset.'
-        ),
 
-        'update' => array(
-            'error'   => 'Field was not updated, please try again',
-            'success' => 'Field updated successfully.'
-        ),
-
-        'delete' => array(
-            'confirm'   	=> 'Are you sure you wish to delete this field?',
-            'error'   => 'There was an issue deleting the field. Please try again.',
-            'success' => 'The field was deleted successfully.',
-            'in_use'   => 'Field is still in use.',
-        )
-
+    'create' => array(
+        'error'   => 'Depreciation class was not created, please try again. :(',
+        'success' => 'Depreciation class created successfully. :)'
     ),
 
-    'fieldset' => array(
-
-        'does_not_exist' => 'Fieldset does not exist',
-
-        'create' => array(
-            'error'   => 'Fieldset was not created, please try again.',
-            'success' => 'Fieldset created successfully.'
-        ),
-
-        'update' => array(
-            'error'   => 'Fieldset was not updated, please try again',
-            'success' => 'Fieldset updated successfully.'
-        ),
-
-        'delete' => array(
-            'confirm'   	=> 'Are you sure you wish to delete this fieldset?',
-            'error'   => 'There was an issue deleting the fieldset. Please try again.',
-            'success' => 'The fieldset was deleted successfully.',
-            'in_use'   => 'Fieldset is still in use.',
-        )
-
+    'update' => array(
+        'error'   => 'Depreciation class was not updated, please try again',
+        'success' => 'Depreciation class updated successfully.'
     ),
 
-
-
-
-
+    'delete' => array(
+        'confirm'   => 'Are you sure you wish to delete this depreciation class?',
+        'error'   => 'There was an issue deleting the depreciation class. Please try again.',
+        'success' => 'The depreciation class was deleted successfully.'
+    )
 
 );
