da93f8e544909a72667d3ad42ac961ea5556da25
snipe <snipe@snipe.net>
2022-06-13 16:55:31 -0700

    New translations message.php (Slovak) Fixed: New translations message.php from Crowdin

--- resources/lang/sk-SK/message.php
+++ resources/lang/sk-SK/message.php
@@ -1,25 +1,84 @@
 <?php
 
-return array(
+return [
 
-    'does_not_exist' => 'Depreciation class does not exist.',
-    'assoc_users'	 => 'This depreciation is currently associated with one or more models and cannot be deleted. Please delete the models, and then try deleting again. ',
+    'undeployable' 		=> '<strong>Warning: </strong> This asset has been marked as currently undeployable.
+                        If this status has changed, please update the asset status.',
+    'does_not_exist' 	=> 'Asset does not exist.',
+    'does_not_exist_or_not_requestable' => 'That asset does not exist or is not requestable.',
+    'assoc_users'	 	=> 'This asset is currently checked out to a user and cannot be deleted. Please check the asset in first, and then try deleting again. ',
+
+    'create' => [
+        'error'   		=> 'Asset was not created, please try again. :(',
+        'success' 		=> 'Asset created successfully. :)',
+    ],
+
+    'update' => [
+        'error'   			=> 'Asset was not updated, please try again',
+        'success' 			=> 'Asset updated successfully.',
+        'nothing_updated'	=>  'No fields were selected, so nothing was updated.',
+        'no_assets_selected'  =>  'No assets were selected, so nothing was updated.',
+    ],
+
+    'restore' => [
+        'error'   		=> 'Asset was not restored, please try again',
+        'success' 		=> 'Asset restored successfully.',
+    ],
+
+    'audit' => [
+        'error'   		=> 'Asset audit was unsuccessful. Please try again.',
+        'success' 		=> 'Asset audit successfully logged.',
+    ],
 
 
-    'create' => array(
-        'error'   => 'Depreciation class was not created, please try again. :(',
-        'success' => 'Depreciation class created successfully. :)'
-    ),
+    'deletefile' => [
+        'error'   => 'File not deleted. Please try again.',
+        'success' => 'File successfully deleted.',
+    ],
 
-    'update' => array(
-        'error'   => 'Depreciation class was not updated, please try again',
-        'success' => 'Depreciation class updated successfully.'
-    ),
+    'upload' => [
+        'error'   => 'File(s) not uploaded. Please try again.',
+        'success' => 'File(s) successfully uploaded.',
+        'nofiles' => 'You did not select any files for upload, or the file you are trying to upload is too large',
+        'invalidfiles' => 'One or more of your files is too large or is a filetype that is not allowed. Allowed filetypes are png, gif, jpg, doc, docx, pdf, and txt.',
+    ],
 
-    'delete' => array(
-        'confirm'   => 'Are you sure you wish to delete this depreciation class?',
-        'error'   => 'There was an issue deleting the depreciation class. Please try again.',
-        'success' => 'The depreciation class was deleted successfully.'
-    )
+    'import' => [
+        'error'                 => 'Some items did not import correctly.',
+        'errorDetail'           => 'The following Items were not imported because of errors.',
+        'success'               => 'Your file has been imported',
+        'file_delete_success'   => 'Your file has been been successfully deleted',
+        'file_delete_error'      => 'The file was unable to be deleted',
+    ],
 
-);
+
+    'delete' => [
+        'confirm'   	=> 'Are you sure you wish to delete this asset?',
+        'error'   		=> 'There was an issue deleting the asset. Please try again.',
+        'nothing_updated'   => 'No assets were selected, so nothing was deleted.',
+        'success' 		=> 'The asset was deleted successfully.',
+    ],
+
+    'checkout' => [
+        'error'   		=> 'Asset was not checked out, please try again',
+        'success' 		=> 'Asset checked out successfully.',
+        'user_does_not_exist' => 'That user is invalid. Please try again.',
+        'not_available' => 'That asset is not available for checkout!',
+        'no_assets_selected' => 'You must select at least one asset from the list',
+    ],
+
+    'checkin' => [
+        'error'   		=> 'Asset was not checked in, please try again',
+        'success' 		=> 'Asset checked in successfully.',
+        'user_does_not_exist' => 'That user is invalid. Please try again.',
+        'already_checked_in'  => 'That asset is already checked in.',
+
+    ],
+
+    'requests' => [
+        'error'   		=> 'Asset was not requested, please try again',
+        'success' 		=> 'Asset requested successfully.',
+        'canceled'      => 'Checkout request successfully canceled',
+    ],
+
+];
