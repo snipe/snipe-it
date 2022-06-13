263daf8b0a2ce1c15521c4393710f836a6dff306
snipe <snipe@snipe.net>
2022-06-13 16:49:04 -0700

    New translations general.php (English, Indonesia) Fixed: New translations general.php from Crowdin

--- resources/lang/en-ID/general.php
+++ resources/lang/en-ID/general.php
@@ -1,20 +1,16 @@
 <?php
 
-return array(
-    'accessory_category' 				=> 'Accessory Category',
-    'accessory_name'  					=> 'Accessory Name',
-    'checkout'  							=> 'Checkout Accessory',
-    'checkin'  							=> 'Checkin Accessory',
-    'create'  							=> 'Create Accessory',
-    'edit'  							=> 'Edit Accessory',
-    'eula_text'							=> 'Category EULA',
-    'eula_text_help'					=> 'This field allows you to customize your EULAs for specific types of assets. If you only have one EULA for all of your assets, you can check the box below to use the primary default.',
-    'require_acceptance'				=> 'Require users to confirm acceptance of assets in this category.',
-    'no_default_eula'					=> 'No primary default EULA found. Add one in Settings.',
-    'total'  							=> 'Total',
-    'remaining'  						=> 'Avail',
-    'update'  							=> 'Update Accessory',
-    'use_default_eula'					=> 'Use the <a href="#" data-toggle="modal" data-target="#eulaModal">primary default EULA</a> instead.',
-    'use_default_eula_disabled'			=> '<del>Use the primary default EULA instead.</del> No primary default EULA is set. Please add one in Settings.',
-
-);
+return [
+    'about_asset_depreciations'  			=> 'About Asset Depreciations',
+    'about_depreciations'  					=> 'You can set up asset depreciations to depreciate assets based on straight-line depreciation.',
+    'asset_depreciations'  					=> 'Asset Depreciations',
+    'create'  					            => 'Create Depreciation',
+    'depreciation_name'  					=> 'Depreciation Name',
+    'depreciation_min'                      => 'Floor Value of Depreciation',
+    'number_of_months'  					=> 'Number of Months',
+    'update'  					            => 'Update Depreciation',
+    'depreciation_min'                      => 'Minimum Value after Depreciation',
+    'no_depreciations_warning'               => '<strong>Warning: </strong>
+                      You do not currently have any depreciations set up.
+                      Please set up at least one depreciation to view the depreciation report.',
+];
