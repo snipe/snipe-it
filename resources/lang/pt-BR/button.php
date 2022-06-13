3be0d6701a58f2abe40d243731ba222925a19a68
snipe <snipe@snipe.net>
2022-06-13 16:57:45 -0700

    New translations button.php (Portuguese, Brazilian) Fixed: New translations button.php from Crowdin

--- resources/lang/pt-BR/button.php
+++ resources/lang/pt-BR/button.php
@@ -1,24 +1,6 @@
 <?php
 
-return [
-    'actions' 	                => 'Ações',
-    'add'    	                => 'Adicionar Novo',
-    'cancel'                    => 'Cancelar',
-    'checkin_and_delete'  	    => 'Atribua e Exclua Usuário',
-    'delete'  	                => 'Excluir',
-    'edit'    	                => 'Editar',
-    'restore' 	                => 'Restaurar',
-    'remove'                    => 'Remove',
-    'request'                   => 'Solicitação',
-    'submit'  	                => 'Enviar',
-    'upload'                    => 'Carregar',
-    'select_file'				=> 'Selecionar arquivo...',
-    'select_files'				=> 'Selecionar arquivos...',
-    'generate_labels'           => '{1} Gerar Etiqueta|[2,*] Gerar Etiquetas',
-    'send_password_link'        => 'Enviar Link de redefinição de senha',
-    'go'                        => 'Go',
-    'bulk_actions'              => 'Bulk Actions',
-    'add_maintenance'           => 'Add Maintenance',
-    'append'                    => 'Append',
-    'new'                       => 'New',
-];
+return array(
+
+    'process' => 'Process',
+);
