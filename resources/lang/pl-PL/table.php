9899543a3a1148d07fc4984037af3150c0351d20
snipe <snipe@snipe.net>
2022-06-13 16:44:00 -0700

    New translations table.php (Polish) Fixed: New translations table.php from Crowdin

--- resources/lang/pl-PL/table.php
+++ resources/lang/pl-PL/table.php
@@ -1,11 +1,5 @@
 <?php
 
 return array(
-	'dl_csv'      				=> 'Download CSV',
-	'eula_text'      			=> 'Licencja',
-    'id'      					=> 'ID',
-    'require_acceptance'      	=> 'Acceptance',
-    'title'      				=> 'Accessory Name',
-
-
+    'title'      				=> 'Consumable Name',
 );
