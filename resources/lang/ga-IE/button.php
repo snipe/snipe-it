b04818b9fadf39dbdc949cf72a9f3c493bebe2f0
snipe <snipe@snipe.net>
2022-06-13 16:56:47 -0700

    New translations button.php (Irish) Fixed: New translations button.php from Crowdin

--- resources/lang/ga-IE/button.php
+++ resources/lang/ga-IE/button.php
@@ -1,24 +1,6 @@
 <?php
 
-return [
-    'actions' 	                => 'Gníomhartha',
-    'add'    	                => 'Cuir Nua',
-    'cancel'                    => 'Cealaigh',
-    'checkin_and_delete'  	    => 'Checkin & Scrios Úsáideoir',
-    'delete'  	                => 'Scrios',
-    'edit'    	                => 'Athraigh',
-    'restore' 	                => 'Athchóirigh',
-    'remove'                    => 'Remove',
-    'request'                   => 'Iarratas',
-    'submit'  	                => 'Cuir isteach',
-    'upload'                    => 'Uaslódáil',
-    'select_file'				=> 'Roghnaigh Comhad ...',
-    'select_files'				=> 'Select Files...',
-    'generate_labels'           => '{1} Generate Label|[2,*] Generate Labels',
-    'send_password_link'        => 'Send Password Reset Link',
-    'go'                        => 'Go',
-    'bulk_actions'              => 'Bulk Actions',
-    'add_maintenance'           => 'Add Maintenance',
-    'append'                    => 'Append',
-    'new'                       => 'New',
-];
+return array(
+
+    'process' => 'Process',
+);
