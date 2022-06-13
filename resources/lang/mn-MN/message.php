7d6b0c20b2bf93daf32f38a006385af5425b2a63
snipe <snipe@snipe.net>
2022-06-13 12:13:41 -0700

    New translations message.php (Mongolian) Fixed: New translations message.php from Crowdin

--- resources/lang/mn-MN/message.php
+++ resources/lang/mn-MN/message.php
@@ -1,21 +1,25 @@
 <?php
 
-    return [
-        'not_found'                    => 'Asset Maintenance you were looking for was not found!',
-        'delete'                       => [
-            'confirm' => 'Are you sure you wish to delete this asset maintenance?',
-            'error'   => 'There was an issue deleting the asset maintenance. Please try again.',
-            'success' => 'The asset maintenance was deleted successfully.',
-        ],
-        'create'                       => [
-            'error'   => 'Asset Maintenance was not created, please try again.',
-            'success' => 'Asset Maintenance created successfully.',
-        ],
-        'edit'                       => [
-            'error'   => 'Asset Maintenance was not edited, please try again.',
-            'success' => 'Asset Maintenance edited successfully.',
-        ],
-        'asset_maintenance_incomplete' => 'Not Completed Yet',
-        'warranty'                     => 'Warranty',
-        'not_warranty'                 => 'Not Warranty',
-    ];
+return array(
+
+    'does_not_exist' => 'Category does not exist.',
+    'assoc_models'	 => 'This category is currently associated with at least one model and cannot be deleted. Please update your models to no longer reference this category and try again. ',
+    'assoc_items'	 => 'This category is currently associated with at least one :asset_type and cannot be deleted. Please update your :asset_type  to no longer reference this category and try again. ',
+
+    'create' => array(
+        'error'   => 'Category was not created, please try again.',
+        'success' => 'Category created successfully.'
+    ),
+
+    'update' => array(
+        'error'   => 'Category was not updated, please try again',
+        'success' => 'Category updated successfully.'
+    ),
+
+    'delete' => array(
+        'confirm'   => 'Are you sure you wish to delete this category?',
+        'error'   => 'There was an issue deleting the category. Please try again.',
+        'success' => 'The category was deleted successfully.'
+    )
+
+);
