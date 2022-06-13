852b23b27a035fb41a61039879eb9d3ae11df41a
snipe <snipe@snipe.net>
2022-06-13 16:46:40 -0700

    New translations message.php (Persian) Fixed: New translations message.php from Crowdin

--- resources/lang/fa-IR/message.php
+++ resources/lang/fa-IR/message.php
@@ -2,56 +2,20 @@
 
 return array(
 
-    'field' => array(
-        'invalid'   => 'That field does not exist.',
-        'already_added'   => 'Field already added',
-
-        'create' => array(
-            'error'   => 'Field was not created, please try again.',
-            'success' => 'Field created successfully.',
-            'assoc_success' => 'Field successfully added to fieldset.'
-        ),
-
-        'update' => array(
-            'error'   => 'Field was not updated, please try again',
-            'success' => 'Field updated successfully.'
-        ),
-
-        'delete' => array(
-            'confirm'   	=> 'Are you sure you wish to delete this field?',
-            'error'   => 'There was an issue deleting the field. Please try again.',
-            'success' => 'The field was deleted successfully.',
-            'in_use'   => 'Field is still in use.',
-        )
-
+    'does_not_exist' => 'Department does not exist.',
+    'assoc_users'	 => 'This department is currently associated with at least one user and cannot be deleted. Please update your users to no longer reference this department and try again. ',
+    'create' => array(
+        'error'   => 'Department was not created, please try again.',
+        'success' => 'Department created successfully.'
     ),
-
-    'fieldset' => array(
-
-        'does_not_exist' => 'Fieldset does not exist',
-
-        'create' => array(
-            'error'   => 'Fieldset was not created, please try again.',
-            'success' => 'Fieldset created successfully.'
-        ),
-
-        'update' => array(
-            'error'   => 'Fieldset was not updated, please try again',
-            'success' => 'Fieldset updated successfully.'
-        ),
-
-        'delete' => array(
-            'confirm'   	=> 'Are you sure you wish to delete this fieldset?',
-            'error'   => 'There was an issue deleting the fieldset. Please try again.',
-            'success' => 'The fieldset was deleted successfully.',
-            'in_use'   => 'Fieldset is still in use.',
-        )
-
+    'update' => array(
+        'error'   => 'Department was not updated, please try again',
+        'success' => 'Department updated successfully.'
     ),
-
-
-
-
-
+    'delete' => array(
+        'confirm'   	=> 'Are you sure you wish to delete this department?',
+        'error'   => 'There was an issue deleting the department. Please try again.',
+        'success' => 'The department was deleted successfully.'
+    )
 
 );
