c734e66fac5818d9cd47f3cb0a556d0f0df7a34a
snipe <snipe@snipe.net>
2022-06-13 16:46:36 -0700

    New translations message.php (Latvian) Fixed: New translations message.php from Crowdin

--- resources/lang/lv-LV/message.php
+++ resources/lang/lv-LV/message.php
@@ -2,36 +2,20 @@
 
 return array(
 
-    'does_not_exist' => 'The accessory [:id] does not exist.',
-    'assoc_users'	 => 'This accessory currently has :count items checked out to users. Please check in the accessories and and try again. ',
-
+    'does_not_exist' => 'Department does not exist.',
+    'assoc_users'	 => 'This department is currently associated with at least one user and cannot be deleted. Please update your users to no longer reference this department and try again. ',
     'create' => array(
-        'error'   => 'The accessory was not created, please try again.',
-        'success' => 'The accessory was successfully created.'
+        'error'   => 'Department was not created, please try again.',
+        'success' => 'Department created successfully.'
     ),
-
     'update' => array(
-        'error'   => 'The accessory was not updated, please try again',
-        'success' => 'The accessory was updated successfully.'
+        'error'   => 'Department was not updated, please try again',
+        'success' => 'Department updated successfully.'
     ),
-
     'delete' => array(
-        'confirm'   => 'Are you sure you wish to delete this accessory?',
-        'error'   => 'There was an issue deleting the accessory. Please try again.',
-        'success' => 'The accessory was deleted successfully.'
-    ),
-
-     'checkout' => array(
-        'error'   		=> 'Accessory was not checked out, please try again',
-        'success' 		=> 'Accessory checked out successfully.',
-        'user_does_not_exist' => 'That user is invalid. Please try again.'
-    ),
-
-    'checkin' => array(
-        'error'   		=> 'Accessory was not checked in, please try again',
-        'success' 		=> 'Accessory checked in successfully.',
-        'user_does_not_exist' => 'That user is invalid. Please try again.'
+        'confirm'   	=> 'Are you sure you wish to delete this department?',
+        'error'   => 'There was an issue deleting the department. Please try again.',
+        'success' => 'The department was deleted successfully.'
     )
-
 
 );
