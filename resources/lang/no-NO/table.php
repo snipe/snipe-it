c6dd264290e077990d94e36afa39e4a483ec5c4f
snipe <snipe@snipe.net>
2022-06-13 20:53:15 -0700

    New translations table.php (Norwegian) Fixed: New translations table.php from Crowdin

--- resources/lang/no-NO/table.php
+++ resources/lang/no-NO/table.php
@@ -1,17 +1,16 @@
 <?php
 
 return array(
-
-    'assigned_to'   	=> 'Assigned To',
-    'checkout'   		=> 'In/Out',
-    'id'      			=> 'ID',
-    'license_email'   	=> 'License Email',
-    'license_name'   	=> 'Licensed To',
-    'purchase_date'   	=> 'Purchase Date',
-    'purchased'   		=> 'Purchased',
-    'seats'   			=> 'Seats',
-    'hardware'   		=> 'Hardware',
-    'serial'   			=> 'Serial',
-    'title'      		=> 'License',
+    'about_manufacturers_title'    => 'About manufacturers',
+    'about_manufacturers_text'  => 'Manufacturers are the companies that create your assets. You can store important support contact information about them here, which will be displayed on your asset detail pages.',
+    'asset_manufacturers'	=> 'Asset Manufacturers',
+    'create'				=> 'Create Manufacturer',
+    'id'   					=> 'ID',
+    'name'      			=> 'Name',
+    'support_email'   		=> 'Support Email',
+    'support_phone'   		=> 'Support Phone',
+    'support_url'   		=> 'Support URL',
+    'update'				=> 'Update Manufacturer',
+    'url'   				=> 'URL',
 
 );
