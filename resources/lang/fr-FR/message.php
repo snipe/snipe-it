af9b6955b0733093d48fe5f190fa720bfd96c146
snipe <snipe@snipe.net>
2022-06-13 20:56:48 -0700

    New translations message.php (French) Fixed: New translations message.php from Crowdin

--- resources/lang/fr-FR/message.php
+++ resources/lang/fr-FR/message.php
@@ -1,29 +1,5 @@
 <?php
 
 return array(
-
-    'does_not_exist' => 'Manufacturer does not exist.',
-    'assoc_users'	 => 'This manufacturer is currently associated with at least one model and cannot be deleted. Please update your models to no longer reference this manufacturer and try again. ',
-
-    'create' => array(
-        'error'   => 'Manufacturer was not created, please try again.',
-        'success' => 'Manufacturer created successfully.'
-    ),
-
-    'update' => array(
-        'error'   => 'Manufacturer was not updated, please try again',
-        'success' => 'Manufacturer updated successfully.'
-    ),
-
-    'restore' => array(
-        'error'   => 'Manufacturer was not restored, please try again',
-        'success' => 'Manufacturer restored successfully.'
-    ),
-
-    'delete' => array(
-        'confirm'   => 'Are you sure you wish to delete this manufacturer?',
-        'error'   => 'There was an issue deleting the manufacturer. Please try again.',
-        'success' => 'The Manufacturer was deleted successfully.'
-    )
-
+    'error'   => 'You must select at least ONE option.'
 );
