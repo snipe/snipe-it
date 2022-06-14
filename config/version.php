3e8837dd6ec6f93dd861eefb3d4cfbb68062140a
snipe <snipe@snipe.net>
2022-06-13 22:13:18 -0700

    Bumped dev branch to 6.0.3
    Signed-off-by: snipe <snipe@snipe.net>

--- config/version.php
+++ config/version.php
@@ -1,10 +1,10 @@
 <?php
 return array (
-  'app_version' => 'v6.0.2',
-  'full_app_version' => 'v6.0.2 - build 7961-g5bc5e9f10',
-  'build_version' => '7961',
+  'app_version' => 'v6.0.3',
+  'full_app_version' => 'v6.0.3 - build 8046-gea1ff1e8b',
+  'build_version' => '8046',
   'prerelease_version' => '',
-  'hash_version' => 'g5bc5e9f10',
-  'full_hash' => 'v6.0.2-69-g5bc5e9f10',
-  'branch' => 'master',
+  'hash_version' => 'gea1ff1e8b',
+  'full_hash' => 'v6.0.3-154-gea1ff1e8b',
+  'branch' => 'develop',
 );
