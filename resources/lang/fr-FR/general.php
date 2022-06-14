1569e9463506563a164cd61df0388bd8beaa397d
snipe <snipe@snipe.net>
2022-06-13 20:56:00 -0700

    New translations general.php (French) Fixed: New translations general.php from Crowdin

--- resources/lang/fr-FR/general.php
+++ resources/lang/fr-FR/general.php
@@ -1,10 +1,10 @@
 <?php
 
-return array(
-    'assigned_location' 	                => 'Assigned to :location Location',
-    'asset_management_system' => 'Asset Management System',
-    'assigned_to' => 'Assigned To:',
-    'manager' => 'Manager',
-    'date' => 'Current Date:',
-
-);
+return [
+    'info'   => 'Select the options you want for your asset report.',
+    'deleted_user' => 'Deleted user',
+    'send_reminder' => 'Send reminder',
+    'reminder_sent' => 'Reminder sent',
+    'acceptance_deleted' => 'Acceptance request deleted',
+    'acceptance_request' => 'Acceptance request'
+];
