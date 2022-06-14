bdc39f3f7fcc5d12437baec176c283c7dee4fd6a
snipe <snipe@snipe.net>
2022-06-13 20:54:37 -0700

    New translations message.php (Hungarian) Fixed: New translations message.php from Crowdin

--- resources/lang/hu-HU/message.php
+++ resources/lang/hu-HU/message.php
@@ -2,28 +2,41 @@
 
 return array(
 
-    'does_not_exist' => 'Manufacturer does not exist.',
-    'assoc_users'	 => 'This manufacturer is currently associated with at least one model and cannot be deleted. Please update your models to no longer reference this manufacturer and try again. ',
+    'does_not_exist' => 'Model does not exist.',
+    'assoc_users'	 => 'This model is currently associated with one or more assets and cannot be deleted. Please delete the assets, and then try deleting again. ',
+
 
     'create' => array(
-        'error'   => 'Manufacturer was not created, please try again.',
-        'success' => 'Manufacturer created successfully.'
+        'error'   => 'Model was not created, please try again.',
+        'success' => 'Model created successfully.',
+        'duplicate_set' => 'An asset model with that name, manufacturer and model number already exists.',
     ),
 
     'update' => array(
-        'error'   => 'Manufacturer was not updated, please try again',
-        'success' => 'Manufacturer updated successfully.'
-    ),
-
-    'restore' => array(
-        'error'   => 'Manufacturer was not restored, please try again',
-        'success' => 'Manufacturer restored successfully.'
+        'error'   => 'Model was not updated, please try again',
+        'success' => 'Model updated successfully.'
     ),
 
     'delete' => array(
-        'confirm'   => 'Are you sure you wish to delete this manufacturer?',
-        'error'   => 'There was an issue deleting the manufacturer. Please try again.',
-        'success' => 'The Manufacturer was deleted successfully.'
-    )
+        'confirm'   => 'Are you sure you wish to delete this asset model?',
+        'error'   => 'There was an issue deleting the model. Please try again.',
+        'success' => 'The model was deleted successfully.'
+    ),
+
+    'restore' => array(
+        'error'   		=> 'Model was not restored, please try again',
+        'success' 		=> 'Model restored successfully.'
+    ),
+
+    'bulkedit' => array(
+        'error'   		=> 'No fields were changed, so nothing was updated.',
+        'success' 		=> 'Models updated.'
+    ),
+
+    'bulkdelete' => array(
+        'error'   		    => 'No models were selected, so nothing was deleted.',
+        'success' 		    => ':success_count model(s) deleted!',
+        'success_partial' 	=> ':success_count model(s) were deleted, however :fail_count were unable to be deleted because they still have assets associated with them.'
+    ),
 
 );
