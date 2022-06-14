2bff87c187552e882cf2ca1c442aa5e949565c88
snipe <snipe@snipe.net>
2022-06-13 17:27:11 -0700

    New translations form.php (Persian) Fixed: New translations form.php from Crowdin

--- resources/lang/fa-IR/form.php
+++ resources/lang/fa-IR/form.php
@@ -1,14 +1,22 @@
 <?php
 
-    return [
-        'asset_maintenance_type' => 'Asset Maintenance Type',
-        'title'                  => 'Title',
-        'start_date'             => 'Start Date',
-        'completion_date'        => 'Completion Date',
-        'cost'                   => 'Cost',
-        'is_warranty'            => 'Warranty Improvement',
-        'asset_maintenance_time' => 'Asset Maintenance Time (in days)',
-        'notes'                  => 'Notes',
-        'update'                 => 'Update Asset Maintenance',
-        'create'                 => 'Create Asset Maintenance'
-    ];
+return array(
+
+    'asset'             => 'Asset',
+    'checkin'           => 'Checkin',
+    'create'            => 'Create License',
+    'expiration'        => 'Expiration Date',
+    'license_key'       => 'Product Key',
+    'maintained'        => 'Maintained',
+    'name'              => 'Software Name',
+    'no_depreciation'   => 'Do Not Depreciate',
+    'purchase_order'    => 'Purchase Order Number',
+    'reassignable'      => 'Reassignable',
+    'remaining_seats'   => 'Remaining Seats',
+    'seats'             => 'Seats',
+    'termination_date'  => 'Termination Date',
+    'to_email'          => 'Licensed to Email',
+    'to_name'           => 'Licensed to Name',
+    'update'            => 'Update License',
+    'checkout_help'     => 'You must check a license out to a hardware asset or a person. You can select both, but the owner of the asset must match the person you\'re checking the asset out to.'
+);
