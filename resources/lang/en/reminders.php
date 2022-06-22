1b6df232aaab0be7eee423d0166240fbf51f229b
snipe <snipe@snipe.net>
2022-06-21 18:41:02 -0700

    Updated string
    Signed-off-by: snipe <snipe@snipe.net>

--- resources/lang/en/reminders.php
+++ resources/lang/en/reminders.php
@@ -17,7 +17,7 @@ return array(
 
     "user"     => "Username or email address is incorrect",
 
-    "token"    =>  "This password reset token is invalid or expired.",
+    "token"    => 'This password reset token is invalid or expired, or does not match the username provided.',
 
     "sent" => "If a matching email address was found, a password reminder has been sent!",
 
