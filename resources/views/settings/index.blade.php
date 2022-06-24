75d19d815d4d52b3918e295f997a48d42283674d
snipe <snipe@snipe.net>
2022-06-24 15:59:40 -0700

    Still show the purge button even if not allowed to avoid confusion
    Signed-off-by: snipe <snipe@snipe.net>

--- resources/views/settings/index.blade.php
+++ resources/views/settings/index.blade.php
@@ -314,7 +314,7 @@
           </div>
         @endif
 
-      @if (config('app.allow_purge')=='true')
+
     <div class="col-md-4 col-lg-3 col-sm-6 col-xl-1">
       <div class="box box-danger">
         <div class="box-body text-center">
@@ -329,10 +329,6 @@
           <p class="help-block">{{ trans('admin/settings/general.purge_help') }}</p>
         </div>
       </div>
-    </div>
-        @endif
-
-
     </div>
   </div>
 
