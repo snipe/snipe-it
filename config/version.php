393c32558b8406a1820863509dbe236315d505e1
snipe <snipe@snipe.net>
2022-06-27 19:31:26 -0700

    Gotta bump that hash up
    Signed-off-by: snipe <snipe@snipe.net>

--- config/version.php
+++ config/version.php
@@ -1,10 +1,10 @@
 <?php
 return array (
   'app_version' => 'v6.0.5',
-  'full_app_version' => 'v6.0.5 - build 8126-g89c234b1c',
-  'build_version' => '8126',
+  'full_app_version' => 'v6.0.5 - build 8229-g94e723a88',
+  'build_version' => '8229',
   'prerelease_version' => '',
-  'hash_version' => 'g89c234b1c',
-  'full_hash' => 'v6.0.5-74-g89c234b1c',
-  'branch' => 'master',
+  'hash_version' => 'g94e723a88',
+  'full_hash' => 'v6.0.5-101-g94e723a88',
+  'branch' => 'develop',
 );
