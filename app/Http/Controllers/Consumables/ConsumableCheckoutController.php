53ff28d2b0d03a91c85ceb1c978a9095619dd2f2
snipe <snipe@snipe.net>
2022-08-01 16:07:56 -0700

    Handle consumables route names
    Signed-off-by: snipe <snipe@snipe.net>

--- app/Http/Controllers/Consumables/ConsumableCheckoutController.php
+++ app/Http/Controllers/Consumables/ConsumableCheckoutController.php
@@ -56,7 +56,7 @@ class ConsumableCheckoutController extends Controller
         // Check if the user exists
         if (is_null($user = User::find($assigned_to))) {
             // Redirect to the consumable management page with error
-            return redirect()->route('checkout/consumable', $consumable)->with('error', trans('admin/consumables/message.checkout.user_does_not_exist'));
+            return redirect()->route('consumables.checkout.show', $consumable)->with('error', trans('admin/consumables/message.checkout.user_does_not_exist'));
         }
 
         // Update the consumable data
