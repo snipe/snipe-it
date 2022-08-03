8e18ce74b25aab169981f7ae0cab9906309c85e1
snipe <snipe@snipe.net>
2022-08-02 19:12:18 -0700

    Update method name
    Signed-off-by: snipe <snipe@snipe.net>

--- app/Presenters/AssetPresenter.php
+++ app/Presenters/AssetPresenter.php
@@ -501,7 +501,7 @@ class AssetPresenter extends Presenter
      * Date the warantee expires.
      * @return false|string
      */
-    public function warrantee_expires()
+    public function warranty_expires()
     {
         if (($this->purchase_date) && ($this->warranty_months)) {
             $date = date_create($this->purchase_date);
