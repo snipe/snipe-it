0172fe6403d938e6b7da98c3371299a3ccf16aa1
Ivan Nieto Vivanco <inietov@gmail.com>
2022-08-11 13:02:25 -0500

    Add Notifiable trait to the CheckoutAcceptance Model

--- app/Models/CheckoutAcceptance.php
+++ app/Models/CheckoutAcceptance.php
@@ -5,10 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Notifications\Notifiable;
 
 class CheckoutAcceptance extends Model
 {
-    use SoftDeletes;
+    use SoftDeletes, Notifiable;
 
     protected $casts = [
         'accepted_at' => 'datetime',
