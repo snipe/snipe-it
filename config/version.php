8a312f69a654cf39706214aef3e1d8d543391838
snipe <snipe@snipe.net>
2022-08-24 16:37:22 -0700

    Bumped hash
    Signed-off-by: snipe <snipe@snipe.net>

--- config/version.php
+++ config/version.php
@@ -1,10 +1,10 @@
 <?php
 return array (
   'app_version' => 'v6.0.9',
-  'full_app_version' => 'v6.0.9 - build 8515-g51f729872',
-  'build_version' => '8515',
+  'full_app_version' => 'v6.0.9 - build 8587-gcca531f8c',
+  'build_version' => '8587',
   'prerelease_version' => '',
-  'hash_version' => 'g51f729872',
-  'full_hash' => 'v6.0.9-103-g51f729872',
-  'branch' => 'master',
+  'hash_version' => 'gcca531f8c',
+  'full_hash' => 'v6.0.9-68-gcca531f8c',
+  'branch' => 'develop',
 );
