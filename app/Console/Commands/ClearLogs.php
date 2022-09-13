d06a5041e4c2e60279b6df142957d2db3e4e3353
Godfrey M <godmartinz@gmail.com>
2022-09-13 12:07:19 -0700

    fixes title

--- app/Console/Commands/ClearLogs.php
+++ app/Console/Commands/ClearLogs.php
@@ -4,7 +4,7 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 
-class CheckoutLicenseToAllUsers extends Command
+class ClearLogs extends Command
 {
     /**
      * The name and signature of the console command.
