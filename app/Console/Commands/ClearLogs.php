80ce2c6f64a940a88308ba280c151c993359ec0b
Godfrey M <godmartinz@gmail.com>
2022-09-19 11:33:51 -0700

    adds clear logs command

---
+++ app/Console/Commands/ClearLogs.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class ClearLogs extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'logs:clear';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Clear log files';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        unlink('storage/logs/laravel.log');
+
+        $this->comment('Logs have been cleared!');
+    }
+}
