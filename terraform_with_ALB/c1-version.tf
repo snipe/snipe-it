b910183e5519b1a4719e9a3330658f49afa90022
rohan1234 <61894395+rohan1234@users.noreply.github.com>
2022-10-10 08:06:06 +0530

    Update c1-version.tf

--- terraform_with_ALB/c1-version.tf
+++ terraform_with_ALB/c1-version.tf
@@ -14,7 +14,7 @@ terraform {
 
 terraform {
   backend "s3" {
-    bucket = "snipe-bucket-tf"
+    bucket = "snipe-terra-tf"
     key    = "path/tf.state"
     region =  "ap-south-1"
     encrypt = true
