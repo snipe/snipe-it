3f6968c5a0dba2652dc4b0a98db9cbc055ba59f8
Godfrey M <godmartinz@gmail.com>
2022-10-26 16:10:37 -0700

    This works..and I'm taking it personally >:(

--- app/Http/Livewire/SlackSettingsForm.php
+++ app/Http/Livewire/SlackSettingsForm.php
@@ -3,22 +3,23 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\Setting;
 use App\Http\Controllers\Api\SettingsController;
 
 
 class SlackSettingsForm extends Component
 {
-    public $setting;
+    public Setting $setting;
 
     protected $rules = [
-         'setting.slack_endpoint'                      => 'url|required_with:slack_channel|starts_with:https://hooks.slack.com/|nullable',
-         'setting.slack_channel'                       => 'required_with:slack_endpoint|starts_with:#|nullable',
-    ];
-
-    public function mount($setting){
-
-        $this->slack_endpoint = $setting->slack_endpoint;
-    }
+        'setting.slack_endpoint'                      => 'url|required_with:slack_channel|starts_with:https://hooks.slack.com/|nullable',
+        'setting.slack_channel'                       => 'required_with:slack_endpoint|starts_with:#|nullable',
+        'setting.slack_botname'                       => 'string|nullable',
+];
+//
+//    public function update($fields){
+//        $this->slacktest($fields);
+//    }
 
     public function render()
     {
