f51b312843398d2883d9d551c18288ef9f3f5539
snipe <snipe@snipe.net>
2022-11-01 19:09:51 -0700

    Added component routes for uploads
    Signed-off-by: snipe <snipe@snipe.net>

--- routes/web/components.php
+++ routes/web/components.php
@@ -25,6 +25,21 @@ Route::group(['prefix' => 'components', 'middleware' => ['auth']], function () {
         [Components\ComponentCheckinController::class, 'store']
     )->name('components.checkin.store');
 
+    Route::post(
+        '{componentId}/upload',
+        [Components\ComponentFilesController::class, 'store']
+    )->name('upload/component');
+
+    Route::delete(
+        '{componentId}/deletefile/{fileId}',
+        [Components\ComponentFilesController::class, 'destroy']
+    )->name('delete/componentfile');
+
+    Route::get(
+        '{componentId}/showfile/{fileId}/{download?}',
+        [Components\ComponentFilesController::class, 'show']
+    )->name('show.componentfile');
+
 });
 
 Route::resource('components', Components\ComponentsController::class, [
