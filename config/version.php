4173dec8f03f0d76df0f71ac1bc661b5c38b7470
snipe <snipe@snipe.net>
2022-11-03 14:18:13 -0700

    Bumped version
    Signed-off-by: snipe <snipe@snipe.net>
    
    (cherry picked from commit 7c21158680397727c3dbb36ae8e3e60779d78d2d)

--- config/version.php
+++ config/version.php
@@ -1,10 +1,10 @@
 <?php
 return array (
-  'app_version' => 'v6.0.10',
-  'full_app_version' => 'v6.0.10 - build 8610-g22710afee',
-  'build_version' => '8610',
+  'app_version' => 'v6.0.13',
+  'full_app_version' => 'v6.0.13 - build 8923-g30f4af3ac',
+  'build_version' => '8923',
   'prerelease_version' => '',
-  'hash_version' => 'g22710afee',
-  'full_hash' => 'v6.0.10-91-g22710afee',
-  'branch' => 'master',
+  'hash_version' => 'g30f4af3ac',
+  'full_hash' => 'v6.0.13-97-g30f4af3ac',
+  'branch' => 'develop',
 );
