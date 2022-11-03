50fde2ceb92e505c65bea30a33c247f6f7cd35eb
Andrew Murray <andrew.murray@knownhost.com>
2022-11-03 19:45:01 -0400

    Created migrations for serials table

---
+++ database/migrations/2022_10_12_003241_create_serials_table.php
@@ -0,0 +1,55 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSerialsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // Check if the table already exists
+        if (!Schema::hasTable('serials')) {
+            Schema::create('serials', function (Blueprint $table) {
+                $table->id();
+                // Associated component ID
+                $table->unsignedBigInteger('component_id');
+                // Serial number assigned to asset ID
+                $table->unsignedBigInteger('asset_id')->nullable();
+                // Serial number (unique)
+                $table->string('serial_number')->unique();
+                // Serial number notes
+                $table->text('notes')->nullable();
+                // Serial number status (0 = available, 1 = assigned, 2 = reserved)
+                $table->tinyInteger('status')->default(0);
+                // Serial number created at and updated at timestamps
+                $table->timestamps();
+
+                // Foreign key constraints
+                $table->foreign('component_id')->references('id')->on('components')->onDelete('cascade');
+                $table->foreign('asset_id')->references('id')->on('assets')->onDelete('set null');
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        // Drop foreign key constraints
+        Schema::table('serials', function (Blueprint $table) {
+            $table->dropForeign(['component_id']);
+            $table->dropForeign(['asset_id']);
+        });
+
+        Schema::dropIfExists('serials');
+    }
+}
---
+++ database/migrations/2022_10_14_010501_add_purchase_order_date_warranty_date_purchase_cost_column_to_serials_table.php
@@ -0,0 +1,58 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddPurchaseOrderDateWarrantyDatePurchaseCostColumnToSerialsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('serials', function (Blueprint $table) {
+            $table->string('purchase_order')->nullable();
+            $table->date('purchase_date')->nullable();
+            $table->date('warranty_date')->nullable();
+            $table->decimal('purchase_cost', 10, 2)->nullable();
+            $table->bigInteger('supplier_id')->unsigned()->nullable();
+
+            $table->foreign('supplier_id')->references('id')->on('suppliers');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('serials', function (Blueprint $table) {
+            // Make sure the column exists before trying to drop it
+            if (Schema::hasColumn('serials', 'purchase_order')) {
+                $table->dropColumn('purchase_order');
+            }
+
+            if (Schema::hasColumn('serials', 'purchase_date')) {
+                $table->dropColumn('purchase_date');
+            }
+
+            if (Schema::hasColumn('serials', 'warranty_date')) {
+                $table->dropColumn('warranty_date');
+            }
+
+            if (Schema::hasColumn('serials', 'purchase_cost')) {
+                $table->dropColumn('purchase_cost');
+            }
+
+            if (Schema::hasColumn('serials', 'supplier_id')) {
+                $table->dropColumn('supplier_id');
+                $table->dropForeign('serials_supplier_id_foreign');
+            }
+        });
+    }
+}
