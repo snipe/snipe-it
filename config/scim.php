e7bdcc5fe51c46777354164d6c24f2d45798835f
Brady Wetherington <bwetherington@grokability.com>
2022-11-09 17:40:18 +0000

    Document SCIM env vars, update SCIM library to our latest.

--- config/scim.php
+++ config/scim.php
@@ -1,6 +1,8 @@
 <?php
 
 return [
+    "trace" => env("SCIM_TRACE",false),
+    // below, if we ever get 'sure' that we can change this default to 'true' we should
+    "standards_compliance" => env('SCIM_STANDARDS_COMPLIANCE', false),
     "publish_routes" => false,
-    "trace" => env("SCIM_TRACE",false)
 ];
