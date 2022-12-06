36901d271bd7fbb014ab2e0fc1ab8f8d434ff66c
Godfrey M <godmartinz@gmail.com>
2022-12-05 16:28:19 -0800

    adds csvmatch for remote. Im a bit lost though lol

--- app/Importer/UserImporter.php
+++ app/Importer/UserImporter.php
@@ -57,6 +57,7 @@ class UserImporter extends ItemImporter
         $this->item['employee_num'] = $this->findCsvMatch($row, 'employee_num');
         $this->item['department_id'] = $this->createOrFetchDepartment($this->findCsvMatch($row, 'department'));
         $this->item['manager_id'] = $this->fetchManager($this->findCsvMatch($row, 'manager_first_name'), $this->findCsvMatch($row, 'manager_last_name'));
+        $this->item['remote'] = $this->findCsvMatch($row, 'remote');
 
         $user_department = $this->findCsvMatch($row, 'department');
         if ($this->shouldUpdateField($user_department)) {
