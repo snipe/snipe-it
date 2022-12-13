4442b446b994bf81c03175485d69922c0e8619cb
Godfrey M <godmartinz@gmail.com>
2022-12-13 10:30:37 -0800

    adds serial to components tab of assets

--- resources/views/hardware/view.blade.php
+++ resources/views/hardware/view.blade.php
@@ -964,6 +964,7 @@
                                         <th>{{ trans('general.name') }}</th>
                                         <th>{{ trans('general.qty') }}</th>
                                         <th>{{ trans('general.purchase_cost') }}</th>
+                                        <th>{{trans('admin/hardware/form.serial')}}</th>
                                         </thead>
                                         <tbody>
                                         <?php $totalCost = 0; ?>
@@ -977,6 +978,7 @@
                                                     </td>
                                                     <td>{{ $component->pivot->assigned_qty }}</td>
                                                     <td>{{ Helper::formatCurrencyOutput($component->purchase_cost) }} each</td>
+                                                    <td>{{$component->serial}}</td>
 
                                                     <?php $totalCost = $totalCost + ($component->purchase_cost *$component->pivot->assigned_qty) ?>
                                                 </tr>
