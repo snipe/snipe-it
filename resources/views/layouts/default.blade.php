c5c37fa95f919c0bcc9a916fe0864fc1dc501135
snipe <snipe@snipe.net>
2023-01-10 01:54:26 -0800

    Fixed copypasta
    Signed-off-by: snipe <snipe@snipe.net>

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -419,7 +419,7 @@
                     <?php $status_navs = \App\Models\Statuslabel::where('show_in_nav', '=', 1)->withCount('assets as asset_count')->get(); ?>
                     @if (count($status_navs) > 0)
                         @foreach ($status_navs as $status_nav)
-                            <li{!! (Request::is('statuslabels/'.$status_nav->id) == 'Deployed' ? ' class="active"' : '') !!}><a href="{{ route('statuslabels.show', ['statuslabel' => $status_nav->id]) }}">
+                            <li{!! (Request::is('statuslabels/'.$status_nav->id) ? ' class="active"' : '') !!}><a href="{{ route('statuslabels.show', ['statuslabel' => $status_nav->id]) }}">
                                 <i class="fas fa-circle text-grey fa-fw" aria-hidden="true"{!!  ($status_nav->color!='' ? ' style="color: '.e($status_nav->color).'"' : '') !!}></i>
                                  {{ $status_nav->name }} ({{ $status_nav->asset_count }})</a></li>
                         @endforeach
