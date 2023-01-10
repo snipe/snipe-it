7846251d25d91617f36961e5b7f8fe68eb0b9ca8
snipe <snipe@snipe.net>
2023-01-09 20:43:57 -0800

    Re-added return for no results (derp)
    Signed-off-by: snipe <snipe@snipe.net>

--- app/Http/Controllers/Api/AssetsController.php
+++ app/Http/Controllers/Api/AssetsController.php
@@ -394,6 +394,9 @@ class AssetsController extends Controller
 
         }
 
+        // If there are 0 results, return the "no such asset" response
+        return response()->json(Helper::formatStandardApiResponse('error', null, trans('admin/hardware/message.does_not_exist')), 200);
+
     }
 
     /**
