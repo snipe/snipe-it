2e22eeade548c18228a9852e63a0472051bad29e
Godfrey M <godmartinz@gmail.com>
2023-01-25 14:29:20 -0800

    adds one input

--- app/Http/Livewire/SlackSettingsForm.php
+++ app/Http/Livewire/SlackSettingsForm.php
@@ -11,6 +11,11 @@ class SlackSettingsForm extends Component
 {
     public Setting $setting;
 
+    protected $rules = [
+        'setting.slack_endpoint'                      => 'url|required_with:slack_channel|starts_with:https://hooks.slack.com/|nullable',
+        'setting.slack_channel'                       => 'required_with:slack_endpoint|starts_with:#|nullable',
+        'setting.slack_botname'                       => 'string|nullable',
+        ];
 
 
     public function render()
--- resources/views/settings/slack.blade.php
+++ resources/views/settings/slack.blade.php
@@ -50,22 +50,7 @@
                     <div class="col-md-12" style="border-top: 0px;">
 
 
-                        <!-- slack endpoint -->
-                        <div class="form-group required {{ $errors->has('slack_endpoint') ? 'error' : '' }}">
-                            <div class="col-md-2">
-                                {{ Form::label('slack_endpoint', trans('admin/settings/general.slack_endpoint')) }}
-                            </div>
-                            <div class="col-md-10">
-                                @if (config('app.lock_passwords')===true)
-                                    {{ Form::text('slack_endpoint', old('slack_endpoint', $setting->slack_endpoint), array('class' => 'form-control','disabled'=>'disabled','placeholder' => 'https://hooks.slack.com/services/XXXXXXXXXXXXXXXXXXXXX', 'id' => 'slack_endpoint')) }}
-                                    <p class="text-warning"><i class="fas fa-lock"></i> {{ trans('general.feature_disabled') }}</p>
-
-                                @else
-                                    {{ Form::text('slack_endpoint', old('slack_endpoint', $setting->slack_endpoint), array('class' => 'form-control','placeholder' => 'https://hooks.slack.com/services/XXXXXXXXXXXXXXXXXXXXX', 'id' => 'slack_endpoint')) }}
-                                @endif
-                                {!! $errors->first('slack_endpoint', '<span class="alert-msg" aria-hidden="true">:message</span>') !!}
-                            </div>
-                        </div>
+                        @livewire('slack-settings-form',  ['setting' => $setting])
 
                         <!-- slack channel -->
                         <div class="form-group required {{ $errors->has('slack_channel') ? 'error' : '' }}">
