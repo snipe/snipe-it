a8643e5d35f1535bd28000bcbbaa44dc64b36a0a
akemidx <kojotek.dx@gmail.com>
2023-02-01 15:55:16 -0500

    cleaning up/adding variable

--- resources/lang/en/admin/components/general.php
+++ resources/lang/en/admin/components/general.php
@@ -12,5 +12,5 @@ return array(
     'remaining' 			             => 'Remaining',
     'total' 			                 => 'Total',
     'update'                            => 'Update Component',
-    'checkin_limit'            => 'Amount checked in must be equal to or less than this amount'
+    'checkin_limit'            => 'Amount checked in must be equal to or less than :assigned_qty'
 );
