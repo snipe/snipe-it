de1141491aa7441e7153f43e3cb73f0de18a9895
snipe <snipe@snipe.net>
2023-01-31 19:41:42 -0800

    Replaced help text
    Signed-off-by: snipe <snipe@snipe.net>

--- resources/views/custom_fields/fieldsets/edit.blade.php
+++ resources/views/custom_fields/fieldsets/edit.blade.php
@@ -1,13 +1,17 @@
 @extends('layouts/edit-form', [
     'createText' => trans('admin/custom_fields/general.create_fieldset') ,
     'updateText' => trans('admin/custom_fields/general.update_fieldset'),
+    'helpText' => trans('admin/custom_fields/general.about_fieldsets_text'),
+    'helpPosition' => 'right',
     'formAction' => (isset($item->id)) ? route('fieldsets.update', ['fieldset' => $item->id]) : route('fieldsets.store'),
 ])
 
-@section('inputFields')
-
-  @include ('partials.forms.edit.name', ['translated_name' => trans('general.name')])
-
-
-
+@section('content')
+    @parent
 @stop
+
+@section('inputFields')
+@include ('partials.forms.edit.name', ['translated_name' => trans('general.name')])
+@stop
+
+
