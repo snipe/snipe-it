89eff23e44ebe454916e4b87e7c7b16bca982f3d
Marcus Moore <contact@marcusmoore.io>
2023-02-02 17:41:32 -0800

    Continue implementing tests

--- tests/Feature/Api/Users/UsersForSelectListTest.php
+++ tests/Feature/Api/Users/UsersForSelectListTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Api\Users;
 
+use App\Models\Company;
 use App\Models\Setting;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -37,14 +38,43 @@ class UsersForSelectListTest extends TestCase
 
     public function testUsersScopedToCompanyWhenMultipleFullCompanySupportEnabled()
     {
-        $this->markTestIncomplete();
+        Setting::factory()->withMultipleFullCompanySupport()->create();
 
+        [$jedi, $sith] = Company::factory()->count(2)->create();
+
+        User::factory()
+            ->for($sith)
+            ->create(['first_name' => 'Darth', 'last_name' => 'Vader']);
+
+        User::factory()
+            ->for($jedi)
+            ->count(3)
+            ->sequence(
+                ['first_name' => 'Luke', 'last_name' => 'Skywalker'],
+                ['first_name' => 'Obi-Wan', 'last_name' => 'Kenobi'],
+                ['first_name' => 'Anakin', 'last_name' => 'Skywalker'],
+            )
+            ->create();
+
+        Passport::actingAs($jedi->users->first());
+        $response = $this->getJson(route('api.users.selectlist'));
+        $response->assertOk();
+
+        $results = collect($response->json('results'));
+
+        $this->assertEquals($jedi->users->count(), $results->count());
+
+        $this->assertTrue(
+            $results->pluck('text')->contains(fn($text) => str_contains($text, $jedi->users->first()->first_name))
+        );
+
+        $this->assertFalse(
+            $results->pluck('text')->contains(fn($text) => str_contains($text, $sith->users->first()->first_name))
+        );
     }
 
     public function testUsersScopedToCompanyDuringSearchWhenMultipleFullCompanySupportEnabled()
     {
-        $this->markTestIncomplete();
-
-
+        
     }
 }
