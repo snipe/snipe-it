bdac0f16ceb5b7ecbf894b3692f09565140c06c6
Marcus Moore <contact@marcusmoore.io>
2023-02-23 13:39:05 -0800

    Set purchase date field to the correct format in asset factory

--- database/factories/AssetFactory.php
+++ database/factories/AssetFactory.php
@@ -44,7 +44,7 @@ class AssetFactory extends Factory
             'user_id' => 1,
             'asset_tag' => $this->faker->unixTime('now'),
             'notes'   => 'Created by DB seeder',
-            'purchase_date' => $this->faker->dateTimeBetween('-1 years', 'now', date_default_timezone_get()),
+            'purchase_date' => $this->faker->dateTimeBetween('-1 years', 'now', date_default_timezone_get())->format('Y-m-d'),
             'purchase_cost' => $this->faker->randomFloat(2, '299.99', '2999.99'),
             'order_number' => $this->faker->numberBetween(1000000, 50000000),
             'supplier_id' => Supplier::all()->random()->id,
