9b522006f353426966843514d807d205a69038aa
snipe <snipe@snipe.net>
2023-03-01 14:05:53 -0800

    Usew route group for locations
    Signed-off-by: snipe <snipe@snipe.net>

--- routes/web.php
+++ routes/web.php
@@ -40,23 +40,29 @@ Route::group(['middleware' => 'auth'], function () {
         'parameters' => ['category' => 'category_id'],
     ]);
 
-    Route::get('locations/{locationId}/clone',
-        [LocationsController::class, 'getClone']
-    )->name('clone/license');
 
-    Route::get(
-        'locations/{locationId}/printassigned',
-        [LocationsController::class, 'print_assigned']
-    )->name('locations.print_assigned');
-
-    Route::get(
-        'locations/{locationId}/printallassigned',
-        [LocationsController::class, 'print_all_assigned']
-    )->name('locations.print_all_assigned');
 
     /*
-    * Locations
-    */
+     * Locations
+     */
+
+    Route::group(['prefix' => 'locations', 'middleware' => ['auth']], function () {
+        
+        Route::get('{locationId}/clone',
+            [LocationsController::class, 'getClone']
+        )->name('clone/license');
+
+        Route::get(
+            '{locationId}/printassigned',
+            [LocationsController::class, 'print_assigned']
+        )->name('locations.print_assigned');
+
+        Route::get(
+            '{locationId}/printallassigned',
+            [LocationsController::class, 'print_all_assigned']
+        )->name('locations.print_all_assigned');
+    });
+
     Route::resource('locations', LocationsController::class, [
         'parameters' => ['location' => 'location_id'],
     ]);
