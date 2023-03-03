8a5d426ccd7579155935a38c7eaa2463e77d7264
Ivan Nieto Vivanco <inietov@gmail.com>
2023-03-02 19:33:32 -0600

    Use correct LicenseSeat property

--- app/Http/Controllers/Licenses/LicenseCheckinController.php
+++ app/Http/Controllers/Licenses/LicenseCheckinController.php
@@ -61,7 +61,7 @@ class LicenseCheckinController extends Controller
         $license = License::find($licenseSeat->license_id);
 
         // LicenseSeat is not assigned, it can't be checked in
-        if (is_null($licenseSeat->assignedTo) && is_null($licenseSeat->asset_id)) {
+        if (is_null($licenseSeat->assigned_to) && is_null($licenseSeat->asset_id)) {
             return redirect()->route('licenses.index')->with('error', trans('admin/licenses/message.checkin.error'));
         }
 
