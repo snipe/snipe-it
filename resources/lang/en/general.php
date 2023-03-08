e55d02fd596978988b929683ac37b09df59df769
snipe <snipe@snipe.net>
2023-03-07 18:53:53 -0800

    Added new string
    Signed-off-by: snipe <snipe@snipe.net>

--- resources/lang/en/general.php
+++ resources/lang/en/general.php
@@ -148,6 +148,7 @@ return [
     'filetypes_accepted_help'    => 'Accepted filetype is :types. Max upload size allowed is :size.|Accepted filetypes are :types. Max upload size allowed is :size.',
     'filetypes_size_help'   => 'Max upload size allowed is :size.',
     'image_filetypes_help'  => 'Accepted filetypes are jpg, webp, png, gif, and svg. Max upload size allowed is :size.',
+    'unaccepted_image_type'  => 'This image file was not readable. Accepted filetypes are jpg, webp, png, gif, and svg. The mimetype of this file is :mimetype.',
     'import'         	    => 'Import',
     'importing'         	=> 'Importing',
     'importing_help'        => 'You can import assets, accessories, licenses, components, consumables, and users via CSV file. <br><br>The CSV should be comma-delimited and formatted with headers that match the ones in the <a href="https://snipe-it.readme.io/docs/importing" target="_new">sample CSVs in the documentation</a>.',
