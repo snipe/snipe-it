379c5f6790f838d6bb9c85dd2ff5fa6abaedb450
snipe <snipe@snipe.net>
2023-03-07 18:54:09 -0800

    Reworked the layout for logo upoloads
    Signed-off-by: snipe <snipe@snipe.net>

--- resources/views/partials/forms/edit/uploadLogo.blade.php
+++ resources/views/partials/forms/edit/uploadLogo.blade.php
@@ -1,12 +1,15 @@
+<!-- {{ $logoVariable }}logo image upload -->
+
 <div class="form-group">
     <div class="col-md-3">
-        <label{!!  $errors->has($logoVariable) ? ' class="alert-msg"' : '' !!} for="{{ $logoVariable }}">{{ ucwords(str_replace('_', ' ', $logoVariable)) }}</label>
+        <label {!! $errors->has($logoVariable) ? 'class="alert-msg"' : '' !!} for="{{ $logoVariable }}">
+        {{ ucwords(str_replace('_', ' ', $logoVariable)) }}
+        </label>
     </div>
     <div class="col-md-9">
         <label class="btn btn-default">
             {{ trans('button.select_file')  }}
-            <input type="file" name="{{ $logoVariable }}" class="js-uploadFile" id="{{ $logoId }}"
-                   data-maxsize="{{ $maxSize ?? Helper::file_upload_max_size() }}"
+            <input type="file" name="{{ $logoVariable }}" class="js-uploadFile" id="{{ $logoId }}" accept="image/gif,image/jpeg,image/webp,image/png,image/svg,image/svg+xml" data-maxsize="{{ $maxSize ?? Helper::file_upload_max_size() }}"
                    style="display:none; max-width: 90%">
         </label>
 
@@ -25,25 +28,31 @@
     </div>
 
     <div class="col-md-9 col-md-offset-3">
-        <div class="col-md-6 no-padding">
 
             @if (($setting->$logoVariable!='') && (Storage::disk('public')->exists(e($snipeSettings->$logoVariable))))
+            <div class="pull-left" style="padding-right: 20px;">
+                <a href="{{ Storage::disk('public')->url(e($snipeSettings->$logoVariable)) }}"{!! ($logoVariable!='favicon') ? ' data-toggle="lightbox"' : '' !!}>
+                    <img id="{{ $logoId }}-imagePreview" style="height: 80px; padding-bottom: 5px;" alt="" src="{{ Storage::disk('public')->url(e($snipeSettings->$logoVariable)) }}">
+                </a>
+            </div>
+            @endif
 
-            <a href="{{ Storage::disk('public')->url(e($snipeSettings->$logoVariable)) }}" {{ ($logoVariable!='favicon') ? ' data-toggle="lightbox"' : '' }}>
-                <img id="{{ $logoId }}-imagePreview" style="max-height: 80px;" alt="" src="{{ Storage::disk('public')->url(e($snipeSettings->$logoVariable)) }}">
-            </a>
+            <div id="{{ $logoId }}-previewContainer" style="display: none;">
+                <img id="{{ $logoId }}-imagePreview" style="height: 80px;">
+            </div>
 
-            <label for="{{ $logoClearVariable }}" style="font-weight: normal">
-                {{ Form::checkbox($logoClearVariable, '1', Request::old($logoClearVariable),array('class' => 'minimal')) }}
-                Remove current {{ ucwords(str_replace('_', ' ', $logoVariable)) }} image
-            </label>
-           @else
-                <img id="{{ $logoId }}-imagePreview" style="max-height: 80px;" alt="" src="{{ Storage::disk('public')->url(e($snipeSettings->$logoVariable)) }}">
-           @endif
 
-        </div>
 
     </div>
+    @if (($setting->$logoVariable!='') && (Storage::disk('public')->exists(e($snipeSettings->$logoVariable))))
+
+    <div class="col-md-9 col-md-offset-3">
+        <label id="{{ $logoId }}-deleteCheckbox" for="{{ $logoClearVariable }}" style="font-weight: normal">
+            {{ Form::checkbox($logoClearVariable, '1', Request::old($logoClearVariable),array('class' => 'minimal')) }}
+            Remove current {{ ucwords(str_replace('_', ' ', $logoVariable)) }} image
+        </label>
+    </div>
+    @endif
 
 
 
