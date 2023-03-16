f47a28f7903c9b88fe9575cd93edfa15b91b3ef0
Marcus Moore <contact@marcusmoore.io>
2023-03-16 15:40:16 -0700

    Actually remove unused methods

--- database/factories/AssetFactory.php
+++ database/factories/AssetFactory.php
@@ -288,36 +288,6 @@ class AssetFactory extends Factory
         });
     }
 
-    // public function assignedToUser()
-    // {
-    //     return $this->state(function () {
-    //         return [
-    //             'assigned_to' => User::factory(),
-    //             'assigned_type' => User::class,
-    //         ];
-    //     });
-    // }
-    //
-    // public function assignedToLocation()
-    // {
-    //     return $this->state(function () {
-    //         return [
-    //             'assigned_to' => Location::factory(),
-    //             'assigned_type' => Location::class,
-    //         ];
-    //     });
-    // }
-    //
-    // public function assignedToAsset()
-    // {
-    //     return $this->state(function () {
-    //         return [
-    //             'assigned_to' => Asset::factory(),
-    //             'assigned_type' => Asset::class,
-    //         ];
-    //     });
-    // }
-
     public function requiresAcceptance()
     {
         return $this->state(function () {
