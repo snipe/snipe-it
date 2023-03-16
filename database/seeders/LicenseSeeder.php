31630e3677e61a85b6f494d68488c45c6e86d2e5
Marcus Moore <contact@marcusmoore.io>
2023-03-16 13:47:02 -0700

    Seed licenses with categories

--- database/seeders/LicenseSeeder.php
+++ database/seeders/LicenseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use App\Models\License;
 use App\Models\LicenseSeat;
 use App\Models\Supplier;
@@ -14,15 +15,33 @@ class LicenseSeeder extends Seeder
         License::truncate();
         LicenseSeat::truncate();
 
+        if (! Category::count()) {
+            $this->call(CategorySeeder::class);
+        }
+
+        $categoryIds = Category::all()->pluck('id');
+
         if (! Supplier::count()) {
             $this->call(SupplierSeeder::class);
         }
 
         $supplierIds = Supplier::all()->pluck('id');
 
-        License::factory()->count(1)->photoshop()->create(['supplier_id' => $supplierIds->random()]);
-        License::factory()->count(1)->acrobat()->create(['supplier_id' => $supplierIds->random()]);
-        License::factory()->count(1)->indesign()->create(['supplier_id' => $supplierIds->random()]);
-        License::factory()->count(1)->office()->create(['supplier_id' => $supplierIds->random()]);
+        License::factory()->count(1)->photoshop()->create([
+            'category_id' => $categoryIds->random(),
+            'supplier_id' => $supplierIds->random(),
+        ]);
+        License::factory()->count(1)->acrobat()->create([
+            'category_id' => $categoryIds->random(),
+            'supplier_id' => $supplierIds->random(),
+        ]);
+        License::factory()->count(1)->indesign()->create([
+            'category_id' => $categoryIds->random(),
+            'supplier_id' => $supplierIds->random(),
+        ]);
+        License::factory()->count(1)->office()->create([
+            'category_id' => $categoryIds->random(),
+            'supplier_id' => $supplierIds->random(),
+        ]);
     }
 }
