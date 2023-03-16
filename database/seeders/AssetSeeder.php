daf6e9fa4bd550bedb107ca13a3628ef3658e638
Marcus Moore <contact@marcusmoore.io>
2023-03-16 14:23:40 -0700

    Seed assets with randomized locations

--- database/seeders/AssetSeeder.php
+++ database/seeders/AssetSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\Asset;
 use App\Models\Location;
 use App\Models\Supplier;
+use Illuminate\Database\Eloquent\Factories\Sequence;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
@@ -28,97 +29,191 @@ class AssetSeeder extends Seeder
 
         $supplierIds = Supplier::all()->pluck('id');
 
-        Asset::factory()->count(1000)->laptopMbp()->create([
-            'rtd_location_id' => $locationIds->random(),
-            'supplier_id' => $supplierIds->random(),
-        ]);
-        Asset::factory()->count(50)->laptopMbpPending()->create([
-            'rtd_location_id' => $locationIds->random(),
-            'supplier_id' => $supplierIds->random(),
-        ]);
-        Asset::factory()->count(50)->laptopMbpArchived()->create([
-            'rtd_location_id' => $locationIds->random(),
-            'supplier_id' => $supplierIds->random(),
-        ]);
-        Asset::factory()->count(50)->laptopAir()->create([
-            'rtd_location_id' => $locationIds->random(),
-            'supplier_id' => $supplierIds->random(),
-        ]);
-        Asset::factory()->count(5)->laptopSurface()->create([
-            'rtd_location_id' => $locationIds->random(),
-            'supplier_id' => $supplierIds->random(),
-        ]);
-        Asset::factory()->count(5)->laptopXps()->create([
-            'rtd_location_id' => $locationIds->random(),
-            'supplier_id' => $supplierIds->random(),
-        ]);
-        Asset::factory()->count(5)->laptopSpectre()->create([
-            'rtd_location_id' => $locationIds->random(),
-            'supplier_id' => $supplierIds->random(),
-        ]);
-        Asset::factory()->count(5)->laptopZenbook()->create([
-            'rtd_location_id' => $locationIds->random(),
-            'supplier_id' => $supplierIds->random(),
-        ]);
-        Asset::factory()->count(3)->laptopYoga()->create([
-            'rtd_location_id' => $locationIds->random(),
-            'supplier_id' => $supplierIds->random(),
-        ]);
+        Asset::factory()
+            ->count(1000)
+            ->laptopMbp()
+            ->state(new Sequence(fn($sequence) => [
+                'rtd_location_id' => $locationIds->random(),
+                'supplier_id' => $supplierIds->random(),
+            ]))
+            ->create();
 
-        Asset::factory()->count(30)->desktopMacpro()->create([
-            'rtd_location_id' => $locationIds->random(),
-            'supplier_id' => $supplierIds->random(),
-        ]);
-        Asset::factory()->count(30)->desktopLenovoI5()->create([
-            'rtd_location_id' => $locationIds->random(),
-            'supplier_id' => $supplierIds->random(),
-        ]);
-        Asset::factory()->count(30)->desktopOptiplex()->create([
-            'rtd_location_id' => $locationIds->random(),
-            'supplier_id' => $supplierIds->random(),
-        ]);
+        Asset::factory()
+            ->count(50)
+            ->laptopMbpPending()
+            ->state(new Sequence(fn($sequence) => [
+                'rtd_location_id' => $locationIds->random(),
+                'supplier_id' => $supplierIds->random(),
+            ]))
+            ->create();
 
-        Asset::factory()->count(5)->confPolycom()->create([
-            'rtd_location_id' => $locationIds->random(),
-            'supplier_id' => $supplierIds->random(),
-        ]);
-        Asset::factory()->count(2)->confPolycomcx()->create([
-            'rtd_location_id' => $locationIds->random(),
-            'supplier_id' => $supplierIds->random(),
-        ]);
+        Asset::factory()
+            ->count(50)
+            ->laptopMbpArchived()
+            ->state(new Sequence(fn($sequence) => [
+                'rtd_location_id' => $locationIds->random(),
+                'supplier_id' => $supplierIds->random(),
+            ]))
+            ->create();
 
-        Asset::factory()->count(12)->tabletIpad()->create([
-            'rtd_location_id' => $locationIds->random(),
-            'supplier_id' => $supplierIds->random(),
-        ]);
-        Asset::factory()->count(4)->tabletTab3()->create([
-            'rtd_location_id' => $locationIds->random(),
-            'supplier_id' => $supplierIds->random(),
-        ]);
+        Asset::factory()
+            ->count(50)
+            ->laptopAir()
+            ->state(new Sequence(fn($sequence) => [
+                'rtd_location_id' => $locationIds->random(),
+                'supplier_id' => $supplierIds->random(),
+            ]))
+            ->create();
 
-        Asset::factory()->count(27)->phoneIphone11()->create([
-            'rtd_location_id' => $locationIds->random(),
-            'supplier_id' => $supplierIds->random(),
-        ]);
-        Asset::factory()->count(40)->phoneIphone12()->create([
-            'rtd_location_id' => $locationIds->random(),
-            'supplier_id' => $supplierIds->random(),
-        ]);
+        Asset::factory()
+            ->count(5)
+            ->laptopSurface()
+            ->state(new Sequence(fn($sequence) => [
+                'rtd_location_id' => $locationIds->random(),
+                'supplier_id' => $supplierIds->random(),
+            ]))
+            ->create();
 
-        Asset::factory()->count(10)->ultrafine()->create([
-            'rtd_location_id' => $locationIds->random(),
-            'supplier_id' => $supplierIds->random(),
-        ]);
-        Asset::factory()->count(10)->ultrasharp()->create([
-            'rtd_location_id' => $locationIds->random(),
-            'supplier_id' => $supplierIds->random(),
-        ]);
+        Asset::factory()
+            ->count(5)
+            ->laptopXps()
+            ->state(new Sequence(fn($sequence) => [
+                'rtd_location_id' => $locationIds->random(),
+                'supplier_id' => $supplierIds->random(),
+            ]))
+            ->create();
+
+        Asset::factory()
+            ->count(5)
+            ->laptopSpectre()
+            ->state(new Sequence(fn($sequence) => [
+                'rtd_location_id' => $locationIds->random(),
+                'supplier_id' => $supplierIds->random(),
+            ]))
+            ->create();
+
+        Asset::factory()
+            ->count(5)
+            ->laptopZenbook()
+            ->state(new Sequence(fn($sequence) => [
+                'rtd_location_id' => $locationIds->random(),
+                'supplier_id' => $supplierIds->random(),
+            ]))
+            ->create();
+
+        Asset::factory()
+            ->count(3)
+            ->laptopYoga()
+            ->state(new Sequence(fn($sequence) => [
+                'rtd_location_id' => $locationIds->random(),
+                'supplier_id' => $supplierIds->random(),
+            ]))
+            ->create();
+
+        Asset::factory()
+            ->count(30)
+            ->desktopMacpro()
+            ->state(new Sequence(fn($sequence) => [
+                'rtd_location_id' => $locationIds->random(),
+                'supplier_id' => $supplierIds->random(),
+            ]))
+            ->create();
+
+        Asset::factory()
+            ->count(30)
+            ->desktopLenovoI5()
+            ->state(new Sequence(fn($sequence) => [
+                'rtd_location_id' => $locationIds->random(),
+                'supplier_id' => $supplierIds->random(),
+            ]))
+            ->create();
+
+        Asset::factory()
+            ->count(30)
+            ->desktopOptiplex()
+            ->state(new Sequence(fn($sequence) => [
+                'rtd_location_id' => $locationIds->random(),
+                'supplier_id' => $supplierIds->random(),
+            ]))
+            ->create();
+
+        Asset::factory()
+            ->count(5)
+            ->confPolycom()
+            ->state(new Sequence(fn($sequence) => [
+                'rtd_location_id' => $locationIds->random(),
+                'supplier_id' => $supplierIds->random(),
+            ]))
+            ->create();
+
+        Asset::factory()
+            ->count(2)
+            ->confPolycomcx()
+            ->state(new Sequence(fn($sequence) => [
+                'rtd_location_id' => $locationIds->random(),
+                'supplier_id' => $supplierIds->random(),
+            ]))
+            ->create();
+
+        Asset::factory()
+            ->count(12)
+            ->tabletIpad()
+            ->state(new Sequence(fn($sequence) => [
+                'rtd_location_id' => $locationIds->random(),
+                'supplier_id' => $supplierIds->random(),
+            ]))
+            ->create();
+
+        Asset::factory()
+            ->count(4)
+            ->tabletTab3()
+            ->state(new Sequence(fn($sequence) => [
+                'rtd_location_id' => $locationIds->random(),
+                'supplier_id' => $supplierIds->random(),
+            ]))
+            ->create();
+
+        Asset::factory()
+            ->count(27)
+            ->phoneIphone11()
+            ->state(new Sequence(fn($sequence) => [
+                'rtd_location_id' => $locationIds->random(),
+                'supplier_id' => $supplierIds->random(),
+            ]))
+            ->create();
+
+        Asset::factory()
+            ->count(40)
+            ->phoneIphone12()
+            ->state(new Sequence(fn($sequence) => [
+                'rtd_location_id' => $locationIds->random(),
+                'supplier_id' => $supplierIds->random(),
+            ]))
+            ->create();
+
+        Asset::factory()
+            ->count(10)
+            ->ultrafine()
+            ->state(new Sequence(fn($sequence) => [
+                'rtd_location_id' => $locationIds->random(),
+                'supplier_id' => $supplierIds->random(),
+            ]))
+            ->create();
+
+        Asset::factory()
+            ->count(10)
+            ->ultrasharp()
+            ->state(new Sequence(fn($sequence) => [
+                'rtd_location_id' => $locationIds->random(),
+                'supplier_id' => $supplierIds->random(),
+            ]))
+            ->create();
 
         $del_files = Storage::files('assets');
         foreach ($del_files as $del_file) { // iterate files
-            Log::debug('Deleting: '.$del_files);
+            Log::debug('Deleting: ' . $del_files);
             try {
-                Storage::disk('public')->delete('assets'.'/'.$del_files);
+                Storage::disk('public')->delete('assets' . '/' . $del_files);
             } catch (\Exception $e) {
                 Log::debug($e);
             }
