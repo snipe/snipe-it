0dfb51f6421ab1552662d11267c56126b908a8d9
snipe <snipe@snipe.net>
2023-03-16 18:18:32 -0700

    User merge blade
    Signed-off-by: snipe <snipe@snipe.net>

---
+++ resources/views/users/confirm-merge.blade.php
@@ -0,0 +1,107 @@
+@extends('layouts/default')
+{{-- Page title --}}
+@section('title')
+    {!! trans('general.merge_users') !!}
+    @parent
+@stop
+
+{{-- Page content --}}
+@section('content')
+
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="box box-default">
+                <form class="form-horizontal" role="form" method="post" action="{{ route('users.merge.save') }}">
+                    <div class="box-body">
+                        <!-- CSRF Token -->
+                        {{csrf_field()}}
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="callout callout-danger">
+                                    <i class="fas fa-exclamation-triangle"></i>
+                                    {{ trans('general.warning_merge_information', array('count' => count($users))) }}
+                                </div>
+                            </div>
+                        </div>
+
+                        @if (config('app.lock_passwords'))
+                            <div class="col-md-12">
+                                <div class="callout callout-warning">
+                                    <p>{{ trans('general.feature_disabled') }}</p>
+                                </div>
+                            </div>
+                        @endif
+
+                        <div class="col-md-12">
+                            <div class="table-responsive">
+                                <table class="display table table-hover">
+                                    <thead>
+                                    <tr>
+                                        <th class="col-md-1"></th>
+                                        <th class="col-md-6">{{ trans('general.name') }}</th>
+                                        <th class="col-md-5">{{ trans('general.groups') }}</th>
+                                        <th class="col-md-5">{{ trans('general.assets') }}</th>
+                                        <th class="col-md-5">{{ trans('general.accessories') }}</th>
+                                        <th class="col-md-5">{{ trans('general.licenses') }}</th>
+                                        <th class="col-md-5">{{ trans('general.consumables') }}</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    @foreach ($users as $user)
+                                        <tr {!! ($user->isSuperUser() ? ' class="danger"':'') !!}>
+                                            <td>
+                                                <input type="radio" name="merge_into_id" value="{{ $user->id }}" class="minimal" checked="checked">
+                                            </td>
+
+                                            <td>
+                                              <span {!! (Auth::user()->id==$user->id ? ' style="text-decoration: line-through"' : '') !!}>
+                                                {{ $user->present()->fullName() }} ({{ $user->username }}) (#{{ $user->id }})
+                                              </span>
+                                                {{ (Auth::id()==$user->id ? ' (cannot delete yourself)' : '') }}
+                                            </td>
+                                            <td>
+                                                @foreach ($user->groups as $group)
+                                                    <a href=" {{ route('groups.update', $group->id) }}" class="label  label-default">
+                                                        {{ $group->name  }}
+                                                    </a>&nbsp;
+                                                @endforeach
+                                            </td>
+                                            <td>
+                                                {{ number_format($user->assets()->count())  }}
+                                            </td>
+                                            <td>
+                                                {{ number_format($user->accessories()->count())  }}
+                                            </td>
+                                            <td>
+                                                {{ number_format($user->licenses()->count())  }}
+                                            </td>
+                                            <td>
+                                                {{ number_format($user->consumables()->count())  }}
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                    </tbody>
+
+                                </table>
+                            </div> <!--/table-responsive-->
+                        </div><!--/col-md-12-->
+                    </div> <!--/box-body-->
+                    <div class="box-footer text-right">
+                        <a class="btn btn-link pull-left" href="{{ URL::previous() }}">{{ trans('button.cancel') }}</a>
+                        <button type="submit" class="btn btn-success"><i class="fas fa-check icon-white" aria-hidden="true"></i> {{ trans('button.submit') }}</button>
+                    </div><!-- /.box-footer -->
+
+                    @foreach ($users as $user)
+                        <input type="hidden" name="ids_to_merge[]" value="{{ $user->id }}">
+                    @endforeach
+
+                </form>
+            </div>
+        </div>
+    </div>
+
+@stop
+
+@section('moar_scripts')
+
+@stop
