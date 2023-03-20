a0e547c7fba4d93a2eabc06b85bf5a9e3b66280f
Marcus Moore <contact@marcusmoore.io>
2023-03-20 14:18:59 -0700

    Remove some unused imports

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,15 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\Setting;
-use Database\Seeders\AccessorySeeder;
-use Database\Seeders\ActionlogSeeder;
-use Database\Seeders\AssetModelSeeder;
-use Database\Seeders\AssetSeeder;
-use Database\Seeders\CategorySeeder;
-use Database\Seeders\CompanySeeder;
-use Database\Seeders\ComponentSeeder;
-use Database\Seeders\ConsumableSeeder;
-use Database\Seeders\CustomFieldSeeder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Artisan;
