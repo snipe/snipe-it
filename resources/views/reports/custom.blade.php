09fc6e4274e0f2ba1b7ae2fbe8e9a6a88ecbd7a4
Godfrey M <godmartinz@gmail.com>
2023-03-28 09:30:35 -0700

    adds a multiselect to partials for companies

--- resources/views/reports/custom.blade.php
+++ resources/views/reports/custom.blade.php
@@ -281,7 +281,7 @@
 
             {!! trans('general.report_fields_info') !!}
 
-            @include ('partials.forms.edit.company-select', ['translated_name' => trans('general.company'), 'fieldname' => 'by_company_id', 'hide_new' => 'true'])
+            @include ('partials.forms.edit.company-multi-select', ['translated_name' => trans('general.company'), 'fieldname' => 'by_company_id', 'hide_new' => 'true'])
             @include ('partials.forms.edit.location-select', ['translated_name' => trans('general.location'), 'fieldname' => 'by_location_id', 'hide_new' => 'true'])
             @include ('partials.forms.edit.location-select', ['translated_name' => trans('admin/hardware/form.default_location'), 'fieldname' => 'by_rtd_location_id', 'hide_new' => 'true'])
           @include ('partials.forms.edit.department-select', ['translated_name' => trans('general.department'), 'fieldname' => 'by_dept_id', 'hide_new' => 'true'])
