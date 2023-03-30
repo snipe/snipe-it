524249d4d707a62067e52e503c4dbcbb577da606
Marcus Moore <contact@marcusmoore.io>
2023-03-30 16:58:16 -0700

    Implement tests for webhook notifications on accessory checkout

---
+++ tests/Feature/Notifications/AccessoryCheckoutWebhookNotificationTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Tests\Feature\Notifications;
+
+
+use App\Events\CheckoutableCheckedOut;
+use App\Models\Accessory;
+use App\Models\Setting;
+use App\Models\User;
+use App\Notifications\CheckoutAccessoryNotification;
+use Illuminate\Notifications\AnonymousNotifiable;
+use Illuminate\Support\Facades\Notification;
+use Tests\TestCase;
+
+class AccessoryCheckoutWebhookNotificationTest extends TestCase
+{
+    public function testWebhookNotificationsAreSentOnAccessoryCheckoutWhenWebhookSettingEnabled()
+    {
+        Notification::fake();
+
+        Setting::factory()->withWebhookEnabled()->create();
+
+        event(new CheckoutableCheckedOut(
+            Accessory::factory()->appleBtKeyboard()->create(),
+            User::factory()->create(),
+            User::factory()->superuser()->create(),
+            ''
+        ));
+
+        Notification::assertSentTo(
+            new AnonymousNotifiable,
+            CheckoutAccessoryNotification::class,
+            function ($notification, $channels, $notifiable) {
+                return $notifiable->routes['slack'] === Setting::getSettings()->webhook_endpoint;
+            }
+        );
+    }
+
+    public function testWebhookNotificationsAreNotSentOnAccessoryCheckoutWhenWebhookSettingNotEnabled()
+    {
+        Notification::fake();
+
+        Setting::factory()->withWebhookDisabled()->create();
+
+        event(new CheckoutableCheckedOut(
+            Accessory::factory()->appleBtKeyboard()->create(),
+            User::factory()->create(),
+            User::factory()->superuser()->create(),
+            ''
+        ));
+
+        Notification::assertNotSentTo(new AnonymousNotifiable, CheckoutAccessoryNotification::class);
+    }
+}
