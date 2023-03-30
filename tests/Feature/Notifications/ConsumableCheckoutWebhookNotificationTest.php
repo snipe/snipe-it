b2292db3c84705fdd14fa2c78ba253a51f98350c
Marcus Moore <contact@marcusmoore.io>
2023-03-30 16:40:53 -0700

    Implement tests for webhook notifications on consumable checkout

---
+++ tests/Feature/Notifications/ConsumableCheckoutWebhookNotificationTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Tests\Feature\Notifications;
+
+use App\Events\CheckoutableCheckedOut;
+use App\Models\Consumable;
+use App\Models\Setting;
+use App\Models\User;
+use App\Notifications\CheckoutConsumableNotification;
+use Illuminate\Notifications\AnonymousNotifiable;
+use Illuminate\Support\Facades\Notification;
+use Tests\TestCase;
+
+class ConsumableCheckoutWebhookNotificationTest extends TestCase
+{
+    public function testWebhookNotificationsAreSentOnConsumableCheckoutWhenWebhookSettingEnabled()
+    {
+        Notification::fake();
+
+        Setting::factory()->withWebhookEnabled()->create();
+
+        event(new CheckoutableCheckedOut(
+            Consumable::factory()->cardstock()->create(),
+            User::factory()->create(),
+            User::factory()->superuser()->create(),
+            ''
+        ));
+
+        Notification::assertSentTo(
+            new AnonymousNotifiable,
+            CheckoutConsumableNotification::class,
+            function ($notification, $channels, $notifiable) {
+                return $notifiable->routes['slack'] === Setting::getSettings()->webhook_endpoint;
+            }
+        );
+    }
+
+    public function testWebhookNotificationsAreNotSentOnConsumableCheckoutWhenWebhookSettingNotEnabled()
+    {
+        Notification::fake();
+
+        Setting::factory()->withWebhookDisabled()->create();
+
+        event(new CheckoutableCheckedOut(
+            Consumable::factory()->cardstock()->create(),
+            User::factory()->create(),
+            User::factory()->superuser()->create(),
+            ''
+        ));
+
+        Notification::assertNotSentTo(new AnonymousNotifiable, CheckoutConsumableNotification::class);
+    }
+}
