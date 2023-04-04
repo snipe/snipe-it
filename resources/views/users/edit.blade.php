5ed9385685cfa56761c081b2a275310b0c1677ff
snipe <snipe@snipe.net>
2023-04-03 17:09:25 -0700

    Clearer tab name for user info
    Signed-off-by: snipe <snipe@snipe.net>

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -75,12 +75,12 @@
         <!-- Custom Tabs -->
       <div class="nav-tabs-custom">
         <ul class="nav nav-tabs">
-          <li class="active"><a href="#tab_1" data-toggle="tab">{{ trans('general.information') }} </a></li>
+          <li class="active"><a href="#info" data-toggle="tab">{{ trans('general.information') }} </a></li>
           <li><a href="#permissions" data-toggle="tab">{{ trans('general.permissions') }} </a></li>
         </ul>
 
         <div class="tab-content">
-          <div class="tab-pane active" id="tab_1">
+          <div class="tab-pane active" id="info">
             <div class="row">
               <div class="col-md-12">
                 <!-- First Name -->
