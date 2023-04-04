21448f6adfd9d637e3d950af93e091617e296d49
snipe <snipe@snipe.net>
2023-04-03 17:38:02 -0700

    Model edit - removing icheck
    Signed-off-by: snipe <snipe@snipe.net>

--- resources/views/models/edit.blade.php
+++ resources/views/models/edit.blade.php
@@ -43,8 +43,8 @@
 @if (($item->image) && ($item->image!=''))
     <div class="form-group{{ $errors->has('image_delete') ? ' has-error' : '' }}">
         <div class="col-md-9 col-md-offset-3">
-            <label for="image_delete">
-                {{ Form::checkbox('image_delete', '1', old('image_delete'), ['class'=>'minimal','aria-label'=>'image_delete']) }}
+            <label for="image_delete" class="form-control">
+                {{ Form::checkbox('image_delete', '1', old('image_delete'), ['aria-label'=>'image_delete']) }}
                 {{ trans('general.image_delete') }}
                 {!! $errors->first('image_delete', '<span class="alert-msg">:message</span>') !!}
             </label>
