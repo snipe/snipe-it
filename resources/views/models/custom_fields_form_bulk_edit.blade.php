d56a4e717332de2c22b3e2837977065b4b789483
slong753 <spencer@spencerlong.com>
2023-04-05 15:16:46 -0500

    cleanup+formatting

--- resources/views/models/custom_fields_form_bulk_edit.blade.php
+++ resources/views/models/custom_fields_form_bulk_edit.blade.php
@@ -1,26 +1,24 @@
 @php
-$fields = [];
-$modelNames = [];
-foreach($models as $model) {
-    $modelNames[] = $model->name;
-}
-
+    $fields = [];
+    $modelNames = [];
+    foreach($models as $model) {
+        $modelNames[] = $model->name;
+    }
 @endphp
 @foreach($models as $model)
-    
 @if (($model) && ($model->fieldset))
-  @foreach($model->customFields AS $field)
-  @php
-        if (in_array($field->db_column_name(), $fields)) {
-            $duplicate = true;
-            continue; 
-        } else {
-            $duplicate = false;
-        }
-       $fields[] = $field->db_column_name(); 
+    @foreach($model->customFields AS $field)
+
+@php
+    if (in_array($field->db_column_name(), $fields)) {
+        $duplicate = true;
+        continue; 
+    } else {
+        $duplicate = false;
+    }
+    $fields[] = $field->db_column_name(); 
 @endphp
 
-    
     <div class="form-group{{ $errors->has($field->db_column_name()) ? ' has-error' : '' }}">
       <label for="{{ $field->db_column_name() }}" class="col-md-3 control-label">{{ $field->name }} </label>
       <div class="col-md-7 col-sm-12{{ ($field->pivot->required=='1') ? ' required' : '' }}">
@@ -29,12 +27,11 @@ foreach($models as $model) {
           @if ($field->element!='text')
               <!-- Listbox -->
               @if ($field->element=='listbox')
-                   {{ Form::select($field->db_column_name(), $field->formatFieldValuesAsArray(),
+                  {{ Form::select($field->db_column_name(), $field->formatFieldValuesAsArray(),
                   Request::old($field->db_column_name(),(isset($item) ? Helper::gracefulDecrypt($field, htmlspecialchars($item->{$field->db_column_name()}, ENT_QUOTES)) : $field->defaultValue($model->id))), ['class'=>'format select2 form-control']) }}
 
               @elseif ($field->element=='textarea')
                   <textarea class="col-md-6 form-control" id="{{ $field->db_column_name() }}" name="{{ $field->db_column_name() }}">{{ Request::old($field->db_column_name(),(isset($item) ? Helper::gracefulDecrypt($field, $item->{$field->db_column_name()}) : $field->defaultValue($model->id))) }}</textarea>
-
               @elseif ($field->element=='checkbox')
                     <!-- Checkboxes -->
                   @foreach ($field->formatFieldValuesAsArray() as $key => $value)
@@ -45,32 +42,29 @@ foreach($models as $model) {
                           </label>
                       </div>
                   @endforeach
-
-              @elseif ($field->element=='radio')
-              @foreach ($field->formatFieldValuesAsArray() as $value)
-
+            @elseif ($field->element=='radio')
+            @foreach ($field->formatFieldValuesAsArray() as $value)
               <div>
                   <label>
                       <input type="radio" value="{{ $value }}" name="{{ $field->db_column_name() }}" class="minimal" {{ isset($item) ? ($item->{$field->db_column_name()} == $value ? ' checked="checked"' : '') : (Request::old($field->db_column_name()) != '' ? ' checked="checked"' : (in_array($value, explode(', ', $field->defaultValue($model->id))) ? ' checked="checked"' : '')) }}>
                       {{ $value }}
                   </label>
               </div>
-          @endforeach
+            @endforeach
 
-              @endif
+            @endif
 
-
-          @else
+            @else
             <!-- Date field -->
 
-                @if ($field->format=='DATE')
+            @if ($field->format=='DATE')
 
-                        <div class="input-group col-md-5" style="padding-left: 0px;">
-                            <div class="input-group date" data-provide="datepicker" data-date-format="yyyy-mm-dd" data-autoclose="true" data-date-clear-btn="true">
-                                <input type="text" class="form-control" placeholder="{{ trans('general.select_date') }}" name="{{ $field->db_column_name() }}" id="{{ $field->db_column_name() }}" readonly value="{{ old($field->db_column_name(),(isset($item) ? Helper::gracefulDecrypt($field, $item->{$field->db_column_name()}) : $field->defaultValue($model->id))) }}"  style="background-color:inherit">
-                                <span class="input-group-addon"><i class="fas fa-calendar" aria-hidden="true"></i></span>
-                            </div>
-                        </div>
+            <div class="input-group col-md-5" style="padding-left: 0px;">
+                <div class="input-group date" data-provide="datepicker" data-date-format="yyyy-mm-dd" data-autoclose="true" data-date-clear-btn="true">
+                    <input type="text" class="form-control" placeholder="{{ trans('general.select_date') }}" name="{{ $field->db_column_name() }}" id="{{ $field->db_column_name() }}" readonly value="{{ old($field->db_column_name(),(isset($item) ? Helper::gracefulDecrypt($field, $item->{$field->db_column_name()}) : $field->defaultValue($model->id))) }}"  style="background-color:inherit">
+                    <span class="input-group-addon"><i class="fas fa-calendar" aria-hidden="true"></i></span>
+                </div>
+            </div>
 
 
                 @else
@@ -88,11 +82,13 @@ foreach($models as $model) {
         @endif
 
 
+        <p>On Models:
              @foreach($field->assetModels() as $assetModel) 
                 @if(in_array($assetModel->name, $modelNames)) 
-                    <p>{{$assetModel->name}}</p>
+                    {{$assetModel->name}}{{($loop->last) ? '' : ', '}}
                 @endif 
             @endforeach 
+            </p> 
 
               
               
