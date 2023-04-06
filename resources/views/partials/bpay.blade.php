74b471bbad24a69f509a5705cdb5a6d072efc0fe
snipe <snipe@snipe.net>
2023-04-06 16:24:46 -0700

    Check for more specifics in env vars
    Signed-off-by: snipe <snipe@snipe.net>

--- resources/views/partials/bpay.blade.php
+++ resources/views/partials/bpay.blade.php
@@ -1,5 +1,5 @@
 @can('admin')
-    @if (config('services.baremetrics.enabled'))
+    @if ((config('services.baremetrics.enabled')=='true') && (config('services.baremetrics.app_key')) && (config('services.baremetrics.stripe_id')))
         <script>
             !function(){if(window.barepay&&window.barepay.created)window.console&&console.error&&console.error("Barepay snippet included twice.");else{window.barepay={created:!0};var a=document.createElement("script");a.src="https://baremetrics-dunning.baremetrics.com/js/application.js",a.async=!0;var b=document.getElementsByTagName("script")[0];b.parentNode.insertBefore(a,b),
 
@@ -10,5 +10,8 @@
 
             }}();
         </script>
+        @else
+            FARTS
+
     @endif
 @endcan
