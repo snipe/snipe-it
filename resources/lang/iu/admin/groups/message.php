8befc2317834c24e599bf0fedc9ea3ff4a6367b9
snipe <snipe@snipe.net>
2023-04-06 19:38:17 -0700

    Updated Inuktitut
    Signed-off-by: snipe <snipe@snipe.net>

--- resources/lang/iu/admin/groups/message.php
+++ resources/lang/iu/admin/groups/message.php
@@ -3,7 +3,7 @@
 return array(
 
     'group_exists'        => 'Group already exists!',
-    'group_not_found'     => 'Group [:id] does not exist.',
+    'group_not_found'     => 'Group ID :id does not exist.',
     'group_name_required' => 'The name field is required',
 
     'success' => array(
