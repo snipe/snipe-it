94035e5e4bff2101ca425470c5807f525a7f42fc
snipe <snipe@snipe.net>
2023-04-06 19:24:19 -0700

    Updated Spanish, Venezuela
    Signed-off-by: snipe <snipe@snipe.net>

--- resources/lang/es-VE/admin/settings/message.php
+++ resources/lang/es-VE/admin/settings/message.php
@@ -11,8 +11,8 @@ return [
         'file_deleted'          => 'El archivo de respaldo fue eliminado satisfactoriamente. ',
         'generated'             => 'Un nuevo archivo de respaldo ha sido creado con éxito.',
         'file_not_found'        => 'El archivo de respaldo no puede ser encontrado en el servidor.',
-        'restore_warning'       => 'Yes, restore it. I acknowledge that this will overwrite any existing data currently in the database. This will also log out all of your existing users (including you).',
-        'restore_confirm'       => 'Are you sure you wish to restore your database from :filename?'
+        'restore_warning'       => 'Sí, restaurarlo. Reconozco que esto sobrescribirá cualquier dato existente actualmente en la base de datos. Esto también cerrará la sesión de todos sus usuarios existentes (incluido usted).',
+        'restore_confirm'       => '¿Está seguro que desea restaurar su base de datos desde :filename?'
     ],
     'purge' => [
         'error'     => 'Ha ocurrido un error mientras se realizaba el purgado. ',
@@ -20,25 +20,25 @@ return [
         'success'               => 'Registros de borrado purgados correctamente.',
     ],
     'mail' => [
-        'sending' => 'Sending Test Email...',
-        'success' => 'Mail sent!',
-        'error' => 'Mail could not be sent.',
-        'additional' => 'No additional error message provided. Check your mail settings and your app log.'
+        'sending' => 'Enviando correo electrónico...',
+        'success' => '¡Correo enviado!',
+        'error' => 'El correo no pudo ser enviado.',
+        'additional' => 'Ningún mensaje de error adicional proporcionado. Comprueba la configuración de tu correo y el registro de tu aplicación.'
     ],
     'ldap' => [
-        'testing' => 'Testing LDAP Connection, Binding & Query ...',
-        '500' => '500 Server Error. Please check your server logs for more information.',
-        'error' => 'Something went wrong :(',
-        'sync_success' => 'A sample of 10 users returned from the LDAP server based on your settings:',
-        'testing_authentication' => 'Testing LDAP Authentication...',
-        'authentication_success' => 'User authenticated against LDAP successfully!'
+        'testing' => 'Probando conexión LDAP, Binding & Query ...',
+        '500' => 'Error 500 del servidor. Por favor, compruebe los registros de su servidor para más información.',
+        'error' => 'Algo salió mal :(',
+        'sync_success' => 'Una muestra de 10 usuarios devueltos desde el servidor LDAP basado en su configuración:',
+        'testing_authentication' => 'Probando autenticación LDAP...',
+        'authentication_success' => 'Usuario autenticado contra LDAP con éxito!'
     ],
-    'slack' => [
-        'sending' => 'Sending Slack test message...',
-        'success_pt1' => 'Success! Check the ',
-        'success_pt2' => ' channel for your test message, and be sure to click SAVE below to store your settings.',
-        '500' => '500 Server Error.',
-        'error' => 'Something went wrong. Slack responded with: :error_message',
-        'error_misc' => 'Something went wrong. :( ',
+    'webhook' => [
+        'sending' => 'Enviando mensaje de prueba de :app...',
+        'success_pt1' => '¡Éxito! Comprueba el ',
+        'success_pt2' => ' para su mensaje de prueba, y asegúrese de hacer clic en GUARDAR abajo para guardar su configuración.',
+        '500' => 'Error 500 del servidor.',
+        'error' => 'Algo salió mal. :app respondió con: :error_message',
+        'error_misc' => 'Algo salió mal. :( ',
     ]
 ];
