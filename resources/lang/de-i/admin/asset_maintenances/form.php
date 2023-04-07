70653419cd7a98a33af2d1b73dc9707dbb8aa261
snipe <snipe@snipe.net>
2023-04-06 19:19:24 -0700

    Updated German Informal
    Signed-off-by: snipe <snipe@snipe.net>

--- resources/lang/de-i/admin/asset_maintenances/form.php
+++ resources/lang/de-i/admin/asset_maintenances/form.php
@@ -4,11 +4,11 @@
         'asset_maintenance_type' => 'Wartungsart',
         'title'                  => 'Bezeichnung',
         'start_date'             => 'Startdatum',
-        'completion_date'        => 'Fertigstellungstermin',
+        'completion_date'        => 'Abschlussdatum',
         'cost'                   => 'Kosten',
         'is_warranty'            => 'Garantie Verbesserung',
-        'asset_maintenance_time' => 'Tage',
+        'asset_maintenance_time' => 'Wartungszeit (in Tagen)',
         'notes'                  => 'Notizen',
-        'update'                 => 'Aktualisieren',
-        'create'                 => 'Erstellen'
+        'update'                 => 'Wartung bearbeiten',
+        'create'                 => 'Wartung erstellen'
     ];
