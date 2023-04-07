5f56b30d19495f3a06d47f1203db1e2e611137f3
snipe <snipe@snipe.net>
2023-04-06 19:16:42 -0700

    Updated Czech
    Signed-off-by: snipe <snipe@snipe.net>

--- resources/lang/cs/admin/asset_maintenances/form.php
+++ resources/lang/cs/admin/asset_maintenances/form.php
@@ -1,14 +1,14 @@
 <?php
 
     return [
-        'asset_maintenance_type' => 'Druh údržby',
+        'asset_maintenance_type' => 'Druh údržby majetku',
         'title'                  => 'Název',
-        'start_date'             => 'Začátek',
-        'completion_date'        => 'Dokončeno',
+        'start_date'             => 'Počáteční datum',
+        'completion_date'        => 'Datum dokončení',
         'cost'                   => 'Cena',
         'is_warranty'            => 'Rozšíření záruky',
-        'asset_maintenance_time' => 'Dnů',
+        'asset_maintenance_time' => 'Doba údržby majetku (ve dnech)',
         'notes'                  => 'Poznámky',
-        'update'                 => 'Aktualizace',
-        'create'                 => 'Vytvořit'
+        'update'                 => 'Upravit údržbu zařízení',
+        'create'                 => 'Založit údržbu zařízení'
     ];
