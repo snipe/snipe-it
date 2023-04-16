ff9af7ea740b1148f266fbde3c6dd92d3723e9ae
snipe <snipe@snipe.net>
2023-04-16 15:23:15 -0700

    Added genetic modal partial
    Signed-off-by: snipe <snipe@snipe.net>

--- resources/views/modals/confirm-action.blade.php
+++ resources/views/modals/confirm-action.blade.php
@@ -1,31 +1,27 @@
 <!-- Modal -->
-<div class="modal fade" id="confirmAction" tabindex="-1" role="dialog" aria-labelledby="confirmActionLabel" aria-hidden="true">
+<div class="modal modal-purple fade" id="{{ $modal_name }}" tabindex="-1" role="dialog" aria-labelledby="{{ $modal_name }}Label" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                <h4 class="modal-title" id="confirmActionLabel">{{ trans('general.file_upload') }}</h4>
+                <h4 class="modal-title" id="{{ $modal_name }}Label">{{ $title }}</h4>
             </div>
-            {{ Form::open([
-            'method' => 'POST',
-            'route' => ['upload/'.$item_type, $item_id],
-            'files' => true,
-            'class' => 'form-horizontal' ]) }}
+            <form action="{{ $route }}" method="POST" class="form-horizontal">
             <input type="hidden" name="_token" value="{{ csrf_token() }}" />
             <div class="modal-body">
 
                 <div class="row">
                     <div class="col-md-12">
-                        Blah
+                        {{ $body }}
                     </div>
                 </div>
 
             </div> <!-- /.modal-body-->
             <div class="modal-footer">
                 <a href="#" class="pull-left" data-dismiss="modal">{{ trans('button.cancel') }}</a>
-                <button type="submit" class="btn btn-primary">{{ trans('button.upload') }}</button>
+                <button type="submit" class="btn btn-primary">{{ trans('general.confirm') }}</button>
             </div>
-            {{ Form::close() }}
+            </form>
         </div>
     </div>
 </div>
