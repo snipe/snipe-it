0eadc00f4d0315b9cf35b94b17939851482f121c
snipe <snipe@snipe.net>
2023-04-18 04:37:04 -0700

    Removed unneeded css class
    Signed-off-by: snipe <snipe@snipe.net>

--- resources/views/modals/confirm-action.blade.php
+++ resources/views/modals/confirm-action.blade.php
@@ -1,5 +1,5 @@
 <!-- Modal -->
-<div class="modal modal-purple fade" id="{{ $modal_name }}" tabindex="-1" role="dialog" aria-labelledby="{{ $modal_name }}Label" aria-hidden="true">
+<div class="modal fade" id="{{ $modal_name }}" tabindex="-1" role="dialog" aria-labelledby="{{ $modal_name }}Label" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
