645f6ed692bf33a09eccd680a11e3ab685ecafde
Marcus Moore <contact@marcusmoore.io>
2023-04-19 17:52:44 -0700

    Remove unneeded doc block

--- database/factories/LicenseSeatFactory.php
+++ database/factories/LicenseSeatFactory.php
@@ -7,11 +7,6 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 
 class LicenseSeatFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array
-     */
     public function definition()
     {
         return [
