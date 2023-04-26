497d17835f04fcca81fb6e20d5087917426a082b
akemidx <kojotek.dx@gmail.com>
2023-04-26 17:06:08 -0400

    requested edits

--- app/Http/Controllers/ManufacturersController.php
+++ app/Http/Controllers/ManufacturersController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ImageUploadRequest;
-use App\Http\Requests\SaveManufacturerRequest;
 use App\Models\Manufacturer;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
--- app/Models/Manufacturer.php
+++ app/Models/Manufacturer.php
@@ -24,7 +24,7 @@ class Manufacturer extends SnipeModel
         'name'   => 'required|min:2|max:255|unique:manufacturers,name,NULL,id,deleted_at,NULL',
         'url'   => 'url|nullable',
         'support_email'   => 'email|nullable',
-        'support_url'   => ['starts_with:http://,https://,afp://,facetime://,file://,irc://','nullable'],
+        'support_url'   => 'starts_with:http://,https://,afp://,facetime://,file://,irc://','nullable',
     ];
 
     protected $hidden = ['user_id'];
