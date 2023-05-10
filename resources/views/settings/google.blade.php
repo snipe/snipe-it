f57e39191d134a49403384f8e57b4b91bf970d72
snipe <snipe@snipe.net>
2023-05-10 00:55:16 -0700

    Added google settings blade
    Signed-off-by: snipe <snipe@snipe.net>

---
+++ resources/views/settings/google.blade.php
@@ -0,0 +1,100 @@
+@extends('layouts/default')
+
+{{-- Page title --}}
+@section('title')
+    Google
+    @parent
+@stop
+
+@section('header_right')
+    <a href="{{ route('settings.index') }}" class="btn btn-primary"> {{ trans('general.back') }}</a>
+@stop
+
+
+{{-- Page content --}}
+@section('content')
+
+
+
+    {{ Form::open(['method' => 'POST', 'files' => false, 'autocomplete' => 'off', 'class' => 'form-horizontal', 'role' => 'form' ]) }}
+    <!-- CSRF Token -->
+    {{csrf_field()}}
+
+    <div class="row">
+        <div class="col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2">
+
+
+            <div class="panel box box-default">
+                <div class="box-header with-border">
+                    <h2 class="box-title">
+                        <i class="fa-brands fa-google"></i> Google
+                    </h2>
+                </div>
+                <div class="box-body">
+
+
+                    <div class="col-md-12">
+
+                        <!-- Google login -->
+                        <div class="form-group {{ $errors->has('google') ? 'error' : '' }}">
+                            <div class="col-md-3">
+                                {{ Form::label('google_login', 'Google Login') }}
+                            </div>
+                            <div class="col-md-8">
+                                <label class="form-control">
+                                    <span class="sr-only">{{ trans('admin/settings/general.pwd_secure_uncommon') }}</span>
+                                    {{ Form::checkbox('google_login', '1', old('google_login', $setting->google_login),array( 'aria-label'=>'google_login')) }}
+                                    Enable google login
+                                </label>
+                            </div>
+                        </div>
+
+
+                        <!-- Google Client ID -->
+                        <div class="form-group {{ $errors->has('google_client_id') ? 'error' : '' }}">
+                            <div class="col-md-3">
+                                {{ Form::label('google_client_id', 'Client ID') }}
+                            </div>
+                            <div class="col-md-9">
+                                {{ Form::text('google_client_id', Request::old('google_client_id', $setting->google_client_id), ['class' => 'form-control','placeholder' => trans('general.example') .'000000000000-XXXXXXXXXXX.apps.googleusercontent.com', $setting->demoMode]) }}
+                                {!! $errors->first('google_client_id', '<span class="alert-msg" aria-hidden="true">:message</span>') !!}
+                                @if (config('app.lock_passwords')===true)
+                                    <p class="text-warning"><i class="fas fa-lock" aria-hidden="true"></i> {{ trans('general.feature_disabled') }}</p>
+                                @endif
+                            </div>
+                        </div>
+
+                        <!-- Google Client Secret -->
+                        <div class="form-group {{ $errors->has('google_client_secret') ? 'error' : '' }}">
+                            <div class="col-md-3">
+                                {{ Form::label('google_client_secret', 'Client Secret') }}
+                            </div>
+                            <div class="col-md-9">
+                                {{ Form::text('google_client_secret', Request::old('google_client_secret', $setting->google_client_secret), ['class' => 'form-control','placeholder' => trans('general.example') .'XXXXXXXXXXXX', $setting->demoMode]) }}
+                                {!! $errors->first('google_client_secret', '<span class="alert-msg" aria-hidden="true">:message</span>') !!}
+                                @if (config('app.lock_passwords')===true)
+                                    <p class="text-warning"><i class="fas fa-lock" aria-hidden="true"></i> {{ trans('general.feature_disabled') }}</p>
+                                @endif
+                            </div>
+                        </div>
+
+                    </div>
+
+
+                </div> <!--/.box-body-->
+                <div class="box-footer">
+                    <div class="text-left col-md-6">
+                        <a class="btn btn-link text-left" href="{{ route('settings.index') }}">{{ trans('button.cancel') }}</a>
+                    </div>
+                    <div class="text-right col-md-6">
+                        <button type="submit" class="btn btn-success"><i class="fas fa-check icon-white" aria-hidden="true"></i> {{ trans('general.save') }}</button>
+                    </div>
+
+                </div>
+            </div> <!-- /box -->
+        </div> <!-- /.col-md-8-->
+    </div> <!-- /.row-->
+
+    {{Form::close()}}
+
+@stop
