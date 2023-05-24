fb5e33c50ff598458360c8b492f911b552c70d21
snipe <snipe@snipe.net>
2023-05-24 16:03:15 -0700

    Updated hash
    Signed-off-by: snipe <snipe@snipe.net>

--- config/version.php
+++ config/version.php
@@ -1,11 +1,10 @@
 <?php
 return array (
   'app_version' => 'v6.1.1-pre',
-  'full_app_version' => 'v6.1.1-pre - build 10653-g11cd875c6',
-  'build_version' => '10653',
+  'full_app_version' => 'v6.1.1-pre - build 10727-gf1b4bba3a',
+  'build_version' => '10727',
   'prerelease_version' => '',
-  'hash_version' => 'g11cd875c6',
-  'full_hash' => 'v6.1.1-pre-411-g11cd875c6',
-  'branch' => 'master',
+  'hash_version' => 'gf1b4bba3a',
+  'full_hash' => 'v6.1.1-pre-485-gf1b4bba3a',
+  'branch' => 'develop',
 );
-
