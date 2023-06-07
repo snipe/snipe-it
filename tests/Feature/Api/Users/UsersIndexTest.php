6f5252449e4e5dad664c77ff4e73da4cd518fc7e
Marcus Moore <contact@marcusmoore.io>
2023-06-07 16:25:18 -0700

    Scaffold test before removing scopeCompanyables call from UsersController

---
+++ tests/Feature/Api/Users/UsersIndexTest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Tests\Feature\Api\Users;
+
+use App\Models\Company;
+use App\Models\User;
+use Laravel\Passport\Passport;
+use Tests\Support\InteractsWithResponses;
+use Tests\Support\InteractsWithSettings;
+use Tests\TestCase;
+
+class UsersIndexTest extends TestCase
+{
+    use InteractsWithResponses;
+    use InteractsWithSettings;
+
+    public function testUsersIndexAdheresToCompanyScoping()
+    {
+        $this->markTestIncomplete();
+
+        [$companyA, $companyB] = Company::factory()->count(2)->create();
+
+        $userA = User::factory()->for($companyA)->create();
+        $userB = User::factory()->for($companyB)->create();
+
+        $superUser = $companyA->users()->save(User::factory()->superuser()->make());
+        $userInCompanyA = $companyA->users()->save(User::factory()->viewUsers()->make());
+        $userInCompanyB = $companyB->users()->save(User::factory()->viewUsers()->make());
+
+        $this->settings->disableMultipleFullCompanySupport();
+
+        Passport::actingAs($superUser);
+        $response = $this->getJson(route('api.users.index'));
+        $this->assertResponseContainsInRows($response, $userA, 'first_name');
+        $this->assertResponseContainsInRows($response, $userB, 'first_name');
+
+        Passport::actingAs($userInCompanyA);
+        $response = $this->getJson(route('api.users.index'));
+        $this->assertResponseContainsInRows($response, $userA, 'first_name');
+        $this->assertResponseContainsInRows($response, $userB, 'first_name');
+
+        Passport::actingAs($userInCompanyB);
+        $response = $this->getJson(route('api.users.index'));
+        $this->assertResponseContainsInRows($response, $userA, 'first_name');
+        $this->assertResponseContainsInRows($response, $userB, 'first_name');
+
+        $this->settings->enableMultipleFullCompanySupport();
+
+        Passport::actingAs($superUser);
+        $response = $this->getJson(route('api.users.index'));
+        $this->assertResponseContainsInRows($response, $userA, 'first_name');
+        $this->assertResponseContainsInRows($response, $userB, 'first_name');
+
+        Passport::actingAs($userInCompanyA);
+        $response = $this->getJson(route('api.users.index'));
+        $this->assertResponseContainsInRows($response, $userA, 'first_name');
+        $this->assertResponseDoesNotContainInRows($response, $userB, 'first_name');
+
+        Passport::actingAs($userInCompanyB);
+        $response = $this->getJson(route('api.users.index'));
+        $this->assertResponseDoesNotContainInRows($response, $userA, 'first_name');
+        $this->assertResponseContainsInRows($response, $userB, 'first_name');
+    }
+}
