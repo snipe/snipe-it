7672861b96ac5ba1487d99153cc6ce1a6b0c344d
Marcus Moore <contact@marcusmoore.io>
2023-06-07 17:38:23 -0700

    Scaffold test before removing scopeCompanyables call from DashboardController

---
+++ tests/Feature/DashboardTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Company;
+use App\Models\User;
+use Tests\Support\InteractsWithSettings;
+use Tests\TestCase;
+
+class DashboardTest extends TestCase
+{
+    use InteractsWithSettings;
+
+    public function testUsersWithoutAdminAccessAreRedirected()
+    {
+        $this->actingAs(User::factory()->create())
+            ->get(route('home'))
+            ->assertRedirect(route('view-assets'));
+    }
+
+    public function testUserCountIsScopedByCompany()
+    {
+        $this->markTestIncomplete();
+
+        $this->settings->enableMultipleFullCompanySupport();
+
+        $companyA = Company::factory()->has(User::factory()->count(1))->create();
+        Company::factory()->has(User::factory()->count(3))->create();
+
+        $adminForCompanyA = $companyA->users()->save(User::factory()->admin()->make());
+
+        $this->actingAs($adminForCompanyA)
+            ->get(route('home'))
+            ->assertOk()
+            ->assertViewIs('dashboard')
+            ->assertViewHas('counts', fn($counts) => $counts['user'] === $companyA->users->count());
+    }
+}
