fd55c99b87c5e68d524940a2fbfcad155cf0a8dc
Marcus Moore <contact@marcusmoore.io>
2023-06-21 16:29:44 -0700

    Remove scopeCompanyables call from AssetsController@index

--- app/Http/Controllers/Api/AssetsController.php
+++ app/Http/Controllers/Api/AssetsController.php
@@ -115,7 +115,7 @@ class AssetsController extends Controller
             $allowed_columns[] = $field->db_column_name();
         }
 
-        $assets = Company::scopeCompanyables(Asset::select('assets.*'), 'company_id', 'assets')
+        $assets = Asset::select('assets.*')
             ->with('location', 'assetstatus', 'company', 'defaultLoc','assignedTo',
                 'model.category', 'model.manufacturer', 'model.fieldset','supplier'); //it might be tempting to add 'assetlog' here, but don't. It blows up update-heavy users.
 
