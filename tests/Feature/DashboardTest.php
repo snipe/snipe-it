7cb22d3d49b1b8707f8f44f09e39c4aba28d2670
Marcus Moore <contact@marcusmoore.io>
2023-06-26 14:00:53 -0700

    Remove incomplete tests

--- tests/Feature/DashboardTest.php
+++ tests/Feature/DashboardTest.php
@@ -2,7 +2,6 @@
 
 namespace Tests\Feature;
 
-use App\Models\Company;
 use App\Models\User;
 use Tests\Support\InteractsWithSettings;
 use Tests\TestCase;
@@ -16,25 +15,5 @@ class DashboardTest extends TestCase
         $this->actingAs(User::factory()->create())
             ->get(route('home'))
             ->assertRedirect(route('view-assets'));
-    }
-
-    public function testUserCountIsScopedByCompany()
-    {
-        $this->markTestIncomplete(
-            'Waiting for removal of Company::scopeCompanyables in DashboardController@index'
-        );
-
-        $this->settings->enableMultipleFullCompanySupport();
-
-        $companyA = Company::factory()->has(User::factory()->count(1))->create();
-        Company::factory()->has(User::factory()->count(3))->create();
-
-        $adminForCompanyA = $companyA->users()->save(User::factory()->admin()->make());
-
-        $this->actingAs($adminForCompanyA)
-            ->get(route('home'))
-            ->assertOk()
-            ->assertViewIs('dashboard')
-            ->assertViewHas('counts', fn($counts) => $counts['user'] === $companyA->users->count());
     }
 }
