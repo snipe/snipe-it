9591442f1e4ab557dfa5cfb1941e4dc1a6652ee8
Ivan Nieto Vivanco <inietov@gmail.com>
2023-07-06 10:46:21 -0600

    Evaluate the radio input to update location only or default location too

--- app/Http/Controllers/Assets/AssetCheckinController.php
+++ app/Http/Controllers/Assets/AssetCheckinController.php
@@ -102,6 +102,10 @@ class AssetCheckinController extends Controller
         if ($request->filled('location_id')) {
             \Log::debug('NEW Location ID: '.$request->get('location_id'));
             $asset->location_id = $request->get('location_id');
+
+            if ($request->get('update_default_location') == 0){
+                $asset->rtd_location_id = $request->get('location_id');
+            }
         }
 
         $checkin_at = date('Y-m-d H:i:s');
