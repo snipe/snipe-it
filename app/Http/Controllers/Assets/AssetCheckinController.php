743e852998b5add55e3e1bab4832a3a36cee4f85
Ivan Nieto Vivanco <inietov@gmail.com>
2023-07-06 10:38:48 -0600

    Add radio buttons to alter asset checkin location behavior

--- app/Http/Controllers/Assets/AssetCheckinController.php
+++ app/Http/Controllers/Assets/AssetCheckinController.php
@@ -102,7 +102,6 @@ class AssetCheckinController extends Controller
         if ($request->filled('location_id')) {
             \Log::debug('NEW Location ID: '.$request->get('location_id'));
             $asset->location_id = $request->get('location_id');
-            $asset->rtd_location_id = $request->get('location_id');
         }
 
         $checkin_at = date('Y-m-d H:i:s');
