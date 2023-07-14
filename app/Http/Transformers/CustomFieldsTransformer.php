3078cab7ee83d606710acc09d630c19f750600cb
snipe <snipe@snipe.net>
2023-07-14 09:09:43 +0100

    Added ability to show/hide custom fields in list views by default
    Signed-off-by: snipe <snipe@snipe.net>

--- app/Http/Transformers/CustomFieldsTransformer.php
+++ app/Http/Transformers/CustomFieldsTransformer.php
@@ -49,6 +49,7 @@ class CustomFieldsTransformer
             'required'   =>  (($field->pivot) && ($field->pivot->required=='1')) ? true : false,
             'display_in_user_view' =>  ($field->display_in_user_view =='1') ? true : false,
             'auto_add_to_fieldsets' =>  ($field->auto_add_to_fieldsets == '1') ? true : false,
+            'show_in_listview'  => ($field->show_in_listview == '1') ? true : false,
             'created_at' => Helper::getFormattedDateObject($field->created_at, 'datetime'),
             'updated_at' => Helper::getFormattedDateObject($field->updated_at, 'datetime'),
         ];
