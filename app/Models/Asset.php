58954cfad5aac9cca7967efa4361960186690448
Marcus Moore <contact@marcusmoore.io>
2023-07-17 17:06:51 -0700

    Enable querying between last checking dates

--- app/Models/Asset.php
+++ app/Models/Asset.php
@@ -649,6 +649,13 @@ class Asset extends Depreciable
             ->withTrashed();
     }
 
+    public function scopeLastCheckinBetween($query, $start, $end)
+    {
+        return $query->whereHas('lastCheckin', function ($query) use ($start, $end) {
+            $query->whereBetween('action_date', [$start, $end]);
+        });
+    }
+
     /**
      * Get the asset's user requests
      *
