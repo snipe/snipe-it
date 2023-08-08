94bef046b88c8fa33f293f16f328368c8873d841
snipe <snipe@snipe.net>
2023-08-08 12:50:38 +0100

    Added mailgun endpoint option, defaulting to the US
    Signed-off-by: snipe <snipe@snipe.net>

--- config/services.php
+++ config/services.php
@@ -25,6 +25,7 @@ return [
     'mailgun' => [
         'domain' => env('MAILGUN_DOMAIN'),
         'secret' => env('MAILGUN_SECRET'),
+        'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
     ],
 
     'mandrill' => [
