18ff810d7e92f317f17096d174534ef16f9abfd8
snipe <snipe@snipe.net>
2023-08-28 20:51:52 +0100

    Reverse orderof parent
    Signed-off-by: snipe <snipe@snipe.net>

--- app/Models/SCIMUser.php
+++ app/Models/SCIMUser.php
@@ -9,7 +9,8 @@ class SCIMUser extends User
     protected $throwValidationExceptions = true; // we want model-level validation to fully THROW, not just return false
 
     public function __construct(array $attributes = []) {
-        parent::__construct($attributes);
         $this->noPassword();
+        parent::__construct($attributes);
+
     }
 }
