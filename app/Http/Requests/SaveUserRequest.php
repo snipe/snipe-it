4caa501996a64ad8f5cc3f30397e491362ec49b9
Marcus Moore <contact@marcusmoore.io>
2023-09-07 13:28:32 -0700

    Relax property type check

--- app/Http/Requests/SaveUserRequest.php
+++ app/Http/Requests/SaveUserRequest.php
@@ -32,7 +32,7 @@ class SaveUserRequest extends FormRequest
     public function rules()
     {
         $rules = [
-            'department_id' => 'nullable|integer|exists:departments,id',
+            'department_id' => 'nullable|exists:departments,id',
             'manager_id' => 'nullable|exists:users,id',
         ];
 
