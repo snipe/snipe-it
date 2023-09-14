dcae5503c86ac7072a8affc8d058be2430798aae
snipe <snipe@snipe.net>
2023-09-14 13:52:57 +0100

    Use $attributes array
    Signed-off-by: snipe <snipe@snipe.net>

--- app/Models/SCIMUser.php
+++ app/Models/SCIMUser.php
@@ -9,8 +9,7 @@ class SCIMUser extends User
     protected $throwValidationExceptions = true; // we want model-level validation to fully THROW, not just return false
 
     public function __construct(array $attributes = []) {
-        $this->noPassword();
+        $attributes['password'] = $this->noPassword();
         parent::__construct($attributes);
-
     }
 }
