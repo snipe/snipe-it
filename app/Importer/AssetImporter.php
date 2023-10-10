758d3aadb4c3e23709e2c70714879452f2774f57
snipe <snipe@snipe.net>
2023-10-10 11:52:35 +0100

    Added trim to import values
    Signed-off-by: snipe <snipe@snipe.net>

--- app/Importer/AssetImporter.php
+++ app/Importer/AssetImporter.php
@@ -80,13 +80,13 @@ class AssetImporter extends ItemImporter
             $this->log('No Matching Asset, Creating a new one');
             $asset = new Asset;
         }
-        $this->item['notes'] = $this->findCsvMatch($row, 'asset_notes');
-        $this->item['image'] = $this->findCsvMatch($row, 'image');
-        $this->item['requestable'] = ($this->fetchHumanBoolean($this->findCsvMatch($row, 'requestable')) == 1) ? '1' : 0;
+        $this->item['notes'] = trim($this->findCsvMatch($row, 'asset_notes'));
+        $this->item['image'] = trim($this->findCsvMatch($row, 'image'));
+        $this->item['requestable'] = trim(($this->fetchHumanBoolean($this->findCsvMatch($row, 'requestable'))) == 1) ? '1' : 0;
         $asset->requestable = $this->item['requestable'];
-        $this->item['warranty_months'] = intval($this->findCsvMatch($row, 'warranty_months'));
+        $this->item['warranty_months'] = intval(trim($this->findCsvMatch($row, 'warranty_months')));
         $this->item['model_id'] = $this->createOrFetchAssetModel($row);
-        $this->item['byod'] = ($this->fetchHumanBoolean($this->findCsvMatch($row, 'byod')) == 1) ? '1' : 0;
+        $this->item['byod'] = ($this->fetchHumanBoolean(trim($this->findCsvMatch($row, 'byod'))) == 1) ? '1' : 0;
 
 
         // If no status ID is found
