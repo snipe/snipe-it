b2aed7feeac598100134db8b7edceea4a5c58b94
snipe <snipe@snipe.net>
2023-10-13 12:30:26 +0100

    Removed temp column
    Signed-off-by: snipe <snipe@snipe.net>

--- database/migrations/2023_01_21_225350_add_eol_date_on_assets_table.php
+++ database/migrations/2023_01_21_225350_add_eol_date_on_assets_table.php
@@ -20,14 +20,6 @@ class AddEolDateOnAssetsTable extends Migration
             if (!Schema::hasColumn('assets', 'asset_eol_date')) {
                 $table->date('asset_eol_date')->after('purchase_date')->nullable()->default(null);
             }
-
-            // This is a temporary shim so we don't have to modify the asset observer for migrations where
-            // there is a large version difference. (See the AssetObserver notes). This column gets created
-            // later in 2023_07_13_052204_denormalized_eol_and_add_column_for_explicit_date_to_assets.php
-            // but we have to temporarily create it now so the save method below doesn't break
-            if (!Schema::hasColumn('assets', 'eol_explicit')) {
-                $table->boolean('eol_explicit')->default(false)->after('asset_eol_date');
-            }
         });
 
         // Chunk the model query to get the models that do have an EOL date
@@ -37,7 +29,7 @@ class AddEolDateOnAssetsTable extends Migration
 
                     if ($asset->purchase_date!='') {
                         $asset->asset_eol_date = $asset->present()->eol_date();
-                        $asset->save();
+                        $asset->saveQuietly();
                     }
 
                 }
