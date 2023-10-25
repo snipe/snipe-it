ca1845efd777d3eb23bb474309a17607253d024e
Ivan Nieto Vivanco <inietov@gmail.com>
2023-10-25 16:41:31 -0600

    Only log checkin event if the assigned_to property is not empty

--- app/Importer/AssetImporter.php
+++ app/Importer/AssetImporter.php
@@ -145,8 +145,10 @@ class AssetImporter extends ItemImporter
             //-- user_id is a property of the abstract class Importer, which this class inherits from and it's setted by
             //-- the class that needs to use it (command importer or GUI importer inside the project).
             if (isset($target)) {
-                if ($asset->assigned_to != $target->id){
-                    event(new CheckoutableCheckedIn($asset, User::find($asset->assigned_to), Auth::user(), $asset->notes, date('Y-m-d H:i:s')));
+                if (!is_null($asset->assigned_to)){
+                    if ($asset->assigned_to != $target->id){
+                        event(new CheckoutableCheckedIn($asset, User::find($asset->assigned_to), Auth::user(), $asset->notes, date('Y-m-d H:i:s')));
+                    }
                 }
 
                 $asset->fresh()->checkOut($target, $this->user_id, date('Y-m-d H:i:s'), null, $asset->notes, $asset->name);
