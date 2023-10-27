c792032e5ed3ed0d0ac5902ca2f7a86ce8a88d40
Robert-Azelis <82208283+Robert-Azelis@users.noreply.github.com>
2023-10-27 20:41:26 +0200

    Added eol_explicit checkbox
    user take decision if want to set manually eol_date or leave it for calculation on the base model_eol

--- resources/views/partials/forms/edit/eol_date.blade.php
+++ resources/views/partials/forms/edit/eol_date.blade.php
@@ -1,7 +1,10 @@
 <!-- EOL Date -->
 <div class="form-group {{ $errors->has('asset_eol_date') ? ' has-error' : '' }}">
     <label for="asset_eol_date" class="col-md-3 control-label">{{ trans('admin/hardware/form.eol_date') }}</label>
-    <div class="input-group col-md-4">
+	<div class="col-md-1 control-label">
+		<input type="checkbox" value="1" name="eol_explicit" id="eol_explicit_active" {{ old('eol_explicit', $item->eol_explicit) == '1' ? ' checked="checked"' : '' }}>
+	</div>    
+    <div class="input-group col-md-4" id="eol_date_row">
         <div class="input-group date" data-provide="datepicker" data-date-clear-btn="true" data-date-format="yyyy-mm-dd"  data-autoclose="true">
             <input type="text" class="form-control" placeholder="{{ trans('general.select_date') }}" name="asset_eol_date" id="asset_eol_date" readonly value="{{  old('asset_eol_date', optional($item->asset_eol_date)->format('Y-m-d') ?? $item->asset_eol_date ?? '')  }}"  style="background-color:inherit" />
             <span class="input-group-addon"><i class="fas fa-calendar" aria-hidden="true"></i></span>
