0cc3545ebf7e7cb4d53dc79b5aac57512ca87ccb
MouseMadeContent <34419201+MouseMadeContent@users.noreply.github.com>
2023-11-05 19:05:29 +0100

    Update 2023_11_05_061721_add_ldaplogingroup.php

--- database/migrations/2023_11_05_061721_add_ldaplogingroup.php
+++ database/migrations/2023_11_05_061721_add_ldaplogingroup.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddFieldLdapLoginGroup extends Migration
+class AddDBFieldLdapLoginGroup extends Migration
 {
     /**
      * Run the migrations.
