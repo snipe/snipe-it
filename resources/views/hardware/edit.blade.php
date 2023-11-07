2c864659a8b28510d78812841748bb7e73a19b3c
Robert-Azelis <82208283+Robert-Azelis@users.noreply.github.com>
2023-11-07 17:41:30 +0100

    Added part of script to manage eol_explicit checkbox

--- resources/views/hardware/edit.blade.php
+++ resources/views/hardware/edit.blade.php
@@ -166,6 +166,23 @@
         fetchCustomFields()
     });
     @endif
+	
+    // If the "eol_explicit" check box is checked, show then the ability to set eol_date
+    $(document).ready(function() {
+        $("#eol_explicit_active").change(function() {
+            if ((this.checked)) {
+                $("#eol_date_row").show();
+            } else {
+                $("#eol_date_row").hide();
+            }
+        });
+	
+        if({{ $item->eol_explicit ? 'true' : 'false' }}) {
+            $("#eol_date_row").show();
+        } else {
+            $("#eol_date_row").hide();
+        };
+    });
 
     var transformed_oldvals={};
 
