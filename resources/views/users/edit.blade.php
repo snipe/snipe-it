63065bab5d32beed747b5907e7a79ab382e41c90
akemidx <kojotek.dx@gmail.com>
2023-11-28 17:51:43 -0500

    adding in icons for clarity, since there are two places called settings

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -553,7 +553,7 @@
                             </div>
                                  @endif
                            @else
-                               <p>{{ trans('admin/users/table.nogroup') }} <code>{{ trans('admin/settings/general.admin_settings') }} > {{ trans('general.groups') }}</code> </p>
+                                      <p>{{ trans('admin/users/table.nogroup') }} <code>{{ trans('admin/settings/general.admin_settings') }} <i class="fa fa-cogs"></i> > {{ trans('general.groups') }} <i class="fas fa-user-friends"></i></code> </p>
                            @endif
 
                               </div>
