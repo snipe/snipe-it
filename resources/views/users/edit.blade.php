108658520d7afc8037165e905867f1e932475702
akemidx <kojotek.dx@gmail.com>
2023-11-28 17:15:51 -0500

    translating no group message on user edit page

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -553,7 +553,7 @@
                             </div>
                                  @endif
                            @else
-                               <p>No groups have been created yet. Visit <code>Admin Settings > Permission Groups</code> to add one.</p>
+                               <p>{{ trans('admin/users/table.nogroup') }} <code>{{ trans('admin/settings/general.admin_settings') }} > {{ trans('general.groups') }}</code> </p>
                            @endif
 
                               </div>
