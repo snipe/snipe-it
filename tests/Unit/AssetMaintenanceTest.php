f9697887e8aa18c80e76321a075399df7f1a4808
snipe <snipe@snipe.net>
2023-12-12 13:41:35 +0000

    Removed extraneous test
    Signed-off-by: snipe <snipe@snipe.net>

--- tests/Unit/AssetMaintenanceTest.php
+++ tests/Unit/AssetMaintenanceTest.php
@@ -46,7 +46,5 @@ class AssetMaintenanceTest extends TestCase
         $this->assertTrue($c->completion_date === null);
         $c->completion_date = '0000-00-00';
         $this->assertTrue($c->completion_date === null);
-        $c->completion_date = '2017-05-12';
-        $this->assertTrue($c->completion_date == Carbon::parse('2017-05-12'));
     }
 }
