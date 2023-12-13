337d86333043c679312df46b2e0f2e9afdf7489d
snipe <snipe@snipe.net>
2023-12-13 13:52:31 +0000

    Missed closing label on radio buttons
    Signed-off-by: snipe <snipe@snipe.net>

--- resources/views/livewire/custom-field-set-default-values-for-model.blade.php
+++ resources/views/livewire/custom-field-set-default-values-for-model.blade.php
@@ -63,6 +63,7 @@
                                     @foreach(explode("\r\n", $field->field_values) as $field_value)
                                     <label class="col-md-3 form-control" for="{{ str_slug($field_value) }}">
                                         <input id="{{ str_slug($field_value) }}" aria-label="{{ str_slug($field->name) }}"  type='radio' name="default_values[{{ $field->id }}]" value="{{$field_value}}" {{ $field->defaultValue($model_id) == $field_value ? 'checked="checked"': '' }} />{{ $field_value }}
+                                    </label>
                                     @endforeach
 
                                 @elseif($field->element == "checkbox")
