a5947b8109381e8e73274a4b337c7c505d28c262
snipe <snipe@snipe.net>
2023-12-19 20:34:45 +0000

    Added warning in middleware
    Signed-off-by: snipe <snipe@snipe.net>

--- app/Http/Middleware/CheckLocale.php
+++ app/Http/Middleware/CheckLocale.php
@@ -36,6 +36,10 @@ class CheckLocale
 
         }
 
+        if (config('app.locale') != Helper::mapLegacyLocale($language)) {
+           \Log::warning('Your current APP_LOCALE in your .env is set to "'.config('app.locale').'" and should be updated to be "'.Helper::mapLegacyLocale($language).'" in '.base_path().'/.env. Translations may display unexpectedly until this is updated.');
+        }
+
         \App::setLocale(Helper::mapLegacyLocale($language));
         return $next($request);
     }
