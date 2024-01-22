219540281fb6f415dc16cd4e0754d755f700f5db
Marcus Moore <contact@marcusmoore.io>
2024-01-22 12:42:01 -0800

    Add trailing commas

--- resources/views/reports/custom.blade.php
+++ resources/views/reports/custom.blade.php
@@ -280,61 +280,61 @@
                 'multiple' => 'true',
                 'fieldname' => 'by_company_id[]',
                 'hide_new' => 'true',
-                'selected' => $template->selectValues('by_company_id', \App\Models\Company::class)
+                'selected' => $template->selectValues('by_company_id', \App\Models\Company::class),
             ])
             @include ('partials.forms.edit.location-select', [
                 'translated_name' => trans('general.location'),
                 'multiple' => 'true',
                 'fieldname' => 'by_location_id[]',
                 'hide_new' => 'true',
-                'selected' => $template->selectValues('by_location_id', \App\Models\Location::class)
+                'selected' => $template->selectValues('by_location_id', \App\Models\Location::class),
             ])
             @include ('partials.forms.edit.location-select', [
                 'translated_name' => trans('admin/hardware/form.default_location'),
                 'multiple' => 'true',
                 'fieldname' => 'by_rtd_location_id[]',
                 'hide_new' => 'true',
-                'selected' => $template->selectValues('by_rtd_location_id', \App\Models\Location::class)
+                'selected' => $template->selectValues('by_rtd_location_id', \App\Models\Location::class),
              ])
             @include ('partials.forms.edit.department-select', [
                 'translated_name' => trans('general.department'),
                 'fieldname' => 'by_dept_id',
                 'hide_new' => 'true',
-                'selected' => $template->selectValue('by_dept_id', \App\Models\Department::class)
+                'selected' => $template->selectValue('by_dept_id', \App\Models\Department::class),
             ])
             @include ('partials.forms.edit.supplier-select', [
                 'translated_name' => trans('general.supplier'),
                 'fieldname' => 'by_supplier_id[]',
                 'multiple' => 'true',
                 'hide_new' => 'true',
-                'selected' => $template->selectValues('by_supplier_id', \App\Models\Supplier::class)
+                'selected' => $template->selectValues('by_supplier_id', \App\Models\Supplier::class),
             ])
             @include ('partials.forms.edit.model-select', [
                 'translated_name' => trans('general.asset_model'),
                 'fieldname' => 'by_model_id[]',
                 'multiple' => 'true',
                 'hide_new' => 'true',
-                'selected' => $template->selectValues('by_model_id', \App\Models\AssetModel::class)
+                'selected' => $template->selectValues('by_model_id', \App\Models\AssetModel::class),
             ])
             @include ('partials.forms.edit.manufacturer-select', [
                 'translated_name' => trans('general.manufacturer'),
                 'fieldname' => 'by_manufacturer_id',
                 'hide_new' => 'true',
-                'selected' => $template->selectValue('by_manufacturer_id', \App\Models\Manufacturer::class)
+                'selected' => $template->selectValue('by_manufacturer_id', \App\Models\Manufacturer::class),
              ])
             @include ('partials.forms.edit.category-select', [
                 'translated_name' => trans('general.category'),
                 'fieldname' => 'by_category_id',
                 'hide_new' => 'true',
                 'category_type' => 'asset',
-                'selected' => $template->selectValue('by_category_id', \App\Models\Category::class)
+                'selected' => $template->selectValue('by_category_id', \App\Models\Category::class),
             ])
             @include ('partials.forms.edit.status-select', [
                 'translated_name' => trans('admin/hardware/form.status'),
                 'fieldname' => 'by_status_id[]',
                 'multiple' => 'true',
                 'hide_new' => 'true',
-                'selected' => $template->selectValues('by_status_id', \App\Models\Statuslabel::class)
+                'selected' => $template->selectValues('by_status_id', \App\Models\Statuslabel::class),
             ])
 
             <!-- Order Number -->
