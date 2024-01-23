0881301b6d110ea3b01c6c65f0f35b246e266deb
Marcus Moore <contact@marcusmoore.io>
2024-01-23 13:55:20 -0800

    Fix language strings

--- resources/lang/en-US/admin/reports/general.php
+++ resources/lang/en-US/admin/reports/general.php
@@ -13,5 +13,12 @@ return [
         'user_state' => 'User State',
         'user_country' => 'User Country',
         'user_zip' => 'User Zip'
-    ]
+    ],
+    'saved_templates' => 'Saved Templates',
+    'save_template' =>  'Save Template',
+    'select_a_template' => 'Select a Template',
+    'apply_template' => 'Apply Template',
+    'generate_template' => 'Generate Template',
+    'apply_and_generate' => 'Apply and Generate',
+    'template_name' => 'Enter Template Name',
 ];
