8a5b469ff853a5156ec55b4e61bd8a2a819385a6
Godfrey M <godmartinz@gmail.com>
2024-01-31 12:54:30 -0800

    adds # to webhook channel trigger

--- app/Http/Livewire/SlackSettingsForm.php
+++ app/Http/Livewire/SlackSettingsForm.php
@@ -80,12 +80,12 @@ class SlackSettingsForm extends Component
         $this->webhook_botname = $this->setting->webhook_botname;
         $this->webhook_options = $this->setting->webhook_selected;
         if($this->webhook_selected == 'microsoft' || $this->webhook_selected == 'google'){
-            $this->webhook_channel = null;
+            $this->webhook_channel = '#NA';
         }
 
 
         if($this->setting->webhook_endpoint != null && $this->setting->webhook_channel != null){
-            $this->isDisabled= '#NA';
+            $this->isDisabled= '';
         }
 
     }
