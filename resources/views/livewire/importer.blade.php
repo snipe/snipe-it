575362f4dc19fbc8f17c79cc90146375e0c837a5
akemidx <kojotek.dx@gmail.com>
2024-02-06 15:38:07 -0500

    adding in line to null out the active file on click of button

--- resources/views/livewire/importer.blade.php
+++ resources/views/livewire/importer.blade.php
@@ -133,8 +133,11 @@
                                                         <i class="fa-solid fa-list-check" aria-hidden="true"></i>
                                                         <span class="sr-only">{{ trans('general.import') }}</span>
                                                     </button>
+                                                    <a href="#" wire:click="$set('activeFile',null)">
                                                     <button class="btn btn-sm btn-danger" wire:click="destroy({{ $currentFile->id }})">
-                                                        <i class="fas fa-trash icon-white" aria-hidden="true"></i><span class="sr-only"></span></button>
+                                                        <i class="fas fa-trash icon-white" aria-hidden="true"></i><span class="sr-only"></span>
+                                                    </button>
+                                                    </a>
                                     			</td>
                                     		</tr>
 
