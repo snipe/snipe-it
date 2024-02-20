972b198248b37858d62c3d7271716e27fe4df2e8
snipe <snipe@snipe.net>
2024-02-20 17:27:16 +0000

    Removed debugging
    Signed-off-by: snipe <snipe@snipe.net>

--- app/Http/Controllers/AssetModelsController.php
+++ app/Http/Controllers/AssetModelsController.php
@@ -442,7 +442,6 @@ class AssetModelsController extends Controller
             $del_count = 0;
 
             foreach ($models as $model) {
-                \Log::debug($model->id);
 
                 if ($model->assets_count > 0) {
                     $del_error_count++;
@@ -452,8 +451,6 @@ class AssetModelsController extends Controller
                 }
             }
 
-            \Log::debug($del_count);
-            \Log::debug($del_error_count);
 
             if ($del_error_count == 0) {
                 return redirect()->route('models.index')
