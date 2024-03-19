0d0984a400c0fb86e9c0a1c3035ad3faa5bb725f
Godfrey M <godmartinz@gmail.com>
2024-03-19 14:26:40 -0700

    Bulk Delete Locations does not work [sc-25100]

--- app/Models/Location.php
+++ app/Models/Location.php
@@ -106,11 +106,12 @@ class Location extends SnipeModel
      */
     public function isDeletable()
     {
+
         return Gate::allows('delete', $this)
-                && ($this->assets_count === 0)
-                && ($this->assigned_assets_count === 0)
-                && ($this->children_count === 0)
-                && ($this->users_count === 0);
+                && ($this->assets_count == 0)
+                && ($this->assigned_assets_count == 0)
+                && ($this->children_count == 0)
+                && ($this->users_count == 0);
     }
 
     /**
