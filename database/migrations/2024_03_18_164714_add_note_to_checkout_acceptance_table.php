43c32fa1bf08c8e38fe776fa8e9a74804e08e772
Godfrey M <godmartinz@gmail.com>
2024-04-03 11:47:08 -0700

    fix class name in migration

--- database/migrations/2024_03_18_164714_add_note_to_checkout_acceptance_table.php
+++ database/migrations/2024_03_18_164714_add_note_to_checkout_acceptance_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddDeclineMsgToCheckoutAcceptanceTable extends Migration
+class AddNoteToCheckoutAcceptanceTable extends Migration
 {
     /**
      * Run the migrations.
