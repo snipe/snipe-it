df5cacf8a2814c631f89b94b213a60b5b2789f16
Godfrey M <godmartinz@gmail.com>
2024-04-03 11:06:05 -0700

    removed front end if statement

--- resources/views/partials/label2-field-definitions.blade.php
+++ resources/views/partials/label2-field-definitions.blade.php
@@ -348,9 +348,7 @@
                             </optgroup>
                             <optgroup label="Custom Fields">
                                 @foreach($customFields as $customField)
-                                    @if($customField->field_encrypted != 1)
                                     <option value="{{ $customField->db_column }}">{{ $customField->name }}</option>
-                                    @endif
                                 @endforeach
                             </optgroup>
                         </select>
