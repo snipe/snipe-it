a8f26e26ec21238337ea26d4b3f5ba1547569555
Godfrey M <godmartinz@gmail.com>
2024-04-25 10:59:42 -0700

    added migration to git

---
+++ database/migrations/2024_04_25_174702_add_ldap_company_to_settings_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddLdapCompanyToSettingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('settings', function (Blueprint $table) {
+            $table->string('ldap_company')->after('ldap_jobtitle')->nullable()->default(null);$table->string('ldap_ou_sync_type')->after('ldap_location')->default('location');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('settings', function (Blueprint $table) {
+            $table->dropColumn('ldap_company');
+        });
+    }
+}
