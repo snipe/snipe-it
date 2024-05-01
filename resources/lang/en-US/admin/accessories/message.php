4a4689adfed3bc9ccee322a6578e7af1e9be4ba8
Godfrey M <godmartinz@gmail.com>
2024-05-01 09:12:13 -0700

    adds corrections, error messages

--- resources/lang/en-US/admin/accessories/message.php
+++ resources/lang/en-US/admin/accessories/message.php
@@ -26,7 +26,8 @@ return array(
         'error'   		=> 'Accessory was not checked out, please try again',
         'success' 		=> 'Accessory checked out successfully.',
         'unavailable'   => 'Accessory is not available for checkout. Check quantity available',
-        'user_does_not_exist' => 'That user is invalid. Please try again.'
+        'user_does_not_exist' => 'That user is invalid. Please try again.',
+        'invalid_qty'           => 'Please enter a valid quantity.',
     ),
 
     'checkin' => array(
