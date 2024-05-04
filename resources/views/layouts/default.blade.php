fa0bea87e656e838dfba7857713a0717e832347a
Mustafa Online <sawdjg@gmail.com>
2024-05-04 13:17:48 +0200

    Improve RTL support
    I'm starting to make some improvements to RTL support, more PR's to come to address other parts.

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}"
+dir="{{ in_array(app()->getLocale(),['ar-SA','fa-IR', 'he-IL']) ? 'rtl' : 'ltr' }}">
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
