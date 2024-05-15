0734d5b0b600c19481c6ad54a2a3ba1f3ba9b286
snipe <snipe@snipe.net>
2024-05-15 14:21:42 +0100

    Added help text
    Signed-off-by: snipe <snipe@snipe.net>

--- resources/lang/en-US/general.php
+++ resources/lang/en-US/general.php
@@ -202,6 +202,8 @@ return [
     'new_password'          => 'New Password',
     'next'					=> 'Next',
     'next_audit_date'		=> 'Next Audit Date',
+    'next_audit_date_help'  => 'If you use auditing in your organization, this is usually automatically calculated based on the asset&apos;s last audit date and audit frequency (in <code>Admin Settings &gt; Alerts</code>) and you can leave this blank. You can manually set this date here if you need to, but it must be later than the last audit date. ',
+    'audit_images_help'     => 'You can find audit images in the asset\'s history tab.',
     'no_email'              => 'No email address associated with this user',
     'last_audit'		    => 'Last Audit',
     'new'					=> 'new!',
