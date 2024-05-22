969f028b481af275669393c1efd478664687a987
akemidx <kojotek.dx@gmail.com>
2024-05-22 17:29:29 -0400

    modal added to resource/views/modal

---
+++ resources/views/modals/add-note.blade.php
@@ -0,0 +1,25 @@
+{{-- See snipeit_modals.js for what powers this --}}
+<div class="modal-dialog">
+    <div class="modal-content">
+        <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <h2 class="modal-title">{{ trans('general.add_note')  }}</h2>
+        </div>
+
+        <div class="modal-body">
+            <form action="{{ route('api.notes.store') }}" onsubmit="return false"> <!-- This is gonna become an action log item and not the original route -->
+                <div class="alert alert-danger" id="modal_error_msg" style="display:none"></div>
+                {{ Form::label('note', trans('general.add_note'), array('class' => 'col-md-3 control-label')) }}
+                <div class="col-md-8">
+                    <textarea class="col-md-6 form-control" id="note" name="note">{{ old('note') }}</textarea>
+                    {!! $errors->first('note', '<span class="alert-msg" aria-hidden="true"><i class="fas fa-times" aria-hidden="true"></i> :message</span>') !!}
+                </div>
+            </form>
+        </div>
+
+        <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">{{ trans('button.cancel') }}</button>
+            <button type="button" class="btn btn-primary" id="modal-save">{{ trans('general.save') }}</button>
+        </div>
+    </div><!-- /.modal-content -->
+</div><!-- /.modal-dialog -->
