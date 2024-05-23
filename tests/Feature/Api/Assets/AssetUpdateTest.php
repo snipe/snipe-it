8ce577db376b8158d361372337fe751f6566f73b
spencerrlongg <spencer@spencerlong.com>
2024-05-23 15:51:26 -0500

    adds @snipe's rules for undeleted assigned targets

--- tests/Feature/Api/Assets/AssetUpdateTest.php
+++ tests/Feature/Api/Assets/AssetUpdateTest.php
@@ -306,4 +306,121 @@ class AssetUpdateTest extends TestCase
         $asset->refresh();
         $this->assertEquals("encrypted value should not change", Crypt::decrypt($asset->{$field->db_column_name()}));
     }
+
+    public function testCheckoutToUserOnAssetUpdate()
+    {
+        $asset = Asset::factory()->create();
+        $user = User::factory()->editAssets()->create();
+        $assigned_user = User::factory()->create();
+
+        $response = $this->actingAsForApi($user)
+            ->patchJson(route('api.assets.update', $asset->id), [
+                'assigned_user' => $assigned_user->id,
+            ])
+            ->assertOk()
+            ->assertStatusMessageIs('success')
+            ->json();
+
+        $asset->refresh();
+        $this->assertEquals($assigned_user->id, $asset->assigned_to);
+        $this->assertEquals($asset->assigned_type, 'App\Models\User');
+    }
+
+    public function testCheckoutToDeletedUserFailsOnAssetUpdate()
+    {
+        $asset = Asset::factory()->create();
+        $user = User::factory()->editAssets()->create();
+        $assigned_user = User::factory()->deleted()->create();
+
+        $this->actingAsForApi($user)
+            ->patchJson(route('api.assets.update', $asset->id), [
+                'assigned_user' => $assigned_user->id,
+            ])
+            ->assertOk()
+            ->assertStatusMessageIs('error')
+            ->json();
+
+        $asset->refresh();
+        $this->assertNull($asset->assigned_to);
+        $this->assertNull($asset->assigned_type);
+    }
+
+    public function testCheckoutToLocationOnAssetUpdate()
+    {
+        $asset = Asset::factory()->create();
+        $user = User::factory()->editAssets()->create();
+        $assigned_location = Location::factory()->create();
+
+        $this->actingAsForApi($user)
+            ->patchJson(route('api.assets.update', $asset->id), [
+                'assigned_location' => $assigned_location->id,
+            ])
+            ->assertOk()
+            ->assertStatusMessageIs('success')
+            ->json();
+
+        $asset->refresh();
+        $this->assertEquals($assigned_location->id, $asset->assigned_to);
+        $this->assertEquals($asset->assigned_type, 'App\Models\Location');
+
+    }
+
+    public function testCheckoutToDeletedLocationFailsOnAssetUpdate()
+    {
+        $asset = Asset::factory()->create();
+        $user = User::factory()->editAssets()->create();
+        $assigned_location = Location::factory()->deleted()->create();
+
+        $this->actingAsForApi($user)
+            ->patchJson(route('api.assets.update', $asset->id), [
+                'assigned_location' => $assigned_location->id,
+            ])
+            ->assertOk()
+            ->assertStatusMessageIs('error')
+            ->json();
+
+        $asset->refresh();
+        $this->assertNull($asset->assigned_to);
+        $this->assertNull($asset->assigned_type);
+    }
+
+    public function testCheckoutAssetOnAssetUpdate()
+    {
+        $asset = Asset::factory()->create();
+        $user = User::factory()->editAssets()->create();
+        $assigned_asset = Asset::factory()->create();
+
+        $this->actingAsForApi($user)
+            ->patchJson(route('api.assets.update', $asset->id), [
+                'assigned_asset'   => $assigned_asset->id,
+                'checkout_to_type' => 'user',
+            ])
+            ->assertOk()
+            ->assertStatusMessageIs('success')
+            ->json();
+
+        $asset->refresh();
+        $this->assertEquals($assigned_asset->id, $asset->assigned_to);
+        $this->assertEquals($asset->assigned_type, 'App\Models\Asset');
+
+    }
+
+    public function testCheckoutToDeletedAssetFailsOnAssetUpdate()
+    {
+        $asset = Asset::factory()->create();
+        $user = User::factory()->editAssets()->create();
+        $assigned_asset = Asset::factory()->deleted()->create();
+
+        $this->actingAsForApi($user)
+            ->patchJson(route('api.assets.update', $asset->id), [
+                'assigned_asset' => $assigned_asset->id,
+            ])
+            ->assertOk()
+            ->assertStatusMessageIs('error')
+            ->json();
+
+        $asset->refresh();
+        $this->assertNull($asset->assigned_to);
+        $this->assertNull($asset->assigned_type);
+    }
 }
