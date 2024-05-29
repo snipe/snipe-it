ee75b30683a9d71009f901c2e757abfc01961e44
Marcus Moore <contact@marcusmoore.io>
2024-05-29 14:06:27 -0700

    Enable legacy_model_binding

--- config/livewire.php
+++ config/livewire.php
@@ -101,7 +101,7 @@ return [
     |
     */
 
-    'legacy_model_binding' => false,
+    'legacy_model_binding' => true,
 
     /*
     |---------------------------------------------------------------------------
