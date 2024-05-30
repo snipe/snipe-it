c8279c0b99067a7391d88d89277e74f17c9620ec
snipe <snipe@snipe.net>
2024-05-30 23:06:17 +0100

    Moved asset-only date stuff into the asset importer
    Signed-off-by: snipe <snipe@snipe.net>

--- app/Importer/ItemImporter.php
+++ app/Importer/ItemImporter.php
@@ -79,26 +79,6 @@ class ItemImporter extends Importer
             $this->item['purchase_date'] = date('Y-m-d', strtotime($this->findCsvMatch($row, 'purchase_date')));
         }
 
-        $this->item['last_audit_date'] = null;
-        if ($this->findCsvMatch($row, 'last_audit_date') != '') {
-            $this->item['last_audit_date'] = date('Y-m-d', strtotime($this->findCsvMatch($row, 'last_audit_date')));
-        }
-
-        $this->item['next_audit_date'] = null;
-        if ($this->findCsvMatch($row, 'next_audit_date') != '') {
-            $this->item['next_audit_date'] = date('Y-m-d', strtotime($this->findCsvMatch($row, 'next_audit_date')));
-        }
-
-        $this->item['asset_eol_date'] = null;
-            if($this->findCsvMatch($row, 'asset_eol_date') != '') {
-                $csvMatch = $this->findCsvMatch($row, 'asset_eol_date');
-                try {
-                    $this->item['asset_eol_date'] = CarbonImmutable::parse($csvMatch)->format('Y-m-d');
-                } catch (\Exception $e) {
-                    Log::info($e->getMessage());
-                    $this->log('Unable to parse date: '.$csvMatch);
-                }
-        }
 
         $this->item['qty'] = $this->findCsvMatch($row, 'quantity');
         $this->item['requestable'] = $this->findCsvMatch($row, 'requestable');
