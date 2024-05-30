ea5839523d21a2af1de4e44ced2aeb4e5f529a28
akemidx <kojotek.dx@gmail.com>
2024-05-30 15:22:45 -0400

    modal tinkering

--- resources/views/modals/add-note.blade.php
+++ resources/views/modals/add-note.blade.php
@@ -16,12 +16,11 @@
                     <textarea class="col-md-6 form-control" id="note" name="note">{{ old('note') }}</textarea>
                     {!! $errors->first('note', '<span class="alert-msg" aria-hidden="true"><i class="fas fa-times" aria-hidden="true"></i> :message</span>') !!}
                 </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">{{ trans('button.cancel') }}</button>
+                    <button type="button" class="btn btn-primary" id="modal-save">{{ trans('general.save') }}</button>
+                </div>
             </form>
-        </div>
-
-        <div class="modal-footer">
-            <button type="button" class="btn btn-default" data-dismiss="modal">{{ trans('button.cancel') }}</button>
-            <button type="button" class="btn btn-primary" id="modal-save">{{ trans('general.save') }}</button>
         </div>
     </div><!-- /.modal-content -->
 </div><!-- /.modal-dialog -->
