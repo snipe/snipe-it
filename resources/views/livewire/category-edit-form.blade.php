a3dea99bbbe3c9762a06840911a9c8a8f639d449
Marcus Moore <contact@marcusmoore.io>
2024-06-17 14:24:16 -0700

    Fix category edit form component

--- resources/views/livewire/category-edit-form.blade.php
+++ resources/views/livewire/category-edit-form.blade.php
@@ -18,12 +18,12 @@
         <div class="col-md-9 col-md-offset-3">
             @if ($defaultEulaText!='')
                 <label class="form-control">
-                    {{ Form::checkbox('use_default_eula', '1', null, ['wire:model.live' => 'useDefaultEula', 'aria-label'=>'use_default_eula']) }}
+                    {{ Form::checkbox('use_default_eula', '1', $useDefaultEula, ['wire:model.live' => 'useDefaultEula', 'aria-label'=>'use_default_eula']) }}
                     <span>{!! trans('admin/categories/general.use_default_eula') !!}</span>
                 </label>
             @else
                 <label class="form-control form-control--disabled">
-                    {{ Form::checkbox('use_default_eula', '0', null, ['wire:model.live' => 'useDefaultEula', 'class'=>'disabled','disabled' => 'disabled', 'aria-label'=>'use_default_eula']) }}
+                    {{ Form::checkbox('use_default_eula', '0', $useDefaultEula, ['wire:model.live' => 'useDefaultEula', 'class'=>'disabled','disabled' => 'disabled', 'aria-label'=>'use_default_eula']) }}
                     <span>{!! trans('admin/categories/general.use_default_eula_disabled') !!}</span>
                 </label>
             @endif
@@ -34,7 +34,7 @@
     <div class="form-group">
         <div class="col-md-9 col-md-offset-3">
             <label class="form-control">
-                {{ Form::checkbox('require_acceptance', '1', null, ['wire:model.live' => 'requireAcceptance', 'aria-label'=>'require_acceptance']) }}
+                {{ Form::checkbox('require_acceptance', '1', $requireAcceptance, ['wire:model.live' => 'requireAcceptance', 'aria-label'=>'require_acceptance']) }}
                 {{ trans('admin/categories/general.require_acceptance') }}
             </label>
         </div>
@@ -44,7 +44,7 @@
     <div class="form-group">
         <div class="col-md-9 col-md-offset-3">
             <label class="form-control">
-                {{ Form::checkbox('checkin_email', '1', null, ['wire:model.live' => 'sendCheckInEmail', 'aria-label'=>'checkin_email', 'disabled' => $this->sendCheckInEmailDisabled]) }}
+                {{ Form::checkbox('checkin_email', '1', $sendCheckInEmail, ['wire:model.live' => 'sendCheckInEmail', 'aria-label'=>'checkin_email', 'disabled' => $this->sendCheckInEmailDisabled]) }}
                 {{ trans('admin/categories/general.checkin_email') }}
             </label>
             @if ($this->shouldDisplayEmailMessage)
