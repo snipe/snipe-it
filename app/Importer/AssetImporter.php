45dbe5cb77a6a2ab2cce603d50d083a3e71b4c8a
snipe <snipe@snipe.net>
2024-06-20 15:05:38 +0100

    Return the id, not the entire status label
    Signed-off-by: snipe <snipe@snipe.net>

--- app/Importer/AssetImporter.php
+++ app/Importer/AssetImporter.php
@@ -20,7 +20,7 @@ class AssetImporter extends ItemImporter
     {
         parent::__construct($filename);
 
-        $this->defaultStatusLabelId = Statuslabel::first();
+        $this->defaultStatusLabelId = Statuslabel::first()->id;
         
         if (!is_null(Statuslabel::deployable()->first())) {
             $this->defaultStatusLabelId = Statuslabel::deployable()->first()->id;
