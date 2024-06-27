74cff8aea13980d89c6e5ffbcb487277dc83aaf7
akemidx <kojotek.dx@gmail.com>
2024-06-27 18:02:32 -0400

    padding for buttons on modal popup

--- resources/views/modals/add-note.blade.php
+++ resources/views/modals/add-note.blade.php
@@ -11,16 +11,19 @@
                 <input type="hidden" name="type" value="{{request("type")}}"/>
                 <input type="hidden" name="id" value="{{request("id")}}"/>
                 <div class="alert alert-danger" id="modal_error_msg" style="display:none"></div>
-                {{ Form::label('note', trans('general.add_note'), array('class' => 'col-md-3 control-label')) }}
-                <div class="col-md-8">
-                    <textarea class="col-md-6 form-control" id="note" name="note">{{ old('note') }}</textarea>
-                    {!! $errors->first('note', '<span class="alert-msg" aria-hidden="true"><i class="fas fa-times" aria-hidden="true"></i> :message</span>') !!}
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-default" data-dismiss="modal">{{ trans('button.cancel') }}</button>
-                    <button type="button" class="btn btn-primary" id="modal-save">{{ trans('general.save') }}</button>
+
+                <div class="row">
+                    {{ Form::label('note', trans('general.add_note'), array('class' => 'col-md-3 control-label')) }}
+                    <div class="col-md-8">
+                        <textarea class="form-control" id="note" name="note">{{ old('note') }}</textarea>
+                        {!! $errors->first('note', '<span class="alert-msg" aria-hidden="true"><i class="fas fa-times" aria-hidden="true"></i> :message</span>') !!}
+                    </div>
                 </div>
             </form>
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">{{ trans('button.cancel') }}</button>
+            <button type="button" class="btn btn-primary" id="modal-save">{{ trans('general.save') }}</button>
         </div>
     </div><!-- /.modal-content -->
 </div><!-- /.modal-dialog -->
