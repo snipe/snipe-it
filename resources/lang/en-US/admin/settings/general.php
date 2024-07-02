0c27de1dc6616fbba22abea67ca1bfc3b1ff1a46
Godfrey M <godmartinz@gmail.com>
2024-07-02 15:43:13 -0700

    fix typo

--- resources/lang/en-US/admin/settings/general.php
+++ resources/lang/en-US/admin/settings/general.php
@@ -377,7 +377,7 @@ return [
     'timezone' => 'Timezone',
     'profile_edit'          => 'Edit Profile',
     'profile_edit_help'          => 'Allow users to edit their own profiles.',
-    'required_serial_error' => 'All current assets must have a serial before you can select this option. There are currently :count Assets without serials',
+    'required_serial_error' => 'All current assets must have a serial before you can select this option. There are currently :count Assets without serials.',
 
 
 
