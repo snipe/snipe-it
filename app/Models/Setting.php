9527aac242c2ecde08feb25b5c5841ab9944d8ad
snipe <snipe@snipe.net>
2024-07-04 13:23:17 +0100

    Make site name required at the model
    Signed-off-by: snipe <snipe@snipe.net>

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -49,6 +49,7 @@ class Setting extends Model
      * @var array
      */
     protected $rules = [
+          'site_name'                           => 'required',
           'brand'                               => 'required|min:1|numeric',
           'qr_text'                             => 'max:31|nullable',
           'alert_email'                         => 'email_array|nullable',
