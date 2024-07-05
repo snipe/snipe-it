f32bf35c2213eb7054db958e33283f14207ddcbe
snipe <snipe@snipe.net>
2024-07-05 10:50:03 +0100

    Fixed another rename :-/
    Signed-off-by: snipe <snipe@snipe.net>

--- tests/Feature/Locations/Api/IndexLocationsTest.php
+++ tests/Feature/Locations/Api/IndexLocationsTest.php
@@ -8,7 +8,7 @@ use App\Models\User;
 use Illuminate\Testing\Fluent\AssertableJson;
 use Tests\TestCase;
 
-class LocationIndexTest extends TestCase
+class IndexLocationsTest extends TestCase
 {
     public function testViewingLocationIndexRequiresAuthentication()
     {
