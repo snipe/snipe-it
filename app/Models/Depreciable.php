aaa28583379338e37a1fe2f83bbc3b24c6d8deca
Godfrey M <godmartinz@gmail.com>
2024-07-16 12:25:19 -0700

    battling with handling depreciation percentage and amount

--- app/Models/Depreciable.php
+++ app/Models/Depreciable.php
@@ -67,7 +67,7 @@ class Depreciable extends SnipeModel
      * @return float|int
      */
     public function getLinearDepreciatedValue() // TODO - for testing it might be nice to have an optional $relative_to param here, defaulted to 'now'
-    {
+    {  ;
         if (($this->get_depreciation()) && ($this->purchase_date)) {
             $months_passed = ($this->purchase_date->diff(now())->m)+($this->purchase_date->diff(now())->y*12);
         } else {
@@ -78,7 +78,7 @@ class Depreciable extends SnipeModel
             //if there is a floor use it
             if(!$this->get_depreciation()->depreciation_min == null) {
 
-                $current_value = $this->get_depreciation()->depreciation_min;
+                $current_value = $this->calculateDepreciation();
 
             }else{
                 $current_value = 0;
@@ -86,7 +86,7 @@ class Depreciable extends SnipeModel
         }
         else {
             // The equation here is (Purchase_Cost-Floor_min)*(Months_passed/Months_til_depreciated)
-            $current_value = round(($this->purchase_cost-($this->purchase_cost - ($this->get_depreciation()->depreciation_min)) * ($months_passed / $this->get_depreciation()->months)), 2);
+            $current_value = round(($this->purchase_cost-($this->purchase_cost - ($this->calculateDepreciation())) * ($months_passed / $this->get_depreciation()->months)), 2);
 
         }
 
@@ -95,7 +95,7 @@ class Depreciable extends SnipeModel
 
     public function getMonthlyDepreciation(){
 
-        return ($this->purchase_cost-$this->get_depreciation()->depreciation_min)/$this->get_depreciation()->months;
+        return ($this->purchase_cost-$this->calculateDepreciation())/$this->get_depreciation()->months;
 
     }
 
@@ -187,5 +187,17 @@ class Depreciable extends SnipeModel
     protected function getDateTime($time = null)
     {
         return new \DateTime($time);
+    }
+    private function calculateDepreciation(){
+        $depreciation_min = 0;
+        if($this->get_depreciation()->depreciation_type === 'percentage') {
+            $depreciation_percent= $this->get_depreciation()->depreciation_min / 100;
+            $depreciation_min= $this->purchase_cost * $depreciation_percent;
+            return $depreciation_min;
+        }
+        else{
+            $depreciation_min = $this->get_depreciation()->depreciation_min;
+            return $depreciation_min;
+        }
     }
 }
