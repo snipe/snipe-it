590d13061a64b4e78a7e6b57468d56153a13833b
snipe <snipe@snipe.net>
2024-07-24 20:57:26 +0100

    One more test
    Signed-off-by: snipe <snipe@snipe.net>

--- tests/Feature/Locations/Api/LocationsViewTest.php
+++ tests/Feature/Locations/Api/LocationsViewTest.php
@@ -20,6 +20,14 @@ class LocationsViewTest extends TestCase
     public function testViewingLocationAssetIndexRequiresPermission()
     {
         $location = Location::factory()->create();
+        $this->actingAsForApi(User::factory()->create())
+            ->getJson(route('api.locations.viewassets', $location->id))
+            ->assertForbidden();
+    }
+
+    public function testViewingLocationAssetIndex()
+    {
+        $location = Location::factory()->create();
         Asset::factory()->count(3)->assignedToLocation($location)->create();
 
         $this->actingAsForApi(User::factory()->superuser()->create())
