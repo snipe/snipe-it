fccfce2ae8bfd3618a02658a1326ed8a36bf81e9
akemidx <kojotek.dx@gmail.com>
2024-07-30 19:27:37 -0400

    first thought

--- resources/views/account/accept/create.blade.php
+++ resources/views/account/accept/create.blade.php
@@ -134,11 +134,13 @@
         });
 
         $('#submit-button').on("click", function (event) {
-            if (signaturePad.isEmpty()) {
-                alert("Please provide signature first.");
-                return false;
-            } else {
-                $('#signature_output').val(signaturePad.toDataURL());
+            if(signaturePad != null) {
+                if (signaturePad.isEmpty()) {
+                    alert("Please provide signature first.");
+                    return false;
+                } else {
+                    $('#signature_output').val(signaturePad.toDataURL());
+                }
             }
         });
 
