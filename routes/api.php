fbaf738454519183bfe304ff9b746cd2edd9df2d
snipe <snipe@snipe.net>
2024-08-07 13:22:27 +0100

    Add assigned route
    Signed-off-by: snipe <snipe@snipe.net>

--- routes/api.php
+++ routes/api.php
@@ -716,6 +716,13 @@ Route::group(['prefix' => 'v1', 'middleware' => ['api', 'throttle:api']], functi
                 'assets'
             ]
             )->name('api.locations.viewassets');
+
+            Route::get('{location}/accessories/assigned',
+                [
+                    Api\LocationsController::class,
+                    'assignedAccessories'
+                ]
+            )->name('api.locations.assigned_accessories');
     
         }); 
     
