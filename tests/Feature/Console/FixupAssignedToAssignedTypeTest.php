ae0a6d66f36ffa564657b7409a99883eac1a2039
Marcus Moore <contact@marcusmoore.io>
2024-08-13 12:16:08 -0700

    Update tests
    https://masteringlaravel.io/daily/2024-02-09-watch-out-for-this-when-testing-artisan-commands

--- tests/Feature/Console/FixupAssignedToAssignedTypeTest.php
+++ tests/Feature/Console/FixupAssignedToAssignedTypeTest.php
@@ -10,7 +10,6 @@ class FixupAssignedToAssignedTypeTest extends TestCase
 {
     public function testEmptyAssignedType()
     {
-        $this->markTestIncomplete();
         $asset = Asset::factory()->create();
         $user = User::factory()->create();
         $admin = User::factory()->admin()->create();
@@ -18,15 +17,10 @@ class FixupAssignedToAssignedTypeTest extends TestCase
         $asset->checkOut($user, $admin);
         $asset->assigned_type=null; //blank out the assigned type
         $asset->save();
-        print "Okay we set everything up~!!!\n";
 
-        $output = $this->artisan('snipeit:assigned-to-fixup --debug')->assertExitCode(0);
-        print "artisan ran!\n";
-        dump($output);
-        $asset = Asset::find($asset->id);
-        print "\n we refreshed the asset?";
-        dump($asset);
-        $this->assertEquals(User::class, $asset->assigned_type);
+        $this->artisan('snipeit:assigned-to-fixup --debug')->assertExitCode(0);
+
+        $this->assertEquals(User::class, $asset->fresh()->assigned_type);
     }
 
     public function testInvalidAssignedTo()
@@ -37,21 +31,15 @@ class FixupAssignedToAssignedTypeTest extends TestCase
         $admin = User::factory()->admin()->create();
 
         $asset->checkOut($user, $admin);
-//        $asset->checkIn($user, $admin); //no such method btw
         $asset->assigned_type=null;
         $asset->assigned_to=null;
         $asset->saveOrFail(); //*should* generate a 'checkin'?
 
         $asset->assigned_to=$user->id; //incorrectly mark asset as partially checked-out
         $asset->saveOrFail();
-        print "Okay we set everything up for test TWO~!!!\n";
 
-        $output = $this->artisan('snipeit:assigned-to-fixup --debug')->assertExitCode(0);
-        print "artisan ran TWO!\n";
-        dump($output);
-        $asset = Asset::find($asset->id);
-        print "\n we refreshed the asset?";
-        dump($asset);
-        $this->assertNull($asset->assigned_to);
+        $this->artisan('snipeit:assigned-to-fixup --debug')->assertExitCode(0);
+
+        $this->assertNull($asset->fresh()->assigned_to);
     }
 }
