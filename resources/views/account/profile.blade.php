6216b4fc0dcfa2d870197faca6b3b468e5d4aeee
snipe <snipe@snipe.net>
2024-08-15 11:35:56 +0100

    Use native browser control for required in addition to the css effect
    Signed-off-by: snipe <snipe@snipe.net>

--- resources/views/account/profile.blade.php
+++ resources/views/account/profile.blade.php
@@ -20,7 +20,7 @@
           <label for="first_name" class="col-md-3 control-label">{{ trans('general.first_name') }}
           </label>
           <div class="col-md-8 required">
-            <input class="form-control" type="text" name="first_name" id="first_name" value="{{ old('first_name', $user->first_name) }}" />
+            <input class="form-control" type="text" name="first_name" id="first_name" value="{{ old('first_name', $user->first_name) }}" required />
             {!! $errors->first('first_name', '<span class="alert-msg" aria-hidden="true"><i class="fas fa-times" aria-hidden="true"></i> :message</span>') !!}
           </div>
         </div>
