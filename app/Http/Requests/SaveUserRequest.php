70e5e0f9df91c10d94568c98168be1f5fdc5686a
spencerrlongg <spencer@spencerlong.com>
2024-08-16 12:52:06 -0500

    get rid of dd

--- app/Http/Requests/SaveUserRequest.php
+++ app/Http/Requests/SaveUserRequest.php
@@ -31,7 +31,6 @@ class SaveUserRequest extends FormRequest
      */
     public function rules()
     {
-        //dd($this->user);
         $rules = [
             'department_id' => 'nullable|exists:departments,id',
             'manager_id' => 'nullable|exists:users,id',
