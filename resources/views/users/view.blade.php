9d484077ae030020e3b1cd585783d8db03c5c0f5
Godfrey M <godmartinz@gmail.com>
2024-08-20 14:24:37 -0700

    missed a file

--- resources/views/users/view.blade.php
+++ resources/views/users/view.blade.php
@@ -159,9 +159,9 @@
                 </div>
               </div>
             @endif
-
+        <div class="info-stack-container">
             <!-- Start button column -->
-            <div class="col-md-3 col-xs-12 col-sm-push-9">
+            <div class="col-md-3 col-xs-12 col-sm-push-9 info-stack">
 
               
 
@@ -306,7 +306,7 @@
  
             <!-- End button column -->
           
-            <div class="col-md-9 col-xs-12 col-sm-pull-3">
+            <div class="col-md-9 col-xs-12 col-sm-pull-3 info-stack">
 
                <div class="row-new-striped">
                 
@@ -765,6 +765,7 @@
                    @endif
                   </div> <!--/end striped container-->
                 </div> <!-- end col-md-9 -->
+             </div><!-- end info-stack-container-->
           </div> <!--/.row-->
         </div><!-- /.tab-pane -->
 
