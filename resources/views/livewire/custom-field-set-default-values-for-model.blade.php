680cce3ee2d47357aa9daf5878b87a2016e35410
Marcus Moore <contact@marcusmoore.io>
2024-08-20 16:42:29 -0700

    Align indents

--- resources/views/livewire/custom-field-set-default-values-for-model.blade.php
+++ resources/views/livewire/custom-field-set-default-values-for-model.blade.php
@@ -88,16 +88,16 @@
                                 @elseif($field->element == "radio")
 
                                     @foreach(explode("\r\n", $field->field_values) as $field_value)
-                                    <label class="col-md-3 form-control" for="{{ str_slug($field_value) }}" wire:key="radio-{{ $field_value }}">
-                                        <input
-                                            id="{{ str_slug($field_value) }}"
-                                            aria-label="{{ str_slug($field->name) }}"
-                                            type="radio"
-                                            name="default_values[{{ $field->id }}]"
-                                            value="{{$field_value}}"
-                                            wire:model="selectedValues.{{ $field->db_column }}"
-                                        />{{ $field_value }}
-                                    </label>
+                                        <label class="col-md-3 form-control" for="{{ str_slug($field_value) }}" wire:key="radio-{{ $field_value }}">
+                                            <input
+                                                id="{{ str_slug($field_value) }}"
+                                                aria-label="{{ str_slug($field->name) }}"
+                                                type="radio"
+                                                name="default_values[{{ $field->id }}]"
+                                                value="{{$field_value}}"
+                                                wire:model="selectedValues.{{ $field->db_column }}"
+                                            />{{ $field_value }}
+                                        </label>
                                     @endforeach
 
                                 @elseif($field->element == "checkbox")
@@ -126,7 +126,7 @@
 
                 @endforeach
 
-                @endif
+            @endif
 
     @endif
 </span>
