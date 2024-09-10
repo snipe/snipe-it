fddbdafb99cf739b435220a7a4e259a27040f03a
snipe <snipe@snipe.net>
2024-09-10 19:03:56 +0100

    Fixed conflict
    Signed-off-by: snipe <snipe@snipe.net>

--- config/version.php
+++ config/version.php
@@ -4,13 +4,7 @@ return array (
   'full_app_version' => 'v7.0.12 - build 15249-g713bb104e',
   'build_version' => '15249',
   'prerelease_version' => '',
-<<<<<<< HEAD
-  'hash_version' => 'g46ed07642',
-  'full_hash' => 'v7.0.11-133-g46ed07642',
-  'branch' => 'master',
-=======
   'hash_version' => 'g713bb104e',
   'full_hash' => 'v7.0.12-338-g713bb104e',
-  'branch' => 'develop',
->>>>>>> origin/develop
+  'branch' => 'master',
 );
