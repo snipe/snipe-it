8eb1c487bcaa1fb6ec71d85985226d49c01e3cea
Marcus Moore <contact@marcusmoore.io>
2024-09-16 16:52:10 -0700

    implement FMCS test for accessory index

--- tests/Feature/Accessories/Api/IndexAccessoryTest.php
+++ tests/Feature/Accessories/Api/IndexAccessoryTest.php
@@ -2,16 +2,55 @@
 
 namespace Tests\Feature\Accessories\Api;
 
+use App\Models\Accessory;
+use App\Models\Company;
 use App\Models\User;
+use Tests\Concerns\TestsMultipleFullCompanySupport;
 use Tests\Concerns\TestsPermissionsRequirement;
 use Tests\TestCase;
 
-class IndexAccessoryTest extends TestCase implements TestsPermissionsRequirement
+class IndexAccessoryTest extends TestCase implements TestsMultipleFullCompanySupport, TestsPermissionsRequirement
 {
     public function testRequiresPermission()
     {
         $this->actingAsForApi(User::factory()->create())
             ->getJson(route('api.accessories.index'))
             ->assertForbidden();
+    }
+
+    public function testAdheresToMultipleFullCompanySupportScoping()
+    {
+        [$companyA, $companyB] = Company::factory()->count(2)->create();
+
+        $accessoryA = Accessory::factory()->for($companyA)->create(['name' => 'Accessory A']);
+        $accessoryB = Accessory::factory()->for($companyB)->create(['name' => 'Accessory B']);
+        $accessoryC = Accessory::factory()->for($companyB)->create(['name' => 'Accessory C']);
+
+        $superUser = $companyA->users()->save(User::factory()->superuser()->make());
+        $userInCompanyA = $companyA->users()->save(User::factory()->viewAccessories()->make());
+        $userInCompanyB = $companyB->users()->save(User::factory()->viewAccessories()->make());
+
+        $this->settings->enableMultipleFullCompanySupport();
+
+        $this->actingAsForApi($userInCompanyA)
+            ->getJson(route('api.accessories.index'))
+            ->assertOk()
+            ->assertResponseContainsInRows($accessoryA)
+            ->assertResponseDoesNotContainInRows($accessoryB)
+            ->assertResponseDoesNotContainInRows($accessoryC);
+
+        $this->actingAsForApi($userInCompanyB)
+            ->getJson(route('api.accessories.index'))
+            ->assertOk()
+            ->assertResponseDoesNotContainInRows($accessoryA)
+            ->assertResponseContainsInRows($accessoryB)
+            ->assertResponseContainsInRows($accessoryC);
+
+        $this->actingAsForApi($superUser)
+            ->getJson(route('api.accessories.index'))
+            ->assertOk()
+            ->assertResponseContainsInRows($accessoryA)
+            ->assertResponseContainsInRows($accessoryB)
+            ->assertResponseContainsInRows($accessoryC);
     }
 }
