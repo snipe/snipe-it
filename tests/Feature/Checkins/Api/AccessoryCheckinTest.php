7b31df7c148566274527317bb5bf006e5764fda8
Marcus Moore <contact@marcusmoore.io>
2024-09-17 09:11:58 -0700

    Begin additional test case

--- tests/Feature/Checkins/Api/AccessoryCheckinTest.php
+++ tests/Feature/Checkins/Api/AccessoryCheckinTest.php
@@ -3,11 +3,13 @@
 namespace Tests\Feature\Checkins\Api;
 
 use App\Models\Accessory;
+use App\Models\Company;
 use App\Models\User;
+use Tests\Concerns\TestsFullMultipleCompaniesSupport;
 use Tests\Concerns\TestsPermissionsRequirement;
 use Tests\TestCase;
 
-class AccessoryCheckinTest extends TestCase implements TestsPermissionsRequirement
+class AccessoryCheckinTest extends TestCase implements TestsFullMultipleCompaniesSupport, TestsPermissionsRequirement
 {
     public function testRequiresPermission()
     {
@@ -16,5 +18,25 @@ class AccessoryCheckinTest extends TestCase implements TestsPermissionsRequireme
         $this->actingAsForApi(User::factory()->create())
             ->postJson(route('api.accessories.checkin', $accessory))
             ->assertForbidden();
+    }
+
+    public function testAdheresToFullMultipleCompaniesSupportScoping()
+    {
+        $this->markTestIncomplete();
+
+        $this->withoutExceptionHandling();
+
+        [$companyA, $companyB] = Company::factory()->count(2)->create();
+
+        $userInCompanyA = User::factory()->for($companyA)->checkinAccessories()->create();
+        $accessoryForCompanyB = Accessory::factory()->for($companyB)->checkedOutToUser()->create();
+
+        $this->settings->enableMultipleFullCompanySupport();
+
+        $this->actingAsForApi($userInCompanyA)
+            ->postJson(route('api.accessories.checkin', $accessoryForCompanyB))
+            ->assertStatusMessageIs('error');
+
+        // @todo:
     }
 }
