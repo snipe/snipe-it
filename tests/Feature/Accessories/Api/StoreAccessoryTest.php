607f29030f13f82b40ccacefeec5f329f250b887
Marcus Moore <contact@marcusmoore.io>
2024-09-18 11:53:27 -0700

    Stub out test

--- tests/Feature/Accessories/Api/StoreAccessoryTest.php
+++ tests/Feature/Accessories/Api/StoreAccessoryTest.php
@@ -2,17 +2,43 @@
 
 namespace Tests\Feature\Accessories\Api;
 
+use App\Models\Category;
+use App\Models\Company;
 use App\Models\User;
+use Tests\Concerns\TestsFullMultipleCompaniesSupport;
 use Tests\Concerns\TestsPermissionsRequirement;
 use Tests\TestCase;
 
-class StoreAccessoryTest extends TestCase implements TestsPermissionsRequirement
+class StoreAccessoryTest extends TestCase implements TestsFullMultipleCompaniesSupport, TestsPermissionsRequirement
 {
     public function testRequiresPermission()
     {
         $this->actingAsForApi(User::factory()->create())
             ->postJson(route('api.accessories.store'))
             ->assertForbidden();
+    }
+
+    public function testAdheresToFullMultipleCompaniesSupportScoping()
+    {
+        $this->markTestSkipped('This behavior is not implemented');
+
+        [$companyA, $companyB] = Company::factory()->count(2)->create();
+        $userInCompanyA = User::factory()->for($companyA)->createAccessories()->create();
+
+        $this->settings->enableMultipleFullCompanySupport();
+
+        // attempt to store an accessory for company B
+        $this->actingAsForApi($userInCompanyA)
+            ->postJson(route('api.accessories.store'), [
+                'category_id' => Category::factory()->forAccessories()->create()->id,
+                'name' => 'Accessory A',
+                'qty' => 1,
+                'company_id' => $companyB->id,
+            ])->assertStatusMessageIs('error');
+
+        $this->assertDatabaseMissing('accessories', [
+            'name' => 'Accessory A',
+        ]);
     }
 
     public function testValidation()
