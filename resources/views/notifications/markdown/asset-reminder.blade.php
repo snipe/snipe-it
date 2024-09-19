65eba30038ddebd2bdfac8b11c26d4ee55e2dd49
snipe <snipe@snipe.net>
2024-09-19 17:23:34 +0100

    Fixed markdown in acceptance reminder
    Signed-off-by: snipe <snipe@snipe.net>

--- resources/views/notifications/markdown/asset-reminder.blade.php
+++ resources/views/notifications/markdown/asset-reminder.blade.php
@@ -1,11 +1,11 @@
 @component('mail::message')
-    # {{ trans('mail.hello') }} {{ $assigned_to}},
+# {{ trans('mail.hello') }} {{ $assigned_to}},
 
-    {{trans('mail.item_checked_reminder', ['count' => $count])}}
-    [{{ trans('general.click_here')}}]({{$accept_url}})
+{{trans('mail.item_checked_reminder', ['count' => $count])}}
+[{{ trans('general.click_here')}}]({{$accept_url}})
 
-    {{ trans('mail.best_regards') }}
+{{ trans('mail.best_regards') }}
 
-    {{ $snipeSettings->site_name }}
+{{ $snipeSettings->site_name }}
 
 @endcomponent
