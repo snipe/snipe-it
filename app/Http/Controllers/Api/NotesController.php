b813dcd9d0905045482c65251205c9cba91e0ded
akemidx <kojotek.dx@gmail.com>
2024-09-25 18:54:30 -0400

    requested changes from github

--- app/Http/Controllers/Api/NotesController.php
+++ app/Http/Controllers/Api/NotesController.php
@@ -8,16 +8,24 @@ use App\Http\Controllers\Controller;
 use App\Models\Asset;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rule;
 
 class NotesController extends Controller
 {
     public function store(Request $request)
     {
+        $validated = $request->validate([
+            'note' => 'required|string|max:500',
+            'type' => [
+                'required',
+                Rule::in(['asset']),
+            ],
+        ]);
+
         $item = Asset::findOrFail($request->input("id"));
         $this->authorize('update', $item);
-
-        $validated = $request->validate([
-            'note' => 'required|string|max:500']);
+        // This can be made dynamic by using $request->input('type') to determine which model type to add the note to.
+        // For now, we are only placing this on Assets
 
         event(new NoteAdded($item, Auth::user(), $validated['note']));
 
