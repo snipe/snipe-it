edca3f432ce1384e0b087d8209787e60beac1219
snipe <snipe@snipe.net>
2024-09-25 20:30:58 +0100

    Removed gate for delete
    Signed-off-by: snipe <snipe@snipe.net>

--- resources/views/partials/users-bulk-actions.blade.php
+++ resources/views/partials/users-bulk-actions.blade.php
@@ -7,26 +7,24 @@
               'id' => 'usersBulkForm']) }}
 
         @if (request('status')!='deleted')
-            @can('delete', \App\Models\User::class)
-                <div id="users-toolbar">
-                    <label for="bulk_actions" class="sr-only">{{ trans('general.bulk_actions') }}</label>
-                    <select name="bulk_actions" class="form-control select2" style="min-width:300px;" aria-label="bulk_actions">
+            <div id="users-toolbar">
+                <label for="bulk_actions" class="sr-only">{{ trans('general.bulk_actions') }}</label>
+                <select name="bulk_actions" class="form-control select2" style="min-width:300px;" aria-label="bulk_actions">
 
-                        @can('update', \App\Models\User::class)
-                            <option value="edit">{{ trans('general.bulk_edit') }}</option>
-                            <option value="merge">{!! trans('general.merge_users') !!}</option>
-                        @endcan
+                    @can('update', \App\Models\User::class)
+                        <option value="edit">{{ trans('general.bulk_edit') }}</option>
+                        <option value="merge">{!! trans('general.merge_users') !!}</option>
+                    @endcan
 
-                        @can('delete', \App\Models\User::class)
-                            <option value="delete">{!! trans('general.bulk_checkin_delete') !!}</option>
-                        @endcan
+                    @can('delete', \App\Models\User::class)
+                        <option value="delete">{!! trans('general.bulk_checkin_delete') !!}</option>
+                    @endcan
 
-                        <option value="bulkpasswordreset">{{ trans('button.send_password_link') }}</option>
-                        <option value="print">{{ trans('admin/users/general.print_assigned') }}</option>
-                    </select>
-                    <button class="btn btn-primary" id="bulkUserEditButton" disabled>{{ trans('button.go') }}</button>
-                </div>
-            @endcan
+                    <option value="bulkpasswordreset">{{ trans('button.send_password_link') }}</option>
+                    <option value="print">{{ trans('admin/users/general.print_assigned') }}</option>
+                </select>
+                <button class="btn btn-primary" id="bulkUserEditButton" disabled>{{ trans('button.go') }}</button>
+            </div>
         @endif
         {{ Form::close() }}
     </div>
