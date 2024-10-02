b4f9284d417e30c266d5e6b6d99702e82a34beda
snipe <snipe@snipe.net>
2024-10-02 21:34:31 +0100

    Added migration
    Signed-off-by: snipe <snipe@snipe.net>

---
+++ database/migrations/2024_09_23_131515_add_delimiter_to_imports.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('imports', function (Blueprint $table) {
+            $table->string('delimiter', 1)->nullable()->default(',');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('imports', function (Blueprint $table) {
+            $table->dropColumn('delimiter');
+        });
+    }
+};
