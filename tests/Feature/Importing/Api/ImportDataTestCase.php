88d549e7c5ee8fd385d897be557dbbac60a66f73
Marcus Moore <contact@marcusmoore.io>
2024-10-03 13:40:37 -0700

    Remove unused data provider method

--- tests/Feature/Importing/Api/ImportDataTestCase.php
+++ tests/Feature/Importing/Api/ImportDataTestCase.php
@@ -11,26 +11,4 @@ abstract class ImportDataTestCase extends TestCase
     {
         return $this->postJson(route('api.imports.importFile', $parameters), $parameters);
     }
-
-    /**
-     * @todo Add more permissions.
-     */
-    public static function permissionsTestData(): array
-    {
-        return [
-            '`admin`'        => ['admin'],
-            '`reports.view`' => ['reports.view'],
-            'only `assets` permission' => [
-                'assets.view',
-                'assets.create',
-                'assets.edit',
-                'assets.delete',
-                'assets.checkout',
-                'assets.checkin',
-                'assets.audit',
-                'assets.view.requestable',
-                'assets.view.encrypted_custom_fields'
-            ]
-        ];
-    }
 }
