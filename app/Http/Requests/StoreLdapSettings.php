d9fbf330e5302355b4c59d14fec7311e2375fb24
snipe <snipe@snipe.net>
2024-10-09 20:33:15 +0100

    Fixed translations
    Signed-off-by: snipe <snipe@snipe.net>

--- app/Http/Requests/StoreLdapSettings.php
+++ app/Http/Requests/StoreLdapSettings.php
@@ -22,19 +22,11 @@ class StoreLdapSettings extends FormRequest
      */
     public function rules(): array
     {
+        \Log::error('boop');
         return [
             'ldap_username_field' => 'not_in:sAMAccountName|required_if:ldap_enabled,1',
             'ldap_auth_filter_query' => 'not_in:uid=samaccountname|required_if:ldap_enabled,1',
             'ldap_filter' => 'nullable|regex:"^[^(]"|required_if:ldap_enabled,1',
-        ];
-    }
-
-    public function messages(): array
-    {
-        return [
-            'ldap_username_field' => trans('validation.custom.ldap_username_field.not_in'),
-            'ldap_auth_filter_query' => trans('validation.custom.ldap_auth_filter_query.not_in'),
-            'ldap_filter' => trans('validation.custom.ldap_filter.regex'),
         ];
     }
 
--- resources/lang/en-US/validation.php
+++ resources/lang/en-US/validation.php
@@ -208,9 +208,11 @@ return [
         'radio_buttons'        => ':attribute is invalid.',
         'invalid_value_in_field' => 'Invalid value included in this field',
 
-        'ldap_username_field.not_in' => '<code>sAMAccountName</code> (mixed case) will likely not work. You should use <code>samaccountname</code> (lowercase) instead. ',
-        'ldap_auth_filter_query.not_in' => '<code>uid=samaccountname</code> is probably not a valid auth filter. You probably want <code>uid=</code> ',
-        'ldap_filter.regex' => 'This value should probably not be wrapped in parentheses.',
+        'ldap_username_field' => [
+            'not_in' =>         '<code>sAMAccountName</code> (mixed case) will likely not work. You should use <code>samaccountname</code> (lowercase) instead.'
+        ],
+        'ldap_auth_filter_query' => ['not_in' => '<code>uid=samaccountname</code> is probably not a valid auth filter. You probably want <code>uid=</code> '],
+        'ldap_filter' => ['regex' => 'This value should probably not be wrapped in parentheses.'],
 
         ],
     /*
