2883e791938812a54ad2e4865cfcabd40bbb5589
snipe <snipe@snipe.net>
2024-10-09 19:30:55 +0100

    Removed unecessary assets creation
    Signed-off-by: snipe <snipe@snipe.net>

--- tests/Feature/Settings/AlertsSettingTest.php
+++ tests/Feature/Settings/AlertsSettingTest.php
@@ -14,10 +14,20 @@ class AlertsSettingTest extends TestCase
 {
     public function testPermissionRequiredToViewAlertSettings()
     {
-        $asset = Asset::factory()->create();
         $this->actingAs(User::factory()->create())
             ->get(route('settings.alerts.index'))
             ->assertForbidden();
+    }
+
+    public function testAdminCCEmailArrayCanBeSaved()
+    {
+        $response = $this->actingAs(User::factory()->superuser()->create())
+            ->post(route('settings.alerts.save', ['alert_email' => 'me@example.com,you@example.com']))
+            ->assertStatus(302)
+            ->assertValid('alert_email')
+            ->assertRedirect(route('settings.index'))
+            ->assertSessionHasNoErrors();
+        $this->followRedirects($response)->assertSee('alert-success');
     }
 
 }
