870dc747db51fa7e0f0ea7d4fac9d857f10191cd
Godfrey M <godmartinz@gmail.com>
2024-10-22 14:27:32 -0700

    oops

--- app/Http/Controllers/Api/StatuslabelsController.php
+++ app/Http/Controllers/Api/StatuslabelsController.php
@@ -96,7 +96,7 @@ class StatuslabelsController extends Controller
 
         if (! $request->filled('type')) {
 
-            return response()->json(Helper::formatStandardApiResponse('error', null, ['type' => 'Status label type is required.']));
+            return response()->json(Helper::formatStandardApiResponse('error', null, ['type' => ['Status label type is required.']]));
         }
 
         $statuslabel = new Statuslabel;
