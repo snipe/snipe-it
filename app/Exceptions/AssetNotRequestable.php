c76cccbb68dc55300127204653e269f8f460e606
spencerrlongg <spencer@spencerlong.com>
2024-10-23 12:56:36 -0500

    rm extraneous methods in exception

--- app/Exceptions/AssetNotRequestable.php
+++ app/Exceptions/AssetNotRequestable.php
@@ -6,22 +6,4 @@ use Exception;
 
 class AssetNotRequestable extends Exception
 {
-    private $errorMessage = 'Asset not requestable';
-
-    public function __construct($errorMessage = null)
-    {
-        $this->errorMessage = $errorMessage;
-
-        parent::__construct($errorMessage);
-    }
-
-    public function __toString()
-    {
-        return is_null($this->errorMessage) ? 'This asset is not requestable.' : $this->errorMessage;
-    }
-
-    public function getTranslatedMessage()
-    {
-        return trans($this->errorMessage);
-    }
 }
