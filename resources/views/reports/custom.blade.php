30dc5fa0cfe95643c71c049cb43018b3e3ce4272
Marcus Moore <contact@marcusmoore.io>
2024-10-23 13:21:44 -0700

    Allow retrieving eol start and end dates

--- resources/views/reports/custom.blade.php
+++ resources/views/reports/custom.blade.php
@@ -480,9 +480,9 @@
               <div class="form-group asset_eol_date-range">
                   <label for="asset_eol_date" class="col-md-3 control-label">{{ trans('admin/hardware/form.eol_date') }}</label>
                   <div class="input-daterange input-group col-md-6" id="datepicker">
-                      <input type="text" class="form-control" name="asset_eol_date_start" aria-label="asset_eol_date_start">
+                      <input type="text" class="form-control" name="asset_eol_date_start" aria-label="asset_eol_date_start" value="{{ $template->textValue('asset_eol_date_start') }}">
                       <span class="input-group-addon">to</span>
-                      <input type="text" class="form-control" name="asset_eol_date_end" aria-label="asset_eol_date_end">
+                      <input type="text" class="form-control" name="asset_eol_date_end" aria-label="asset_eol_date_end" value="{{ $template->textValue('asset_eol_date_end') }}">
                   </div>
               </div>
 
