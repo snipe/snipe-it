a89e17212eacde1a6882ae755b7fec63e1d99a39
snipe <snipe@snipe.net>
2024-11-13 23:46:30 +0000

    Bumped version
    Signed-off-by: snipe <snipe@snipe.net>

--- config/version.php
+++ config/version.php
@@ -1,10 +1,10 @@
 <?php
 return array (
-  'app_version' => 'v7.0.14-pre',
-  'full_app_version' => 'v7.0.14-pre - build 15942-g3bf33aa68',
+  'app_version' => 'v7.0.14',
+  'full_app_version' => 'v7.0.14 - build 15942-g3bf33aa68',
   'build_version' => '15942',
   'prerelease_version' => '',
   'hash_version' => 'g3bf33aa68',
-  'full_hash' => 'v7.0.14-pre-420-g3bf33aa68',
+  'full_hash' => 'v7.0.14-420-g3bf33aa68',
   'branch' => 'master',
 );
