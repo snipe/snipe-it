87756cac66896919a1b517291e87327c36f803c1
Marcus Moore <contact@marcusmoore.io>
2024-11-21 14:30:17 -0800

    Remove debugging

--- resources/views/account/accept/create.blade.php
+++ resources/views/account/accept/create.blade.php
@@ -117,13 +117,10 @@
         // https://github.com/szimek/signature_pad#handling-high-dpi-screens
         // (This also causes canvas to be cleared.)
         function resizeCanvas() {
-            console.log('resizeCanvas');
             // When zoomed out to less than 100%, for some very strange reason,
             // some browsers report devicePixelRatio as less than 1
             // and only part of the canvas is cleared then.
-            let devicePixelRatio = window.devicePixelRatio;
-            console.log({devicePixelRatio})
-            var ratio = Math.max(devicePixelRatio || 1, 1);
+            var ratio = Math.max(window.devicePixelRatio || 1, 1);
             canvas.width = canvas.offsetWidth * ratio;
             canvas.height = canvas.offsetHeight * ratio;
             canvas.getContext("2d").scale(ratio, ratio);
