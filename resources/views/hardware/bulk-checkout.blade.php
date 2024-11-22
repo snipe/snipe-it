026de32f3177938a2deb75de653c1a19f4bc4a55
Florent Bervas <florentdotme@gmail.com>
2024-11-22 10:36:20 +0000

    fix webui cols/rows to prevent design issues when checking out many assets

--- resources/views/hardware/bulk-checkout.blade.php
+++ resources/views/hardware/bulk-checkout.blade.php
@@ -21,75 +21,88 @@
     {{ Form::open(['method' => 'POST', 'class' => ['form-horizontal','checkout-form'], 'role' => 'form', 'id' => 'checkinout-form' ]) }}
         <!-- left column -->
         <div class="col-md-6">
-            <div class="box box-default">
-                <div class="box-header with-border">
-                    <h2 class="box-title"> {{ trans('admin/hardware/general.bulk_checkout') }} </h2>
+            <div class="row">
+                <div class="box box-default">
+                    <div class="box-header with-border">
+                        <h2 class="box-title"> {{ trans('admin/hardware/general.bulk_checkout') }} </h2>
+                    </div>
+                    <div class="box-body">
+                        {{csrf_field()}}
+                        <!-- Checkout selector -->
+                        @include ('partials.forms.checkout-selector', ['user_select' => 'true','asset_select' => 'true', 'location_select' => 'true'])
+
+                        @include ('partials.forms.edit.user-select', ['translated_name' => trans('general.user'), 'fieldname' => 'assigned_user', 'required'=>'true'])
+                        @include ('partials.forms.edit.asset-select', ['translated_name' => trans('general.asset'), 'fieldname' => 'assigned_asset', 'unselect' => 'true', 'style' => 'display:none;'])
+                        @include ('partials.forms.edit.location-select', ['translated_name' => trans('general.location'), 'fieldname' => 'assigned_location', 'style' => 'display:none;'])
+
+                        <!-- Checkout/Checkin Date -->
+                        <div class="form-group {{ $errors->has('checkout_at') ? 'error' : '' }}">
+                            <label for="checkout_at" class="col-sm-3 control-label">
+                                {{ trans('admin/hardware/form.checkout_date') }}
+                            </label>
+                            <div class="col-md-8">
+                                <div class="input-group date col-md-5" data-provide="datepicker" data-date-format="yyyy-mm-dd" data-date-end-date="0d" data-date-clear-btn="true">
+                                    <input type="text" class="form-control" placeholder="{{ trans('general.select_date') }}" name="checkout_at" id="checkout_at" value="{{ old('checkout_at') }}">
+                                    <span class="input-group-addon"><i class="fas fa-calendar" aria-hidden="true"></i></span>
+                                </div>
+                                {!! $errors->first('checkout_at', '<span class="alert-msg" aria-hidden="true"><i class="fas fa-times" aria-hidden="true"></i> :message</span>') !!}
+                            </div>
+                        </div>
+
+                        <!-- Expected Checkin Date -->
+                        <div class="form-group {{ $errors->has('expected_checkin') ? 'error' : '' }}">
+                            <label for="expected_checkin" class="col-sm-3 control-label">
+                                {{ trans('admin/hardware/form.expected_checkin') }}
+                            </label>
+                            <div class="col-md-8">
+                                <div class="input-group date col-md-5" data-provide="datepicker" data-date-format="yyyy-mm-dd" data-date-start-date="0d" data-date-clear-btn="true">
+                                    <input type="text" class="form-control" placeholder="{{ trans('general.select_date') }}" name="expected_checkin" id="expected_checkin" value="{{ old('expected_checkin') }}">
+                                    <span class="input-group-addon"><x-icon type="calendar" /></span>
+                                </div>
+                                {!! $errors->first('expected_checkin', '<span class="alert-msg" aria-hidden="true"><i class="fas fa-times" aria-hidden="true"></i> :message</span>') !!}
+                            </div>
+                        </div>
+
+
+                        <!-- Note -->
+                        <div class="form-group {{ $errors->has('note') ? 'error' : '' }}">
+                            <label for="note" class="col-sm-3 control-label">
+                                {{ trans('general.notes') }}
+                            </label>
+                            <div class="col-md-8">
+                                <textarea class="col-md-6 form-control" id="note" name="note">{{ old('note') }}</textarea>
+                                {!! $errors->first('note', '<span class="alert-msg" aria-hidden="true"><i class="fas fa-times" aria-hidden="true"></i> :message</span>') !!}
+                            </div>
+                        </div>
+
+                        @include ('partials.forms.edit.asset-select', [
+                            'translated_name' => trans('general.assets'),
+                            // Fieldname is empty so not included in the form
+                            'fieldname' => 'bulk_assets',
+                            'multiple' => 'true',
+                            'asset_status_type' => 'RTD',
+                            'required' => true
+                        ])
+
+                    </div> <!--./box-body-->
+                    <div class="box-footer">
+                        <a class="btn btn-link" href="{{ URL::previous() }}"> {{ trans('general.back') }}</a>
+                        <button type="submit" id="checkinout_button" class="btn btn-primary pull-right"><i class="fas fa-check icon-white" aria-hidden="true"></i> {{ trans('general.checkout') }}</button>
+                    </div>
                 </div>
-                <div class="box-body">
-                    {{csrf_field()}}
-                    <!-- Checkout selector -->
-                    @include ('partials.forms.checkout-selector', ['user_select' => 'true','asset_select' => 'true', 'location_select' => 'true'])
-
-                    @include ('partials.forms.edit.user-select', ['translated_name' => trans('general.user'), 'fieldname' => 'assigned_user', 'required'=>'true'])
-                    @include ('partials.forms.edit.asset-select', ['translated_name' => trans('general.asset'), 'fieldname' => 'assigned_asset', 'unselect' => 'true', 'style' => 'display:none;'])
-                    @include ('partials.forms.edit.location-select', ['translated_name' => trans('general.location'), 'fieldname' => 'assigned_location', 'style' => 'display:none;'])
-
-                    <!-- Checkout/Checkin Date -->
-                    <div class="form-group {{ $errors->has('checkout_at') ? 'error' : '' }}">
-                        <label for="checkout_at" class="col-sm-3 control-label">
-                            {{ trans('admin/hardware/form.checkout_date') }}
-                        </label>
-                        <div class="col-md-8">
-                            <div class="input-group date col-md-5" data-provide="datepicker" data-date-format="yyyy-mm-dd" data-date-end-date="0d" data-date-clear-btn="true">
-                                <input type="text" class="form-control" placeholder="{{ trans('general.select_date') }}" name="checkout_at" id="checkout_at" value="{{ old('checkout_at') }}">
-                                <span class="input-group-addon"><i class="fas fa-calendar" aria-hidden="true"></i></span>
-                            </div>
-                            {!! $errors->first('checkout_at', '<span class="alert-msg" aria-hidden="true"><i class="fas fa-times" aria-hidden="true"></i> :message</span>') !!}
+                {{Form::close()}}
+            </div>
+            <div class="row">
+                <div id="current_assets_box" class="box box-primary" style="display:none;">
+                    <div class="box-header with-border">
+                        <h2 class="box-title">{{ trans('admin/users/general.current_assets') }}</h2>
+                    </div>
+                    <div class="box-body">
+                        <div id="current_assets_content">
                         </div>
                     </div>
-
-                    <!-- Expected Checkin Date -->
-                    <div class="form-group {{ $errors->has('expected_checkin') ? 'error' : '' }}">
-                        <label for="expected_checkin" class="col-sm-3 control-label">
-                            {{ trans('admin/hardware/form.expected_checkin') }}
-                        </label>
-                        <div class="col-md-8">
-                            <div class="input-group date col-md-5" data-provide="datepicker" data-date-format="yyyy-mm-dd" data-date-start-date="0d" data-date-clear-btn="true">
-                                <input type="text" class="form-control" placeholder="{{ trans('general.select_date') }}" name="expected_checkin" id="expected_checkin" value="{{ old('expected_checkin') }}">
-                                <span class="input-group-addon"><x-icon type="calendar" /></span>
-                            </div>
-                            {!! $errors->first('expected_checkin', '<span class="alert-msg" aria-hidden="true"><i class="fas fa-times" aria-hidden="true"></i> :message</span>') !!}
-                        </div>
-                    </div>
-
-
-                    <!-- Note -->
-                    <div class="form-group {{ $errors->has('note') ? 'error' : '' }}">
-                        <label for="note" class="col-sm-3 control-label">
-                            {{ trans('general.notes') }}
-                        </label>
-                        <div class="col-md-8">
-                            <textarea class="col-md-6 form-control" id="note" name="note">{{ old('note') }}</textarea>
-                            {!! $errors->first('note', '<span class="alert-msg" aria-hidden="true"><i class="fas fa-times" aria-hidden="true"></i> :message</span>') !!}
-                        </div>
-                    </div>
-
-                    @include ('partials.forms.edit.asset-select', [
-                        'translated_name' => trans('general.assets'),
-                        // Fieldname is empty so not included in the form
-                        'fieldname' => 'bulk_assets',
-                        'multiple' => 'true',
-                        'asset_status_type' => 'RTD',
-                        'required' => true
-                    ])
-
-                </div> <!--./box-body-->
-                <div class="box-footer">
-                    <a class="btn btn-link" href="{{ URL::previous() }}"> {{ trans('general.back') }}</a>
-                    <button type="submit" id="checkinout_button" class="btn btn-primary pull-right"><i class="fas fa-check icon-white" aria-hidden="true"></i> {{ trans('general.checkout') }}</button>
                 </div>
             </div>
-            {{Form::close()}}
         </div> <!--/.col-md-6-->
 
         <div class="col-md-6">
@@ -118,20 +131,6 @@
                         <tbody>
                         </tbody>
                     </table>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-md-6" id="current_assets_box" style="display:none;">
-            <div class="box box-primary">
-                <div class="box-header with-border">
-                    <h2 class="box-title">{{ trans('admin/users/general.current_assets') }}</h2>
-                </div>
-                <div class="box-body">
-                    <div id="current_assets_content">
-                    </div>
-                </div>
                 </div>
             </div>
         </div>
