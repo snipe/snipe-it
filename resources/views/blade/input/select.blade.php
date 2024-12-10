5d4eb365c75296ad415c56ebbef05eb9eb8a24f7
Marcus Moore <contact@marcusmoore.io>
2024-12-10 13:59:40 -0800

    Add comments

--- resources/views/blade/input/select.blade.php
+++ resources/views/blade/input/select.blade.php
@@ -1,4 +1,6 @@
 @props([
+    // items can either be provided as key => value pairs
+    // or passed in via the default $slot
     'items',
     'selected' => null,
     'includeEmpty' => false,
@@ -7,11 +9,12 @@
 
 <select
     {{ $attributes->class(['select2', 'livewire-select2' => $forLivewire]) }}
-    @if ($forLivewire) data-livewire-component="{{ $this->getId() }}" @endif
+    @if($forLivewire) data-livewire-component="{{ $this->getId() }}" @endif
 >
     @if($includeEmpty)
         <option value=""></option>
     @endif
+    {{-- map the simple key => value pairs when nothing is passed in via the slot --}}
     @if($slot->isEmpty())
         @foreach($items as $key => $value)
             <option value="{{ $key }}" @selected($selected === $key)>{{ $value }}</option>
