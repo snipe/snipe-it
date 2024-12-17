9c677768f8a85ba6c7edc3e745054285e3ad02c2
Marcus Moore <contact@marcusmoore.io>
2024-12-17 15:52:27 -0800

    Use attribute spreading in text area

--- resources/views/blade/input/textarea.blade.php
+++ resources/views/blade/input/textarea.blade.php
@@ -1,7 +1,4 @@
 @props([
-    'name',
-    // use name if id is not provided
-    'id' => $id ?? $name,
     'value' => '',
     'cols' => 50,
     'rows' => 10,
@@ -9,8 +6,6 @@
 
 <textarea
     {{ $attributes->merge(['class' => 'form-control']) }}
-    name="{{ $name }}"
-    id="{{ $id }}"
     cols="{{ $cols }}"
     rows="{{ $rows }}"
 >{{ $value }}</textarea>
