e849235d4ec4810f0ccd7a6a25df2c1a768294b5
Marcus Moore <contact@marcusmoore.io>
2024-12-19 13:32:26 -0800

    Add csrf token and method input to component

--- resources/views/blade/form.blade.php
+++ resources/views/blade/form.blade.php
@@ -6,10 +6,14 @@
 
 <form
     action="{{ $route }}"
-    method="{{ $method }}"
+    method="POST"
     accept-charset="UTF-8"
     @if($files) enctype="multipart/form-data" @endif
     {{ $attributes }}
 >
+    @csrf
+    @if(in_array(strtoupper($method), ['PUT', 'PATCH', 'DELETE']))
+        <input type="hidden" name="_method" value="{{ strtoupper($method) }}">
+    @endif
     {{ $slot }}
 </form>
