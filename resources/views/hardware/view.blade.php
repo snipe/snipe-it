0bb2e98af317302cfe8d478335e6d555722932c0
snipe <snipe@snipe.net>
2025-01-14 12:33:59 +0000

    Built assets
    Signed-off-by: snipe <snipe@snipe.net>

--- resources/views/hardware/view.blade.php
+++ resources/views/hardware/view.blade.php
@@ -215,6 +215,16 @@
                                     @endif
                                 @endif
 
+                                        <!-- Add notes -->
+                                        @can('update', \App\Models\Asset::class)
+                                            <div class="col-md-12 hidden-print" style="padding-top: 5px;">
+                                                <a href='{{ route('modal.show', 'add-note') }}?type=asset&id={{$asset->id}}' style="width: 100%" data-toggle="modal" data-target="#createModal" data-select='add-note_select_id' data-refresh="assetHistory" data-hasnopayload="true" class="btn btn-sm btn-primary btn-block btn-social hidden-print">
+                                                    <x-icon type="note" />
+                                                    {{ trans('general.add_note') }}</a>
+                                            </div>
+                                        @endcan
+
+
 
 
                                     @can('audit', \App\Models\Asset::class)
@@ -252,15 +262,6 @@
                                         {{ trans_choice('button.generate_labels', 1) }}</button>
                                         {{ Form::close() }}
                                 </div>
-
-                                <!-- Add notes -->
-                                @can('update', \App\Models\Asset::class)
-                                    <div class="col-md-12 hidden-print" style="padding-top: 5px;">
-                                        <a href='{{ route('modal.show', 'add-note') }}?type=asset&id={{$asset->id}}' style="width: 100%" data-toggle="modal" data-target="#createModal" data-select='add-note_select_id' data-refresh="assetHistory" data-hasnopayload="true" class="btn btn-sm btn-primary btn-block btn-social hidden-print">
-                                            <x-icon type="note" />
-                                            {{ trans('general.add_note') }}</a>
-                                    </div>
-                                @endcan
 
                                 @can('delete', $asset)
                                     <div class="col-md-12 hidden-print" style="padding-top: 30px; padding-bottom: 30px;">
