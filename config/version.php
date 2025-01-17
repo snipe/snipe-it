fed7c2dc1695dd6dace65e2945523340873e28b0
snipe <snipe@snipe.net>
2025-01-17 16:38:04 +0000

    Bumped hash
    Signed-off-by: snipe <snipe@snipe.net>

--- config/version.php
+++ config/version.php
@@ -1,10 +1,10 @@
 <?php
 return array (
   'app_version' => 'v7.1.15',
-  'full_app_version' => 'v7.1.15 - build 16052-g25bfd3e84',
-  'build_version' => '16052',
+  'full_app_version' => 'v7.1.15 - build 16526-g5e465fa41',
+  'build_version' => '16526',
   'prerelease_version' => '',
-  'hash_version' => 'g25bfd3e84',
-  'full_hash' => 'v7.1.15-105-g25bfd3e84',
-  'branch' => 'master',
+  'hash_version' => 'g5e465fa41',
+  'full_hash' => 'v7.1.15-472-g5e465fa41',
+  'branch' => 'develop',
 );
