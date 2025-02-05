ecc1bd69b926847e1bae2b1dc39a2572c3ad6c23
Marcus Moore <contact@marcusmoore.io>
2025-02-05 13:19:16 -0800

    Migrate form label in department select partial

--- resources/views/partials/forms/edit/department-select.blade.php
+++ resources/views/partials/forms/edit/department-select.blade.php
@@ -1,6 +1,6 @@
 <div id="assigned_user" class="form-group{{ $errors->has($fieldname) ? ' has-error' : '' }}">
 
-    {{ Form::label($fieldname, $translated_name, array('class' => 'col-md-3 control-label')) }}
+    <label for="{{ $fieldname }}" class="col-md-3 control-label">{{ $translated_name }}</label>
 
     <div class="col-md-6">
         <select class="js-data-ajax" data-endpoint="departments" data-placeholder="{{ trans('general.select_department') }}" name="{{ $fieldname }}" style="width: 100%" id="department_select" aria-label="{{ $fieldname }}"{{ (isset($multiple) && ($multiple=='true')) ? " multiple='multiple'" : '' }}>
