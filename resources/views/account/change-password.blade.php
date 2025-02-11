5cf314a314318d246f2a7a18a6bb50b2ed575b61
Marcus Moore <contact@marcusmoore.io>
2025-02-11 13:18:01 -0800

    Migrate Form::open in change password view

--- resources/views/account/change-password.blade.php
+++ resources/views/account/change-password.blade.php
@@ -11,7 +11,7 @@
 
 <div class="row">
     <div class="col-md-9">
-    {{ Form::open(['method' => 'POST', 'files' => true, 'class' => 'form-horizontal', 'autocomplete' => 'off']) }}
+    <form method="POST" action="{{ route('account.password.update') }}" accept-charset="UTF-8" class="form-horizontal" autocomplete="off" enctype="multipart/form-data">
     <!-- CSRF Token -->
         <input type="hidden" name="_token" value="{{ csrf_token() }}" />
         <div class="box box-default">
--- routes/web.php
+++ routes/web.php
@@ -285,7 +285,7 @@ Route::group(['prefix' => 'account', 'middleware' => ['auth']], function () {
     Route::get('menu', [ProfileController::class, 'getMenuState'])->name('account.menuprefs');
 
     Route::get('password', [ProfileController::class, 'password'])->name('account.password.index');
-    Route::post('password', [ProfileController::class, 'passwordSave']);
+    Route::post('password', [ProfileController::class, 'passwordSave'])->name('account.password.update');
 
     Route::get('api', [ProfileController::class, 'api'])->name('user.api');
 
