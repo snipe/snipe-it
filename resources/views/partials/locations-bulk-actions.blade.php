ac5a409cdf944a79cde2c3a1be2a8b0016abdf3b
Marcus Moore <contact@marcusmoore.io>
2025-02-12 11:51:17 -0800

    Migrate Form::open and Form::close in locations bulk actions partial

--- resources/views/partials/locations-bulk-actions.blade.php
+++ resources/views/partials/locations-bulk-actions.blade.php
@@ -1,11 +1,13 @@
 @can('delete', \App\Models\Location::class)
     <div id="locationsBulkEditToolbar">
-    {{ Form::open([
-              'method' => 'POST',
-              'route' => ['locations.bulkdelete.show'],
-              'class' => 'form-inline',
-              'id' => 'locationsBulkForm']) }}
-
+    <form
+        method="POST"
+        action="{{ route('locations.bulkdelete.show') }}"
+        accept-charset="UTF-8"
+        class="form-inline"
+        id="locationsBulkForm"
+    >
+            @csrf
             <div id="locations-toolbar">
                 <label for="bulk_actions" class="sr-only">{{ trans('general.bulk_actions') }}</label>
                 <select name="bulk_actions" class="form-control select2" style="width: 200px;" aria-label="bulk_actions">
@@ -14,7 +16,7 @@
                 <button class="btn btn-primary" id="bulkLocationsEditButton" disabled>{{ trans('button.go') }}</button>
             </div>
 
-    {{ Form::close() }}
+    </form>
     </div>
 @endcan
 
