e348572738db3d57ddee2d40d481df9051c5bfc7
akemidx <kojotek.dx@gmail.com>
2025-02-11 19:56:45 -0500

    trans choice

--- resources/views/notifications/markdown/asset-reminder.blade.php
+++ resources/views/notifications/markdown/asset-reminder.blade.php
@@ -1,7 +1,7 @@
 @component('mail::message')
 # {{ trans('mail.hello') }} {{ $assigned_to }},
 
-{{ trans_choice('mail.item_checked_reminder', ['count' => $count]) }}
+{{ trans_choice('mail.item_checked_reminder', $count) }}
 [{{ trans('general.click_here')}}]({{ $accept_url }})
 
 {{ trans('mail.best_regards') }}
