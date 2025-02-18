53c084c6c0009ff60c4a9c662ed4b243422795ed
Marcus Moore <contact@marcusmoore.io>
2025-02-18 15:43:58 -0800

    WIP: begin to store notes

--- app/Http/Controllers/NotesController.php
+++ app/Http/Controllers/NotesController.php
@@ -2,12 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\NoteAdded;
+use App\Models\Asset;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class NotesController extends Controller
 {
     public function store(Request $request)
     {
-        dd($request->all());
+        // @todo: validation
+
+        $item = Asset::findOrFail($request->input('id'));
+
+        // @todo: authorization
+
+        event(new NoteAdded($item, Auth::user(), $request->input('note')));
+
+        return redirect()
+            // @todo: make dynamic
+            ->route('hardware.show', $request->input('id'))
+            ->withFragment('history')
+            // @todo: translate
+            ->with('success', 'Success!');
     }
 }
