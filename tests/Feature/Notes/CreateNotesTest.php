3d69721af013c4d9e0dab5361f7e46fec29eafc7
Marcus Moore <contact@marcusmoore.io>
2025-02-18 16:16:00 -0800

    Scaffold tests

---
+++ tests/Feature/Notes/CreateNotesTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Feature\Notes;
+
+use Tests\TestCase;
+
+class CreateNotesTest extends TestCase
+{
+    public function testRequiresPermission()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testValidation()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testCanCreateNote()
+    {
+        $this->markTestIncomplete();
+    }
+}
