7f665e00059fa5f657dd879cf7c13dab4bb26e61
snipe <snipe@snipe.net>
2025-02-19 16:34:48 +0000

    De-uglifying the maintenances detail page
    Signed-off-by: snipe <snipe@snipe.net>

--- resources/views/asset_maintenances/view.blade.php
+++ resources/views/asset_maintenances/view.blade.php
@@ -9,95 +9,123 @@ use Carbon\Carbon;
 @parent
 @stop
 
+@section('header_right')
+  <div class="pull-right">
+    <a href="{{ route('maintenances.edit', $assetMaintenance) }}" class="btn btn-default pull-right">
+      {{ trans('general.update') }}</a>
+
+    <a href="{{ route('maintenances.index') }}" class="btn btn-primary text-right" style="margin-right: 10px;">{{ trans('general.back') }}</a>
+  </div>
+@stop
+
+
 {{-- Page content --}}
 @section('content')
-<div class="row header">
-  <div class="col-md-12">
-    <h2 class="title">
-      {{ trans('admin/asset_maintenances/general.view') }}
-      {{ " - " . $assetMaintenance->title }}
-    </h2>
+  <div class="row">
+    <div class="col-md-9">
 
-    <div class="btn-group pull-right">
-      <div class="dropdown">
-        <button class="btn btn-default dropdown-toggle" data-toggle="dropdown">{{ trans('button.actions') }}
-          <span class="caret"></span>
-        </button>
-        <ul class="dropdown-menu pull-right" role="menu" aria-labelledby="dropdownMenu1">
-          <li role="presentation"><a href="{{ route('maintenances.update', $assetMaintenance->id) }}">{{ trans('admin/asset_maintenances/general.edit') }}</a></li>
-        </ul>
-      </div>
-    </div>
-  </div>
-</div>
+      <div class="box box-default">
+        <div class="box-body">
+          <div class="row-new-striped">
+            <div class="row">
 
-<div class="user-profile ">
-  <div class="row profile">
-    <div class="col-md-9 bio">
-      <!-- 1st Row Begin -->
-      <div class="row">
-        <div class="col-md-12 col-sm-12" style="padding-bottom: 10px; margin-left: 15px; word-wrap: break-word;">
-          <strong>{{ trans('admin/asset_maintenances/form.asset_maintenance_type') }}: </strong>
-          {{ $assetMaintenance->asset_maintenance_type }}
-        </div>
-      </div>
-      <!-- 1st Row End -->
+                <div class="col-md-3">
+                  {{ trans('admin/asset_maintenances/form.asset_maintenance_type') }}
+                </div>
+                <div class="col-md-9">
+                  {{ $assetMaintenance->asset_maintenance_type }}
+                </div>
 
-      <!-- 2nd Row Begin -->
-      <div class="row">
-        <div class="col-md-3 col-sm-3" style="padding-bottom: 10px; margin-left: 15px; word-wrap: break-word;">
-          <strong>{{ trans('admin/asset_maintenances/table.asset_name') }}: </strong>
-          <a href="{{ route('hardware.show', $assetMaintenance->asset_id) }}">
-            {{ $assetMaintenance->asset->name }}
-          </a>
-        </div>
-        <div class="col-md-3 col-sm-3" style="padding-bottom: 10px; margin-left: 15px; word-wrap: break-word;">
-          <strong>{{ trans('general.supplier') }}: </strong>
-          <a href="{{ route('suppliers.show', $assetMaintenance->supplier_id) }}">
-            {{ $assetMaintenance->supplier->name }}
-          </a>
-        </div>
-      </div>
-      <!-- 2nd Row End -->
-      <!-- 3rd Row Begin -->
-      <div class="row">
-        <div class="col-md-3 col-sm-3" style="padding-bottom: 10px; margin-left: 15px; word-wrap: break-word;">
-          <strong>{{ trans('admin/asset_maintenances/form.start_date') }}: </strong>
-            <?php $startDate = Carbon::parse($assetMaintenance->start_date); ?>
-          {{ $startDate->toDateString() }}
-        </div>
-        <div class="col-md-3 col-sm-3" style="padding-bottom: 10px; margin-left: 15px; word-wrap: break-word;">
-          <strong>{{ trans('admin/asset_maintenances/form.completion_date') }}: </strong>
-          {{ $completionDate = $assetMaintenance->completion_date }}
-          {{ $completionDate ? $completionDate : trans('admin/asset_maintenances/message.asset_maintenance_incomplete') }}
-        </div>
-        <div class="col-md-3 col-sm-3" style="padding-bottom: 10px; margin-left: 15px; word-wrap: break-word;">
-          <strong>{{ trans('admin/asset_maintenances/form.asset_maintenance_time') }}: </strong>
-          {{ $assetMaintenance->asset_maintenance_time }}
-        </div>
-      </div>
-      <!-- 3rd Row End -->
-      <!-- 4th Row Begin -->
-      <div class="row">
-        <div class="col-md-3 col-sm-3" style="padding-bottom: 10px; margin-left: 15px; word-wrap: break-word;">
-          <strong>{{ trans('admin/asset_maintenances/form.cost') }}: </strong>
-          {{ trans( 'general.currency' ) . Helper::formatCurrencyOutput($assetMaintenance->cost) }}
-        </div>
-        <div class="col-md-3 col-sm-3" style="padding-bottom: 10px; margin-left: 15px; word-wrap: break-word;">
-          <strong>{{ trans('admin/asset_maintenances/form.is_warranty') }}: </strong>
-          {{ $assetMaintenance->is_warranty ? trans('admin/asset_maintenances/message.warranty') : trans('admin/asset_maintenances/message.not_warranty') }}
-        </div>
-      </div>
-      <!-- 4th Row End -->
-      <!-- 5th Row Begin -->
-      <div class="row">
-        <div class="col-md-12 col-sm-12" style="padding-bottom: 10px; margin-left: 15px; word-wrap: break-word;">
-          <strong>{{ trans('admin/asset_maintenances/form.notes') }}: </strong>
-          {!! nl2br(Helper::parseEscapedMarkedownInline($assetMaintenance->notes)) !!}
-        </div>
-      </div>
-      <!-- 5th Row End -->
-    </div> <!-- col-md-9 bio end -->
-  </div> <!-- row profile end -->
-</div> <!-- user-profile end -->
+            </div> <!-- /row -->
+
+            <div class="row">
+              <div class="col-md-3">
+                {{ trans('general.asset') }}
+              </div>
+              <div class="col-md-9">
+                <a href="{{ route('hardware.show', $assetMaintenance->asset_id) }}">
+                  {{ $assetMaintenance->asset->present()->fullName }}
+                </a>
+              </div>
+            </div> <!-- /row -->
+
+
+
+            <div class="row">
+              <div class="col-md-3">
+                {{ trans('general.supplier') }}
+              </div>
+              <div class="col-md-9">
+                <a href="{{ route('suppliers.show', $assetMaintenance->supplier_id) }}">
+                  {{ $assetMaintenance->supplier->name }}
+                </a>
+              </div>
+            </div> <!-- /row -->
+
+            <div class="row">
+              <div class="col-md-3">
+                {{ trans('admin/asset_maintenances/form.start_date') }}
+              </div>
+              <div class="col-md-9">
+                {{ Helper::getFormattedDateObject($assetMaintenance->start_date, 'date', false) }}
+              </div>
+            </div> <!-- /row -->
+
+            <div class="row">
+              <div class="col-md-3">
+                {{ trans('admin/asset_maintenances/form.completion_date') }}
+              </div>
+              <div class="col-md-9">
+                @if ($assetMaintenance->completion_date)
+                  {{ Helper::getFormattedDateObject($assetMaintenance->completion_date, 'date', false) }}
+                @else
+                  {{ trans('admin/asset_maintenances/message.asset_maintenance_incomplete') }}
+                @endif
+              </div>
+            </div> <!-- /row -->
+
+            <div class="row">
+              <div class="col-md-3">
+                {{ trans('admin/asset_maintenances/form.asset_maintenance_time') }}
+              </div>
+              <div class="col-md-9">
+                {{ $assetMaintenance->asset_maintenance_time }}
+              </div>
+            </div> <!-- /row -->
+
+            <div class="row">
+              <div class="col-md-3">
+                {{ trans('admin/asset_maintenances/form.cost') }}
+              </div>
+              <div class="col-md-9">
+                {{ trans( 'general.currency' ) . Helper::formatCurrencyOutput($assetMaintenance->cost) }}
+              </div>
+            </div> <!-- /row -->
+
+            <div class="row">
+              <div class="col-md-3">
+                {{ trans('admin/asset_maintenances/form.is_warranty') }}
+              </div>
+              <div class="col-md-9">
+                {{ $assetMaintenance->is_warranty ? trans('admin/asset_maintenances/message.warranty') : trans('admin/asset_maintenances/message.not_warranty') }}
+              </div>
+            </div> <!-- /row -->
+
+            <div class="row">
+              <div class="col-md-3">
+                {{ trans('admin/asset_maintenances/form.notes') }}
+              </div>
+              <div class="col-md-9">
+                {!! nl2br(Helper::parseEscapedMarkedownInline($assetMaintenance->notes)) !!}
+              </div>
+            </div> <!-- /row -->
+
+
+          </div><!-- /row-new-striped -->
+      </div><!-- /box-body -->
+    </div><!-- /box -->
+
+    </div> <!-- col-md-9  end -->
+  </div> <!-- row  end -->
+
 @stop
