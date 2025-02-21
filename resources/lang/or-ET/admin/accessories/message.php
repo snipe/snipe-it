44942cc5936ced3ad1b88572f8041f18dce27877
Adugna <gizawadugna@gmail.com>
2025-02-22 02:20:45 +0300

    new file:   account/general.php 	new file:   admin/accessories/general.php 	new file:   admin/accessories/message.php 	new file:   admin/accessories/table.php 	new file:   admin/asset_maintenances/form.php 	new file:   admin/asset_maintenances/general.php 	new file:   admin/asset_maintenances/message.php 	new file:   admin/asset_maintenances/table.php 	new file:   admin/categories/general.php 	new file:   admin/categories/message.php 	new file:   admin/categories/table.php 	new file:   admin/companies/general.php

---
+++ resources/lang/or-ET/admin/accessories/message.php
@@ -0,0 +1,43 @@
+<?php
+
+return array(
+
+    'does_not_exist' => 'The accessory [:id] does not exist.',
+    'not_found' => 'That accessory was not found.',
+    'assoc_users'	 => 'This accessory currently has :count items checked out to users. Please check in the accessories and and try again. ',
+
+    'create' => array(
+        'error'   => 'The accessory was not created, please try again.',
+        'success' => 'The accessory was successfully created.'
+    ),
+
+    'update' => array(
+        'error'   => 'The accessory was not updated, please try again',
+        'success' => 'The accessory was updated successfully.'
+    ),
+
+    'delete' => array(
+        'confirm'   => 'Are you sure you wish to delete this accessory?',
+        'error'   => 'There was an issue deleting the accessory. Please try again.',
+        'success' => 'The accessory was deleted successfully.'
+    ),
+
+     'checkout' => array(
+        'error'   		=> 'Accessory was not checked out, please try again',
+        'success' 		=> 'Accessory checked out successfully.',
+        'unavailable'   => 'Accessory is not available for checkout. Check quantity available',
+        'user_does_not_exist' => 'That user is invalid. Please try again.',
+         'checkout_qty' => array(
+            'lte'  => 'There is currently only one available accessory of this type, and you are trying to check out :checkout_qty. Please adjust the checkout quantity or the total stock of this accessory and try again.|There are :number_currently_remaining total available accessories, and you are trying to check out :checkout_qty. Please adjust the checkout quantity or the total stock of this accessory and try again.',
+            ),
+           
+    ),
+
+    'checkin' => array(
+        'error'   		=> 'Accessory was not checked in, please try again',
+        'success' 		=> 'Accessory checked in successfully.',
+        'user_does_not_exist' => 'That user is invalid. Please try again.'
+    )
+
+
+);
