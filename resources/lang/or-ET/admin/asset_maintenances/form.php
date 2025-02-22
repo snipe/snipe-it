0bc691661bf93e8fffdc7946a83fc4c79ec3b2b2
Adugna <gizawadugna@gmail.com>
2025-02-22 17:15:28 +0300

    modified:   form.php

--- resources/lang/or-ET/admin/asset_maintenances/form.php
+++ resources/lang/or-ET/admin/asset_maintenances/form.php
@@ -1,14 +1,21 @@
 <?php
+/*
+|--------------------------------------------------------------------------
+| Language Lines - Afaan Oromoo
+|--------------------------------------------------------------------------
+| Translated by Adugna Gizaw - Snipe IT Localization Partner
+| Contact: gizawadugna@gmail.com | +251925582067
+*/
 
     return [
-        'asset_maintenance_type' => 'Asset Maintenance Type',
-        'title'                  => 'Title',
-        'start_date'             => 'Start Date',
-        'completion_date'        => 'Completion Date',
-        'cost'                   => 'Cost',
-        'is_warranty'            => 'Warranty Improvement',
-        'asset_maintenance_time' => 'Asset Maintenance Time (in days)',
-        'notes'                  => 'Notes',
-        'update'                 => 'Update Asset Maintenance',
-        'create'                 => 'Create Asset Maintenance'
+        'asset_maintenance_type' => 'Gosa Ittifayyadama Qabeenya',
+        'title'                  => 'Mata Duree',
+        'start_date'             => 'Guyyaa Jalqabaa',
+        'completion_date'        => 'Guyyaa Xumura',
+        'cost'                   => 'Gatii',
+        'is_warranty'            => 'Fooyya’iinsa Warrantii',
+        'asset_maintenance_time' => 'Yeroo Ittifayyadama Qabeenya (guyyaa keessa)',
+        'notes'                  => 'Yaadannoo',
+        'update'                 => 'Dhangii Ittifayyadama Qabeenya Haaromsuu',
+        'create'                 => 'Ittifayyadama Qabeenya Uummuu'
     ];
