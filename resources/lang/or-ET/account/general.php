d3075f18c97dfb318990a6c1644a67c12b89521d
Adugna <gizawadugna@gmail.com>
2025-02-22 15:58:08 +0300

    modified:   general.php

--- resources/lang/or-ET/account/general.php
+++ resources/lang/or-ET/account/general.php
@@ -1,17 +1,24 @@
 <?php
+/*
+|--------------------------------------------------------------------------
+| Language Lines - Afaan Oromoo
+|--------------------------------------------------------------------------
+| Translated by Adugna Gizaw - Snipe IT Localization Partner
+| Contact: gizawadugna@gmail.com | +251925582067
+*/
 
 return array(
-    'personal_api_keys' => 'Personal API Keys',
-    'personal_access_token' => 'Personal Access Token',
-    'personal_api_keys_success' => 'Personal API Key :key created sucessfully',
-    'here_is_api_key' => 'Here is your new personal access token. This is the only time it will be shown so do not lose it! You may now use this token to make API requests.',
-    'api_key_warning' => 'When generating an API token, be sure to copy it down immediately as they will not be visible to you again.',
-    'api_base_url' => 'Your API base url is located at:',
+    'personal_api_keys' => 'Kiiwwan API Dhuunfaa',
+    'personal_access_token' => 'Tokeni Argamsiisa Dhuunfaa',
+    'personal_api_keys_success' => 'Kii API Dhuunfaa :key sirriitti uumame',
+    'here_is_api_key' => 'Kun tokeni argamsiisa haaraa keeti. Yeroo tokko qofa mul\'ata - hin dagatinaa! Tokeni kun API gaafannoo gochuuf fayyadami.',
+    'api_key_warning' => 'Tokeni API uumuu keessatti, kan duraatti niqabadhu - irra deebi\'anii hin agarru.',
+    'api_base_url' => 'URL API keessan bakka armaan gadiitti argama:',
     'api_base_url_endpoint' => '/&lt;endpoint&gt;',
-    'api_token_expiration_time' => 'API tokens are set to expire in:',
-    'api_reference' => 'Please check the <a href="https://snipe-it.readme.io/reference" target="_blank">API reference</a> to find specific API endpoints and additional API documentation.',
-    'profile_updated' => 'Account successfully updated',
-    'no_tokens' => 'You have not created any personal access tokens.',
-    'enable_sounds' => 'Enable sound effects',
-    'enable_confetti' => 'Enable confetti effects',
+    'api_token_expiration_time' => 'Tokeni API guyyaa :days gidduutti xumurama:',
+    'api_reference' => '<a href="https://snipe-it.readme.io/reference" target="_blank">API reference</a> ilaaluuf endpoint fi galmee API biro argachuuf.',
+    'profile_updated' => 'Akkaawuntii sirriitti haaromsame',
+    'no_tokens' => 'Tokeni dhuunfaa hin uumne.',
+    'enable_sounds' => 'Saandii itti fayyadamaa',
+    'enable_confetti' => 'Konfetti itti fayyadamaa',
 );
