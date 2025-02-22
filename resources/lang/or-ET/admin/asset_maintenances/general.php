24c5eb893bb435bb5d87fe88031f863d2f02a8e7
Adugna <gizawadugna@gmail.com>
2025-02-22 18:03:01 +0300

    modified:   general.php

--- resources/lang/or-ET/admin/asset_maintenances/general.php
+++ resources/lang/or-ET/admin/asset_maintenances/general.php
@@ -1,16 +1,23 @@
 <?php
+/*
+|--------------------------------------------------------------------------
+| Language Lines - Afaan Oromoo
+|--------------------------------------------------------------------------
+| Translated by Adugna Gizaw - Snipe IT Localization Partner
+| Contact: gizawadugna@gmail.com | +251925582067
+*/
 
-    return [
-        'asset_maintenances' => 'Asset Maintenances',
-        'edit'               => 'Edit Asset Maintenance',
-        'delete'             => 'Delete Asset Maintenance',
-        'view'               => 'View Asset Maintenance Details',
-        'repair'             => 'Repair',
-        'maintenance'        => 'Maintenance',
-        'upgrade'            => 'Upgrade',
-        'calibration'        => 'Calibration',
-        'software_support'   => 'Software Support',
-        'hardware_support'   => 'Hardware Support',
-        'configuration_change'   => 'Configuration Change',
-        'pat_test'           => 'PAT Test',
-    ];
+return [
+    'asset_maintenances' => 'Suphiinsa Qabeenyaa',
+    'edit'               => 'Suphiinsa Qabeenyaa Jijjiiraa',
+    'delete'             => 'Suphaa Qabeenyaa Haquu',
+    'view'               => 'Ibsa Tajaajilaa Qabeenyaa',
+    'repair'             => 'Suphuun Hojiitti deebisuu',
+    'maintenance'        => 'Suphaa',
+    'upgrade'            => 'Haala Haaraatti ol guddisuu',
+    'calibration'        => 'Safartuu gochuu',
+    'software_support'   => 'Deeggarsa Sooftiweerii',
+    'hardware_support'   => 'Deeggarsa Haardiiweerii',
+    'configuration_change' => 'Jijjiirama Saagiinsaa',
+    'pat_test'           => 'Yaalii PAT',
+];
