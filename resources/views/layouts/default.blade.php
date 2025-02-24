22f7a9ddd44e1a381c6c41753369c102742ac3bc
snipe <snipe@snipe.net>
2025-02-24 13:46:33 +0000

    Fixed stray tag
    Signed-off-by: snipe <snipe@snipe.net>

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -214,7 +214,7 @@ dir="{{ Helper::determineLanguageDirection() }}">
                                     </a>
                                     <ul class="dropdown-menu">
                                         @can('create', \App\Models\Asset::class)
-                                            <li{!! (Request::is('hardware/create') ? ' class="active">' : '') !!}>
+                                            <li{!! (Request::is('hardware/create') ? ' class="active"' : '') !!}>
                                                 <a href="{{ route('hardware.create') }}" tabindex="-1">
                                                     <x-icon type="assets" />
                                                     {{ trans('general.asset') }}
