d88c79366c2e27ed97a7b84835bd7b6033967cb3
Marcus Moore <contact@marcusmoore.io>
2025-02-25 14:27:07 -0800

    Replace Form::checkbox on google settings page

--- resources/views/settings/google.blade.php
+++ resources/views/settings/google.blade.php
@@ -54,7 +54,7 @@
                             <div class="col-md-8 col-md-offset-3">
                                 <label class="form-control{{ (config('app.lock_passwords')===true) ? ' form-control--disabled': '' }}">
                                     <span class="sr-only">{{ trans('admin/settings/general.pwd_secure_uncommon') }}</span>
-                                    {{ Form::checkbox('google_login', '1', old('google_login', $setting->google_login),array('aria-label'=>'google_login', (config('app.lock_passwords')===true) ? 'disabled': '')) }}
+                                    <input type="checkbox" name="google_login" value="1" @checked(old('google_login', $setting->google_login)) @disabled(config('app.lock_passwords')) aria-label="google_login">
                                     {{ trans('admin/settings/general.enable_google_login') }}
                                 </label>
                                 <p class="help-block">{{ trans('admin/settings/general.enable_google_login_help') }}</p>
