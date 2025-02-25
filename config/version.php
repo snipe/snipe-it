c3a2e81afde5ac6c2804fc8512707bad31aaddf6
snipe <snipe@snipe.net>
2025-02-25 12:18:22 +0000

    Updated version
    Signed-off-by: snipe <snipe@snipe.net>

--- config/version.php
+++ config/version.php
@@ -1,10 +1,10 @@
 <?php
 return array (
-  'app_version' => 'v8.0.0',
-  'full_app_version' => 'v8.0.0 - build 16962-g982cfeca32',
-  'build_version' => '16962',
+  'app_version' => 'v8.0.1',
+  'full_app_version' => 'v8.0.1 - build 16992-g18c7cbbbb',
+  'build_version' => '16992',
   'prerelease_version' => '',
-  'hash_version' => 'g982cfeca32',
-  'full_hash' => 'v8.0.0-172-g982cfeca32',
-  'branch' => 'master',
+  'hash_version' => 'g18c7cbbbb',
+  'full_hash' => 'v8.0.1-28-g18c7cbbbb',
+  'branch' => 'develop',
 );
