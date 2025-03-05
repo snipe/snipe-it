f2b44f7002edacb00dfc2a2b6f9529c11df1ffb1
Marcus Moore <contact@marcusmoore.io>
2025-03-04 16:46:43 -0800

    Replace Form::label in company select partial

--- resources/views/partials/forms/edit/company-select.blade.php
+++ resources/views/partials/forms/edit/company-select.blade.php
@@ -2,7 +2,7 @@
 @if (($snipeSettings->full_multiple_companies_support=='1') && (!Auth::user()->isSuperUser()))
     <!-- full company support is enabled and this user isn't a superadmin -->
     <div class="form-group">
-    {{ Form::label($fieldname, $translated_name, array('class' => 'col-md-3 control-label')) }}
+        <label for="{{ $fieldname }}" class="col-md-3 control-label">{{ $translated_name }}</label>
         <div class="col-md-6">
             <select class="js-data-ajax" disabled="true" data-endpoint="companies" data-placeholder="{{ trans('general.select_company') }}" name="{{ $fieldname }}" style="width: 100%" id="company_select" aria-label="{{ $fieldname }}"{{ (isset($multiple) && ($multiple=='true')) ? " multiple='multiple'" : '' }}>
                 @if ($company_id = old($fieldname, (isset($item)) ? $item->{$fieldname} : ''))
@@ -19,7 +19,7 @@
 @else
     <!-- full company support is enabled or this user is a superadmin -->
     <div id="{{ $fieldname }}" class="form-group{{ $errors->has($fieldname) ? ' has-error' : '' }}">
-        {{ Form::label($fieldname, $translated_name, array('class' => 'col-md-3 control-label')) }}
+        <label for="{{ $fieldname }}" class="col-md-3 control-label">{{ $translated_name }}</label>
         <div class="col-md-8">
             <select class="js-data-ajax" data-endpoint="companies" data-placeholder="{{ trans('general.select_company') }}" name="{{ $fieldname }}" style="width: 100%" id="company_select"{{ (isset($multiple) && ($multiple=='true')) ? " multiple='multiple'" : '' }}>
                 @isset ($selected)
