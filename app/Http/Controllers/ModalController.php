c73dba4a435a44e24adf5de62a6edd34de917d7f
snipe <snipe@snipe.net>
2025-03-12 17:28:05 +0000

    Better visual spacing
    Signed-off-by: snipe <snipe@snipe.net>

--- app/Http/Controllers/ModalController.php
+++ app/Http/Controllers/ModalController.php
@@ -41,10 +41,11 @@ class ModalController extends Controller
         $view = view("modals.${type}");
 
             if ($type == "statuslabel") {
-            $view->with('statuslabel_types', Helper::statusTypeList());
-        }
-        if (in_array($type, ['kit-model', 'kit-license', 'kit-consumable', 'kit-accessory'])) {
-            $view->with('kitId', $itemId);
+                $view->with('statuslabel_types', Helper::statusTypeList());
+            }
+
+            if (in_array($type, ['kit-model', 'kit-license', 'kit-consumable', 'kit-accessory'])) {
+                $view->with('kitId', $itemId);
             }
             return $view;
         }
