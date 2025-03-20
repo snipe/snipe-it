976b3dc5aea7c84f5ea1fbe1066c53cc07cd3941
Marcus Moore <contact@marcusmoore.io>
2025-03-20 13:38:12 -0700

    Improve test case

--- tests/Feature/Checkouts/Ui/BulkAssetCheckoutTest.php
+++ tests/Feature/Checkouts/Ui/BulkAssetCheckoutTest.php
@@ -2,26 +2,38 @@
 
 namespace Tests\Feature\Checkouts\Ui;
 
+use App\Mail\CheckoutAssetMail;
+use App\Models\Asset;
 use App\Models\User;
+use Illuminate\Support\Facades\Mail;
+use PHPUnit\Framework\ExpectationFailedException;
 use Tests\TestCase;
 
 class BulkAssetCheckoutTest extends TestCase
 {
     public function testHandleMissingModelBeingIncluded()
     {
+        Mail::fake();
+
         $this->actingAs(User::factory()->checkoutAssets()->create())
             ->post(route('hardware.bulkcheckout.store'), [
                 'selected_assets' => [
-                    1,
+                    Asset::factory()->requiresAcceptance()->create()->id,
                     9999999,
                 ],
                 'checkout_to_type' => 'user',
-                'assigned_user' => User::factory()->create()->id,
+                'assigned_user' => User::factory()->create(['email' => 'someone@example.com'])->id,
                 'assigned_asset' => null,
                 'checkout_at' => null,
                 'expected_checkin' => null,
                 'note' => null,
             ])
             ->assertSessionHas('error', trans_choice('admin/hardware/message.multi-checkout.error', 2));
+
+        try {
+            Mail::assertNotSent(CheckoutAssetMail::class);
+        } catch (ExpectationFailedException $e) {
+            $this->fail('Asset checkout email was sent when the entire checkout failed.');
+        }
     }
 }
