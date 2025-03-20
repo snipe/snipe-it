0ceda098ffc02ff900417891358df3b0dcda50d6
Marcus Moore <contact@marcusmoore.io>
2025-03-20 16:10:01 -0700

    Spilt test scenarios

--- tests/Feature/Users/Ui/ViewUserTest.php
+++ tests/Feature/Users/Ui/ViewUserTest.php
@@ -8,22 +8,35 @@ use Tests\TestCase;
 
 class ViewUserTest extends TestCase
 {
-    public function testPermissionsForUserDetailPage()
+
+    public function testRequiresPermissionToViewUser()
+    {
+        $this->actingAs(User::factory()->create())
+            ->get(route('users.show', User::factory()->create()))
+            ->assertStatus(403);
+    }
+
+    public function testCanViewUser()
+    {
+        $actor = User::factory()->viewUsers()->create();
+
+        $this->actingAs($actor)
+            ->get(route('users.show', User::factory()->create()))
+            ->assertOk()
+            ->assertStatus(200);
+    }
+
+    public function testCannotViewUserFromAnotherCompany()
     {
         $this->settings->enableMultipleFullCompanySupport();
 
         [$companyA, $companyB] = Company::factory()->count(2)->create();
 
-        $superuser = User::factory()->superuser()->create();
+        $actor = User::factory()->for($companyA)->viewUsers()->create();
         $user = User::factory()->for($companyB)->create();
 
-        $this->actingAs(User::factory()->editUsers()->for($companyA)->create())
+        $this->actingAs($actor)
             ->get(route('users.show', $user))
             ->assertStatus(302);
-
-        $this->actingAs($superuser)
-            ->get(route('users.show', $user))
-            ->assertOk()
-            ->assertStatus(200);
     }
 }
