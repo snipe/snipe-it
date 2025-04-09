100db23210571e22a7cf1987be4461d627d759a2
Godfrey M <godmartinz@gmail.com>
2025-04-09 11:54:59 -0700

    add checks that the target is not null, and redirects back with error messages

--- resources/lang/en-US/admin/hardware/message.php
+++ resources/lang/en-US/admin/hardware/message.php
@@ -19,6 +19,11 @@ return [
         'success_linked' => 'Asset with tag :tag was created successfully. <strong><a href=":link" style="color: white;">Click here to view</a></strong>.',
         'multi_success_linked' => 'Asset with tag :links was created successfully.|:count assets were created succesfully. :links.',
         'partial_failure' => 'An asset was unable to be created. Reason: :failures|:count assets were unable to be created. Reasons: :failures',
+        'target_not_found' => [
+            'user' => 'The assigned user could not be found.',
+            'asset' => 'The assigned asset could not be found.',
+            'location' => 'The assigned location could not be found.',
+        ],
     ],
 
     'update' => [
