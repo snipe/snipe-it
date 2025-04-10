9086e5dba7fe06ec4e62e3284d79aa8da85c1730
Marcus Moore <contact@marcusmoore.io>
2025-04-10 13:03:50 -0700

    Scaffold some asset importer tests

---
+++ tests/Unit/Importer/AssetImportTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Unit\Importer;
+
+use Tests\TestCase;
+
+class AssetImportTest extends TestCase
+{
+    public function test_uses_first_deployable_status_label_as_default_if_one_exists()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function test_uses_first_status_label_as_default_if_deployable_status_label_does_not_exist()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function test_creates_default_status_label_if_one_does_not_exist()
+    {
+        $this->markTestIncomplete();
+    }
+}
