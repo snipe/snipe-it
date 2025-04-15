e1abdd1c7b2cbf0256cf5decbed52b5d74d848e2
snipe <snipe@snipe.net>
2025-04-15 20:11:26 +0100

    Removed deprecation resulting in Creation of dynamic property
    Signed-off-by: snipe <snipe@snipe.net>

--- app/Console/Commands/GeneratePersonalAccessToken.php
+++ app/Console/Commands/GeneratePersonalAccessToken.php
@@ -2,11 +2,9 @@
 
 namespace App\Console\Commands;
 
-use App\Helpers\Helper;
 use Illuminate\Console\Command;
 use App\Models\User;
 use Laravel\Passport\TokenRepository;
-use Illuminate\Contracts\Validation\Factory as ValidationFactory;
 use Illuminate\Support\Facades\DB;
 
 class GeneratePersonalAccessToken extends Command
@@ -43,9 +41,9 @@ class GeneratePersonalAccessToken extends Command
      *
      * @return void
      */
-    public function __construct(TokenRepository $tokenRepository, ValidationFactory $validation)
+    public function __construct(TokenRepository $tokenRepository)
     {
-        $this->validation = $validation;
+        //$this->validation = $validation;
         $this->tokenRepository = $tokenRepository;
         parent::__construct();
     }
