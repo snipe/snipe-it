277564436bb44146cab14616c50addcdfcd2ca88
snipe <snipe@snipe.net>
2025-04-15 20:45:13 +0100

    Bumped hash
    Signed-off-by: snipe <snipe@snipe.net>

--- config/version.php
+++ config/version.php
@@ -1,10 +1,10 @@
 <?php
 return array (
   'app_version' => 'v8.0.4',
-  'full_app_version' => 'v8.0.4 - build 17333-gaf408bb45',
-  'build_version' => '17333',
+  'full_app_version' => 'v8.0.4 - build 17611-gfab1a6c33',
+  'build_version' => '17611',
   'prerelease_version' => '',
-  'hash_version' => 'gaf408bb45',
-  'full_hash' => 'v8.0.4-135-gaf408bb45',
+  'hash_version' => 'gfab1a6c33',
+  'full_hash' => 'v8.0.5-413-gfab1a6c33',
   'branch' => 'develop',
 );
