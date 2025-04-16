e9be4ef2f4ea4b5df62839361f1cf7230ddb49b9
akemidx <kojotek.dx@gmail.com>
2025-04-16 18:15:10 -0400

    js script

--- resources/views/hardware/checkout.blade.php
+++ resources/views/hardware/checkout.blade.php
@@ -168,8 +168,9 @@
                             <div class="col-sm-3 control-label" ></div>
                             <div class="col-md-8">
                                 <label class="form-control">
-                                    <input type="checkbox" value="1" name="log_audit" {{ (old('log_audit')) == '1' ? ' checked="checked"' : '' }} aria-label="log_audit">
+                                    <input type="checkbox" value="1" name="log_audit" {{ (old('log_audit')) == '1' ? ' checked="checked"' : '' }} aria-label="log_audit" id="auditcheckbox" onclick="auditCheckbox()">
                                     {{ trans('admin/settings/general.log_audit') }}
+                                    <p id="audittext" style="display:none">Checkbox is CHECKED!</p>
                                 </label>
                                 <p class="help-block">{{ trans('admin/settings/general.log_audit_help_text')  . " " .  trans('general.checkout') . "." }}</p>
                             </div>
@@ -238,6 +239,22 @@
 
 @section('moar_scripts')
     @include('partials/assets-assigned')
+
+    <script> //testing script
+        function auditCheckbox() {
+            // Get the checkbox
+            var checkBox = document.getElementById("auditcheckbox");
+            // Get the output text
+            var text = document.getElementById("audittext");
+
+            // If the checkbox is checked, display the fields
+            if (checkBox.checked == true){
+                text.style.display = "block";
+            } else {
+                text.style.display = "none";
+            }
+    </script>
+
     <script>
         // Only display the audit fields if the checkbox is selected
         $(".format").change(function(){
@@ -250,6 +267,26 @@
         });
         }).change();
     </script>
+
+    <script>
+        $(document).ready(function() {
+            $("#optional_info").on("click", function () {
+                $('#optional_details').fadeToggle(100);
+                $('#optional_info_icon').toggleClass('checkbox);
+                var optional_info_open = $('#optional_info_icon').hasClass('checkbox');
+                document.cookie = "optional_info_open=" + optional_info_open + '; path=/';
+            });
+            var all_cookies = document.cookie.split(';')
+            for (var i in all_cookies) {
+                var trimmed_cookie = all_cookies[i].trim(' ')
+                if (trimmed_cookie.startsWith('optional_info_open=')) {
+                    elems = all_cookies[i].split('=', 2)
+                    if (elems[1] == 'true') {
+                        $('#optional_info').trigger('click');
+                    }
+                }
+            }
+        });
 
     <script>
         //        $('#checkout_at').datepicker({
