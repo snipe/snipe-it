96d5e072fe7f4469cb3053f7ff500ed4243ed698
snipe <snipe@snipe.net>
2025-04-17 22:39:04 +0100

    Fixed flaky tests on user creation
    Signed-off-by: snipe <snipe@snipe.net>

--- tests/Feature/Users/Ui/CreateUserTest.php
+++ tests/Feature/Users/Ui/CreateUserTest.php
@@ -7,14 +7,38 @@ use Tests\TestCase;
 
 class CreateUserTest extends TestCase
 {
+
+    public function testPermissionRequiredToCreateUser()
+    {
+        $this->actingAs(User::factory()->create())
+            ->get(route('users.create'))
+            ->assertForbidden();
+    }
+
     public function testPageRenders()
     {
-        $admin = User::factory()->createUsers()->create();
-        $response = $this->actingAs(User::factory()->superuser()->create())
+        $this->actingAs(User::factory()->createUsers()->create())
             ->get(route('users.create'))
             ->assertOk();
-        $response->assertDontSee($admin->first_name);
-        $response->assertDontSee($admin->last_name);
-        $response->assertDontSee($admin->email);
+
+    }
+
+    public function testCanCreateUser()
+    {
+
+        $response = $this->actingAs(User::factory()->createUsers()->viewUsers()->create())
+            ->from(route('users.index'))
+            ->post(route('users.store'), [
+                'first_name' => 'Test First Name',
+                'last_name' => 'Test Last Name',
+                'username' => 'testuser',
+                'password' => 'testpassword1235!!',
+                //'notes' => 'Test Note',
+            ])
+            ->assertStatus(302)
+            ->assertRedirect(route('users.index'));
+
+        $this->followRedirects($response)->assertSee('Success');
+
     }
 }
