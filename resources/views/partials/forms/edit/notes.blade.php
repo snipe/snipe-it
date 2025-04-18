ec7d0b33667e67c317ef5fd83529612e376804ca
Daniel O'Connor <daniel.oconnor@gmail.com>
2025-04-19 03:49:19 +0930

    Ensure notes has a placeholder

--- resources/views/partials/forms/edit/notes.blade.php
+++ resources/views/partials/forms/edit/notes.blade.php
@@ -2,7 +2,7 @@
 <div class="form-group{{ $errors->has('notes') ? ' has-error' : '' }}">
     <label for="notes" class="col-md-3 control-label">{{ trans('general.notes') }}</label>
     <div class="col-md-7 col-sm-12">
-        <textarea class="col-md-6 form-control" id="notes" aria-label="notes" name="notes" style="min-width:100%;">{{ old('notes', $item->notes) }}</textarea>
+        <textarea class="col-md-6 form-control" id="notes" aria-label="notes" name="notes" style="min-width:100%;" placeholder="{{ trans('general.placeholders.notes') }}">{{ old('notes', $item->notes) }}</textarea>
         {!! $errors->first('notes', '<span class="alert-msg" aria-hidden="true"><i class="fas fa-times" aria-hidden="true"></i> :message</span>') !!}
     </div>
 </div>
