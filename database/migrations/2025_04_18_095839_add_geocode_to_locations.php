e3365e29fd019a561cc746ee93b3174f51936e83
Daniel O'Connor <daniel.oconnor@gmail.com>
2025-04-18 19:35:10 +0930

    Add database columns

---
+++ database/migrations/2025_04_18_095839_add_geocode_to_locations.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('locations', function (Blueprint $table) {
+	    $table->float('latitude', 5)->nullable();
+            $table->float('longitude', 5)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('locations', function (Blueprint $table) {
+	    $table->dropColumn('latitude');
+	    $table->dropColumn('longitude');
+        });
+    }
+};
