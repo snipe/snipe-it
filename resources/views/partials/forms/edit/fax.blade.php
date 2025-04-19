d4c7afae2145167e7ca43accff6374ca4d58b35f
Daniel O'Connor <daniel.oconnor@gmail.com>
2025-04-19 13:15:49 +0930

    Add placeholders

--- resources/views/partials/forms/edit/fax.blade.php
+++ resources/views/partials/forms/edit/fax.blade.php
@@ -1,7 +1,7 @@
 <div class="form-group {{ $errors->has('fax') ? ' has-error' : '' }}">
     <label for="fax" class="col-md-3 control-label">{{ trans('admin/suppliers/table.fax') }}</label>
     <div class="col-md-7">
-        <input class="form-control" type="text" name="fax" aria-label="fax" id="fax" value="{{ old('fax', $item->fax) }}"  maxlength="34"  />
+        <input class="form-control" type="text" name="fax" aria-label="fax" id="fax" value="{{ old('fax', $item->fax) }}"  placeholder="{{ trans('general.example') . '+1 234 5678' }}" maxlength="34"  />
         {!! $errors->first('fax', '<span class="alert-msg" aria-hidden="true"><i class="fas fa-times" aria-hidden="true"></i> :message</span>') !!}
     </div>
 </div>
--- resources/views/partials/forms/edit/url.blade.php
+++ resources/views/partials/forms/edit/url.blade.php
@@ -1,7 +1,7 @@
 <div class="form-group {{ $errors->has('url') ? ' has-error' : '' }}">
     <label for="url" class="col-md-3 control-label">{{ trans('general.url') }}</label>
     <div class="col-md-7">
-        <input class="form-control" name="url" type="url" id="url" value="{{ old('url', $item->url) }}">
+        <input class="form-control" name="url" type="url" id="url" value="{{ old('url', $item->url) }}" placeholder="{{ trans('general.example') . "https://example.com/" }}">
         {!! $errors->first('url', '<span class="alert-msg" aria-hidden="true"><i class="fas fa-times" aria-hidden="true"></i> :message</span>') !!}
     </div>
 </div>
