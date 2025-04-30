15c5e6634aea7f2393058ca09b523f4550987fd1
impactitsol <99200747+impactitsol@users.noreply.github.com>
2025-04-30 15:25:11 +0100

    Update table.php
    Changed Asset Name to Hostname

--- resources/lang/en-GB/admin/asset_maintenances/table.php
+++ resources/lang/en-GB/admin/asset_maintenances/table.php
@@ -2,7 +2,7 @@
 
     return [
         'title'         => 'Asset Maintenance',
-        'asset_name'    => 'Asset Name',
+        'asset_name'    => 'Hostame',
         'is_warranty'   => 'Warranty',
         'dl_csv'        => 'Download CSV',
     ];
