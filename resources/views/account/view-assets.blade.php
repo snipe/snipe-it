ccc598fe8414c4fbb738143120f46bbf7b21f2f9
Marcus Moore <contact@marcusmoore.io>
2025-04-30 15:01:08 -0700

    Extract row component

--- resources/views/account/view-assets.blade.php
+++ resources/views/account/view-assets.blade.php
@@ -459,65 +459,7 @@
                       $counter = 1
                     @endphp
                     @foreach ($user->assets as $asset)
-                      <tr>
-                        <td>{{ $counter }}</td>
-                        <td>
-                          @if (($asset->image) && ($asset->image!=''))
-                            <img src="{{ Storage::disk('public')->url(app('assets_upload_path').e($asset->image)) }}" style="max-height: 30px; width: auto" class="img-responsive" alt="">
-                          @elseif (($asset->model) && ($asset->model->image!=''))
-                            <img src="{{ Storage::disk('public')->url(app('models_upload_path').e($asset->model->image)) }}" style="max-height: 30px; width: auto" class="img-responsive" alt="">
-                          @endif
-                        </td>
-                        <td>
-                          @if (($asset->model) && ($asset->model->category))
-                          {{ $asset->model->category->name }}
-                          @endif
-                        </td>
-                        <td>
-                          {{ $asset->asset_tag }}
-                        </td>
-                        <td>
-                          {{ $asset->name }}
-                        </td>
-                        <td>
-                            {{ $asset->model->name }}
-                        </td>
-                        <td>
-                          {{ $asset->model->model_number }}
-                        </td>
-                        <td>
-                          {{ $asset->serial }}
-                        </td>
-                        <td>
-                          {{ ($asset->defaultLoc) ? $asset->defaultLoc->name : '' }}
-                        </td>
-                        <td>
-                          {{ ($asset->location) ? $asset->location->name : '' }}
-                        </td>
-                        @can('self.view_purchase_cost')
-                        <td>
-                          {!! Helper::formatCurrencyOutput($asset->purchase_cost) !!}
-                        </td>
-                        @endcan
-
-                        <td>
-                          {{ ($asset->asset_eol_date != '') ? Helper::getFormattedDateObject($asset->asset_eol_date, 'date', false) : null }}
-                        </td>
-
-                        <td>
-                          {{ Helper::getFormattedDateObject($asset->last_audit_date, 'datetime', false) }}
-                        </td>
-                        <td>
-                          {{ Helper::getFormattedDateObject($asset->next_audit_date, 'date', false) }}
-                        </td>
-
-                        @foreach ($field_array as $db_column => $field_value)
-                          <td>
-                            {{ $asset->{$db_column} }}
-                          </td>
-                        @endforeach
-
-                      </tr>
+                      <x-asset-table-row :asset="$asset" :field_array="$field_array" :counter="$counter" />
 
                       @php
                         $counter++
