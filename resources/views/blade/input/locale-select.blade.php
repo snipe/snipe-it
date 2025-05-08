75924be95881fee3361146a40a72997c48c3438d
Marcus Moore <contact@marcusmoore.io>
2025-05-08 15:02:35 -0700

    Introduce locale select component and make replacement on profile page

---
+++ resources/views/blade/input/locale-select.blade.php
@@ -0,0 +1,24 @@
+@props([
+    'name' => 'locale',
+    'selected' => null,
+])
+<div>
+    <select
+        name="{{ $name }}"
+        {{ $attributes->merge(['class' => 'select2', 'style' => 'width:100%']) }}
+        aria-label="{{ $name }}"
+        data-placeholder="{{ trans('localizations.select_language') }}"
+    >
+        <option value=""  role="option">{{ trans('localizations.select_language') }}</option>'
+        @foreach (trans('localizations.languages') as $abbr => $locale)
+            <option
+                value="{{ $abbr }}"
+                role="option"
+                @selected($abbr == $selected)
+                aria-selected="{{ $abbr == $selected ? 'true' : 'false' }}"
+            >
+                {{ $locale }}
+            </option>
+        @endforeach
+    </select>
+</div>
