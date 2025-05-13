e3642bb513abed14e118a89ca75d711a0df371a7
Marcus Moore <contact@marcusmoore.io>
2025-05-12 17:07:17 -0700

    Remove unneeded div

--- resources/views/blade/input/locale-select.blade.php
+++ resources/views/blade/input/locale-select.blade.php
@@ -2,23 +2,21 @@
     'name' => 'locale',
     'selected' => null,
 ])
-<div>
-    <select
-        name="{{ $name }}"
-        {{ $attributes->merge(['class' => 'select2', 'style' => 'width:100%']) }}
-        aria-label="{{ $name }}"
-        data-placeholder="{{ trans('localizations.select_language') }}"
-    >
-        <option value=""  role="option">{{ trans('localizations.select_language') }}</option>'
-        @foreach (trans('localizations.languages') as $abbr => $locale)
-            <option
-                value="{{ $abbr }}"
-                role="option"
-                @selected($abbr == $selected)
-                aria-selected="{{ $abbr == $selected ? 'true' : 'false' }}"
-            >
-                {{ $locale }}
-            </option>
-        @endforeach
-    </select>
-</div>
+<select
+    name="{{ $name }}"
+    {{ $attributes->merge(['class' => 'select2', 'style' => 'width:100%']) }}
+    aria-label="{{ $name }}"
+    data-placeholder="{{ trans('localizations.select_language') }}"
+>
+    <option value=""  role="option">{{ trans('localizations.select_language') }}</option>'
+    @foreach (trans('localizations.languages') as $abbr => $locale)
+        <option
+            value="{{ $abbr }}"
+            role="option"
+            @selected($abbr == $selected)
+            aria-selected="{{ $abbr == $selected ? 'true' : 'false' }}"
+        >
+            {{ $locale }}
+        </option>
+    @endforeach
+</select>
