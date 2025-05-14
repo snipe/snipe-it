e07b0f65a1ff5533d4f11af1ab3b1069776c20e4
snipe <snipe@snipe.net>
2025-05-14 17:33:12 +0200

    Clearer text on status label types
    Signed-off-by: snipe <snipe@snipe.net>

--- resources/lang/en-US/admin/statuslabels/table.php
+++ resources/lang/en-US/admin/statuslabels/table.php
@@ -1,14 +1,14 @@
 <?php
 
 return array(
-    'about'      	=> 'About Status Labels',
+    'about'      	=> 'About Status Types',
     'archived'      	=> 'Archived',
     'create'      	=> 'Create Status Label',
     'color'      	=> 'Chart Color',
     'default_label' => 'Default Label',
     'default_label_help' => 'This is used to ensure your most commonly used status labels appear at the top of the select box when creating/editing assets.',
     'deployable'      	=> 'Deployable',
-    'info'      	=> 'Status labels are used to describe the various states your assets could be in. They may be out for repair, lost/stolen, etc. You can create new status labels for deployable, pending and archived assets.',
+    'info'      	=> 'Status label types are used to describe the various states your assets could be in. They may be out for repair, lost/stolen, etc. You can create new status labels for your deployable, pending and archived assets according to your own workflow. For more information, <a href="https://snipe-it.readme.io/docs/overview#status-labels" target="_blank">see the documentation <i class="fa fa-external-link"></i></a>.',
     'name'      	=> 'Status Name',
     'pending'      	=> 'Pending',
     'status_type'   => 'Status Type',
