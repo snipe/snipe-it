851cb29fc6d01fd4aebab3d5a7997eb8a2d733d1
snipe <snipe@snipe.net>
2025-05-20 15:05:51 +0100

    Set note to null if blank
    Signed-off-by: snipe <snipe@snipe.net>

--- app/Http/Transformers/UploadedFilesTransformer.php
+++ app/Http/Transformers/UploadedFilesTransformer.php
@@ -4,17 +4,10 @@ namespace App\Http\Transformers;
 
 use App\Helpers\Helper;
 use App\Helpers\StorageHelper;
-use App\Models\Accessory;
 use App\Models\Actionlog;
-use App\Models\Asset;
-use App\Models\AssetModel;
-use App\Models\Component;
-use App\Models\Consumable;
-use App\Models\License;
-use App\Models\Location;
-use App\Models\User;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Storage;
 
 class UploadedFilesTransformer
 {
@@ -33,21 +26,19 @@ class UploadedFilesTransformer
     {
         $snipeModel = $file->item_type;
 
-
-
-        \Log::error(StorageHelper::getFiletype($file->uploads_file_path()));
         $array = [
             'id' => (int) $file->id,
             'icon' => Helper::filetype_icon($file->filename),
             'filename' => e($file->filename),
             'inline' => StorageHelper::allowSafeInline($file->uploads_file_path()),
             'filetype' => StorageHelper::getFiletype($file->uploads_file_path()),
+            'exists_on_disk' => (Storage::exists($file->uploads_file_path()) ? true : false),
             'url' => $file->uploads_file_url(),
             'created_by' => ($file->adminuser) ? [
                 'id' => (int) $file->adminuser->id,
                 'name'=> e($file->adminuser->present()->fullName),
             ] : null,
-            'note' => e($file->note),
+            'note' =>  ($file->note) ? e($file->note) : null,
             'created_at' => Helper::getFormattedDateObject($file->created_at, 'datetime'),
             'deleted_at' => Helper::getFormattedDateObject($file->deleted_at, 'datetime'),
         ];
