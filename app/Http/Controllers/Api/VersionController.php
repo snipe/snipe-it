291feda2aeba1c026ae331c0df9d219e557d1d92
NebelKreis <43917728+NebelKreis@users.noreply.github.com>
2025-05-20 08:40:37 +0200

    Chore: Fixed Codacy-reported code quality issues

--- app/Http/Controllers/Api/VersionController.php
+++ app/Http/Controllers/Api/VersionController.php
@@ -9,18 +9,20 @@ class VersionController extends Controller
 {
     /**
      * Get the current version of Snipe-IT
-     * 
+     *
      * @author [Nebelkreis] [https://github.com/NebelKreis]
-     * 
+     *
      * @return JsonResponse Returns JSON response with version information
      */
     public function index(): JsonResponse
     {
-        return response()->json([
-            'version' => config('version.app_version'),
-            'full_version' => config('version.full_app_version'),
-            'build_version' => config('version.build_version'),
-            'hash_version' => config('version.hash_version')
-        ]);
+        return response()->json(
+            [
+                'version' => config('version.app_version'),
+                'full_version' => config('version.full_app_version'),
+                'build_version' => config('version.build_version'),
+                'hash_version' => config('version.hash_version')
+            ]
+        );
     }
 }
