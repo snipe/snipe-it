8c90efe7452799f0fcde93e4c259d5bb0efc5ec0
Marcus Moore <contact@marcusmoore.io>
2025-05-22 15:02:14 -0700

    Add authorization check

--- resources/views/blade/full-user-name.blade.php
+++ resources/views/blade/full-user-name.blade.php
@@ -2,12 +2,22 @@
     'user'
 ])
 
-@if ($user)
-    @if (! $user->trashed())
-        {{-- if the user is in database but soft-deleted --}}
-        <a href="{{ route('users.show', $user->id) }}">{{ $user->present()->fullName() }}</a>
+@if($user)
+    @can('view', $user)
+        @if(! $user->trashed())
+            {{-- if the user is in database but soft-deleted --}}
+            <a href="{{ route('users.show', $user->id) }}">{{ $user->present()->fullName() }}</a>
+        @else
+            {{-- if the user exists --}}
+            <s><a href="{{ route('users.show', $user->id) }}">{{ $user->present()->fullName() }}</a></s>
+        @endif
     @else
-        {{-- if the user exists --}}
-        <s><a href="{{ route('users.show', $user->id) }}">{{ $user->present()->fullName() }}</a></s>
-    @endif
+        @if(! $user->trashed())
+            {{-- if the user is in database but soft-deleted --}}
+            <span>{{ $user->present()->fullName() }}</span>
+        @else
+            {{-- if the user exists --}}
+            <s><span>{{ $user->present()->fullName() }}</span></s>
+        @endif
+    @endcan
 @endif
