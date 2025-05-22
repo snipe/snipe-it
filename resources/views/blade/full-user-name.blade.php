7cc216eb38e0356ae73dc650b9ecc9194c090882
Marcus Moore <contact@marcusmoore.io>
2025-05-22 15:05:24 -0700

    Extract variable

--- resources/views/blade/full-user-name.blade.php
+++ resources/views/blade/full-user-name.blade.php
@@ -3,21 +3,25 @@
 ])
 
 @if($user)
+    @php
+        $fullName = $user->present()->fullName();
+    @endphp
+
     @can('view', $user)
         @if(! $user->trashed())
             {{-- if the user is in database but soft-deleted --}}
-            <a href="{{ route('users.show', $user->id) }}">{{ $user->present()->fullName() }}</a>
+            <a href="{{ route('users.show', $user->id) }}">{{ $fullName }}</a>
         @else
             {{-- if the user exists --}}
-            <s><a href="{{ route('users.show', $user->id) }}">{{ $user->present()->fullName() }}</a></s>
+            <s><a href="{{ route('users.show', $user->id) }}">{{ $fullName }}</a></s>
         @endif
     @else
         @if(! $user->trashed())
             {{-- if the user is in database but soft-deleted --}}
-            <span>{{ $user->present()->fullName() }}</span>
+            <span>{{ $fullName }}</span>
         @else
             {{-- if the user exists --}}
-            <s><span>{{ $user->present()->fullName() }}</span></s>
+            <s><span>{{ $fullName }}</span></s>
         @endif
     @endcan
 @endif
